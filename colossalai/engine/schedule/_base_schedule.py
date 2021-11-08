Add gradient accumulation, fix lr scheduler

colossalai/engine/schedule/_base_schedule.py
@@ -20,6 +20,8 @@ class BaseSchedule(ABC):
 
     def __init__(self):
         self.logger = get_global_dist_logger()
+        self.grad_accum = 1
+        self.training = False
 
     @staticmethod
     def _move_tensor(element):
@@ -28,12 +30,32 @@ class BaseSchedule(ABC):
                 return element.to(get_current_device()).detach()
         return element
 
-    def _move_to_device(self, data):
-        if isinstance(data, (tuple, list)):
-            data = tuple([self._move_tensor(d) for d in data])
-        elif torch.is_tensor(data):
-            data = data.to(get_current_device()).detach()
-        return data
+    def initialize(self,
+                   dataloader=None,
+                   model=None,
+                   criterion=None,
+                   optimizer=None):
+        """Initializes the schedule and set parameters before running.
+
+        :param dataloader: DataLoader in training or evaluation
+        :param model: The neural network model
+        :param criterion: Criterion for calculating loss
+        :param optimizer: Optimizer for updating the parameters
+        """
+        self.dataloader = dataloader
+        assert model is not None, "Schedule requires a model"
+        self.model = model
+        assert criterion is not None, "Schedule requires a criterion"
+        self.criterion = criterion
+        assert optimizer is not None, "Schedule requires an optimizer"
+        self.optimizer = optimizer
+        self.initialized = True
+
+    def check_initialized(self):
+        """Checks whether the schedule is initialized.
+        """
+        assert self.initialized, \
+            'Schedule is not initialized. Call schedule.initialize(...) before using it.'
 
     def load_batch(self, data_iter):
         """Loads a batch from data iterator. It returns the data and labels which are
@@ -47,35 +69,53 @@ class BaseSchedule(ABC):
         data, label = next(data_iter)
         return self._move_to_device(data), self._move_to_device(label)
 
-    def initialize(self, model, optimizer):
-        """Initializes the model and the optimizer before training.
-         This is often used in FP16 training.
+    def consume_batch(self):
+        while True:
+            try:
+                self.load_batch()
+            except StopIteration:
+                break
 
-        :param model: The neural network model
-        :param optimizer: Optimizer for updating the parameters
+    def _move_to_device(self, data):
+        if isinstance(data, (
+                tuple,
+                list,
+        )):
+            data = tuple([
+                d.to(get_current_device()).detach() for d in data
+                if torch.is_tensor(d)
+            ])
+        elif torch.is_tensor(data):
+            data = data.to(get_current_device()).detach()
+        return data
+
+    def train(self, dataloader=None, mode=True):
+        """Sets the dataloader to be used and turn the model to 
+        training or evaluation mode.
+
+        :param dataloader: Dataloader to be used
+        :param mode: If True, the model will set as training mode. Otherwise, evaluation mode.
+        """
+        self.check_initialized()
+        self.training = mode
+        if mode:
+            self.model.train()
+        else:
+            self.model.eval()
+        if dataloader is not None:
+            self.dataloader = dataloader
+            self.data_iter = iter(dataloader)
+
+    def zero_grad(self, forward_only=False):
+        """Cleans gradients with the optimizer.
         """
         return model, optimizer
 
-    @abstractmethod
-    def forward_backward_step(self,
-                              data_iter,
-                              model,
-                              criterion,
-                              optimizer=None,
-                              forward_only=False,
-                              grad_accum_size: int = 1,
-                              return_loss=True):
-        """The process function over a batch of dataset for training or evaluation.
-
-        :param data_iter: Data iterator of the dataset
-        :param model: Model used in training or evaluation
-        :param optimizer: Optimizer used in training or evaluation
-        :param criterion: Loss function
-        :param forward_only: If True, the process won't include backward
-        :param grad_accum_size: Steps of gradient accumulation
-        :param return_loss: If False, the loss won't be returned
+    def step(self):
+        """Updates the parameters and learning rate with the optimizer.
         """
-        pass
+        self.check_initialized()
+        self.optimizer.step()
 
     @abstractmethod
     def optimizer_step(self, model, optimizer, grad_clipping: float = 0.0):

colossalai/engine/schedule/_no_pipeline.py
@@ -79,9 +79,24 @@ class NoPipelineSchedule(BaseSchedule):
             self.fp16 = False
             self.amp_type = None
 
-    def initialize(self, model: nn.Module, optimizer: Optimizer):
-        if isinstance(optimizer, (ZeroRedundancyOptimizer_Level_2,
-                                  ZeroRedundancyOptimizer_Level_3)):
+    @property
+    def num_steps(self):
+        length = len(self.dataloader)
+        if self.training:
+            length -= length % self.grad_accum
+        return length
+
+    def initialize(self,
+                   dataloader=None,
+                   model=None,
+                   criterion=None,
+                   optimizer=None):
+        super().initialize(dataloader,
+                           model,
+                           criterion,
+                           optimizer)
+        if isinstance(self.optimizer, (ZeroRedundancyOptimizer_Level_2,
+                                       ZeroRedundancyOptimizer_Level_3)):
             self.use_zero_level_2_3 = True
             assert self.amp_type != AMP_TYPE.PARALLEL, \
                 'ZeRO Level 2 and 3 are mutually exclusive with AMP_TYPE.PARALLEL'
@@ -147,9 +162,8 @@ class NoPipelineSchedule(BaseSchedule):
             if not isinstance(output, (tuple, list)):
                 output = (output,)
             if return_loss:
-                loss = criterion(*output, *label)
-
-        loss /= grad_accum_size
+                loss = self.criterion(*output, *label)
+        loss /= self.grad_accum
 
         if not forward_only:
             # backward
@@ -170,7 +184,7 @@ class NoPipelineSchedule(BaseSchedule):
                 loss.backward()
 
         if return_loss:
-            return output, label, loss * grad_accum_size
+            return output, label, loss * self.grad_accum
         else:
             return output, None, None
 
@@ -183,6 +197,4 @@ class NoPipelineSchedule(BaseSchedule):
             self._torch_amp_scaler.step(optimizer)
             self._torch_amp_scaler.update()
         else:
-            if not self.fp16 and not self.use_zero_level_2_3 and grad_clipping > 0.0:
-                clip_grad_norm_fp32(model.parameters(), grad_clipping)
-            optimizer.step()
+            self.optimizer.step()

colossalai/engine/schedule/_pipeline.py
@@ -116,8 +116,24 @@ class PipelineSchedule(BaseSchedule):
         self.batch_pos += self.microbatch_size
         return (data,), (label,)
 
-    def initialize(self, model, optimizer):
-        if isinstance(optimizer, (ZeroRedundancyOptimizer_Level_2, ZeroRedundancyOptimizer_Level_3)):
+    @property
+    def num_steps(self):
+        length = len(self.dataloader)
+        if self.training:
+            length -= length % self.grad_accum
+        return length
+
+    def initialize(self,
+                   dataloader=None,
+                   model=None,
+                   criterion=None,
+                   optimizer=None):
+        super().initialize(dataloader,
+                           model,
+                           criterion,
+                           optimizer)
+        if isinstance(self.optimizer, (ZeroRedundancyOptimizer_Level_2,
+                                       ZeroRedundancyOptimizer_Level_3)):
             raise TypeError(
                 "Pipeline schedule is currently not compatible with ZeRO Level 2 and Level 3"
             )
@@ -147,8 +163,8 @@ class PipelineSchedule(BaseSchedule):
         if gpc.is_last_rank(ParallelMode.PIPELINE):
             if return_loss:
                 input_tensor, label = self.load_micro_batch()
-                loss_reduced = criterion(output_tensor, *label) \
-                               / (self.num_microbatches * grad_accum_size)
+                loss_reduced = self.criterion(output_tensor, *
+                label) / (self.num_microbatches * self.grad_accum)
                 return_tensors.append(
                     tuple((output_tensor, label[0], loss_reduced)))
                 return loss_reduced
@@ -309,7 +325,7 @@ class PipelineSchedule(BaseSchedule):
                 output, label, loss = tuple(map(list, zip(*return_tensors)))
                 return (torch.cat(output, dim=0),
                         torch.cat(label, dim=0),
-                        sum(loss) * grad_accum_size)
+                        sum(loss) * self.grad_accum)
             else:
                 return tuple((torch.cat(return_tensors, dim=0), None, None))
         else: