optimized 3d layer to fix slow computation ; tested imagenet performance with 3d; reworked lr_scheduler config definition; fixed launch args; fixed some printing issues; simplified apis of 3d layers (#51)

colossalai/nn/loss/cross_entropy_3d.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import os
+
 import torch
 import torch.distributed as dist
-from torch.nn.modules.loss import _Loss
-
-from colossalai.communication import all_gather
+from colossalai.constants import (INPUT_GROUP_3D, OUTPUT_GROUP_3D,
+                                  WEIGHT_GROUP_3D)
 from colossalai.core import global_context as gpc
 from colossalai.nn.layer.parallel_3d._operation import Reduce_3D
-from colossalai.nn.layer.parallel_3d._utils import get_last_group, get_depth_from_env
+from colossalai.nn.layer.parallel_3d._utils import (get_depth_from_env,
+                                                    get_last_group,
+                                                    get_parallel_mode_from_env)
 from colossalai.registry import LOSSES
 from colossalai.utils import get_current_device
-
-
-def accuracy_3d(output, target, input_parallel_mode, weight_parallel_mode):
-    depth = get_depth_from_env()
-    output_parallel_mode = get_last_group(input_parallel_mode,
-                                          weight_parallel_mode)
-    j = gpc.get_local_rank(input_parallel_mode)
-    i = gpc.get_local_rank(weight_parallel_mode)
-    target = torch.chunk(target, depth, dim=0)[i]
-    target = torch.chunk(target, depth, dim=0)[j]
-    output = all_gather(output, -1, output_parallel_mode)
-    prediction = torch.argmax(output, dim=-1)
-    correct = torch.sum(prediction == target)
-    dist.all_reduce(correct, group=gpc.get_group(input_parallel_mode))
-    dist.all_reduce(correct, group=gpc.get_group(weight_parallel_mode))
-    return correct.item()
+from torch.nn.modules.loss import _Loss
 
 
 class _ParallelCrossEntropyLossFunction_3D(torch.autograd.Function):
@@ -112,16 +100,18 @@ class CrossEntropyLoss3D(_Loss):
     :param reduction: whether to average the loss, defaults to True
     :type reduction: bool, optional
     """
-    def __init__(self,
-                 input_parallel_mode,
-                 weight_parallel_mode,
-                 reduction=True):
+    def __init__(
+            self,
+            #  input_parallel_mode,
+            #  weight_parallel_mode,
+            reduction=True,
+            label_smoothing=0.0):
         super().__init__()
         self.depth = get_depth_from_env()
-        self.input_parallel_mode = input_parallel_mode
-        self.weight_parallel_mode = weight_parallel_mode
-        self.output_parallel_mode = get_last_group(input_parallel_mode,
-                                                   weight_parallel_mode)
+        self.input_parallel_mode = get_parallel_mode_from_env(INPUT_GROUP_3D)
+        self.weight_parallel_mode = get_parallel_mode_from_env(WEIGHT_GROUP_3D)
+        self.output_parallel_mode = get_last_group(self.input_parallel_mode,
+                                                   self.weight_parallel_mode)
         self.input_rank = gpc.get_local_rank(self.input_parallel_mode)
         self.weight_rank = gpc.get_local_rank(self.weight_parallel_mode)
         self.reduction_mean = reduction
@@ -141,53 +131,53 @@ class CrossEntropyLoss3D(_Loss):
         return loss
 
 
-@LOSSES.register_module
-class LabelSmoothingCrossEntropy3D(_Loss):
-    """
-    NLL loss with label smoothing, adapted from timm.loss.LabelSmoothingCrossEntropy
+# @LOSSES.register_module
+# class LabelSmoothingCrossEntropy3D(_Loss):
+#     """
+#     NLL loss with label smoothing, adapted from timm.loss.LabelSmoothingCrossEntropy
 
-    :param input_parallel_mode: parallel mode for input tensor
-    :type input_parallel_mode: ParallelMode
-    :param weight_parallel_mode: parallel mode for weight
-    :type weight_parallel_mode: ParallelMode
-    :param smoothing: label smoothing value, defaults to 0.1
-    :type smoothing: float
-    :param reduction: whether to average the loss, defaults to True
-    :type reduction: bool, optional
-    """
-    def __init__(self,
-                 input_parallel_mode,
-                 weight_parallel_mode,
-                 smoothing=0.1,
-                 reduction=True):
-        super().__init__()
-        assert smoothing < 1.0
-        self.smoothing = smoothing
-        self.confidence = 1. - smoothing
-        self.depth = get_depth_from_env()
-        self.input_parallel_mode = input_parallel_mode
-        self.weight_parallel_mode = weight_parallel_mode
-        self.output_parallel_mode = get_last_group(input_parallel_mode,
-                                                   weight_parallel_mode)
-        self.reduction_mean = reduction
+#     :param input_parallel_mode: parallel mode for input tensor
+#     :type input_parallel_mode: ParallelMode
+#     :param weight_parallel_mode: parallel mode for weight
+#     :type weight_parallel_mode: ParallelMode
+#     :param smoothing: label smoothing value, defaults to 0.1
+#     :type smoothing: float
+#     :param reduction: whether to average the loss, defaults to True
+#     :type reduction: bool, optional
+#     """
+#     def __init__(self,
+#                  input_parallel_mode,
+#                  weight_parallel_mode,
+#                  smoothing=0.1,
+#                  reduction=True):
+#         super().__init__()
+#         assert smoothing < 1.0
+#         self.smoothing = smoothing
+#         self.confidence = 1. - smoothing
+#         self.depth = get_depth_from_env()
+#         self.input_parallel_mode = input_parallel_mode
+#         self.weight_parallel_mode = weight_parallel_mode
+#         self.output_parallel_mode = get_last_group(input_parallel_mode,
+#                                                    weight_parallel_mode)
+#         self.reduction_mean = reduction
 
-    def forward(self, logits, targets):
-        # split label partition from the entire batch
-        j = gpc.get_local_rank(self.input_parallel_mode)
-        i = gpc.get_local_rank(self.weight_parallel_mode)
-        targets = torch.chunk(targets, self.depth, dim=0)[i]
-        targets = torch.chunk(targets, self.depth, dim=0)[j]
-        exp_logits = torch.exp(logits)
-        sum_exp_logits = Sum3D.apply(exp_logits, -1, depth,
-                                     self.output_parallel_mode, False)
-        log_probs = torch.log(sum_exp_logits) - logits
-        nll_loss = _ParallelCrossEntropyLossFunction_3D.apply(
-            logits, targets, self.depth, self.output_parallel_mode)
-        smooth_loss = -log_probs.mean(dim=-1)
-        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-        if self.reduction_mean:
-            loss = loss.sum()
-            loss = Reduce_3D.apply(loss, self.depth, self.input_parallel_mode)
-            loss = Reduce_3D.apply(loss, self.depth, self.weight_parallel_mode)
-            loss /= batch_size
-        return loss
+#     def forward(self, logits, targets):
+#         # split label partition from the entire batch
+#         j = gpc.get_local_rank(self.input_parallel_mode)
+#         i = gpc.get_local_rank(self.weight_parallel_mode)
+#         targets = torch.chunk(targets, self.depth, dim=0)[i]
+#         targets = torch.chunk(targets, self.depth, dim=0)[j]
+#         exp_logits = torch.exp(logits)
+#         sum_exp_logits = Sum3D.apply(exp_logits, -1, depth,
+#                                      self.output_parallel_mode, False)
+#         log_probs = torch.log(sum_exp_logits) - logits
+#         nll_loss = _ParallelCrossEntropyLossFunction_3D.apply(
+#             logits, targets, self.depth, self.output_parallel_mode)
+#         smooth_loss = -log_probs.mean(dim=-1)
+#         loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+#         if self.reduction_mean:
+#             loss = loss.sum()
+#             loss = Reduce_3D.apply(loss, self.depth, self.input_parallel_mode)
+#             loss = Reduce_3D.apply(loss, self.depth, self.weight_parallel_mode)
+#             loss /= batch_size
+#         return loss

colossalai/nn/optimizer/_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+from colossalai.utils.common import print_rank_0
 import torch
 from torch._six import inf
 
@@ -10,8 +11,9 @@ except:
     print('Colossalai should be built with cuda extension to use the FP16 optimizer')
 
 from ..multi_tensor_apply import multi_tensor_applier
+
+from colossalai.constants import IS_TENSOR_PARALLEL, TENSOR_PARALLEL_ATTRIBUTES, NUM_PARTITIONS
 import torch.distributed as dist
-from colossalai.constants import IS_TENSOR_PARALLEL, TENSOR_PARALLEL_ATTRIBUTES
 from colossalai.context.parallel_mode import ParallelMode
 from colossalai.core import global_context as gpc
 
@@ -105,7 +107,8 @@ def clip_grad_norm_fp32(parameters, max_norm, norm_type=2):
         no_tensor_parallel_grads = []
         for p in params:
             if is_model_parallel_parameter(p):
-                tensor_parallel_grads.append(p.grad.data)
+                reductor = (gpc.get_world_size(ParallelMode.TENSOR) / getattr(p, NUM_PARTITIONS)) ** (1 / norm_type)
+                tensor_parallel_grads.append(p.grad.data / reductor)
             else:
                 no_tensor_parallel_grads.append(p.grad.data)
         if norm_type == 2.0:

colossalai/nn/optimizer/fp16_optimizer.py
@@ -113,7 +113,7 @@ class DynamicGradScaler:
             if self._hysteresis_tracker <= 0:
                 self._scale = torch.max(self._scale * self.backoff_factor,
                                         self.min_scale)
-            self._logger.info(f'overflow occurs, loss scale is adjusted to {self._scale}')
+            self._logger.info(f'overflow occurs, loss scale is adjusted to {self._scale}', ranks=[0])
         else:
             # If there is no nan/inf, increment the growth tracker.
             self._growth_tracker += 1
@@ -125,10 +125,10 @@ class DynamicGradScaler:
                 # and scale up the loss scale.
                 if self._max_scale is not None and self._scale >= self._max_scale:
                     self._logger.info(
-                        f'Current loss scale {self._scale} has reached the max scale {self._max_scale} allowed')
+                        f'Current loss scale {self._scale} has reached the max scale {self._max_scale} allowed', ranks=[0])
                 else:
                     self._scale = self._scale * self.growth_factor
-                    self._logger.info(f'no consecutive overflow, loss scale is adjusted to {self._scale}')
+                    self._logger.info(f'no consecutive overflow, loss scale is adjusted to {self._scale}', ranks=[0])
 
     def state_dict(self):
         state_dict = {}

tests/test_models/test_vision_transformer/test_vit_3d/test_vit_3d.py
@@ -1,105 +1,205 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import time
-from pathlib import Path
 
-import torch
-from tqdm import tqdm
+import glob
+import os
 
 import colossalai
+import nvidia.dali.fn as fn
+import nvidia.dali.tfrecord as tfrec
+import torch
 from colossalai.context import ParallelMode
 from colossalai.core import global_context as gpc
 from colossalai.logging import get_global_dist_logger
 from colossalai.trainer import Trainer
-from colossalai.trainer.metric import Accuracy3D
-from colossalai.utils import print_rank_0
+from colossalai.utils import (get_global_multitimer,
+                              set_global_multitimer_status)
+from nvidia.dali import types
+from nvidia.dali.pipeline import Pipeline
+from nvidia.dali.plugin.pytorch import DALIClassificationIterator
 
-CONFIG_PATH = Path(__file__).parent.parent.joinpath('configs/vit_3d.py')
+DATASET_PATH = str(os.environ['DATA'])
+# imagenet 100
+# TRAIN_RECS = '/project/scratch/p200012/imagenet-100/train/*'
+# VAL_RECS = '/project/scratch/p200012/imagenet-100/validation/*'
+# TRAIN_IDX = '/project/scratch/p200012/imagenet-100/idx_files/train/*'
+# VAL_IDX = '/project/scratch/p200012/imagenet-100/idx_files/validation/*'
 
-
-def _train_epoch(epoch, engine):
-    logger = get_global_dist_logger()
-    print_rank_0('[Epoch %d] training start' % (epoch), logger)
-    engine.train()
-
-    train_loss = 0
-    batch_cnt = 0
-    num_samples = 0
-    now = time.time()
-    epoch_start = now
-    progress = range(engine._schedule.num_steps)
-    if gpc.get_global_rank() == 0:
-        progress = tqdm(progress, desc='[Epoch %d]' % epoch, miniters=1)
-    for step in progress:
-        cur_lr = engine.get_lr()
-
-        _, targets, loss = engine.step()
-
-        batch_size = targets[0].size(0)
-        train_loss += loss.item()
-        num_samples += batch_size
-        batch_cnt += 1
-
-        batch_time = time.time() - now
-        now = time.time()
-        if gpc.get_global_rank() == 0:
-            print_features = dict(lr='%g' % cur_lr,
-                                  loss='%.3f' % (train_loss / (step + 1)),
-                                  throughput='%.3f (images/sec)' %
-                                             (batch_size / (batch_time + 1e-12)))
-            progress.set_postfix(**print_features)
-
-    epoch_end = time.time()
-    epoch_loss = train_loss / batch_cnt
-    epoch_throughput = num_samples / (epoch_end - epoch_start + 1e-12)
-    print_rank_0(
-        '[Epoch %d] Loss: %.3f | Throughput: %.3f (samples/sec)' %
-        (epoch, epoch_loss, epoch_throughput), logger)
+# imagenet 1000
+TRAIN_RECS = DATASET_PATH + '/train/*'
+VAL_RECS = DATASET_PATH + '/validation/*'
+TRAIN_IDX = DATASET_PATH + '/idx_files/train/*'
+VAL_IDX = DATASET_PATH + '/idx_files/validation/*'
 
 
-def _eval(epoch, engine):
-    logger = get_global_dist_logger()
-    engine.eval()
+class DaliDataloader(DALIClassificationIterator):
+    def __init__(self,
+                 tfrec_filenames,
+                 tfrec_idx_filenames,
+                 shard_id=0,
+                 num_shards=1,
+                 batch_size=128,
+                 num_threads=4,
+                 resize=256,
+                 crop=224,
+                 prefetch=2,
+                 training=True,
+                 gpu_aug=False,
+                 cuda=True):
+        pipe = Pipeline(
+            batch_size=batch_size,
+            num_threads=num_threads,
+            device_id=torch.cuda.current_device() if cuda else None,
+            seed=1024)
+        with pipe:
+            inputs = fn.readers.tfrecord(path=tfrec_filenames,
+                                         index_path=tfrec_idx_filenames,
+                                         random_shuffle=training,
+                                         shard_id=shard_id,
+                                         num_shards=num_shards,
+                                         initial_fill=10000,
+                                         read_ahead=True,
+                                         prefetch_queue_depth=prefetch,
+                                         name='Reader',
+                                         features={
+                                             'image/encoded':
+                                             tfrec.FixedLenFeature(
+                                                 (), tfrec.string, ""),
+                                             'image/class/label':
+                                             tfrec.FixedLenFeature([1],
+                                                                   tfrec.int64,
+                                                                   -1),
+                                         })
+            images = inputs["image/encoded"]
 
-    eval_loss = 0
-    acc = Accuracy3D(True, ParallelMode.PARALLEL_3D_OUTPUT,
-                     ParallelMode.PARALLEL_3D_WEIGHT)
-    total = 0
-    with torch.no_grad():
-        for _ in range(engine._schedule.num_steps):
-            outputs, targets, loss = engine.step()
-            if isinstance(outputs, (list, tuple)):
-                outputs = outputs[0]
-            if isinstance(targets, (list, tuple)):
-                targets = targets[0]
-            eval_loss += loss.item()
-            acc.update(outputs, targets)
-            total += targets.size(0)
+            if training:
+                images = fn.decoders.image(
+                    images,
+                    device='mixed' if gpu_aug else 'cpu',
+                    output_type=types.RGB)
+                images = fn.random_resized_crop(
+                    images, size=crop, device='gpu' if gpu_aug else 'cpu')
+                flip_lr = fn.random.coin_flip(probability=0.5)
+            else:
+                # decode jpeg and resize
+                images = fn.decoders.image(
+                    images,
+                    device='mixed' if gpu_aug else 'cpu',
+                    output_type=types.RGB)
+                images = fn.resize(images,
+                                   device='gpu' if gpu_aug else 'cpu',
+                                   resize_x=resize,
+                                   resize_y=resize,
+                                   dtype=types.FLOAT,
+                                   interp_type=types.INTERP_TRIANGULAR)
+                flip_lr = False
 
-        print_rank_0(
-            '[Epoch %d] Evaluation loss: %.3f | Acc: %.3f%%' %
-            (epoch, eval_loss / engine._schedule.num_steps,
-             acc.get_accumulated_value() * 100), logger)
+            # center crop and normalise
+            images = fn.crop_mirror_normalize(images,
+                                              dtype=types.FLOAT,
+                                              crop=(crop, crop),
+                                              mean=[127.5],
+                                              std=[127.5],
+                                              mirror=flip_lr)
+            label = inputs["image/class/label"] - 1  # 0-999
+            # LSG: element_extract will raise exception, let's flatten outside
+            # label = fn.element_extract(label, element_map=0)  # Flatten
+            if cuda:  # transfer data to gpu
+                pipe.set_outputs(images.gpu(), label.gpu())
+            else:
+                pipe.set_outputs(images, label)
+
+        pipe.build()
+        last_batch_policy = 'DROP' if training else 'PARTIAL'
+        super().__init__(pipe,
+                         reader_name="Reader",
+                         auto_reset=True,
+                         last_batch_policy=last_batch_policy)
+
+    def __iter__(self):
+        # if not reset (after an epoch), reset; if just initialize, ignore
+        if self._counter >= self._size or self._size < 0:
+            self.reset()
+        return self
+
+    def __next__(self):
+        data = super().__next__()
+        img, label = data[0]['data'], data[0]['label']
+        label = label.squeeze()
+        return (img, ), (label, )
 
 
-def train():
+def build_dali_train():
+    return DaliDataloader(sorted(glob.glob(TRAIN_RECS)),
+                          sorted(glob.glob(TRAIN_IDX)),
+                          batch_size=gpc.config.BATCH_SIZE //
+                          gpc.data_parallel_size,
+                          shard_id=gpc.get_local_rank(ParallelMode.DATA),
+                          num_shards=gpc.get_world_size(ParallelMode.DATA),
+                          training=True,
+                          gpu_aug=True,
+                          cuda=True)
+
+
+def build_dali_test():
+    return DaliDataloader(sorted(glob.glob(VAL_RECS)),
+                          sorted(glob.glob(VAL_IDX)),
+                          batch_size=gpc.config.BATCH_SIZE //
+                          gpc.data_parallel_size,
+                          shard_id=gpc.get_local_rank(ParallelMode.DATA),
+                          num_shards=gpc.get_world_size(ParallelMode.DATA),
+                          training=False,
+                          gpu_aug=True,
+                          cuda=True)
+
+
+def train_cifar():
     # init dist
-    engine, train_dataloader, test_dataloader = colossalai.initialize(CONFIG_PATH)
+    engine, train_dataloader, test_dataloader = colossalai.initialize()
     logger = get_global_dist_logger()
+    set_global_multitimer_status(True)
 
     logger.info("Engine is built", ranks=[0])
 
-    trainer = Trainer(engine=engine, verbose=True)
+    trainer = Trainer(engine=engine,
+                      timer=get_global_multitimer(),
+                      verbose=True)
     logger.info("Trainer is built", ranks=[0])
 
     logger.info("Train start", ranks=[0])
     trainer.fit(train_dataloader=train_dataloader,
                 test_dataloader=test_dataloader,
+                # epochs=gpc.config.num_epochs,
+                epochs=5,
+                hooks_cfg=gpc.config.hooks,
+                display_progress=True,
+                test_interval=1)
+
+
+def train_imagenet():
+    # init dist
+    engine, train_dataloader, test_dataloader = colossalai.initialize(
+        train_dataloader=build_dali_train, test_dataloader=build_dali_test)
+    logger = get_global_dist_logger()
+    set_global_multitimer_status(True)
+
+    logger.info("Engine is built", ranks=[0])
+
+    trainer = Trainer(engine=engine,
+                      timer=get_global_multitimer(),
+                      verbose=True)
+    logger.info("Trainer is built", ranks=[0])
+
+    logger.info("Train start", ranks=[0])
+    trainer.fit(train_dataloader=train_dataloader,
+                # test_dataloader=test_dataloader,
                 epochs=gpc.config.num_epochs,
+                max_steps=100,
                 hooks_cfg=gpc.config.hooks,
                 display_progress=True,
                 test_interval=1)
 
 
 if __name__ == '__main__':
-    train()
+    # train_cifar()
+    train_imagenet()