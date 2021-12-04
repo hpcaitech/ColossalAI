944395813e08f3376d625b494021daf66dc88031
ver217 <lhx0217@gmail.com>
2021-12-04 15:47:13 +0800

    Feature/ddp (#49)
    * remove redundancy func in setup (#19) (#20)
    
    * use env to control the language of doc (#24) (#25)
    
    * Support TP-compatible Torch AMP and Update trainer API (#27)
    
    * Add gradient accumulation, fix lr scheduler
    
    * fix FP16 optimizer and adapted torch amp with tensor parallel (#18)
    
    * fixed bugs in compatibility between torch amp and tensor parallel and performed some minor fixes
    
    * fixed trainer
    
    * Revert "fixed trainer"
    
    This reverts commit 2e0b0b76990e8d4e337add483d878c0f61cf5097.
    
    * improved consistency between trainer, engine and schedule (#23)
    
    Co-authored-by: 1SAA <c2h214748@gmail.com>
    
    Co-authored-by: 1SAA <c2h214748@gmail.com>
    Co-authored-by: ver217 <lhx0217@gmail.com>
    
    * add an example of ViT-B/16 and remove w_norm clipping in LAMB (#29)
    
    * add explanation for ViT example (#35) (#36)
    
    * support torch ddp
    
    * fix loss accumulation
    
    * add log for ddp
    
    * change seed
    
    * modify timing hook
    
    Co-authored-by: Frank Lee <somerlee.9@gmail.com>
    Co-authored-by: 1SAA <c2h214748@gmail.com>
    Co-authored-by: binmakeswell <binmakeswell@gmail.com>

--- colossalai/utils/__init__.py
+++ colossalai/utils/__init__.py
@@ -1,5 +1,5 @@
 from .activation_checkpoint import checkpoint
-from .common import print_rank_0, sync_model_param_in_dp, is_dp_rank_0, is_tp_rank_0, is_no_pp_or_last_stage
+from .common import print_rank_0, sync_model_param_in_dp, is_dp_rank_0, is_tp_rank_0, is_no_pp_or_last_stage, is_using_ddp, is_using_pp, ConditionalContext
 from .cuda import get_current_device, synchronize, empty_cache, set_to_cuda
 from .memory import report_memory_usage
 from .timer import MultiTimer, Timer
@@ -18,5 +18,6 @@ def set_global_multitimer_status(mode: bool):
 __all__ = ['checkpoint', 'print_rank_0', 'sync_model_param_in_dp', 'get_current_device',
            'synchronize', 'empty_cache', 'set_to_cuda', 'report_memory_usage', 'Timer', 'MultiTimer',
            'get_global_multitimer', 'set_global_multitimer_status',
-           'is_dp_rank_0', 'is_tp_rank_0', 'is_no_pp_or_last_stage'
+           'is_dp_rank_0', 'is_tp_rank_0', 'is_no_pp_or_last_stage',
+           'is_using_ddp', 'ConditionalContext', 'is_using_pp'
            ]
