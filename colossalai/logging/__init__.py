a951bc60898109b56bff15ccc5c87e163dc25204
ver217 <lhx0217@gmail.com>
2022-01-04 20:03:26 +0800

    update default logger (#100) (#101)

--- colossalai/logging/__init__.py
+++ colossalai/logging/__init__.py
@@ -1,9 +1,11 @@
+from typing import List
 from .logging import DistributedLogger
+import logging
 
 __all__ = ['get_dist_logger', 'DistributedLogger']
 
 
-def get_dist_logger(name='root'):
+def get_dist_logger(name='colossalai'):
     """Get logger instance based on name. The DistributedLogger will create singleton instances,
     which means that only one logger instance is created per name.
 
@@ -14,3 +16,14 @@ def get_dist_logger(name='root'):
     :rtype: :class:`colossalai.logging.DistributedLogger`
     """
     return DistributedLogger.get_instance(name=name)
+
+
+def disable_existing_loggers(except_loggers: List[str] = ['colossalai']):
+    """Set the level of existing loggers to `WARNING`.
+
+    :param except_loggers: loggers in this `list` will be ignored when disabling, defaults to ['colossalai']
+    :type except_loggers: list, optional
+    """
+    for log_name in logging.Logger.manager.loggerDict.keys():
+        if log_name not in except_loggers:
+            logging.getLogger(log_name).setLevel(logging.WARNING)
