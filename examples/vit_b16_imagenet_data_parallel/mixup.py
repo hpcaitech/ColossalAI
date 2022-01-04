f03bcb359bfae1bf1aa420dfa3608b4e10c624a1
ver217 <lhx0217@gmail.com>
2022-01-04 20:35:33 +0800

    update vit example for new API (#98) (#99)

--- examples/vit_b16_imagenet_data_parallel/mixup.py
+++ examples/vit_b16_imagenet_data_parallel/mixup.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 from colossalai.registry import LOSSES
+import torch
+
 
 @LOSSES.register_module
 class MixupLoss(nn.Module):
@@ -7,6 +9,13 @@ class MixupLoss(nn.Module):
         super().__init__()
         self.loss_fn = loss_fn_cls()
 
-    def forward(self, inputs, *args):
-        targets_a, targets_b, lam = args
+    def forward(self, inputs, targets_a, targets_b, lam):
         return lam * self.loss_fn(inputs, targets_a) + (1 - lam) * self.loss_fn(inputs, targets_b)
+
+
+class MixupAccuracy(nn.Module):
+    def forward(self, logits, targets):
+        targets = targets['targets_a']
+        preds = torch.argmax(logits, dim=-1)
+        correct = torch.sum(targets == preds)
+        return correct
