d8b9eb70c6bb580b005b76ba3deefa44b8f538b3
=puck_WCR <46049915+WANG-CR@users.noreply.github.com>
2022-01-07 05:16:45 +0100

    feat: add gpt1d hybrid parallel model and test script

---
+++ benchmark/gpt/experiment/configs/gpt2_pp1d.py
@@ -0,0 +1,33 @@
+from model import GPT2_exlarge_pipeline_1D
+from torch.optim import Adam
+from colossalai.amp import AMP_TYPE
+import torch
+
+
+BATCH_SIZE = 192
+NUM_EPOCHS = 60
+SEQ_LEN = 1024
+NUM_MICRO_BATCHES = 192
+TENSOR_SHAPE = (BATCH_SIZE // NUM_MICRO_BATCHES, SEQ_LEN, 1600)
+
+fp16 = dict(
+    mode=AMP_TYPE.NAIVE
+)
+
+parallel = dict(
+    pipeline=4,
+    tensor=dict(mode='1d', size=1)
+)
+
+optimizer = dict(
+    type=Adam,
+    lr=0.00015,
+    weight_decay=1e-2,
+)
+
+model = dict(
+    type=GPT2_exlarge_pipeline_1D,
+    checkpoint=True,
+    dtype=torch.half,
+    # num_chunks=2,
+)
---
+++ benchmark/gpt/experiment/configs/gpt2_zero3.py
@@ -0,0 +1,38 @@
+from model.gpt import GPT2_exlarge, GPT2_small
+from torch.optim import Adam
+from deepspeed.ops.adam import DeepSpeedCPUAdam
+
+
+BATCH_SIZE = 2
+NUM_EPOCHS = 60
+SEQ_LEN = 1024
+
+zero = dict(
+    level=3,
+    dynamic_loss_scale=True,
+    overlap_comm=True,
+    clip_grad=1.0,
+    offload_optimizer_config=dict(
+        device='cpu',
+        pin_memory=True,
+        fast_init=False,
+    ),
+    offload_param_config=dict(
+        device='cpu',
+        pin_memory=True,
+        max_in_cpu=1e9,
+    )
+)
+
+
+optimizer = dict(
+    type=Adam,
+    lr=0.00015,
+    weight_decay=1e-2,
+)
+
+model = dict(
+    type=GPT2_exlarge,
+    checkpoint=True,
+    # checkpoint_offload=True,
+)
