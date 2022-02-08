975bdf85d857f220dbb4faf7c17466c4a361c5eb
ver217 <lhx0217@gmail.com>
2022-02-08 19:17:11 +0800

    add zero1

--- colossalai/zero/__init__.py
+++ colossalai/zero/__init__.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
-from torch.optim import Optimizer
 from colossalai.amp.naive_amp import NaiveAMPModel
-from colossalai.utils import is_no_pp_or_last_stage
-from colossalai.core import global_context as gpc
 from colossalai.context.parallel_mode import ParallelMode
+from colossalai.core import global_context as gpc
+from torch.optim import Optimizer
 
+from .zero_redundancy_optimizer_level_1 import ZeroRedundancyOptimizer_Level_1
 from .zero_redundancy_optimizer_level_2 import ZeroRedundancyOptimizer_Level_2
 from .zero_redundancy_optimizer_level_3 import ZeroRedundancyOptimizer_Level_3
 
@@ -30,10 +30,11 @@ def convert_to_zero(model: nn.Module,
     :rtype: Tuple
     """
     import deepspeed
-    assert level == 2 or level == 3, 'Only ZERO Optimizer Level 2 and 3 are provided'
+    assert 1 <= level <= 3, 'Only ZERO Optimizer Level 1-3 are provided'
     model = NaiveAMPModel(model, output_to_fp32=False)
-
-    if level == 2:
+    if level == 1:
+        optimizer = ZeroRedundancyOptimizer_Level_1(init_optimizer=optimizer, **zero_config)
+    elif level == 2:
         optimizer = ZeroRedundancyOptimizer_Level_2(init_optimizer=optimizer, **zero_config)
     else:
         optimizer = ZeroRedundancyOptimizer_Level_3(init_optimizer=optimizer, module=model, **zero_config)
