d1ea1f27fe2f0e7efa8a48485c94fff3d5625b4e
ver217 <lhx0217@gmail.com>
2022-02-25 15:26:22 +0800

    polish code and add state dict hook

--- colossalai/zero/_zero3_utils.py
+++ colossalai/zero/_zero3_utils.py
@@ -1,5 +1,6 @@
 
-from typing import Any, Callable, List, Tuple
+from collections import OrderedDict
+from typing import Any, Callable, Dict, List, Tuple, Union
 
 import torch
 import torch.nn.functional as F
@@ -99,3 +100,25 @@ def assert_in_engine(cond: Any, s: Any) -> None:
     if not cond:
         print(s)
         raise AssertionError
+
+
+def replace_state_dict_prefix(
+    state_dict: Union[Dict[str, torch.Tensor], "OrderedDict[str, torch.Tensor]"], old_prefix: str, new_prefix: str
+) -> None:
+    """
+    Replace all keys that match a given old_prefix with a new_prefix (in-place).
+
+    Usage::
+
+        state_dict = {"layer.xyz": torch.tensor(1)}
+        replace_state_dict_prefix(state_dict, "layer.", "module.layer.")
+        assert state_dict == {"module.layer.xyz": torch.tensor(1)}
+    """
+    if old_prefix == new_prefix:
+        raise ValueError("old_prefix and new_prefix must be distinct")
+    for key in list(state_dict.keys()):
+        if not key.startswith(old_prefix):
+            continue
+        new_key = new_prefix + key[len(old_prefix):]
+        state_dict[new_key] = state_dict[key]
+        del state_dict[key]
