127ed7d3497e2bc03d17eea5a819ee0702ca0fa9
1SAA <c2h214748@gmail.com>
2022-02-27 22:28:39 +0800

    Added TPExpert for special situation

--- colossalai/nn/layer/moe/__init__.py
+++ colossalai/nn/layer/moe/__init__.py
@@ -1,5 +1,8 @@
-from .experts import Experts, FFNExperts
+from .experts import Experts, FFNExperts, TPExperts
 from .layers import MoeLayer, Top1Router, Top2Router
-from .utils import NormalNoiseGenerator
+from .utils import NormalNoiseGenerator, build_ffn_experts
 
-__all__ = ['Experts', 'FFNExperts', 'Top1Router', 'Top2Router', 'MoeLayer', 'NormalNoiseGenerator']
+__all__ = [
+    'Experts', 'FFNExperts', 'TPExperts', 'Top1Router', 'Top2Router', 'MoeLayer', 'NormalNoiseGenerator',
+    'build_ffn_experts'
+]
