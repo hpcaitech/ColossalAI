b880b87a39341af622ef31fd25385ebb214d3fe9
ver217 <lhx0217@gmail.com>
2022-03-04 11:49:02 +0800

    impl shard optim v2 and add unit test

--- colossalai/engine/ophooks/_shard_param_ophook.py
+++ colossalai/engine/ophooks/_shard_param_ophook.py
@@ -1,12 +1,16 @@
 import torch
-from . import BaseOpHook
+import torch.distributed as dist
 from colossalai.registry import OPHOOKS
+
+from . import BaseOpHook
+
 
 @OPHOOKS.register_module
 class ShardParamHook(BaseOpHook):
     """
     A hook to process sharded param before and afther FWD and BWD operator executing.
     """
+
     def __init__(self):
         super().__init__()
 
@@ -17,25 +21,32 @@ class ShardParamHook(BaseOpHook):
         for param in module.parameters():
             assert hasattr(param, 'ca_attr')
             param.ca_attr.gather()
+            if dist.get_rank() == 0:
+                print(f'{param._name} pre fwd shape {param.ca_attr.payload("cpu").shape}')
 
     def post_fwd_exec(self, module: torch.nn.Module, *args):
         for param in module.parameters():
             assert hasattr(param, 'ca_attr')
             param.ca_attr.shard()
+            if dist.get_rank() == 0:
+                print(f'{param._name} post fwd shape {param.ca_attr.payload("cpu").shape}')
 
     def pre_bwd_exec(self, module: torch.nn.Module, input, output):
         for param in module.parameters():
             assert hasattr(param, 'ca_attr')
             param.ca_attr.gather()
+            if dist.get_rank() == 0:
+                print(f'{param._name} pre bwd shape {param.ca_attr.payload("cpu").shape}')
 
     def post_bwd_exec(self, module: torch.nn.Module, input):
         for param in module.parameters():
             assert hasattr(param, 'ca_attr')
             param.ca_attr.shard()
+            if dist.get_rank() == 0:
+                print(f'{param._name} post bwd shape {param.ca_attr.payload("cpu").shape}')
 
     def pre_iter(self):
         pass
 
     def post_iter(self):
         pass
-
