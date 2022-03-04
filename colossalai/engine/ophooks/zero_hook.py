a3e153a4d19e19c477c0c358b753efedff8ed63f
jiaruifang <fangjiarui123@gmail.com>
2022-03-04 16:15:27 +0800

    add zero hook.

---
+++ colossalai/engine/ophooks/zero_hook.py
@@ -0,0 +1,45 @@
+import torch
+from . import BaseOpHook
+from colossalai.registry import OPHOOKS
+from colossalai.zero.shard_utils import TensorShardStrategy
+
+
+@OPHOOKS.register_module
+class ZeroHook(BaseOpHook):
+    """
+    A hook to process sharded param for ZeRO method.
+    """
+
+    def __init__(self, process_group):
+        super().__init__()
+        self.shard_strategy = TensorShardStrategy(process_group=process_group)
+
+    def pre_fwd_exec(self, module: torch.nn.Module, *args):
+        for param in module.parameters():
+            assert hasattr(param, 'ca_attr')
+            self.shard_strategy.gather([param.ca_attr.data])
+            param.data = param.ca_attr.data
+
+    def post_fwd_exec(self, module: torch.nn.Module, *args):
+        for param in module.parameters():
+            assert hasattr(param, 'ca_attr')
+            self.shard_strategy.shard([param.ca_attr.data])
+            param.data = None
+
+    def pre_bwd_exec(self, module: torch.nn.Module, input, output):
+        for param in module.parameters():
+            assert hasattr(param, 'ca_attr')
+            self.shard_strategy.gather([param.ca_attr.data])
+            param.data = param.ca_attr.data
+
+    def post_bwd_exec(self, module: torch.nn.Module, input):
+        for param in module.parameters():
+            assert hasattr(param, 'ca_attr')
+            self.shard_strategy.shard([param.ca_attr.data])
+            param.data = None
+
+    def pre_iter(self):
+        pass
+
+    def post_iter(self):
+        pass
