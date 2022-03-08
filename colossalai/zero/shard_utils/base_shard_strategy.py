5ff1745710b1f276e35b462d567f4e9784fa0d92
jiaruifang <fangjiarui123@gmail.com>
2022-03-08 14:01:47 +0800

    polish code

--- colossalai/zero/shard_utils/base_shard_strategy.py
+++ colossalai/zero/shard_utils/base_shard_strategy.py
@@ -7,6 +7,11 @@ from typing import List, Optional
 class BaseShardStrategy(ABC):
 
     def __init__(self, process_group: Optional[dist.ProcessGroup] = None) -> None:
+        """Abstract Shard Strategy. Use to shard a tensors on multiple GPUs.
+
+        Args:
+            process_group (Optional[dist.ProcessGroup], optional): the process group. Defaults to None.
+        """
         self.process_group = process_group
         self.world_size = dist.get_world_size(self.process_group)
         self.local_rank = dist.get_rank(self.process_group)
@@ -14,14 +19,18 @@ class BaseShardStrategy(ABC):
 
     @abstractmethod
     def shard(self, tensor_list: List[ShardedTensor]):
-        r"""
-        sharded the memory of tensor on multiple processes.
+        """shard resulting to only one copy of tensor payloads on multiple processes.
+
+        Args:
+            tensor_list (List[ShardedTensor]): a list of ShardedTensor
         """
         pass
 
     @abstractmethod
     def gather(self, tensor_list: List[ShardedTensor]):
-        r"""
-        duplicate tensor payload on each processes.
+        """gather resulting to duplicated tensor payload on each processes.
+
+        Args:
+            tensor_list (List[ShardedTensor]): a list of ShardedTensor
         """
         pass
