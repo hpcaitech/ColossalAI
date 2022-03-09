afd442cb2e17060ee03cb266736bf89f6adcc905
ExtremeViscent <zhangyiqi55732@sina.com>
2022-03-09 01:44:20 +0000

    [formart] format fixed for kernel\cuda_native codes (#335)

--- colossalai/kernel/cuda_native/layer_norm.py
+++ colossalai/kernel/cuda_native/layer_norm.py
@@ -37,10 +37,10 @@ class FusedLayerNormAffineFunction(torch.autograd.Function):
         input_, weight_, bias_, mean, invvar = ctx.saved_tensors
         grad_input = grad_weight = grad_bias = None
         grad_input, grad_weight, grad_bias \
-          = colossal_layer_norm_cuda.backward_affine(
-            grad_output.contiguous(), mean, invvar,
-            input_, ctx.normalized_shape,
-            weight_, bias_, ctx.eps)
+            = colossal_layer_norm_cuda.backward_affine(
+                grad_output.contiguous(), mean, invvar,
+                input_, ctx.normalized_shape,
+                weight_, bias_, ctx.eps)
 
         return grad_input, grad_weight, grad_bias, None, None
 
