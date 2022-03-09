c35fdbfe5debf8629d2ee1572d97bd18304cb31c
Frank Lee <somerlee.9@gmail.com>
2022-03-09 11:51:22 +0800

    set criterion as optional in colossalai initialize (#336)

--- colossalai/amp/torch_amp/__init__.py
+++ colossalai/amp/torch_amp/__init__.py
@@ -3,12 +3,13 @@ from torch.optim import Optimizer
 from torch.nn.modules.loss import _Loss
 from colossalai.context import Config
 from .torch_amp import TorchAMPOptimizer, TorchAMPModel, TorchAMPLoss
+from typing import Optional
 
 
 def convert_to_torch_amp(model: nn.Module,
                          optimizer: Optimizer,
-                         criterion: _Loss,
-                         amp_config: Config):
+                         criterion: Optional[_Loss] = None,
+                         amp_config: Optional[Config] = None):
     """A helper function to wrap training components with Torch AMP modules
 
     :param model: your model object
@@ -16,16 +17,18 @@ def convert_to_torch_amp(model: nn.Module,
     :param optimizer: your optimizer object
     :type optimizer: :class:`torch.optim.Optimzer`
     :param criterion: your loss function object
-    :type criterion: :class:`torch.nn.modules.loss._Loss`
+    :type criterion: :class:`torch.nn.modules.loss._Loss`, optional
     :param amp_config: configuration for different amp modes
-    :type amp_config: :class:`colossalai.context.Config` or dict
-    
+    :type amp_config: :class:`colossalai.context.Config` or dict, optional
     :return: (model, optimizer, criterion)
     :rtype: Tuple
     """
     model = TorchAMPModel(model)
+    if amp_config is None:
+        amp_config = dict()
     optimizer = TorchAMPOptimizer(optimizer, **amp_config)
-    criterion = TorchAMPLoss(criterion)
+    if criterion:
+        criterion = TorchAMPLoss(criterion)
     return model, optimizer, criterion
 
 
