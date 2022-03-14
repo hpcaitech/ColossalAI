61cfab1d922f9aeb1b878e9fdf6f224d7e3234ac
Jie Zhu <jay.login@icloud.com>
2022-03-14 13:09:49 +0800

    add trainer hook

--- colossalai/trainer/hooks/_mem_tracer_hook.py
+++ colossalai/trainer/hooks/_mem_tracer_hook.py
@@ -2,6 +2,7 @@ from cgitb import Hook
 from colossalai.registry import HOOKS
 from torch import Tensor
 from colossalai.trainer.hooks import BaseHook
+from colossalai.utils.memory_tracer import AsyncMemoryMonitor
 from ._metric_hook import LearningRateMetric, MetricHook
 
 @HOOKS.register_module
@@ -9,27 +10,22 @@ class MemTraceHook(BaseHook):
     """This class allows users to trace memory"""
     def __init__(
         self,
-        truncate_by_epoch: bool = True,
         priority: int = 0,
     ) -> None:
         super().__init__(priority=priority)
-        self._trunc_epoch = truncate_by_epoch
+        self._memory_monitor = AsyncMemoryMonitor()
 
     def after_hook_is_attached(self, trainer):
+        # Initialize the memory monitor      
         self._check_metric_states_initialization(trainer)
         trainer.states['metrics']['train']['LR'] = LearningRateMetric(epoch_only=self.by_epoch,
                                                                       initial_lr=self.lr_scheduler.get_last_lr()[0])
-
-    def before_train(self, trainer):
-        return super().before_train(trainer)
-    
-    def after_train(self, trainer):
-        return super().after_train(trainer)
     
     def before_train_iter(self, trainer):
         return super().before_train_iter(trainer)
 
     def after_train_iter(self, trainer, output: Tensor, label: Tensor, loss: Tensor):
+        trainer.states['metrics']
         return super().after_train_iter(trainer, output, label, loss)
     
     def after_train_epoch(self, trainer):
