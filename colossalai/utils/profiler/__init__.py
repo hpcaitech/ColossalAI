2f08b58b16313d23f279ac7e08e595c99f193d75
Jie Zhu <jay.login@icloud.com>
2022-03-17 15:50:29 +0800

    modify `__init__.py` in profiler

--- colossalai/utils/profiler/__init__.py
+++ colossalai/utils/profiler/__init__.py
@@ -3,4 +3,4 @@ from .pcie_profiler import PcieProfiler
 from .prof_utils import ProfilerContext, BaseProfiler
 from .mem_profiler import MemProfiler
 
-__all__ = [BaseProfiler, CommProfiler, PcieProfiler, MemProfiler, ProfilerContext]
+__all__ = ['BaseProfiler', 'CommProfiler', 'PcieProfiler', 'MemProfiler', 'ProfilerContext']
