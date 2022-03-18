342407b04678e9d329d7d26de0cc9838820900df
number1roy <liangbowen.roy@gmail.com>
2022-03-18 16:15:00 +0800

    doc

--- colossalai/nn/init.py
+++ colossalai/nn/init.py
@@ -208,7 +208,7 @@ def xavier_normal_(scale: float = 2., gain: float = 1.):
     :math:`\mathcal{N}(0, \text{std}^2)` where
 
     .. math::
-        \text{std} = \text{gain} \times \sqrt{\frac{2}{\text{fan_{in}} + \text{fan_{out}}}}
+        \text{std} = \text{gain} \times \sqrt{\frac{2}{\text{fan_in} + \text{fan_out}}}
 
     Also known as Glorot initialization.
 
