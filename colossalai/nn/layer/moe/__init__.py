2f1fa849600a5f024068ef89c0df72b310c8d72a
1SAA <c2h214748@gmail.com>
2022-03-18 16:44:04 +0800

    changed parallelmode to dist process group

--- colossalai/nn/layer/moe/__init__.py
+++ colossalai/nn/layer/moe/__init__.py
@@ -1,8 +1,8 @@
 from .experts import Experts, FFNExperts, TPExperts
 from .layers import MoeLayer, Top1Router, Top2Router
-from .utils import NormalNoiseGenerator, build_ffn_experts
+from .utils import NormalNoiseGenerator, UniformNoiseGenerator, build_ffn_experts
 
 __all__ = [
     'Experts', 'FFNExperts', 'TPExperts', 'Top1Router', 'Top2Router', 'MoeLayer', 'NormalNoiseGenerator',
-    'build_ffn_experts'
+    'UniformNoiseGenerator', 'build_ffn_experts'
 ]
