4f7dfb4443f88ecf34bb204987de7fe3bde62600
ver217 <lhx0217@gmail.com>
2022-03-18 13:17:53 +0800

    update sharded optim and fix zero init ctx

--- tests/test_zero_data_parallel/test_shard_model_v2.py
+++ tests/test_zero_data_parallel/test_shard_model_v2.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-import copy
-from asyncio.log import logger
 from functools import partial
 
 import colossalai
 import pytest
 import torch
 import torch.multiprocessing as mp
-from colossalai.logging import get_dist_logger
+from colossalai.testing import parameterize
 from colossalai.utils import free_port
 from colossalai.zero.init_ctx import ZeroInitContext
 from colossalai.zero.shard_utils import (BucketTensorShardStrategy, TensorShardStrategy)
@@ -20,36 +18,30 @@ from tests.components_to_test.registry import non_distributed_component_funcs
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 from common import CONFIG, check_grads_padding, run_fwd_bwd
-from colossalai.testing import parameterize
 
 
 @parameterize("enable_autocast", [True])
-@parameterize("use_zero_init_ctx", [True])
-@parameterize("shard_strategy", [TensorShardStrategy, BucketTensorShardStrategy])
-def run_model_test(enable_autocast, use_zero_init_ctx, shard_strategy, logger):
+@parameterize("shard_strategy_class", [TensorShardStrategy, BucketTensorShardStrategy])
+def run_model_test(enable_autocast, shard_strategy_class):
     test_models = ['repeated_computed_layers', 'resnet18', 'bert']
-    shard_strategy = shard_strategy()
+    shard_strategy = shard_strategy_class()
     for model_name in test_models:
         get_components_func = non_distributed_component_funcs.get_callable(model_name)
         model_builder, train_dataloader, _, _, criterion = get_components_func()
 
         rm_torch_payload_on_the_fly = False
 
-        if use_zero_init_ctx:
-            with ZeroInitContext(convert_fp16=True,
-                                 target_device=torch.device(f'cpu:0'),
-                                 shard_strategy=shard_strategy,
-                                 shard_param=True,
-                                 rm_torch_payload_on_the_fly=rm_torch_payload_on_the_fly):
-                zero_model = model_builder(checkpoint=True)
-            zero_model = ShardedModelV2(zero_model, shard_strategy, use_memory_tracer=True)
+        with ZeroInitContext(convert_fp16=True,
+                             target_device=torch.cuda.current_device(),
+                             shard_strategy=shard_strategy,
+                             shard_param=True,
+                             rm_torch_payload_on_the_fly=rm_torch_payload_on_the_fly):
+            zero_model = model_builder(checkpoint=True)
+        zero_model = ShardedModelV2(zero_model, shard_strategy, use_memory_tracer=True)
 
-            model = model_builder(checkpoint=True).half()
-            col_model_deepcopy(zero_model, model)
-            model = model.cuda()
-        else:
-            model = model_builder(checkpoint=True).half().cuda()
-            zero_model = ShardedModelV2(copy.deepcopy(model), shard_strategy)
+        model = model_builder(checkpoint=True).half()
+        col_model_deepcopy(zero_model, model)
+        model = model.cuda()
 
         model = DDP(model)
 
@@ -63,15 +55,10 @@ def run_model_test(enable_autocast, use_zero_init_ctx, shard_strategy, logger):
 
             check_grads_padding(model, zero_model, loose=True)
 
-        # logger.debug('overall cuda ', zero_model._memstats_collector._overall_cuda)
-        # logger.debug('model cuda ', zero_model._memstats_collector._model_data_cuda)
-
 
 def run_dist(rank, world_size, port):
     colossalai.launch(config=CONFIG, rank=rank, world_size=world_size, host='localhost', port=port, backend='nccl')
-    logger = get_dist_logger()
-    logger.set_level('DEBUG')
-    run_model_test(logger=logger)
+    run_model_test()
 
 
 @pytest.mark.dist
