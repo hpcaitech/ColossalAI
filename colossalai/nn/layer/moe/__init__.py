5c6902cf7ccee5f0c3fdf540af3529b5e0903ccd
1SAA <c2h214748@gmail.com>
2022-03-22 12:11:41 +0800

    support PR-MOE

--- colossalai/nn/layer/moe/__init__.py
+++ colossalai/nn/layer/moe/__init__.py
@@ -1,8 +1,8 @@
 from .experts import Experts, FFNExperts, TPExperts
-from .layers import MoeLayer, Top1Router, Top2Router
+from .layers import MoeLayer, Top1Router, Top2Router, MoeModule
 from .utils import NormalNoiseGenerator, UniformNoiseGenerator, build_ffn_experts
 
 __all__ = [
     'Experts', 'FFNExperts', 'TPExperts', 'Top1Router', 'Top2Router', 'MoeLayer', 'NormalNoiseGenerator',
-    'UniformNoiseGenerator', 'build_ffn_experts'
+    'UniformNoiseGenerator', 'build_ffn_experts', 'MoeModule'
 ]
