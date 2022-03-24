8c3f45fa9f81e580920c5a5a8000fea81f8d04a4
jiaruifang <fangjiarui123@gmail.com>
2022-03-24 12:20:42 +0800

    polish

--- colossalai/utils/memory_utils/__init__.py
+++ colossalai/utils/memory_utils/__init__.py
@@ -1,3 +1,3 @@
-# from .utils import col_model_data_tensor_move, col_model_data_move_to_cpu
+from .utils import col_model_data_tensor_move, col_model_data_move_to_cpu
 
-# __all__ = ['col_model_data_tensor_move', 'col_model_data_move_to_cpu']
+__all__ = ['col_model_data_tensor_move', 'col_model_data_move_to_cpu']
