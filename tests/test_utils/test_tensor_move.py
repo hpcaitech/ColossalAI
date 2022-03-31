f7df729e95795b8db8ac37eb839ac7c238da0e8c
FredHuang99 <58256299+FredHuang99@users.noreply.github.com>
2022-03-31 11:49:06 +0800

    Update test_tensor_move.py
    Test all the functions that updated in colossalai.utils.memory_utils.utils

--- tests/test_utils/test_tensor_move.py
+++ tests/test_utils/test_tensor_move.py
@@ -1,9 +1,9 @@
 import pytest
 
 from colossalai.utils.cuda import get_current_device
-from colossalai.utils.memory_utils.utils import colo_model_data_tensor_move, colo_model_data_tensor_move_inline
+from colossalai.utils.memory_utils.utils import colo_tensor_mem_usage, colo_model_data_tensor_move, colo_model_data_tensor_move_inline, colo_set_process_memory_fraction, colo_cuda_memory_capacity
 from colossalai.utils import free_port
-from colossalai.zero.sharded_param import ShardedTensor
+from colossalai.zero.sharded_param.tensorful_state import StatefulTensor
 import colossalai
 
 import torch
@@ -11,24 +11,44 @@ import torch
 from functools import partial
 import torch.multiprocessing as mp
 
+def _run_colo_tensor_mem_usage():
+    for t in [StatefulTensor(torch.randn(2, 3)), torch.randn(2, 3)]:
+        cuda_use, cpu_use = colo_tensor_mem_usage(t)
+        print("cuda_use")
+        print(cuda_use)
+        print("cput_use")
+        print(cpu_use)
+
+def _run_colo_set_process_memory_fraction():
+    colo_set_process_memory_fraction(0.5)
+
+def _run_colo_cuda_memory_capacity():
+    frac = colo_cuda_memory_capacity
+    print("cuda memory capacity of the current cuda")
+    print(frac)
+
 
 def _run_colo_model_data_tensor_move_inline():
-    for t in [torch.randn(2, 3), ShardedTensor(torch.randn(2, 3))]:
-        colo_model_data_tensor_move_inline(t, torch.device(f"cuda:{get_current_device()}"))
-        assert t.device == torch.device(f"cuda:{get_current_device()}")
+    for t in [StatefulTensor(torch.randn(2,3)), torch.randn(2,3)]:
+        colo_model_data_tensor_move_inline(t, torch.device("cuda:3"))
+        assert t.device == torch.device("cuda:3")
 
 
 def _run_colo_model_data_tensor_move():
-    for t in [(torch.ones(2, 3), torch.zeros(2, 3).cuda(get_current_device())),
-              (ShardedTensor(torch.ones(2, 3)), ShardedTensor(torch.zeros(2, 3).cuda(get_current_device())))]:
+    for t in [(StatefulTensor(torch.ones(2, 3)), StatefulTensor(torch.zeros(2, 3).cuda(get_current_device()))),
+        (torch.ones(2, 3), torch.zeros(2, 3).cuda(get_current_device()))]:
         cpu_t, cuda_t = t
         colo_model_data_tensor_move(cpu_t, cuda_t)
+        assert cuda_t.device == torch.device(f"cuda:{get_current_device()}")
 
 
 def run_dist(rank, world_size, port):
     colossalai.launch(config={}, rank=rank, world_size=world_size, host='localhost', port=port, backend='nccl')
+    _run_colo_set_process_memory_fraction()
+    _run_colo_cuda_memory_capacity()
     _run_colo_model_data_tensor_move_inline()
     _run_colo_model_data_tensor_move()
+    _run_colo_tensor_mem_usage()
 
 
 @pytest.mark.dist
