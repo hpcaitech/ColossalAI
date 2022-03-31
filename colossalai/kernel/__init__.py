376faaf9913505eeea02dcf2fdd8dbc6d08843d3
Xu Kai <xukai16@foxmail.com>
2022-03-31 14:48:17 +0800

    fix format

--- colossalai/kernel/__init__.py
+++ colossalai/kernel/__init__.py
@@ -1,5 +1,3 @@
 from .cuda_native import LayerNorm, FusedScaleMaskSoftmax, MultiHeadAttention
 
-__all__ = [
-    "LayerNorm", "FusedScaleMaskSoftmax", "MultiHeadAttention"
-]
+__all__ = ["LayerNorm", "FusedScaleMaskSoftmax", "MultiHeadAttention"]
