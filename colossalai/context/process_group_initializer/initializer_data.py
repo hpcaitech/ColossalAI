6afea77ac3733120ecff39df528994e837d5ef2a
zbian <kurisusnowdeng@gmail.com>
2022-03-31 23:05:08 +0800

    added gloo groups for transfer cpu tensors

--- colossalai/context/process_group_initializer/initializer_data.py
+++ colossalai/context/process_group_initializer/initializer_data.py
@@ -34,17 +34,20 @@ class Initializer_Data(ProcessGroupInitializer):
         local_rank = None
         ranks_in_group = None
         process_group = None
+        cpu_group = None
         group_world_size = None
         mode = ParallelMode.DATA
 
         for i in range(self.num_data_parallel_group):
             ranks = [i + j * self.num_data_parallel_group for j in range(self.data_parallel_size)]
             group = dist.new_group(ranks)
+            group_cpu = dist.new_group(ranks, backend='gloo') if dist.get_backend() != 'gloo' else group
 
             if self.rank in ranks:
                 local_rank = ranks.index(self.rank)
                 group_world_size = len(ranks)
                 process_group = group
+                cpu_group = group_cpu
                 ranks_in_group = ranks
 
-        return local_rank, group_world_size, process_group, ranks_in_group, mode
+        return local_rank, group_world_size, process_group, cpu_group, ranks_in_group, mode
