eda13056e25e98822a816ec011b281cea1678c09
jiaruifang <fangjiarui123@gmail.com>
2022-04-01 15:51:12 +0800

    polish

--- colossalai/utils/memory_tracer/__init__.py
+++ colossalai/utils/memory_tracer/__init__.py
@@ -1,4 +1,4 @@
 from .async_memtracer import AsyncMemoryMonitor
 from .memstats_collector import MemStatsCollector
 
-__all__ = ['AsyncMemoryMonitor', 'ModelDataTracer']
+__all__ = ['AsyncMemoryMonitor', 'MemStatsCollector']
