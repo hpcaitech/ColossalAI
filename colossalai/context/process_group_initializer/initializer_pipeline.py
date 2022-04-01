4cc2ae243238d25a976f6d5c3d7831cd09de281b
Jiang Zhuo <58946806+jiangz17THU@users.noreply.github.com>
2022-04-01 17:55:06 +0800

    [NFC] polish <colossalai/context/process_group_initializer/initializer_data.py> code stype (#626)
    Co-authored-by: 姜卓 <jiangzhuo@jiangzhuodeMacBook-Pro.local>

--- colossalai/context/process_group_initializer/initializer_pipeline.py
+++ colossalai/context/process_group_initializer/initializer_pipeline.py
@@ -20,6 +20,7 @@ class Initializer_Pipeline(ProcessGroupInitializer):
         pipeline_parallel_size (int): Size of pipeline parallel
         tensor_parallel_size (int): Size of tensor parallel
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.data_group_size = self.world_size // self.data_parallel_size
@@ -36,9 +37,7 @@ class Initializer_Pipeline(ProcessGroupInitializer):
         for i in range(self.data_parallel_size):
             for j in range(self.pipeline_stage_size):
                 pipe_ranks = list(
-                    range(i * self.data_group_size + j,
-                          (i + 1) * self.data_group_size,
-                          self.pipeline_stage_size))
+                    range(i * self.data_group_size + j, (i + 1) * self.data_group_size, self.pipeline_stage_size))
                 pipe_group_size = len(pipe_ranks)
                 pipe_group = dist.new_group(pipe_ranks)
 
@@ -48,8 +47,6 @@ class Initializer_Pipeline(ProcessGroupInitializer):
                     process_group = pipe_group
                     ranks_in_group = pipe_ranks
                     dist_settings.append(
-                        tuple((local_rank, group_world_size,
-                               process_group, ranks_in_group,
-                               ParallelMode.PIPELINE)))
+                        tuple((local_rank, group_world_size, process_group, ranks_in_group, ParallelMode.PIPELINE)))
 
         return dist_settings
