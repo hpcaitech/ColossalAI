945f1f2febafe98e8785b8922c1dbcb26ccdc49c
jiaruifang <fangjiarui123@gmail.com>
2022-04-11 18:06:27 +0800

    polish code

--- colossalai/engine/ophooks/__init__.py
+++ colossalai/engine/ophooks/__init__.py
@@ -1,3 +1,3 @@
 from .utils import register_ophooks_recursively, BaseOpHook
 
-all = ["BaseOpHook", "MemTracerOpHook", "register_ophooks_recursively"]
+__all__ = ["BaseOpHook", "MemTracerOpHook", "register_ophooks_recursively"]
