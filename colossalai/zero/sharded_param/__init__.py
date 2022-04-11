00c92e54a72ce35b8f021591c36f0d9267140bc4
jiaruifang <fangjiarui123@gmail.com>
2022-04-11 18:08:52 +0800

    polish

--- colossalai/zero/sharded_param/__init__.py
+++ colossalai/zero/sharded_param/__init__.py
@@ -5,7 +5,6 @@ from colossalai.zero.sharded_param.tensor_utils import (colo_model_data_tensor_m
 from colossalai.zero.sharded_param.tensorful_state import TensorState, StatefulTensor
 
 __all__ = [
-    'ShardedTensor', 'ShardedParamV2'
-    'colo_model_data_tensor_move_inline', 'colo_model_data_move_to_cpu', 'colo_model_tensor_clone',
-    'colo_tensor_mem_usage', 'TensorState', 'StatefulTensor'
+    'ShardedTensor', 'ShardedParamV2', 'colo_model_data_tensor_move_inline', 'colo_model_data_move_to_cpu',
+    'colo_model_tensor_clone', 'colo_tensor_mem_usage', 'TensorState', 'StatefulTensor'
 ]
