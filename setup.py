dc2b621c191ba0cba421d39a132f4a1709ade04d
FrankLeeeee <somerlee.9@gmail.com>
2022-04-12 13:20:34 +0800

    [bug] removed zero installation requirements

--- setup.py
+++ setup.py
@@ -213,9 +213,6 @@ setup(
     ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExtension} if ext_modules else {},
     install_requires=fetch_requirements('requirements/requirements.txt'),
-    extras_require={
-        'zero': fetch_requirements('requirements/requirements-zero.txt'),
-    },
     python_requires='>=3.7',
     classifiers=[
         'Programming Language :: Python :: 3',
