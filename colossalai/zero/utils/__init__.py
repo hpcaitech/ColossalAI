3d7dc46d33aade1b5a11ac4c4372132b401ad509
Jiarui Fang <fangjiarui123@gmail.com>
2022-04-14 11:07:29 +0800

    [zero] use factory pattern for tensor_placement_policy (#752)

--- colossalai/zero/utils/__init__.py
+++ colossalai/zero/utils/__init__.py
@@ -1,4 +1,5 @@
 from .stateful_tensor_mgr import StatefulTensorMgr
+from .tensor_placement_policy import TensorPlacementPolicyFactory
 from .zero_hook import ZeroHook
 
-__all__ = ['StatefulTensorMgr', 'ZeroHook']
+__all__ = ['StatefulTensorMgr', 'ZeroHook', 'TensorPlacementPolicyFactory']
