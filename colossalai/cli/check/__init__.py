70ed11d07e9a42c567281074365e7ba6b9562008
FrankLeeeee <somerlee.9@gmail.com>
2022-04-20 12:05:38 +0800

    [cli] added check installation cli

---
+++ colossalai/cli/check/__init__.py
@@ -0,0 +1,13 @@
+import click
+from .check_installation import check_installation
+
+__all__ = ['check']
+
+
+@click.command(help="Check if Colossal-AI is correct based on the given option")
+@click.option('-i', '--installation', is_flag=True, help="Check if Colossal-AI is built correctly")
+def check(installation):
+    if installation:
+        check_installation()
+        return
+    click.echo("No option is given")
