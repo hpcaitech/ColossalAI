3b40af20a6a2fd2cb2ceb9c1b832fa5d4d37f4b0
jiaruifang <fangjiarui123@gmail.com>
2022-04-20 18:09:33 +0800

    polish

--- colossalai/gemini/tensor/__init__.py
+++ colossalai/gemini/tensor/__init__.py
@@ -6,27 +6,27 @@ from .api import (
 def stateful_op_impl(func):
     """
     Provides a way for users to write their own custom sharded operator. This
-    can be used to override existing ShardedTensor operators or write a new
-    one not supported by ShardedTensor. If the operator in question is covered
-    by ``__torch_function__`` dispatch and has a ShardedTensor as any of its
+    can be used to override existing StatefulTensorV2 operators or write a new
+    one not supported by StatefulTensorV2. If the operator in question is covered
+    by ``__torch_function__`` dispatch and has a StatefulTensorV2 as any of its
     parameters, the function provided will be invoked for that operator.
 
     Example::
-        >>> @custom_sharded_op(torch.nn.functional.linear)
+        >>> @custom_stateful_op(torch.nn.functional.linear)
         >>> def my_custom_sharded_linear(types, args, kwargs, process_group):
         >>>   ....
         >>>
         >>> input = torch.rand(10, 32)
         >>> weight = sharded_tensor.rand(32, 16)
         >>> bias = torch.rand(16)
-        >>> # This will call 'my_custom_sharded_linear'
+        >>> # This will call 'my_custom_stateful_linear'
         >>> torch.nn.functional.linear(input, weight, bias)
 
     The types, args and kwargs parameters are the same parameters that are
     passed to ``__torch_function__`` dispatch API
     (https://pytorch.org/docs/stable/notes/extending.html#extending-torch).
     There is an additional ``process_group`` parameter which is the
-    process_group used for the ShardedTensor and can be used by
+    process_group used for the StatefulTensorV2 and can be used by
     implementations for communications within a sharded implementation.
 
     Args:
