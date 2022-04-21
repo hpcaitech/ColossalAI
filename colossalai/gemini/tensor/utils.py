884877d01c1d4b5113a5520b78f9e763a5559d57
jiaruifang <fangjiarui123@gmail.com>
2022-04-21 09:59:59 +0800

    polish

--- colossalai/gemini/tensor/utils.py
+++ colossalai/gemini/tensor/utils.py
@@ -5,64 +5,13 @@ from torch.distributed import distributed_c10d
 from colossalai.gemini.tensor.stateful_tensor import StatefulTensorV2
 
 
-def _convert_tensor(tensor: torch.Tensor, src_rank=0, process_group=None) -> StatefulTensorV2:
-    """
-    Given a :class:`torch.Tensor`, it shards that tensor according to the provided
-    ``sharding_spec``. ``src_rank`` denotes the source rank which would be
-    used as the ground truth of the data which would be scattered as shards
-    across the rest of the ranks.
-
-    Args:
-        tensor (:class:`torch.Tensor`): Tensor needs to be sharded.
-        sharding_spec (:class:`torchshard._shard.sharding_spec.ShardingSpec`): The specification
-            describing how to shard the Tensor.
-
-    Keyword args:
-        src_rank (int, optional): The source rank which is used as the ground truth of
-            the data for the parameter that would be sharded and scattered
-            across the rest of the ranks.
-            Default: 0.
-        process_group (ProcessGroup, optional): The process group to work on. If None,
-            the default process group will be used.
-
-    Returns:
-        A :class:`ShardedTensor` sharded from the given tensor.
-
-    .. warning::
-        Only :class:`torchshard._shard.sharding_spec.ChunkShardingSpec` is
-        currently supported as the ``sharding_spec``.
-    """
+def _convert_tensor(tensor: torch.Tensor) -> StatefulTensorV2:
     if not tensor.is_contiguous():
         raise ValueError('input tensor is not a contiguous Tensor')
-    pass
+    return StatefulTensorV2(tensor)
 
 
-def convert_parameter(module: torch.nn.Module, param_name: str, src_rank=0, process_group=None):
-    """
-    Given a :class:`torch.nn.Module`, a ``param_name`` for a parameter in that
-    module, it converts the parameter to stateful tensors.
-
-    This method replaces ``module.param_name`` with a
-    :class:`torchshard._sharded_tensor.ShardedTensor`
-
-    Args:
-        module (:class:`torch.nn.Module`): Module whose parameter needs to be sharded.
-        param_name (str): Name of the parameter of ``module`` that needs to be sharded.
-        sharding_spec (:class:`torchshard._shard.sharding_spec.ShardingSpec`): The specification
-            describing how to shard the Tensor.
-
-    Keyword args:
-        src_rank (int, optional): The source rank which is used as the ground truth of
-            the data for the parameter that would be sharded and scattered
-            across the rest of the ranks.
-            Default: 0.
-        process_group (ProcessGroup, optional): The process group to work on. If None,
-            the default process group will be used.
-
-    .. warning::
-        Only :class:`torchshard._shard.sharding_spec.ChunkShardingSpec` is
-        currently supported as the ``sharding_spec``.
-    """
+def convert_parameter(module: torch.nn.Module, param_name: str):
     # Perform some validation first.
     if not hasattr(module, param_name):
         raise ValueError(f'module: {module} does not have parameter with name: {param_name}')
@@ -75,7 +24,7 @@ def convert_parameter(module: torch.nn.Module, param_name: str, src_rank=0, proc
     if not tensor.is_contiguous():
         raise ValueError(f'param: {param_name} is not a contiguous Tensor')
 
-    st = _convert_tensor(tensor, src_rank, process_group)
+    st = _convert_tensor(tensor)
 
     # Replace param with ShardedTensor.
 
