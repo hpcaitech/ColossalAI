7029e9cadeede084d4be084f851aa62ecd55314f
Wesley <ziyue.jiang97@gmail.com>
2022-04-22 18:57:50 +0800

    fix unit test

--- tests/test_tensor/test_linear_tp.py
+++ tests/test_tensor/test_linear_tp.py
@@ -1,5 +1,3 @@
-from joblib import Parallel
-from numpy import allclose, require
 import torch
 from colossalai.context.parallel_mode import ParallelMode
 from colossalai.tensor import ColoTensor
@@ -13,48 +11,85 @@ import torch
 import torch.multiprocessing as mp
 from colossalai.logging import get_dist_logger
 from colossalai.testing import parameterize, rerun_if_address_is_in_use
+from colossalai.utils.cuda import get_current_device
 from colossalai.utils import free_port
 from colossalai.core import global_context as gpc
+import torch.distributed as dist
 
-
+def check_equal(A, B):
+    assert torch.allclose(A, B, rtol=1e-3, atol=1e-1) == True
 
 def run_linear_tp1d_row_test():
-    in_dim = 4
-    out_dim = 5
+    device = get_current_device()
+    dtype = torch.float32
+    DEPTH = 4
+    in_features = 4
+    out_features = 5
 
-    fc = torch.nn.Linear(in_dim, out_dim, bias=True)
-    fc_ref = deepcopy(fc)
+    i = gpc.get_local_rank(ParallelMode.PARALLEL_1D)
 
-    input_ref = torch.randn(1, in_dim)
-    input_tensor = input_ref.clone()
+    layer = torch.nn.Linear(in_features, out_features)
 
-    # sharded_weight = ColoTensor.init_from_torch_tensor(fc_ref.weight, "1Drow")
+    A_shape = (2, in_features)
+    A_master = torch.randn(A_shape, dtype=dtype, device=device)
+    dist.broadcast(A_master, src=0)
+    A = A_master.clone()
+    A.requires_grad = True
 
-    # shard weight at begiin
-    world_size = gpc.get_world_size(ParallelMode.PARALLEL_1D)
-    sharded_weight = ColoTensor(out_dim, in_dim // world_size, shard_spec="1Drow")
-    sharded_bias = ColoTensor.init_from_torch_tensor(fc_ref.bias)
+    W_shape = (out_features, in_features)
+    W_master = torch.randn(W_shape, dtype=dtype, device=device)
+    dist.broadcast(W_master, src=0)
+    W = torch.chunk(W_master, DEPTH, dim=-1)[i]
+    W = W.clone()
+    W.requires_grad = True
+
+    B_shape = (out_features)
+    B_master = torch.randn(B_shape, dtype=dtype, device=device)
+    dist.broadcast(B_master, src=0)
+    B = B_master.clone()
+    B.requires_grad = True
 
     # replace the torch nn.Parameters with ShardedTensor
-    delattr(fc, 'weight')
-    setattr(fc, 'weight', sharded_weight)
-    delattr(fc, 'bias')
-    setattr(fc, 'bias', sharded_bias)
+    sharded_weight = ColoTensor.init_from_torch_tensor(W)
+    sharded_weight._shard_spec = "1Drow"
+    sharded_bias = ColoTensor.init_from_torch_tensor(B)
+    delattr(layer, 'weight')
+    setattr(layer, 'weight', sharded_weight)
+    delattr(layer, 'bias')
+    setattr(layer, 'bias', sharded_bias)
+    layer.weight.requires_grad = True
+    layer.bias.requires_grad = True
+    out = layer(A)
 
-    fc.weight.requires_grad = True
-    fc.bias.requires_grad = True
+    A_master = A_master.clone()
+    A_master.requires_grad = True
+    W_master = W_master.clone()
+    W_master.requires_grad = True
+    B_master = B_master.clone()
+    B_master.requires_grad = True
+    C_master = torch.matmul(A_master, W_master.transpose(0, 1)) + B_master
+    C = C_master.clone()
 
-    # torch.nn.functional.linear(torch.randn(1, in_dim), sharded_weight, sharded_bias)
-    out = fc(input_tensor)
-    loss = out.sum()
-    loss.backward()
+    check_equal(out, C)
+    print('linear_row forward: pass')
 
-    out_ref = fc_ref(input_ref)
-    loss_ref = out_ref.sum()
-    loss_ref.backward()
+    grad_shape = C_master.shape
+    grad_master = torch.randn(grad_shape, dtype=dtype, device=get_current_device())
+    dist.broadcast(grad_master, src=0)
+    grad = grad_master.clone()
+    out.backward(grad)
 
-    assert (loss_ref == loss)
-    assert allclose(fc_ref.weight.grad, fc.weight.torch_tensor().grad)
+    grad_master = grad_master.clone()
+    C_master.backward(grad_master)
+
+    W_grad = W_master.grad
+    W_grad = torch.chunk(W_grad, DEPTH, dim=-1)[i]
+    check_equal(W_grad, layer.weight.grad)
+
+    B_grad = B_master.grad
+    check_equal(B_grad, layer.bias.grad)
+
+    print('linear_row backward: pass')
 
 
 def run_dist(rank, world_size, port):
