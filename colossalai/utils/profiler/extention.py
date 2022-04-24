232142f4029047be2ae5d369cca832ac2cee9d62
ver217 <lhx0217@gmail.com>
2022-04-24 17:03:59 +0800

    [utils] refactor profiler (#837)
    * add model data profiler
    
    * add a subclass of torch.profiler.profile
    
    * refactor folder structure
    
    * remove redundant codes
    
    * polish code
    
    * use GeminiMemoryManager
    
    * fix import path
    
    * fix stm profiler ext
    
    * polish comments
    
    * remove useless file

---
+++ colossalai/utils/profiler/extention.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class ProfilerExtension(ABC):
+
+    @abstractmethod
+    def prepare_trace(self):
+        pass
+
+    @abstractmethod
+    def start_trace(self):
+        pass
+
+    @abstractmethod
+    def stop_trace(self):
+        pass
+
+    @abstractmethod
+    def extend_chrome_trace(self, trace: dict) -> dict:
+        pass
