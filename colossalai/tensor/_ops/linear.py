88a63224b37fdeda0a4473fb90d590ceb5e5ad65
Wesley <ziyue.jiang97@gmail.com>
2022-04-28 10:45:06 +0800

    polish

--- colossalai/tensor/_ops/linear.py
+++ colossalai/tensor/_ops/linear.py
@@ -9,6 +9,72 @@ from packaging import version
 from colossalai.tensor import ComputePattern, TensorSpec, ComputePattern, ParallelAction, ColoTensor, ShardPattern
 
 
+def colo_linear_1Drow(input_tensor: ColoTensor, weight: ColoTensor, bias:ColoTensor) -> ColoTensor:
+    parallel_action = weight.shard_spec.get_action_by_compute_pattern(ComputePattern.TP1DRow)
+    # Input:S[1] x Weight:S[0] = Output:P
+    # All-Reduce(Output) + bias = res
+    # Input:S[1]                
+    if input_tensor.is_gathered():
+        # Not splited yet.
+        assert divide(input_tensor.shape[-1], gpc.tensor_parallel_size) == weight.size(-1), \
+        'Invalid shapes in 1Drow forward: input={}, weight={}. Expected last dim of input {}.'.format(
+        input_tensor.shape, weight.size, weight.size(-1) * gpc.tensor_parallel_size)
+        input_per_partition = split_forward_gather_backward(input_tensor.torch_tensor(), parallel_action.parallel_mode, dim=-1)
+    elif input_tensor.shard_pattern == ShardPattern.Col:
+        # Splited by 1Dcol
+        assert input_tensor.shape[-1] == weight.size(-1), \
+        'Invalid shapes in 1Drow forward: input={}, weight={}. Expected last dim of input {}.'.format(
+        input_tensor.shape, weight.size, weight.size(-1))
+        input_per_partition = input_tensor.torch_tensor()
+    else:
+        raise NotImplementedError
+
+    # Output:P
+    partial_output = torch.nn.functional.linear(input_per_partition, weight.torch_tensor())
+    # Reduce(Output)
+    output = reduce_input(partial_output, parallel_action.parallel_mode)
+    # Bias
+    if bias is not None:
+        assert not bias.has_spec(), 'Invalid bias spec for 1Drow Linear op'
+        output = output + bias.torch_tensor()
+    output = ColoTensor.init_from_torch_tensor(output)
+    return output
+
+def colo_linear_1Dcol(input_tensor: ColoTensor, weight: ColoTensor, bias:ColoTensor) -> ColoTensor:
+    # Input:B x Weight:S[1] + Bias:S[1] = Output:S[1]
+    # All-Gather(Output)
+    # Input:B
+    parallel_action = weight.shard_spec.get_action_by_compute_pattern(ComputePattern.TP1DCol)
+    if input_tensor.is_gathered():
+        # Not splited yet.
+        assert input_tensor.shape[-1] == weight.size(-1), \
+            'Invalid shapes in 1Dcol forward: input={}, weight={}. Expected last dim of input {}.'.format(
+                input_tensor.shape, weight.size, weight.size(-1))
+        input_parallel = reduce_grad(input_tensor.torch_tensor(), parallel_action.parallel_mode)
+
+    # Bias:S[1]
+    if bias is not None:
+        assert bias.has_spec() and bias.shard_spec.num_action == 1 and \
+            bias.shard_pattern in [ShardPattern.Col, ShardPattern.Row], \
+                'Invalid bias spec for 1Dcol Linear op'
+
+    output_parallel = torch.nn.functional.linear(input_parallel, weight.torch_tensor(), bias.torch_tensor())
+    
+    output = ColoTensor.init_from_torch_tensor(output_parallel)
+    out_parallel_action_list = [
+        ParallelAction(
+            priority=1, compute_pattern=ComputePattern.Activation, 
+            parallel_mode=parallel_action.parallel_mode
+        )
+    ]
+    output_spec = TensorSpec(out_parallel_action_list)
+    output.set_spec(output_spec, shard=False)
+    output.set_shard_pattern(ShardPattern.Col)
+    if parallel_action.gather_out:
+        # All-Gather(Output)
+        output.gather()
+    return output
+
 @colo_op_impl(torch.nn.functional.linear)
 def colo_linear(types, args, kwargs, pg):
     """Handles ``__torch_function__`` dispatch for ``torch.nn.functional.linear``.
@@ -44,75 +110,9 @@ def colo_linear(types, args, kwargs, pg):
     elif weight.shard_spec.num_action == 1:
         compute_patterns = weight.shard_spec.compute_patterns
         if ComputePattern.TP1DRow in compute_patterns:
-            parallel_action = weight.shard_spec.get_action_by_compute_pattern(ComputePattern.TP1DRow)
-            # Input:S[1] x Weight:S[0] = Output:P
-            # All-Reduce(Output) + bias = res
-            # Input:S[1]                
-            if input_tensor.is_gathered():
-                # Not splited yet.
-                assert divide(input_tensor.shape[-1], gpc.tensor_parallel_size) == weight.size(-1), \
-                'Invalid shapes in 1Drow forward: input={}, weight={}. Expected last dim of input {}.'.format(
-                input_tensor.shape, weight.size, weight.size(-1) * gpc.tensor_parallel_size)
-                input_per_partition = split_forward_gather_backward(input_tensor.torch_tensor(), parallel_action.parallel_mode, dim=-1)
-            elif input_tensor.is_activation():
-                if input_tensor.shard_pattern == ShardPattern.Col:
-                    # Splited by 1Dcol
-                    assert input_tensor.shape[-1] == weight.size(-1), \
-                    'Invalid shapes in 1Drow forward: input={}, weight={}. Expected last dim of input {}.'.format(
-                    input_tensor.shape, weight.size, weight.size(-1))
-                    input_per_partition = input_tensor.torch_tensor()
-                else:
-                    raise NotImplementedError
-            else:
-                raise NotImplementedError
-
-            # Output:P
-            partial_output = torch.nn.functional.linear(input_per_partition, weight.torch_tensor())
-            # Reduce(Output)
-            output = reduce_input(partial_output, parallel_action.parallel_mode)
-            # Bias
-            if bias is not None:
-                assert not bias.has_spec(), 'Invalid bias spec for 1Drow Linear op'
-                output = output + bias.torch_tensor()
-            output = ColoTensor.init_from_torch_tensor(output)
-            return output
+            return colo_linear_1Drow(input_tensor, weight, bias)
         elif ComputePattern.TP1DCol in compute_patterns:
-            # Input:B x Weight:S[1] + Bias:S[1] = Output:S[1]
-            # All-Gather(Output)
-            # Input:B
-            parallel_action = weight.shard_spec.get_action_by_compute_pattern(ComputePattern.TP1DCol)
-            
-            if input_tensor.is_gathered():
-                # Not splited yet.
-                assert input_tensor.shape[-1] == weight.size(-1), \
-                    'Invalid shapes in 1Dcol forward: input={}, weight={}. Expected last dim of input {}.'.format(
-                        input_tensor.shape, weight.size, weight.size(-1))
-                input_parallel = reduce_grad(input_tensor.torch_tensor(), parallel_action.parallel_mode)
-            else:
-                raise NotImplementedError
-            # Bias:S[1]
-            if bias is not None:
-                assert bias.has_spec() and bias.shard_spec.num_action == 1 and \
-                    bias.shard_pattern in [ShardPattern.Col, ShardPattern.Row], \
-                        'Invalid bias spec for 1Dcol Linear op'
-
-            output_parallel = torch.nn.functional.linear(input_parallel, weight.torch_tensor(), bias.torch_tensor())
-            
-            output = ColoTensor.init_from_torch_tensor(output_parallel)
-            out_parallel_action_list = [
-                ParallelAction(
-                    priority=1, compute_pattern=ComputePattern.Activation, 
-                    parallel_mode=parallel_action.parallel_mode
-                )
-            ]
-            output_spec = TensorSpec(out_parallel_action_list)
-            output.set_spec(output_spec, shard=False)
-            output.set_shard_pattern(ShardPattern.Col)
-            if parallel_action.gather_out:
-                # All-Gather(Output)
-                output.gather()
-            return output
-
+            return colo_linear_1Dcol(input_tensor, weight, bias)
         else:
             raise NotImplementedError
     else:
