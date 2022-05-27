a85013f28be05c787294ed3c98fd9e3f81dfc21d
ver217 <lhx0217@gmail.com>
2022-05-27 17:28:43 +0800

    polish code

--- colossalai/zero/utils/zero_hook_v2.py
+++ colossalai/zero/utils/zero_hook_v2.py
@@ -15,22 +15,22 @@ class ZeROHookV2(ParamOpHook):
 
     def __init__(self, chunk_manager: ChunkManager) -> None:
         super().__init__()
-        self.chunk_manager = chunk_manager
-        self.training_phase = TrainingPhase.FORWARD
+        self._chunk_manager = chunk_manager
+        self._training_phase = TrainingPhase.FORWARD
 
     def pre_op(self, params):
         for p in params:
-            self.chunk_manager.trans_tensor_state(p, TensorState.COMPUTE)
-        self.chunk_manager.exec_lazy_release()
+            self._chunk_manager.trans_tensor_state(p, TensorState.COMPUTE)
+        self._chunk_manager.exec_lazy_release()
         # TODO: evict chunks
         for p in params:
-            self.chunk_manager.access_chunk(p)
+            self._chunk_manager.access_chunk(p)
 
     def post_op(self, params):
         for p in params:
-            tensor_state = TensorState.HOLD if self.training_phase == TrainingPhase.FORWARD or not p.requires_grad else TensorState.HOLD_AFTER_BWD
-            self.chunk_manager.trans_tensor_state(p, tensor_state)
-        self.chunk_manager.add_lazy_release_tensors(params)
+            tensor_state = TensorState.HOLD if self._training_phase == TrainingPhase.FORWARD or not p.requires_grad else TensorState.HOLD_AFTER_BWD
+            self._chunk_manager.trans_tensor_state(p, tensor_state)
+        self._chunk_manager.add_lazy_release_tensors(params)
 
     def pre_forward(self, params: List[torch.Tensor]) -> None:
         self.pre_op(params)
@@ -47,11 +47,11 @@ class ZeROHookV2(ParamOpHook):
     @contextmanager
     def switch_training_phase(self, training_phase: TrainingPhase = TrainingPhase.BACKWARD):
         try:
-            old_training_phase = self.training_phase
-            self.training_phase = training_phase
+            old_training_phase = self._training_phase
+            self._training_phase = training_phase
             yield
         finally:
-            self.training_phase = old_training_phase
+            self._training_phase = old_training_phase
 
     switch_to_backward = switch_training_phase
     switch_to_forward = partial(switch_to_backward, training_phase=TrainingPhase.FORWARD)
