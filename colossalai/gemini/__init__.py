850dc277817911620c7e30ae03c538988115c76c
ver217 <lhx0217@gmail.com>
2022-06-22 10:53:58 +0800

    udpate __init__

--- colossalai/gemini/__init__.py
+++ colossalai/gemini/__init__.py
@@ -1,4 +1,5 @@
 from .stateful_tensor_mgr import StatefulTensorMgr
 from .tensor_placement_policy import TensorPlacementPolicyFactory
+from .gemini_mgr import GeminiManager
 
-__all__ = ['StatefulTensorMgr', 'TensorPlacementPolicyFactory']
+__all__ = ['StatefulTensorMgr', 'TensorPlacementPolicyFactory', 'GeminiManager']
