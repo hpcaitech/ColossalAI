d3756ce8d1aae07e5a15b5f18b9c8d556bfe4f67
itok2000u <xiaoye723000@gmail.com>
2022-07-05 09:56:58 +0800

    1d_pass_with_colotensor

--- colossalai/fx/passes/shard_1d_pass.py
+++ colossalai/fx/passes/shard_1d_pass.py
@@ -4,8 +4,10 @@ from torch.fx.node import Node
 from torch.fx.passes.split_module import split_module
 
 import colossalai
+
 from colossalai.context import ParallelMode
 from colossalai.core import global_context as gpc
+from colossalai.tensor import TensorSpec, distspec
 
 
 def all_gather_function(input_):
@@ -26,28 +28,31 @@ def all_reduce_function(input_):
     return input_
 
 
-def weight_split(weight, dim):
-    #TODO: this function will be refactored by using ColoTensor dist_spec when a stable reshaper feature is ready to use.
-    num_partition = gpc.get_world_size(ParallelMode.TENSOR)
-    shape = weight.shape
-    length = shape[dim] // num_partition
-    sharded_weight_list = []
-    for i in range(num_partition):
-        sharded_weight_list.append(weight.narrow(dim, i * length, length))
-    return sharded_weight_list[gpc.get_local_rank(ParallelMode.PARALLEL_1D)]
+def weight_split(weight):
+    # split tensor with tensor_spec
+    assert isinstance(weight, colossalai.tensor.colo_tensor.ColoTensor), \
+        f'The type of the input tensor should be colotensor' \
+        f'Your Input tensor is {type(weight)}'
+    # assert weight.device.type == "meta"
+    tensor_dist_spec = weight.tensor_spec
+    num_partition = tensor_dist_spec.get_process_group_size()
+    dim = 0 if tensor_dist_spec.is_shard_1dcol() else -1
+    dim_size = weight.size(dim)
+    assert dim_size % num_partition == 0, \
+        f'The dimension to split ({dim_size}) is not a multiple of world size ({num_partition}), ' \
+        f'cannot split tensor evenly'
+    spec = TensorSpec(distspec.shard(gpc.get_group(ParallelMode.PARALLEL_1D), [dim], [num_partition]))
+    weight.set_tensor_spec(spec)
+    return weight
 
 
 def replace_all_uses_except_replaced(node, replace_node):
     """
         Replace all uses of ``node`` in the Graph with the Node ``replace_node``,
         except the user of ``node`` is ``replace_node``.
-
         Args:
-
             replace_node (Node): The node to replace all uses of ``node`` with.
-
         Returns:
-
             The list of Nodes on which this change was made.
     """
     to_process = list(node.users)
@@ -81,9 +86,9 @@ def column_shard_linear_pass(gm: torch.fx.GraphModule):
         if node.op == "call_module":
             target_module = node.graph.owning_module.get_submodule(node.target)
             if isinstance(target_module, torch.nn.Linear):
-                target_module.weight.data = weight_split(target_module.weight.data, dim=0)
+                target_module.weight.data = weight_split(target_module.weight.data)
                 if target_module.bias is not None:
-                    target_module.bias.data = weight_split(target_module.bias.data, dim=0)
+                    target_module.bias.data = weight_split(target_module.bias.data)
 
                 # inserting communication node after the sharded linear node
                 with mod_graph.inserting_after(node):
@@ -99,14 +104,14 @@ def row_shard_linear_pass(gm: torch.fx.GraphModule):
         if node.op == "call_module":
             target_module = node.graph.owning_module.get_submodule(node.target)
             if isinstance(target_module, torch.nn.Linear):
-                target_module.weight.data = weight_split(target_module.weight.data, dim=-1)
+                target_module.weight.data = weight_split(target_module.weight.data)
 
                 # insert input sharding node before the sharded linear node
                 with mod_graph.inserting_before(node):
                     input_node_list = list(node._input_nodes.keys())
                     assert len(input_node_list) == 1, 'linear forward must have and only have one input tensor.'
                     input_node = input_node_list[0]
-                    new_input_node = mod_graph.create_node('call_function', weight_split, args=(input_node, -1))
+                    new_input_node = mod_graph.create_node('call_function', weight_split, args=(input_node))
                     replace_all_uses_except_replaced(input_node, new_input_node)
 
                 # inserting communication node after the sharded linear node
@@ -115,6 +120,3 @@ def row_shard_linear_pass(gm: torch.fx.GraphModule):
                     replace_all_uses_except_replaced(node, new_node)
     gm.recompile()
     return gm
-
-
-#TODO: add elementwise op process pass, then we can try to use column and row mixed strategy.
