2b7dca44b5f908fbc00ae5b591b91ed095af086d
YuliangLiu0306 <72588413+YuliangLiu0306@users.noreply.github.com>
2022-07-07 16:22:00 +0800

    [fx]get communication size between partitions (#1224)
    * [CLI] add CLI launcher
    
    * Revert "[CLI] add CLI launcher"
    
    This reverts commit df7e6506d4500af6a9220ef7fe4d3c7b1daebd4c.
    
    * [fx]get communication size between partitions.
    
    * polish

---
+++ tests/test_fx/test_meta_info_prop.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import colossalai
+import colossalai.nn as col_nn
+from torch.fx import symbolic_trace
+from colossalai.fx.passes.meta_info_prop import MetaInfoProp, TensorMetadata
+
+BATCH_SIZE = 2
+DIM_IN = 4
+DIM_OUT = 16
+
+
+def meta_check(meta_info_spec: TensorMetadata, orig_tensor: torch.Tensor):
+    assert meta_info_spec.shape == orig_tensor.shape
+    assert meta_info_spec.dtype == orig_tensor.dtype
+    assert meta_info_spec.requires_grad == orig_tensor.requires_grad
+    assert meta_info_spec.stride == orig_tensor.stride()
+    assert meta_info_spec.numel == orig_tensor.numel()
+
+
+def test_meta_info_prop():
+    model = torch.nn.Linear(DIM_IN, DIM_OUT)
+    input_sample = torch.rand(BATCH_SIZE, DIM_IN)
+    orig_output = model(input_sample)
+    gm = symbolic_trace(model)
+    MetaInfoProp(gm).run(input_sample)
+    for node in gm.graph.nodes:
+        if node.op == 'placeholder':
+            meta_check(node.meta['tensor_meta'], input_sample)
+        if node.op == 'output':
+            meta_check(node.meta['tensor_meta'], orig_output)
+
+
+if __name__ == '__main__':
+    test_meta_info_prop()
