9421154b29320d876eed90bb49ab1f579e1dce67
itok2000u <xiaoye723000@gmail.com>
2022-07-12 09:51:10 +0800

    mlp_pass

--- colossalai/fx/passes/shard_1d_pass.py
+++ colossalai/fx/passes/shard_1d_pass.py
@@ -1,8 +1,15 @@
 import torch
-from colossalai.tensor import ColoTensorSpec, distspec, ProcessGroup, ComputeSpec, ComputePattern, ShardSpec
+from torch.fx.node import map_arg
+from torch.fx.node import Node
+from torch.fx.passes.split_module import split_module
+
+import colossalai
+# from colossalai.tensor import ColoTensor, TensorSpec, distspec, ProcessGroup, ComputeSpec, ComputePattern
+
+ELEMENTWISE_OP = [torch.nn.Dropout, torch.nn.ReLU]
 
 
-def weight_split(weight: torch.Tensor, dim: int) -> torch.nn.parameter.Parameter:
+def weight_split(weight: torch.nn.parameter.Parameter, dim: int) -> torch.nn.parameter.Parameter:
     """weight_split 
     split a nn.Parameter
 
@@ -13,20 +20,22 @@ def weight_split(weight: torch.Tensor, dim: int) -> torch.nn.parameter.Parameter
     Returns:
         _type_: _description_
     """
+    #TODO: This func temporarily works with no materialization
     # Append a Tensor spec to target_module.weight.shard
     # Convert to ColoTensor: colo_tensor = ColoTensor.from_torch_tensor(tensor, spec)
-    assert isinstance(weight, torch.Tensor), \
-        f'The type of the input tensor should be torch.nn.parameter' \
-        f'Your Input tensor is {type(weight)}'
+    # assert isinstance(weight, torch.nn.parameter.Parameter), \
+    #     f'The type of the input tensor should be torch.nn.parameter' \
+    #     f'Your Input tensor is {type(weight)}'
 
     # FIXME() I initialized a PG for this tensor. Only has TP comm group.
     # we only consider the TP-only caes.
-    world_size = torch.distributed.get_world_size()
-    pg = ProcessGroup(tp_degree=world_size)
+    # world_size = torch.distributed.get_world_size()
+    # pg = ProcessGroup(tp_degree=world_size)
 
-    spec = ColoTensorSpec(pg, ShardSpec([dim], [pg.tp_world_size()]), ComputeSpec(ComputePattern.TP1D))
-    # As you has constructed a Spec, why not directly convert the tensor to ColoTensor.
-    setattr(weight, "fx_attr", spec)
+    # spec = TensorSpec(distspec.shard(pg, [dim], [pg.tp_world_size()]), ComputeSpec(ComputePattern.TP1D))
+    # # As you has constructed a Spec, why not directly convert the tensor to ColoTensor.
+    setattr(weight, "fx_attr", (dim, "SHARD", "TP"))
+    # weight.data = ColoTensor(data=weight.data, spec=spec)
     return weight
 
 
@@ -51,9 +60,30 @@ def row_shard_linear_pass(gm: torch.fx.GraphModule):
             target_module = node.graph.owning_module.get_submodule(node.target)
             if isinstance(target_module, torch.nn.Linear):
                 target_module.weight = weight_split(target_module.weight, dim=-1)
-
     gm.recompile()
     return gm
+
+
+def transform_mlp_pass(gm: torch.fx.GraphModule):
+    mod_graph = gm.graph
+    col_shard = True
+    element_op = []
+    for name, func in gm.named_children():
+        if not isinstance(func, torch.nn.Linear):
+            for i in ELEMENTWISE_OP:
+                if isinstance(func, i):
+                    element_op.append(name)
+                    break
+    for node in mod_graph.nodes:
+        if node.op == "call_module" and isinstance(node.graph.owning_module.get_submodule(node.target),
+                                                   torch.nn.Linear):
+            target_module = node.graph.owning_module.get_submodule(node.target)
+            dim = 0 if col_shard else -1
+            target_module.weight = weight_split(target_module.weight, dim=dim)
+            col_shard = not col_shard
+        else:
+            if node.target not in element_op:
+                col_shard = True
 
 
 #TODO: add elementwise op process pass, then we can try to use column and row mixed strategy.
