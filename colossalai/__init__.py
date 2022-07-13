b8197e053be49c3f993d0ae33fcbb7bb8481be4c
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-07-12 19:46:06 -0700

    [NFC] polish colossalai/__init__.py code style (#1285)

--- colossalai/__init__.py
+++ colossalai/__init__.py
@@ -1,5 +1,4 @@
-from .initialize import (initialize, launch, launch_from_openmpi,
-                         launch_from_slurm, launch_from_torch, get_default_parser)
+from .initialize import (initialize, launch, launch_from_openmpi, launch_from_slurm, launch_from_torch,
+                         get_default_parser)
 
 __version__ = '0.0.1'
-
