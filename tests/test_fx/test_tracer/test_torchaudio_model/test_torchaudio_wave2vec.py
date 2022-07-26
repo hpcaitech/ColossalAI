b9bd6447fe2bef21afb26f5d7b907b8736e52f6d
dainiu <19307110036@fudan.edu.cn>
2022-07-26 14:29:14 +0800

    [fx] add torchaudio test

--- tests/test_fx/test_tracer/test_torchaudio_model/test_torchaudio_wave2vec.py
+++ tests/test_fx/test_tracer/test_torchaudio_model/test_torchaudio_wave2vec.py
@@ -19,11 +19,12 @@ MODEL_LIST = [
     wav2vec2_large_lv60k,
 ]
 
+
 def _smoke_test(model, device):
     model = model.to(device=device)
 
     batch_size, num_frames = 3, 1024
-    
+
     def data_gen():
         waveforms = torch.randn(batch_size, num_frames, device=device)
         lengths = torch.randint(
@@ -37,11 +38,12 @@ def _smoke_test(model, device):
         return dict(waveforms=waveforms, lengths=lengths)
 
     trace_and_compare(model, data_gen, need_meta=True, need_concrete=False)
-    
+
+
 def test_wav2vec():
     for model_fn in MODEL_LIST:
         _smoke_test(model_fn(), 'cuda')
-    
-    
+
+
 if __name__ == "__main__":
     test_wav2vec()
