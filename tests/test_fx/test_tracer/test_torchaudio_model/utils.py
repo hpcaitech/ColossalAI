6bf6649f50caab04dfa0328a21ab3b82d411eece
dainiu <19307110036@fudan.edu.cn>
2022-07-26 14:27:51 +0800

    [fx] add torchaudio test

--- tests/test_fx/test_tracer/test_torchaudio_model/utils.py
+++ tests/test_fx/test_tracer/test_torchaudio_model/utils.py
@@ -2,16 +2,17 @@ from colossalai.fx import ColoTracer
 import torch
 from torch.fx import GraphModule, Tracer
 
+
 def trace_and_compare(model, data_gen, need_meta=False, need_concrete=False):
     data = data_gen()
     concrete_args = data if need_concrete else {}
     meta_args = {k: v.to('meta') for k, v in data.items()} if need_meta else {}
     tracer = ColoTracer()
-    
+
     graph = tracer.trace(root=model, concrete_args=concrete_args, meta_args=meta_args)
     gm = GraphModule(model, graph, model.__class__.__name__)
     gm.recompile()
-    
+
     model.eval()
     gm.eval()
 
@@ -20,8 +21,8 @@ def trace_and_compare(model, data_gen, need_meta=False, need_concrete=False):
         fx_out = gm(**data)
     if isinstance(fx_out, tuple):
         for non_fx, fx in zip(non_fx_out, fx_out):
-            assert torch.allclose(
-                non_fx, fx), f'{model.__class__.__name__} has inconsistent outputs, {fx_out} vs {non_fx_out}'
+            assert torch.allclose(non_fx,
+                                  fx), f'{model.__class__.__name__} has inconsistent outputs, {fx_out} vs {non_fx_out}'
     else:
         assert torch.allclose(
             fx_out, non_fx_out), f'{model.__class__.__name__} has inconsistent outputs, {fx_out} vs {non_fx_out}'
