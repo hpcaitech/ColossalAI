69277423121071fe5f8d78f070a2957883add571
csric <mkkt_bkkt@mail.ustc.edu.cn>
2022-08-04 09:46:11 +0800

    test world size

--- tests/test_utils/test_norm_gradient_clipping.py
+++ tests/test_utils/test_norm_gradient_clipping.py
@@ -52,7 +52,7 @@ def run_dist(rank, world_size, port):
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
 def test_zero_clip_grad():
-    world_size = 4
+    world_size = 2
     run_func = partial(run_dist, world_size=world_size, port=free_port())
     mp.spawn(run_func, nprocs=world_size)
 
