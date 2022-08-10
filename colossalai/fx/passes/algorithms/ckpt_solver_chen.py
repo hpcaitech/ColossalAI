a7d56bde00b0762fb755a5b01e1dba7a046096da
dainiu <19307110036@fudan.edu.cn>
2022-08-10 17:39:29 +0800

    [fx] add test for ckpt_solver_chen

--- colossalai/fx/passes/algorithms/ckpt_solver_chen.py
+++ colossalai/fx/passes/algorithms/ckpt_solver_chen.py
@@ -4,7 +4,7 @@ from torch.fx import GraphModule
 
 def chen_greedy(gm: GraphModule, B: int):
     """
-    This is the simple implementation of Algorithm 3 in https://arxiv.org/abs/1604.06174
+    This is the simple implementation of Algorithm 3 in https://arxiv.org/abs/1604.06174.
 
     Usage:
         B = 5 * 1024 * 1024 * 1024  # An approximate memory budget of 5GB
@@ -22,6 +22,11 @@ def chen_greedy(gm: GraphModule, B: int):
     temp = 0
     x = 0
     idx = 0
+    budget = B
+    for n in gm.graph.nodes:
+        B -= getattr(n, 'param_size')
+        assert B > 0, f'The memory budget {budget / 1024 ** 3:.2f} GB is not enough for model parameters of {gm}'
+    print(B)
     for n in gm.graph.nodes:
         temp += getattr(n, 'activation_size')
         if temp > B:
@@ -29,27 +34,28 @@ def chen_greedy(gm: GraphModule, B: int):
             temp = 0
             setattr(n, 'activation_checkpoint', str(idx))
             idx += 1
+    gm.recompile()
     return gm
 
 
 def chen_sqrtn(gm: GraphModule):
     """
-    This is the simple reimplementation of Algorithm 3 in https://arxiv.org/abs/1604.06174
+    This is the theoretical optimal strategy in https://arxiv.org/abs/1604.06174.
 
     Usage:
-        B = 5 * 1024 * 1024 * 1024  # An approximate memory budget of 5GB
         model = resnet18()
         input_sample = torch.rand(4, 3, 224, 224)
         gm = symbolic_trace(model)
         MetaInfoProp(gm).run(input_sample)
-        gm = chen_greedy(gm, B)
+        gm = chen_sqrtn(gm)
 
     Args:
         gm (GraphModule): The module to add checkpoints
-        B (int): The approximate memory budget for this module.
     """
     gm.graph.lint()    # make sure nodes are in topological order
     k = int(len(gm.graph.nodes)**0.5)    # take approximately sqrt(n) checkpoints
     for idx, n in enumerate(gm.graph.nodes):
         if (idx + 1) % k == 0:
-            setattr(n, 'activation checkpoint', str((idx + 1) // k))
+            setattr(n, 'activation_checkpoint', str((idx + 1) // k))
+    gm.recompile()
+    return gm
