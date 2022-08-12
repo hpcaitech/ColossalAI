9b4f46049e70e9e9b800490ecddcdd913e4b4b54
Super Daniel <78588128+super-dainiu@users.noreply.github.com>
2022-08-12 12:33:07 +0800

    [fx] merge development into main (#1)
    * [fx] activation checkpointing using Chen strategies.
    
    * [fx] add test for ckpt_solver_chen
    
    * [fx] add vanilla activation checkpoint search with test on resnet and densenet
    
    * [fx] add a namespace code for solver_chen.
    
    * [fx] fix the false interpretation of algorithm 3 in https://arxiv.org/abs/1604.06174.
    
    * [fx] fix lowercase naming conventions.
    
    * [fx] simplify test for ckpt.

--- tests/test_fx/test_ckpt_solvers/test_ckpt_torchvision.py
+++ tests/test_fx/test_ckpt_solvers/test_ckpt_torchvision.py
@@ -1,4 +1,5 @@
 from ctypes import Union
+from typing import Callable
 from colossalai.fx.passes.algorithms import chen_greedy, chen_sqrtn
 import torch
 import torchvision.models as tm
@@ -18,36 +19,41 @@ def _is_activation_checkpoint_available(gm: GraphModule):
 
 def _is_all_gradient_close(m: torch.nn.Module, gm: GraphModule):
     for m_p, gm_p in zip(m.parameters(), gm.parameters()):
-        if not torch.allclose(m_p, gm_p):
+        if not torch.allclose(m_p.grad, gm_p.grad):
             return False
     return True
 
 
+def check_backward_consistency(m: torch.nn.Module, gm: GraphModule, solver: Callable[[GraphModule], GraphModule],
+                               model_cls: Callable[[], torch.nn.Module]):
+    criterion = torch.nn.MSELoss()
+    data = torch.rand(2, 3, 24, 24)
+    label = torch.rand(2, 5)
+    loss = criterion(m(data), label)
+    loss.backward()
+    loss = criterion(gm(data), label)
+    loss.backward()
+    assert _is_all_gradient_close(m, gm), f'Solver {solver} did not work correctly in backward pass on {model_cls}'
+
+
 def test_ckpt_solver():
-    MODEL_LIST = [tm.resnet18, tm.densenet121]
+    MODEL_LIST = [tm.resnet18]
 
     torch.backends.cudnn.deterministic = True
 
     tracer = ColoTracer()
-    data = torch.rand(1, 3, 224, 224)
-    label = torch.rand(1, 1000)
 
+    data = torch.rand(2, 3, 24, 24)
     for solver in SOLVERS:
         for model_cls in MODEL_LIST:
-            model = model_cls()
-            criterion = torch.nn.MSELoss()
+            model = model_cls(num_classes=5)
             graph = tracer.trace(root=model)
             gm = GraphModule(model, graph, model.__class__.__name__)
             MetaInfoProp(gm).run(data)
             gm = solver(gm)
             assert _is_activation_checkpoint_available(
                 gm), f"Solver {solver} did not annotate {model_cls} with any activation checkpoints"
-            loss = criterion(model(data), label)
-            loss.backward()
-            loss = criterion(gm(data), label)
-            loss.backward()
-            assert _is_all_gradient_close(model,
-                                          gm), f'Solver {solver} did not work correctly in backward pass on {model_cls}'
+            check_backward_consistency(model, gm, solver, model_cls)
 
 
 if __name__ == '__main__':
