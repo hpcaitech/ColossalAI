64b9032e1800cff70e583f2316d3130cd52d76a5
Cypher30 <1529318642@qq.com>
2022-08-16 13:51:52 +0800

    [test] modify test_activation_checkpoint.py

--- tests/test_utils/test_activation_checkpointing.py
+++ tests/test_utils/test_activation_checkpointing.py
@@ -17,8 +17,9 @@ def forward(x, weight):
 
 
 @pytest.mark.gpu
+@pytest.mark.parametrize("use_reentrant", [True, False])
 @pytest.mark.parametrize("cpu_offload", [True, False])
-def test_activation_checkpointing(cpu_offload):
+def test_activation_checkpointing(cpu_offload, use_reentrant):
 
     # as seed manager is singleton
     # if we don't reset seeds here,
@@ -54,59 +55,7 @@ def test_activation_checkpointing(cpu_offload):
     torch.cuda.set_rng_state(data_parallel_cuda_rng_state)
     set_mode(ParallelMode.GLOBAL)
 
-    out = checkpoint(forward, cpu_offload, inputs_, weight_)
-    loss = out.sum()
-    loss.backward()
-
-    assert torch.all(inputs.grad == inputs_.grad), 'Gradient of the input does not match'
-    torch.cuda.empty_cache()
-
-    # as seed manager is singleton
-    # if we don't reset seeds here,
-    # other tests will fail if running together with this test
-    # as other tests can't overwrite the seed set by this test
-    reset_seeds()
-
-
-@pytest.mark.gpu
-@pytest.mark.parametrize("cpu_offload", [True, False])
-def test_activation_checkpointing_reentrant_False(cpu_offload):
-
-    # as seed manager is singleton
-    # if we don't reset seeds here,
-    # other tests might affect this test
-    reset_seeds()
-
-    # We put initilization here to avoid change cuda rng state below
-    inputs = torch.rand(2, 2, requires_grad=True, device='cuda')
-    weight = torch.rand(2, 4, requires_grad=True, device='cuda')
-
-    # Get a copy of input tensors
-    inputs_ = torch.empty(2, 2, requires_grad=True, device='cuda')
-    inputs_.data.copy_(inputs.data)
-    weight_ = torch.empty(2, 4, requires_grad=True, device='cuda')
-    weight_.data.copy_(weight.data)
-
-    add_seed(ParallelMode.GLOBAL, 1024)
-    add_seed(ParallelMode.DATA, 1026)
-    set_mode(ParallelMode.GLOBAL)
-    global_cuda_rng_state = torch.cuda.get_rng_state()
-    set_mode(ParallelMode.DATA)
-    data_parallel_cuda_rng_state = torch.cuda.get_rng_state()
-    set_mode(ParallelMode.GLOBAL)
-
-    out = forward(inputs, weight)
-    loss = out.sum()
-    loss.backward()
-
-    # Recover cuda rng states
-    set_mode(ParallelMode.GLOBAL)
-    torch.cuda.set_rng_state(global_cuda_rng_state)
-    set_mode(ParallelMode.DATA)
-    torch.cuda.set_rng_state(data_parallel_cuda_rng_state)
-    set_mode(ParallelMode.GLOBAL)
-
-    out = checkpoint(forward, cpu_offload, inputs_, weight_, use_reentrant=False)
+    out = checkpoint(forward, cpu_offload, inputs_, weight_, use_reentrant=use_reentrant)
     loss = out.sum()
     loss.backward()
 
@@ -121,4 +70,4 @@ def test_activation_checkpointing_reentrant_False(cpu_offload):
 
 
 if __name__ == "__main__":
-    test_activation_checkpointing_reentrant_False(False)
+    test_activation_checkpointing(False, False)
