fae158e16f08bbe9be04d6a81a43eb41eeedbaba
dainiu <19307110036@fudan.edu.cn>
2022-08-30 15:13:12 +0800

    [fx] fix unexpected error.

--- colossalai/fx/profiler/__init__.py
+++ colossalai/fx/profiler/__init__.py
@@ -4,7 +4,7 @@ except:
     import torch
     print(f'_meta_registrations seems to be incompatible with PyTorch {torch.__version__}.')
 from .meta_tensor import MetaTensor
-from .registry import *
+from .registry import meta_profiler_function, meta_profiler_module
 from .profiler_function import *
 from .profiler_module import *
 from .profiler import *
