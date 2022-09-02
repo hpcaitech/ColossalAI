a9193b546bb55d2f44098db67b041abf85ce2dc6
dainiu <19307110036@fudan.edu.cn>
2022-09-02 22:26:18 +0800

    [fx] skip test for MetaInfoProp.

--- tests/test_fx/test_meta_info_prop.py
+++ tests/test_fx/test_meta_info_prop.py
@@ -6,6 +6,11 @@ from torch.fx import symbolic_trace
 from colossalai.fx.passes.meta_info_prop import MetaInfoProp, TensorMetadata
 
 import pytest
+try:
+    meta_lib = torch.library.Library("aten", "IMPL", "Meta")
+    INCOMPATIBLE = False    # version > 1.12.0
+except:
+    INCOMPATIBLE = True
 
 BATCH_SIZE = 2
 DIM_IN = 4
@@ -19,7 +24,7 @@ def meta_check(meta_info_spec: TensorMetadata, orig_tensor: torch.Tensor):
     assert meta_info_spec.numel == orig_tensor.numel()
 
 
-@pytest.skip(reason='Not compatible with torch < 1.12.0.')
+@pytest.skipif(INCOMPATIBLE, reason='torch version is lower than 1.12.0')
 def test_meta_info_prop():
     model = torch.nn.Linear(DIM_IN, DIM_OUT)
     input_sample = torch.rand(BATCH_SIZE, DIM_IN, device='meta')
