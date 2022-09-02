11df5d86398997be38a18b969bdf8251c33d7322
dainiu <19307110036@fudan.edu.cn>
2022-09-02 13:56:15 +0800

    [fx] support meta tracing for aten level computation graphs like functorch.

--- colossalai/__init__.py
+++ colossalai/__init__.py
@@ -6,4 +6,4 @@ except:
 from .initialize import (initialize, launch, launch_from_openmpi, launch_from_slurm, launch_from_torch,
                          get_default_parser)
 
-__version__ = '0.0.1'
+__version__ = '0.1.9'
