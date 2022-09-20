d397842fa8eed544c194b146e2f0a666cf0e213d
Frank Lee <somerlee.9@gmail.com>
2022-09-20 14:17:21 +0800

    [autoparallel] added new node handler (#1612)

---
+++ colossalai/auto_parallel/solver/op_handler/registry.py
@@ -0,0 +1,25 @@
+class Registry:
+    # TODO: refactor the registry classes used in colossalai.registry, colossalai.fx and here
+
+    def __init__(self, name):
+        self.name = name
+        self.store = {}
+
+    def register(self, source):
+
+        def wrapper(func):
+            self.store[source] = func
+            return func
+
+        return wrapper
+
+    def get(self, source):
+        assert source in self.store
+        target = self.store[source]
+        return target
+
+    def has(self, source):
+        return source in self.store
+
+
+operator_registry = Registry('operator')
