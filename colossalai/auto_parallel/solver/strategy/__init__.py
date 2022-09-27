5975e3f46f7720b2d5505f53d58d93bf213becd2
YuliangLiu0306 <liuyuliang@luchentech.com>
2022-09-27 20:42:59 +0800

    [autoparallel] add conv handler v2

--- colossalai/auto_parallel/solver/strategy/__init__.py
+++ colossalai/auto_parallel/solver/strategy/__init__.py
@@ -1,7 +1,8 @@
 from .strategy_generator import StrategyGenerator_V2
 from .matmul_strategy_generator import DotProductStrategyGenerator, MatVecStrategyGenerator, LinearProjectionStrategyGenerator, BatchedMatMulStrategyGenerator
+from .conv_strategy_generator import ConvStrategyGenerator
 
 __all__ = [
     'StrategyGenerator_V2', 'DotProductStrategyGenerator', 'MatVecStrategyGenerator',
-    'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator'
+    'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator', 'ConvStrategyGenerator'
 ]
