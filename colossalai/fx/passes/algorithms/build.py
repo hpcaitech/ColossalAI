5464deb6baac17d2b3f7e28809fe833a28c7a4c8
Cypher30 <1529318642@qq.com>
2022-09-27 14:24:52 +0800

    [autoparallel] remove build.py

--- colossalai/fx/passes/algorithms/build.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('c_version_dp', sources=['dynamic_programs.c'])
-
-setup(name='a demo c extension', version='0.1', description='This is a demo package', ext_modules=[module1])
