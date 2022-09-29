11ec070e53c285393a38355cfa8a1e3c429bdba3
YuliangLiu0306 <72588413+YuliangLiu0306@users.noreply.github.com>
2022-09-29 12:49:28 +0800

    [hotfix]unit test (#1670)

--- colossalai/auto_parallel/solver/strategy/__init__.py
+++ colossalai/auto_parallel/solver/strategy/__init__.py
@@ -1,8 +1,10 @@
 from .strategy_generator import StrategyGenerator_V2
 from .matmul_strategy_generator import DotProductStrategyGenerator, MatVecStrategyGenerator, LinearProjectionStrategyGenerator, BatchedMatMulStrategyGenerator
 from .conv_strategy_generator import ConvStrategyGenerator
+from .batch_norm_generator import BatchNormStrategyGenerator
 
 __all__ = [
     'StrategyGenerator_V2', 'DotProductStrategyGenerator', 'MatVecStrategyGenerator',
-    'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator', 'ConvStrategyGenerator'
+    'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator', 'ConvStrategyGenerator',
+    'BatchNormStrategyGenerator'
 ]
