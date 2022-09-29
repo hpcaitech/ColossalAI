3f068d140917b842b16fd794447419ebb9ef8e1a
YuliangLiu0306 <72588413+YuliangLiu0306@users.noreply.github.com>
2022-09-29 11:20:59 +0800

    [autoparallel] update CommSpec (#1667)

--- colossalai/tensor/__init__.py
+++ colossalai/tensor/__init__.py
@@ -9,10 +9,11 @@ from .colo_parameter import ColoParameter
 from .utils import convert_parameter, named_params_with_colotensor
 from .dist_spec_mgr import DistSpecManager
 from .param_op_hook import ParamOpHook, ParamOpHookManager
+from .comm_spec import CollectiveCommPattern, CommSpec
 from . import distspec
 
 __all__ = [
     'ColoTensor', 'convert_parameter', 'ComputePattern', 'ComputeSpec', 'named_params_with_colotensor', 'ColoParameter',
     'distspec', 'DistSpecManager', 'ParamOpHook', 'ParamOpHookManager', 'ProcessGroup', 'ColoTensorSpec', 'ShardSpec',
-    'ReplicaSpec'
+    'ReplicaSpec', 'CommSpec', 'CollectiveCommPattern'
 ]
