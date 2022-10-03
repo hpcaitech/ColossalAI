95e1354e1e4814f07387b1c0bfd3a7740f2d85db
Cypher30 <1529318642@qq.com>
2022-10-03 17:11:30 +0800

    [fx] fix device error

--- colossalai/fx/passes/concrete_info_prop.py
+++ colossalai/fx/passes/concrete_info_prop.py
@@ -3,39 +3,11 @@ from colossalai.fx.profiler import GraphInfo
 import torch
 import torch.fx
 from torch.fx.node import Node, Argument, Target
-from torch.utils._pytree import tree_map
-from typing import Any, List, Tuple, NamedTuple, Dict
+from torch.utils._pytree import tree_flatten
+from typing import Any, List, Tuple, NamedTuple, Dict, Optional
 from torch.fx._compatibility import compatibility
 from colossalai.fx.profiler import profile_function, profile_module, profile_method, activation_size
-
-
-@compatibility(is_backward_compatible=True)
-class TensorMetadata(NamedTuple):
-    # TensorMetadata is a structure containing pertinent information
-    # about a tensor within a PyTorch program.
-
-    shape: torch.Size
-    dtype: torch.dtype
-    requires_grad: bool
-    stride: Tuple[int]
-    numel: int
-    is_tensor: bool
-    # TODO: we can add a list of sharding spec here, and record the sharding
-    # behaviour by appending sharding spec into list.
-
-
-def _extract_tensor_metadata(result: torch.Tensor) -> TensorMetadata:
-    """
-    Extract a TensorMetadata NamedTuple describing `result`.
-    """
-    shape = result.shape
-    dtype = result.dtype
-    requires_grad = result.requires_grad
-    stride = result.stride()
-    numel = result.numel()
-    is_tensor = True
-
-    return TensorMetadata(shape, dtype, requires_grad, stride, numel, is_tensor)
+from torch.fx.graph_module import GraphModule
 
 
 @compatibility(is_backward_compatible=True)
@@ -75,6 +47,27 @@ class ConcreteInfoProp(torch.fx.Interpreter):
 
     _is_proped: bool = False
 
+    def run(self, *args, initial_env: Optional[Dict[Node, Any]] = None, enable_io_processing: bool = True) -> Any:
+        """Customized run for ConcreteInfoProp
+        We need to store the device in self.device
+
+        Args:
+            *args: The arguments to the Module to run, in positional order
+            initial_env (Optional[Dict[Node, Any]]): An optional starting environment for execution.
+                This is a dict mapping `Node` to any value. This can be used, for example, to
+                pre-populate results for certain `Nodes` so as to do only partial evaluation within
+                the interpreter.
+            enable_io_processing (bool): If true, we process the inputs and outputs with graph's process_inputs and
+                process_outputs function first before using them.
+
+        Returns:
+            Any: The value returned from executing the Module
+        """
+
+        flatten_args, _ = tree_flatten(args)
+        self.device = next(item for item in flatten_args if hasattr(item, "device")).device
+        return super().run(*args, initial_env, enable_io_processing)
+
     @compatibility(is_backward_compatible=True)
     def run_node(self, n: Node) -> Any:
         """
@@ -92,14 +85,6 @@ class ConcreteInfoProp(torch.fx.Interpreter):
         self._is_proped = True
         result, meta_info = super().run_node(n)
 
-        def extract_tensor_meta(obj):
-            if isinstance(obj, torch.Tensor):
-                return _extract_tensor_metadata(obj)
-            else:
-                return TensorMetadata(None, None, False, None, 0, False)
-
-        tensor_meta = tree_map(extract_tensor_meta, result)
-        n.meta['tensor_meta'] = tensor_meta
         n.meta = {**n.meta, **asdict(meta_info)}    # extend MetaInfo to `n.meta`
         # TODO: the attribute node_size should be removed in the future
         setattr(n, 'node_size', n.meta.get('fwd_mem_tmp', 0) + n.meta.get('fwd_mem_out', 0))
@@ -169,7 +154,7 @@ class ConcreteInfoProp(torch.fx.Interpreter):
             meta_info (MetaInfo): The memory cost and forward & backward time.
         """
         assert not isinstance(target, str)
-        return profile_function(target, "cuda:0")(*args, **kwargs)
+        return profile_function(target, self.device)(*args, **kwargs)
 
     @compatibility(is_backward_compatible=True)
     def call_method(self, target: 'Target', args: Tuple[Argument, ...], kwargs: Dict[str, Any]) -> Any:
@@ -187,7 +172,7 @@ class ConcreteInfoProp(torch.fx.Interpreter):
             result (Any): The argument value that was retrieved
             meta_info (MetaInfo): The memory cost and forward & backward time.
         """
-        return profile_method(target, "cuda:0")(*args, **kwargs)
+        return profile_method(target, self.device)(*args, **kwargs)
 
     @compatibility(is_backward_compatible=True)
     def call_module(self, target: 'Target', args: Tuple[Argument, ...], kwargs: Dict[str, Any]) -> Any:
@@ -209,7 +194,7 @@ class ConcreteInfoProp(torch.fx.Interpreter):
         # Execute the method and return the result
         assert isinstance(target, str)
         submod = self.fetch_attr(target)
-        return profile_module(submod, "cuda:0")(*args, **kwargs)
+        return profile_module(submod, self.device)(*args, **kwargs)
 
     @compatibility(is_backward_compatible=True)
     def output(self, target: 'Target', args: Tuple[Argument, ...], kwargs: Dict[str, Any]) -> Any:
