f6c6a932b8e0f921b2ee9623d2969f30ed614077
YuliangLiu0306 <72588413+YuliangLiu0306@users.noreply.github.com>
2022-10-09 14:49:18 +0800

    [autoparallel] add following node generator (#1673)
    * [autoparallel] add following node generator
    
    * polish code
    
    * polish code
    
    * update name of arguments

--- colossalai/auto_parallel/solver/strategy/__init__.py
+++ colossalai/auto_parallel/solver/strategy/__init__.py
@@ -2,10 +2,12 @@ from .strategy_generator import StrategyGenerator_V2
 from .matmul_strategy_generator import DotProductStrategyGenerator, MatVecStrategyGenerator, LinearProjectionStrategyGenerator, BatchedMatMulStrategyGenerator
 from .conv_strategy_generator import ConvStrategyGenerator
 from .batch_norm_generator import BatchNormStrategyGenerator
+from .getitem_generator import GetItemStrategyGenerator, TensorStrategyGenerator, TensorTupleStrategyGenerator
 from .layer_norm_generator import LayerNormGenerator
 
 __all__ = [
     'StrategyGenerator_V2', 'DotProductStrategyGenerator', 'MatVecStrategyGenerator',
     'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator', 'ConvStrategyGenerator',
-    'BatchNormStrategyGenerator', 'LayerNormGenerator'
+    'BatchNormStrategyGenerator', 'GetItemStrategyGenerator', 'TensorStrategyGenerator', 'TensorTupleStrategyGenerator',
+    'LayerNormGenerator'
 ]
