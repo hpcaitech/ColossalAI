319d654f795616ffc0be18459ee66b5bbfbf4acd
YuliangLiu0306 <72588413+YuliangLiu0306@users.noreply.github.com>
2022-10-13 11:02:22 +0800

    [autoparallel] where_handler_v2 (#1688)
    * where generator
    
    * [autoparallel] where_handler_v2

--- colossalai/auto_parallel/solver/strategy/__init__.py
+++ colossalai/auto_parallel/solver/strategy/__init__.py
@@ -5,11 +5,12 @@ from .batch_norm_generator import BatchNormStrategyGenerator
 from .unary_elementwise_generator import UnaryElementwiseGenerator
 from .getitem_generator import GetItemStrategyGenerator, TensorStrategyGenerator, TensorTupleStrategyGenerator
 from .layer_norm_generator import LayerNormGenerator
+from .where_generator import WhereGenerator
 from .reshape_generator import ReshapeGenerator
 
 __all__ = [
     'StrategyGenerator_V2', 'DotProductStrategyGenerator', 'MatVecStrategyGenerator',
     'LinearProjectionStrategyGenerator', 'BatchedMatMulStrategyGenerator', 'ConvStrategyGenerator',
     'UnaryElementwiseGenerator', 'BatchNormStrategyGenerator', 'GetItemStrategyGenerator', 'TensorStrategyGenerator',
-    'TensorTupleStrategyGenerator', 'LayerNormGenerator', 'ReshapeGenerator'
+    'TensorTupleStrategyGenerator', 'LayerNormGenerator', "WhereGenerator", 'ReshapeGenerator'
 ]
