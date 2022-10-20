b893342f95dfb6bd4f4a96e4b61998336e2c56ad
Super Daniel <78588128+super-dainiu@users.noreply.github.com>
2022-10-20 18:25:05 +0800

    [fx] test tracer on diffuser modules. (#1750)
    * [fx] test tracer on diffuser modules.
    
    * [fx] shorter seq_len.
    
    * Update requirements-test.txt

--- tests/test_fx/test_tracer/test_hf_model/test_hf_t5.py
+++ tests/test_fx/test_tracer/test_hf_model/test_hf_t5.py
@@ -1,10 +1,10 @@
 import pytest
-import transformers
 import torch
+import transformers
 from utils import trace_model_and_compare_output
 
 BATCH_SIZE = 1
-SEQ_LENGHT = 16
+SEQ_LENGTH = 16
 
 
 def test_t5():
@@ -17,13 +17,13 @@ def test_t5():
     config = transformers.T5Config(d_model=128, num_layers=2)
 
     def data_gen():
-        input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGHT), dtype=torch.int64)
-        decoder_input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGHT), dtype=torch.int64)
+        input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGTH), dtype=torch.int64)
+        decoder_input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGTH), dtype=torch.int64)
         kwargs = dict(input_ids=input_ids, decoder_input_ids=decoder_input_ids)
         return kwargs
 
     def data_gen_for_encoder_only():
-        input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGHT), dtype=torch.int64)
+        input_ids = torch.zeros((BATCH_SIZE, SEQ_LENGTH), dtype=torch.int64)
         kwargs = dict(input_ids=input_ids)
         return kwargs
 
