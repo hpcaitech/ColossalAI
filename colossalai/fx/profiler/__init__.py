314552bf8cd265ebc90d0bc86c526b661b991f50
dainiu <19307110036@fudan.edu.cn>
2022-10-21 15:48:50 +0800

    [fx] add time computations.

--- colossalai/fx/profiler/__init__.py
+++ colossalai/fx/profiler/__init__.py
@@ -3,7 +3,13 @@ from .._compatibility import is_compatible_with_meta
 if is_compatible_with_meta():
     from .opcount import flop_mapping
     from .profiler import profile_function, profile_method, profile_module
-    from .shard_utils import calculate_fwd_in, calculate_fwd_out, calculate_fwd_tmp
+    from .shard_utils import (
+        calculate_bwd_time,
+        calculate_fwd_in,
+        calculate_fwd_out,
+        calculate_fwd_time,
+        calculate_fwd_tmp,
+    )
     from .tensor import MetaTensor
 else:
     from .experimental import meta_profiler_function, meta_profiler_module, profile_function, profile_method, profile_module, calculate_fwd_in, calculate_fwd_tmp, calculate_fwd_out
