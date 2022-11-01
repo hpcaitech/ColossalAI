ca4dd09522929013bad47b1e09c9b242f26dbcdb
YuliangLiu0306 <liuyuliang@luchentech.com>
2022-11-01 14:17:28 +0800

    polish code

--- colossalai/fx/tracer/bias_addition_patch/patched_bias_addition_module/linear.py
+++ colossalai/fx/tracer/bias_addition_patch/patched_bias_addition_module/linear.py
@@ -8,12 +8,6 @@ from .bias_addition_module import BiasAdditionModule
 @bias_addition_module.register(torch.nn.Linear)
 class BiasAdditionLinear(BiasAdditionModule):
 
-    def create_non_bias_func_proxy(self):
-        return super().create_non_bias_func_proxy()
-
-    def create_bias_addition_proxy(self, non_bias_func_proxy, bias_proxy):
-        return super().create_bias_addition_proxy(non_bias_func_proxy, bias_proxy)
-
     def generate(self):
         non_bias_linear_func_proxy = self.create_non_bias_func_proxy()
         bias_addition_proxy = self.create_bias_addition_proxy(non_bias_linear_func_proxy, self.bias_proxy)
