3356d3c513ddbde3e0c6e373c1c3dd1c3a44f1ac
Cypher30 <1529318642@qq.com>
2022-11-08 14:11:14 +0800

    [autoparallel] fix batchnorm unit test function declaration

--- tests/test_auto_parallel/test_tensor_shard/test_metainfo/test_batchnorm_metainfo.py
+++ tests/test_auto_parallel/test_tensor_shard/test_metainfo/test_batchnorm_metainfo.py
@@ -15,7 +15,7 @@ from colossalai.utils import free_port
 from tests.test_auto_parallel.test_tensor_shard.test_metainfo.utils import mem_test_for_node_strategy
 
 
-def _batchnorm_module_mem_test(rank, bias, world_size, port):
+def _batchnorm_module_mem_test(rank, world_size, port):
     """This function is for conv memory test
     Test and print real memory cost and estimated, this test will not be executed except with the tag AUTO_PARALLEL
 
@@ -51,9 +51,9 @@ def _batchnorm_module_mem_test(rank, bias, world_size, port):
 @run_on_environment_flag(name='AUTO_PARALLEL')
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
-def test_batchnorm_meta_concrete_info_match(bias=False):
+def test_batchnorm_meta_concrete_info_match():
     world_size = 4
-    run_func_module = partial(_batchnorm_module_mem_test, bias=bias, world_size=world_size, port=free_port())
+    run_func_module = partial(_batchnorm_module_mem_test, world_size=world_size, port=free_port())
     mp.spawn(run_func_module, nprocs=world_size)
 
 
