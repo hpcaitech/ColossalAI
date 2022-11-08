4268ae017bb54fd33b8e520ad9a53acd4c0efd53
アマデウス <kurisusnowdeng@users.noreply.github.com>
2022-11-08 16:22:23 +0800

    [kernel] added jit warmup (#1792)

--- colossalai/nn/layer/parallel_3d/_utils.py
+++ colossalai/nn/layer/parallel_3d/_utils.py
@@ -5,7 +5,6 @@ import torch
 from torch import Tensor
 
 from colossalai.constants import INPUT_GROUP_3D, INPUT_X_WEIGHT_3D, OUTPUT_GROUP_3D, OUTPUT_X_WEIGHT_3D, WEIGHT_GROUP_3D
-from colossalai.context.parallel_mode import ParallelMode
 from colossalai.core import global_context as gpc
 from colossalai.global_variables import tensor_parallel_env as env
 
