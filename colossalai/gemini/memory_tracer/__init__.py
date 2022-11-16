5a86383235ead4ab638271f705a21651da0223eb
jiaruifang <fangjiarui123@gmail.com>
2022-11-16 14:57:21 +0800

    [Gemini] Refactor memstats

--- colossalai/gemini/memory_tracer/__init__.py
+++ colossalai/gemini/memory_tracer/__init__.py
@@ -1,5 +1,10 @@
-from .model_data_memtracer import GLOBAL_MODEL_DATA_TRACER
+from .chunk_memstats_collector import ChunkMemStatsCollector
 from .memory_monitor import AsyncMemoryMonitor, SyncCudaMemoryMonitor
 from .memstats_collector import MemStatsCollector
+from .model_data_memtracer import GLOBAL_MODEL_DATA_TRACER
+from .static_memstats_collector import StaticMemStatsCollector
 
-__all__ = ['AsyncMemoryMonitor', 'SyncCudaMemoryMonitor', 'MemStatsCollector', 'GLOBAL_MODEL_DATA_TRACER']
+__all__ = [
+    'AsyncMemoryMonitor', 'SyncCudaMemoryMonitor', 'MemStatsCollector', 'ChunkMemStatsCollector',
+    'StaticMemStatsCollector', 'GLOBAL_MODEL_DATA_TRACER'
+]
