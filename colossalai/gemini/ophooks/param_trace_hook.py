23adf4ad9a6d21149373b65d990e65e1cfdc759f
superhao1995 <804673818@qq.com>
2022-11-23 23:45:40 +0800

    add sample_model_data func

--- colossalai/gemini/ophooks/param_trace_hook.py
+++ colossalai/gemini/ophooks/param_trace_hook.py
@@ -18,12 +18,12 @@ class ParamMemHook(ParamOpHook):
 
     def __init__(self) -> None:
         super().__init__()
+        self._training_phase = TrainingPhase.FORWARD
         self.mem_monitor = SyncCudaMemoryMonitor()
         self._non_model_data_list = []
         self._model_data_list = []
 
     def _move_params_to_dev(self, params, dev: str) -> int:
-
         assert isinstance(dev, str), f"device should be a str not torch.device"
         comm_volume = 0
         for p in params:
@@ -34,16 +34,23 @@ class ParamMemHook(ParamOpHook):
                 if p.grad.device.type != dev:
                     p.grad = p.grad.to(dev)
                     comm_volume += p.grad.numel() * p.grad.element_size()
-
         return comm_volume
 
+    def sample_model_data(self, params):
+        data_volume = 0
+        for p in params:
+            data_volume += p.data.numel() * p.data.element_size()
+        if self._training_phase == TrainingPhase.BACKWARD:
+            # add param.grad, actually param.grad is None in this time
+            data_volume *= 2
+        self._model_data_list.append(data_volume)
 
     def pre_op(self, params):
         cuda_volume = self.mem_monitor.finish()
         if len(self._model_data_list):
             self._non_model_data_list.append(cuda_volume - self._model_data_list[-1])
-        comm_volume = self._move_params_to_dev(params, 'cuda')
-        self._model_data_list.append(comm_volume)
+        self._move_params_to_dev(params, 'cuda')
+        self.sample_model_data(params)
         self.mem_monitor.start()
 
     def post_op(self, params):
