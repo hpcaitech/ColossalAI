ab7451633e8002bcc1e8ae89eee3303f4818bd72
superhao1995 <804673818@qq.com>
2022-11-23 23:26:04 +0800

    remove data.half

--- colossalai/gemini/memory_tracer/param_tracer_wrapper.py
+++ colossalai/gemini/memory_tracer/param_tracer_wrapper.py
@@ -1,7 +1,6 @@
 import torch.nn
 
 from colossalai.tensor.param_op_hook import ParamOpHookManager
-from colossalai.tensor.colo_parameter import ColoParameter
 from colossalai.gemini.ophooks import ParamMemHook
 from colossalai.nn.parallel.data_parallel import _cast_float
 
@@ -13,20 +12,13 @@ class ParamWrapper(torch.nn.Module):
         self.module = module
         self.param_op_hook = ParamMemHook()
 
-        for p in module.parameters():
-            assert isinstance(p, ColoParameter)
-            if getattr(p, '_ddp_to_ignore', False):
-                p.data = p.data.half()
-                continue
-            p.data = p.data.half()
-
-        self._cast_buffers()
+        self._cast_buffers_to_cuda()
 
     def _pre_forward(self):
         self.param_op_hook.mem_monitor.start()
 
     def forward(self, *args, **kwargs):
-        args, kwargs = _cast_float(args, torch.half), _cast_float(kwargs, torch.half)
+        args, kwargs = _cast_float(args, torch.float), _cast_float(kwargs, torch.float)
         self.module.zero_grad(set_to_none=True)
         self._pre_forward()
         with ParamOpHookManager.use_hooks(self.param_op_hook):
@@ -43,8 +35,6 @@ class ParamWrapper(torch.nn.Module):
         last_model_data = self.param_op_hook._model_data_list[-1]
         self.param_op_hook._non_model_data_list.append(cuda_volume - last_model_data)
 
-    def _cast_buffers_to_half_cuda(self):
+    def _cast_buffers_to_cuda(self):
         for buffer in self.module.buffers():
             buffer.data = buffer.cuda()
-            if torch.is_floating_point(buffer):
-                buffer.data = buffer.half()
