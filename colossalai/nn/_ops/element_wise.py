96134e7be3d9dde14df705766e1cc15ebbc50b0f
Jiarui Fang <fangjiarui123@gmail.com>
2022-11-29 11:19:52 +0800

    [hotfix] add bert test for gemini fwd bwd (#2035)

--- colossalai/nn/_ops/element_wise.py
+++ colossalai/nn/_ops/element_wise.py
@@ -34,17 +34,15 @@ def register_elementwise_op(op):
                                                                         dist_attr=input_tensor.dist_spec))
 
 
-@colo_op_impl(torch.relu_)
-def elementwise_op(input_tensor):
-    torch.relu_(input_tensor.data)
-    return input_tensor
+# @colo_op_impl(torch.relu_)
+# def elementwise_op(input_tensor):
+#     torch.relu_(input_tensor.data)
+#     return input_tensor
 
-
-@colo_op_impl(Tensor.add_)
-def elementwise_op(input_tensor: ColoTensor, *args, **kwargs):
-    input_tensor = input_tensor.data.add_(*args, **kwargs)
-    return input_tensor
-
+# @colo_op_impl(Tensor.add_)
+# def elementwise_op(input_tensor: ColoTensor, *args, **kwargs):
+#     input_tensor = input_tensor.data.add_(*args, **kwargs)
+#     return input_tensor
 
 # Tensor op
 register_elementwise_op(Tensor.abs)
