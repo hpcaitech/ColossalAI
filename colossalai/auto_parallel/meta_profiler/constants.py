c1cf47bfef2f50a539030c06cf75a9f7db5c8d61
Cypher30 <1529318642@qq.com>
2022-12-04 14:52:51 +0800

    [autoparallel] modify constants.py

--- colossalai/auto_parallel/meta_profiler/constants.py
+++ colossalai/auto_parallel/meta_profiler/constants.py
@@ -3,14 +3,10 @@ import operator
 import torch
 import torch.nn as nn
 
+from ..tensor_shard.constants import *
+
 # list of inplace operations
 INPLACE_MODULE = [nn.ReLU]
 
 # list of operations that do not save forward activations
 NO_SAVE_ACTIVATION = [torch.add, torch.sub, operator.add, operator.sub]
-
-# list of binary elementwise operations
-BCAST_FUNC_OP = [
-    torch.add, torch.sub, torch.mul, torch.div, torch.floor_divide, torch.true_divide, operator.add, operator.sub,
-    operator.mul, operator.floordiv, operator.truediv, torch.matmul, operator.pow, torch.pow
-]
