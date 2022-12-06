c3e9e0662b4e7553b6481aadfd85314363e917a1
jiaruifang <fangjiarui123@gmail.com>
2022-12-06 21:59:23 +0800

    [Gemini] remove GLOBAL_MODEL_DATA_TRACER

--- colossalai/gemini/memory_tracer/__init__.py
+++ colossalai/gemini/memory_tracer/__init__.py
@@ -1,11 +1,10 @@
 from .memory_monitor import AsyncMemoryMonitor, SyncCudaMemoryMonitor    # isort:skip
 from .memstats_collector import MemStatsCollector    # isort:skip
-from .model_data_memtracer import GLOBAL_MODEL_DATA_TRACER    # isort:skip
 from .chunk_memstats_collector import ChunkMemStatsCollector    # isort:skip
 from .static_memstats_collector import StaticMemStatsCollector    # isort:skip
 from .memory_stats import MemStats
 
 __all__ = [
     'AsyncMemoryMonitor', 'SyncCudaMemoryMonitor', 'MemStatsCollector', 'ChunkMemStatsCollector',
-    'StaticMemStatsCollector', 'GLOBAL_MODEL_DATA_TRACER', 'MemStats'
+    'StaticMemStatsCollector', 'MemStats'
 ]
