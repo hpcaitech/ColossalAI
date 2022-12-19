bdef9dfdbee1f7f2d4a8e830a6b4bfb791fc67e9
Jiarui Fang <fangjiarui123@gmail.com>
2022-12-20 00:33:58 +0800

    [NFC] remove useless graph node code (#2150)

--- colossalai/nn/graph/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import register_colo_graph
-from .graph_node import GraphContext, GraphGlobalEnv, GraphOpNode
-
-__all__ = ['register_colo_graph', 'GraphContext', 'GraphGlobalEnv', 'GraphOpNode']
