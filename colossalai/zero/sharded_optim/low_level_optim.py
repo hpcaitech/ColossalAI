3017ff106d1d2b736d2fc19ec348e134febfb66b
1SAA <c2h214748@gmail.com>
2022-12-20 14:11:22 +0800

    change init value

--- colossalai/zero/sharded_optim/low_level_optim.py
+++ colossalai/zero/sharded_optim/low_level_optim.py
@@ -39,9 +39,9 @@ class LowLevelZeroOptimizer(ColossalaiOptimizer):
             min_scale=1,
             growth_factor=2,
             backoff_factor=0.5,
-            growth_interval=1000,
+            growth_interval=2000,
             hysteresis=2,
-            max_scale: int = 2**32,
+            max_scale: int = 2**24,
 
     # grad clipping
             clip_grad_norm=0.0,
