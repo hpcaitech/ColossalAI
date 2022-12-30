6324fc5f75b1c10ae060eecafd96184456972574
ziyuhuang123 <202476410@qq.com>
2022-12-30 10:55:34 +0800

    '01'

--- examples/language/gpt/run.sh
+++ examples/language/gpt/run.sh
@@ -2,14 +2,16 @@
 export DISTPAN={$DISTPAN:-"colossalai"}
 
 # The following options only valid when DISTPAN="colossalai"
-export TPDEGREE=${TPDEGREE:-1}
-export GPUNUM=${GPUNUM:-1}
-export PLACEMENT=${PLACEMENT:'const'}
-export USE_SHARD_INIT=${USE_SHARD_INIT:False}
-export BATCH_SIZE=${BATCH_SIZE:-8}
-export MODEL_TYPE=${MODEL_TYPE:"gpt2_medium"}
+export TPDEGREE=${1:1}
+export GPUNUM=${1:8}
+export PLACEMENT=${1:'cpu'}
+export USE_SHARD_INIT=${1:False}
+export BATCH_SIZE=${1:8}
+export MODEL_TYPE=${1:"gpt2_medium"}
 
 echo "${PLACEMENT} is placement"
+echo "${GPUNUM} is GPUNUM"
+echo "${BATCH_SIZE} is BATCH_SIZE"
 
 mkdir -p logs
 #torchrun --standalone --nproc_per_node=${GPUNUM} train_gpt_demo.py --tp_degree=${TPDEGREE} --model_type=${MODEL_TYPE} --batch_size=${BATCH_SIZE} --placement ${PLACEMENT} --shardinit ${USE_SHARD_INIT} --distplan ${DISTPAN} 2>&1 | tee ./logs/${MODEL_TYPE}_${DISTPAN}_gpu_${GPUNUM}_bs_${BATCH_SIZE}_tp_${TPDEGREE}.log
