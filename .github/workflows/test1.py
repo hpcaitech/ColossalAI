fddcabb9296ebd0957ad2e83d040dfede591c5c9
ziyuhuang123 <202476410@qq.com>
2022-12-30 17:53:14 +0800

    '01'

--- .github/workflows/test1.py
+++ .github/workflows/test1.py
@@ -2,14 +2,14 @@ import argparse
 
 # 初始化参数构造器
 parser = argparse.ArgumentParser()
-print('11')
+# print('11')
 # 在参数构造器中添加两个命令行参数
 parser.add_argument('--fileNameList', type=str, default='Siri')
 # parser.add_argument('--message', type=str, default=',Welcom to Python World!')
 
 # 获取所有的命令行参数
 args = parser.parse_args()
-print('22')
+# print('22')
 # end_name = str(args.filename).split(".")[-1]
 #
 # print('Hi ' + str(args.fileNameList) + str(args.message))
