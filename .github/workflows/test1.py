fa0f95745feaab4fba426b62fd5a85e44ec01f03
ziyuhuang123 <202476410@qq.com>
2022-12-30 15:52:12 +0800

    '01'

---
+++ .github/workflows/test1.py
@@ -0,0 +1,31 @@
+import argparse
+
+# 初始化参数构造器
+parser = argparse.ArgumentParser()
+# print('11')
+# 在参数构造器中添加两个命令行参数
+parser.add_argument('--fileNameList', type=str, default='Siri')
+# parser.add_argument('--message', type=str, default=',Welcom to Python World!')
+
+# 获取所有的命令行参数
+args = parser.parse_args()
+# print('22')
+# end_name = str(args.filename).split(".")[-1]
+#
+# print('Hi ' + str(args.fileNameList) + str(args.message))
+
+
+print(args.fileNameList)
+
+
+# name_list = args.fileNameList.split("&&&@@@")
+# # print(name_list, 'name_list')
+# folder_need_check = []
+# for loc in name_list:
+#     if loc.split("/")[0] == "examples":
+#         if loc.split("/")[1] + "/" + loc.split("/")[2] not in folder_need_check:
+#             folder_need_check.append(loc.split("/")[1] + "/" + loc.split("/")[2])
+#
+#
+# for i in folder_need_check:
+#     print(i, end=' ')
