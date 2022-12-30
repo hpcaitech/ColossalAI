a03538b12609e513ef1b711972c483c2c3db98b5
ziyuhuang123 <202476410@qq.com>
2022-12-30 16:35:06 +0800

    '01'

--- .github/workflows/test1.py
+++ .github/workflows/test1.py
@@ -22,7 +22,7 @@ name_list = args.fileNameList.split("&&&@@@")
 print(name_list, 'name_list')
 folder_need_check = []
 for loc in name_list:
-    if loc != 'tutorials':
+    if loc != 'tutorial' and loc != '':
         folder_need_check.append(loc)
 
 print(folder_need_check, 'folder_need_check')
