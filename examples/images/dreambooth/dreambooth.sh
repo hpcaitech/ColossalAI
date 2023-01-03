b77c36c93d1d2ac18301f9c74e64089e30bb2b5b
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2023-01-03 16:48:26 +0800

    create dreambooth.sh from baseline testing

---
+++ examples/images/dreambooth/dreambooth.sh
@@ -0,0 +1,12 @@
+python train_dreambooth.py \
+    --pretrained_model_name_or_path="/home/lczht/data2/stable-diffusion-v1-4" \
+    --instance_data_dir="/data2/users/lczht/diffusers/examples/dreambooth/input" \
+    --output_dir="path-to-save-model" \
+    --instance_prompt="a photo of sks dog" \
+    --resolution=512 \
+    --train_batch_size=1 \
+    --gradient_accumulation_steps=1 \
+    --learning_rate=5e-6 \
+    --lr_scheduler="constant" \
+    --lr_warmup_steps=0 \
+    --num_class_images=200 \
