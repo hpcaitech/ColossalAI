548cc82d3815784e29db847543edc91efda40a73
ziyuhuang123 <202476410@qq.com>
2023-01-03 13:49:57 +0800

    'hzy-example-autocheck-weekly-changed-manually'

--- .github/workflows/changed_file.sh
+++ .github/workflows/changed_file.sh
@@ -2,10 +2,10 @@
 
 res=$1
 res1=($res)
-echo "${res1} is res1"
+echo "${res1} is current location"
 for ii in ${res1[*]}; do
   cd "examples/${ii}"
   sh test_ci.sh
   cd ../../..
-  echo "${ii} haha"
+  echo "${ii} has been executed"
 done
