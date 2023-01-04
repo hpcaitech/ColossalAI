d1e5bafcd44ed93679b13965de40c5f38cda9060
Zangwei Zheng <zangwei@comp.nus.edu.sg>
2023-01-04 10:13:02 +0800

    [NFC] polish colossalai/auto_parallel/tensor_shard/deprecated/__init__.py code style (#2291)

--- colossalai/auto_parallel/tensor_shard/deprecated/__init__.py
+++ colossalai/auto_parallel/tensor_shard/deprecated/__init__.py
@@ -1,6 +1,6 @@
-from .options import SolverOptions
-from .strategies_constructor import StrategiesConstructor
-from .sharding_strategy import ShardingStrategy, StrategiesVector
 from .cost_graph import CostGraph
-from .solver import Solver
 from .graph_analysis import GraphAnalyser
+from .options import SolverOptions
+from .sharding_strategy import ShardingStrategy, StrategiesVector
+from .solver import Solver
+from .strategies_constructor import StrategiesConstructor
