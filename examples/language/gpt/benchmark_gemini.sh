a2239eab1efa94fb9520051599f56b923454ba65
1SAA <c2h214748@gmail.com>
2023-01-04 10:35:30 +0800

    [example] update gemini benchmark bash

--- examples/language/gpt/benchmark_gemini.sh
+++ examples/language/gpt/benchmark_gemini.sh
@@ -1,22 +1,20 @@
-for MODEL_TYPE in "gpt2_medium"
-do
-for BATCH_SIZE in 16
-do
-for GPUNUM in 1 2 4 8
-do
-for TPDEGREE in 1 2 4 8
-do
-if [ ${TPDEGREE} -gt ${GPUNUM} ]
-then
-    continue
-fi
-echo "****************** Begin ***************************"
-echo "* benchmrking MODEL_TYPE ${MODEL_TYPE} BS ${BATCH_SIZE} BS ${BS} GPUNUM ${GPUNUM} TPDEGREE ${TPDEGREE}"
-MODEL_TYPE=${MODEL_TYPE} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} TPDEGREE=${TPDEGREE} bash ./run_gemini.sh
-echo "****************** Finished ***************************"
-echo ""
-echo ""
-done
-done
-done
+for MODEL_TYPE in "gpt2_medium"; do
+  for BATCH_SIZE in 16; do
+    for GPUNUM in 1 2 4 8; do
+      for TPDEGREE in 1 2 4 8; do
+        if [ ${TPDEGREE} -gt ${GPUNUM} ]; then
+          continue
+        fi
+        for PLACEMENT in "cpu" "auto"; do
+          echo "****************** Begin ***************************"
+          echo "* benchmrking MODEL_TYPE ${MODEL_TYPE} BS ${BATCH_SIZE} BS ${BS} GPUNUM ${GPUNUM} TPDEGREE ${TPDEGREE} PLACEMENT ${PLACEMENT}"
+          MODEL_TYPE=${MODEL_TYPE} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} TPDEGREE=${TPDEGREE} PLACEMENT=${PLACEMENT} \
+          bash ./run_gemini.sh
+          echo "****************** Finished ***************************"
+          echo ""
+          echo ""
+        done
+      done
+    done
+  done
 done
