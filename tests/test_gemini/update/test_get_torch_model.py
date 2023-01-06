977eadcff6ec0a0d1134413b29bd598e66084bb8
1SAA <c2h214748@gmail.com>
2023-01-06 11:29:19 +0800

    [gemini] add get static torch model

--- tests/test_gemini/update/test_get_torch_model.py
+++ tests/test_gemini/update/test_get_torch_model.py
@@ -6,8 +6,9 @@ import torch
 import torch.multiprocessing as mp
 
 import colossalai
-from colossalai.nn.parallel.utils import convert_to_torch_module
-from colossalai.tensor import ColoTensor
+from colossalai.nn.parallel import GeminiDDP
+from colossalai.nn.parallel.utils import get_static_torch_model
+from colossalai.tensor import ColoParameter
 from colossalai.testing import parameterize, rerun_if_address_is_in_use
 from colossalai.utils import free_port
 from colossalai.utils.cuda import get_current_device
@@ -15,21 +16,29 @@ from colossalai.utils.model.colo_init_context import ColoInitContext
 from tests.components_to_test.registry import non_distributed_component_funcs
 
 
-@parameterize('model_name', ['resnet18', 'bert'])
+@parameterize('model_name', ['hanging_param_model', 'resnet18', 'gpt2'])
 def run_convert_torch_module(model_name: str):
     get_components_func = non_distributed_component_funcs.get_callable(model_name)
     model_builder, _, _, _, _ = get_components_func()
 
-    with ColoInitContext(device='cpu'):
+    with ColoInitContext(device=torch.device("cpu")):
         model = model_builder(checkpoint=False)
-
-    from colossalai.nn.parallel import GeminiDDP
     model = GeminiDDP(model, device=get_current_device(), placement_policy='auto', pin_memory=True)
-
-    pytorch_model = convert_to_torch_module(model)
+    pytorch_model = get_static_torch_model(model, only_rank_0=False)
 
     for n, p in pytorch_model.named_parameters():
-        assert not isinstance(p, ColoTensor)
+        assert type(p) == torch.nn.Parameter, f"type error: {n} is a {type(p)}"
+
+    # get the static model should not change the original model
+    for n, p in model.named_parameters():
+        assert isinstance(p, ColoParameter)
+
+    for (pn, pm), (cn, cm) in zip(pytorch_model.named_modules(), model.named_modules()):
+        assert pn == cn
+        assert id(pm) != id(cm)
+        for pp, cp in zip(pm.parameters(recurse=False), cm.parameters(recurse=False)):
+            assert id(pp) != id(cp)
+            assert pp.shape == cp.shape
 
 
 def run_dist(rank, world_size, port):
