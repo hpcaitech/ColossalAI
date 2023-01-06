37e5652b2cca5926254932e496743fe822f05a00
ziyuhuang123 <202476410@qq.com>
2023-01-06 17:34:50 +0800

    '1'

--- .github/workflows/scripts/changed_example.py
+++ .github/workflows/scripts/changed_example.py
@@ -12,7 +12,10 @@ def main():
         if loc.split("/")[0] == "examples" and len(loc.split("/")) >= 4:
             folder_need_check.add(loc.split("/")[1] + "/" + loc.split("/")[2])
     # Output the result using print. Then the shell can get the values.
-    print(list(folder_need_check))
+    if len(folder_need_check) == 1 and folder_need_check[0] == '':
+        print('no_file_to_check')
+    else:
+        print(list(folder_need_check))
 
 
 if __name__ == '__main__':
