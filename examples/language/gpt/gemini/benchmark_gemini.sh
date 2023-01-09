31ce17fea7ab691436709830a2f4c2a90f2aa178
1SAA <c2h214748@gmail.com>
2023-01-09 17:24:06 +0800

    change config

--- examples/language/gpt/gemini/benchmark_gemini.sh
+++ examples/language/gpt/gemini/benchmark_gemini.sh
@@ -1,6 +1,6 @@
-for MODEL_TYPE in "gpt2_medium"; do
-  for DISPAN in "zero1" "zero2"; do
-    for BATCH_SIZE in 8; do
+for MODEL_TYPE in "gpt2_10b"; do
+  for DISTPAN in "zero1" "zero2"; do
+    for BATCH_SIZE in 8 16 32; do
       for GPUNUM in 1 2 4 8; do
         for TPDEGREE in 1; do
           if [ ${TPDEGREE} -gt ${GPUNUM} ]; then
@@ -8,8 +8,8 @@ for MODEL_TYPE in "gpt2_medium"; do
           fi
           for PLACEMENT in "auto"; do
             echo "****************** Begin ***************************"
-            echo "+ benchmrking MODEL ${MODEL_TYPE} DISPAN ${DISPAN} GPU ${GPUNUM} BS ${BATCH_SIZE} TP ${TPDEGREE} POLICY ${PLACEMENT}"
-            MODEL_TYPE=${MODEL_TYPE} DISPAN=${DISPAN} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} TPDEGREE=${TPDEGREE} PLACEMENT=${PLACEMENT} \
+            echo "+ benchmrking MODEL ${MODEL_TYPE} DISTPAN ${DISTPAN} GPU ${GPUNUM} BS ${BATCH_SIZE} TP ${TPDEGREE} POLICY ${PLACEMENT}"
+            MODEL_TYPE=${MODEL_TYPE} DISTPAN=${DISTPAN} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} TPDEGREE=${TPDEGREE} PLACEMENT=${PLACEMENT} \
             bash ./run_gemini.sh
             echo "****************** Finished ***************************"
             echo ""
