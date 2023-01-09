667f47969f748798a9c912a46f54bd4983fd3ba1
1SAA <c2h214748@gmail.com>
2023-01-09 16:10:02 +0800

    change the benchmark config file

--- examples/language/gpt/gemini/benchmark_gemini.sh
+++ examples/language/gpt/gemini/benchmark_gemini.sh
@@ -1,12 +1,12 @@
 for MODEL_TYPE in "gpt2_medium"; do
-  for DISPAN in "colossalai"; do
-    for BATCH_SIZE in 16; do
+  for DISPAN in "zero1" "zero2"; do
+    for BATCH_SIZE in 8; do
       for GPUNUM in 1 2 4 8; do
-        for TPDEGREE in 1 2 4 8; do
+        for TPDEGREE in 1; do
           if [ ${TPDEGREE} -gt ${GPUNUM} ]; then
             continue
           fi
-          for PLACEMENT in "cpu" "auto"; do
+          for PLACEMENT in "auto"; do
             echo "****************** Begin ***************************"
             echo "+ benchmrking MODEL ${MODEL_TYPE} DISPAN ${DISPAN} GPU ${GPUNUM} BS ${BATCH_SIZE} TP ${TPDEGREE} POLICY ${PLACEMENT}"
             MODEL_TYPE=${MODEL_TYPE} DISPAN=${DISPAN} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} TPDEGREE=${TPDEGREE} PLACEMENT=${PLACEMENT} \
