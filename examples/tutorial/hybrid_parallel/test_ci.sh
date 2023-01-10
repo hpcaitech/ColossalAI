27b02c07d50728c53fff24fc020c0e8903ef2d19
FrankLeeeee <somerlee.9@gmail.com>
2023-01-10 11:13:01 +0800

    polish code

--- examples/tutorial/hybrid_parallel/test_ci.sh
+++ examples/tutorial/hybrid_parallel/test_ci.sh
@@ -3,5 +3,4 @@ set -euxo pipefail
 
 # pip install -r requirements.txt
 
-python -c "import titans"
-# colossalai run --nproc_per_node 4 train.py --config config.py -s
+torchrun --standalone --nproc_per_node 4 train.py --config config.py -s
