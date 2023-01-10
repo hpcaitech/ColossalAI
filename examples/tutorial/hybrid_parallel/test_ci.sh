21f85e26b994147c0a764cafbb05ec94ee6e753a
FrankLeeeee <somerlee.9@gmail.com>
2023-01-10 10:58:02 +0800

    polish code

--- examples/tutorial/hybrid_parallel/test_ci.sh
+++ examples/tutorial/hybrid_parallel/test_ci.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-set -e
+set -euxo pipefail
+
 pip install -r requirements.txt
 
 colossalai run --nproc_per_node 4 train.py --config config.py -s
-
-ret=$?
-if [ $ret -ne 0 ]; then
-    "This example failed, please fix the bugs above."
-    exit -1
-fi
