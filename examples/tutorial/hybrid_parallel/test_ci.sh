7b5595bf6cddc544a684709153da6a77d7fee710
FrankLeeeee <somerlee.9@gmail.com>
2023-01-10 10:50:40 +0800

    polish code

--- examples/tutorial/hybrid_parallel/test_ci.sh
+++ examples/tutorial/hybrid_parallel/test_ci.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 colossalai run --nproc_per_node 4 train.py --config config.py -s
+
+ret=$?
+if [ $ret -ne 0 ]; then
+    "This example failed, please fix the bugs above."
+    exit -1
+fi
