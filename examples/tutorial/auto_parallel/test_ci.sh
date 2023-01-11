cddb282d45ccb3396ef372b4bb4d0a9bfde2b01e
YuliangLiu0306 <liuyuliang@luchentech.com>
2023-01-11 17:57:23 +0800

    polish

--- examples/tutorial/auto_parallel/test_ci.sh
+++ examples/tutorial/auto_parallel/test_ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# set -euxo pipefail
+set -euxo pipefail
 
-# pip install pulp
-# conda install coin-or-cbc
+pip install titans
+pip install pulp
+conda install coin-or-cbc
 colossalai run --nproc_per_node 4 auto_parallel_with_resnet.py -s
