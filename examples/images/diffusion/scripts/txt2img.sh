f35326881c84398bf40078f401ca95a14df66be1
Fazzie <1240419984@qq.com>
2023-01-31 10:00:37 +0800

    fix README

--- examples/images/diffusion/scripts/txt2img.sh
+++ examples/images/diffusion/scripts/txt2img.sh
@@ -1,6 +1,5 @@
 python scripts/txt2img.py --prompt "Teyvat, Name:Layla, Element: Cryo, Weapon:Sword, Region:Sumeru, Model type:Medium Female, Description:a woman in a blue outfit holding a sword" --plms \
     --outdir ./output \
-    --config /home/lcmql/data2/Genshin/2022-11-18T16-38-46_train_colossalai_teyvattest/checkpoints/last.ckpt \
-    --ckpt /home/lcmql/data2/Genshin/2022-11-18T16-38-46_train_colossalai_teyvattest/configs/2022-11-18T16-38-46-project.yaml  \
+    --ckpt /tmp/2022-11-18T16-38-46_train_colossalai/checkpoints/last.ckpt \
+    --config /tmp/2022-11-18T16-38-46_train_colossalai/configs/2022-11-18T16-38-46-project.yaml  \
     --n_samples 4
-
