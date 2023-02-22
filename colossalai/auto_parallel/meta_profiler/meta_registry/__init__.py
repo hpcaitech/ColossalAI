eae77c831d93aa0190bf3af4cc87ff9a153851d2
Boyuan Yao <70263930+Cypher30@users.noreply.github.com>
2023-02-22 10:28:56 +0800

    [autoparallel] Patch meta information for nodes that will not be handled by SPMD solver (#2823)
    * [autoparallel] non spmd meta information generator
    
    * [autoparallel] patch meta information for non spmd nodes

--- colossalai/auto_parallel/meta_profiler/meta_registry/__init__.py
+++ colossalai/auto_parallel/meta_profiler/meta_registry/__init__.py
@@ -3,6 +3,7 @@ from .binary_elementwise_ops import *
 from .conv import *
 from .embedding import *
 from .linear import *
+from .non_spmd import *
 from .norm import *
 from .pooling import *
 from .tensor import *
