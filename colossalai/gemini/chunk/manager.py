14efa1e556bfa7943e1a5eb784f7a0e26ece006a
HELSON <c2h214748@gmail.com>
2023-02-22 13:59:14 +0800

    [hotfix] fix chunk size can not be divided

--- colossalai/gemini/chunk/manager.py
+++ colossalai/gemini/chunk/manager.py
@@ -72,6 +72,9 @@ class ChunkManager:
 
             if tensor.numel() > chunk_size:
                 chunk_size = tensor.numel()
+                dp_size = tensor.process_group.dp_world_size()
+                chunk_size = chunk_size + (-chunk_size % dp_size)
+
             chunk = Chunk(
                 chunk_size=chunk_size,
                 process_group=tensor.process_group,
