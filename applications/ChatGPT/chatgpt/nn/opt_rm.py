2e16f842a9e5b1fb54e7e41070e9d2bb5cd64d7c
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2023-02-22 16:58:11 +0800

    [chatgpt]support opt & gpt for rm training (#2876)

--- applications/ChatGPT/chatgpt/nn/opt_rm.py
+++ applications/ChatGPT/chatgpt/nn/opt_rm.py
@@ -1,8 +1,7 @@
 from typing import Optional
 
 import torch.nn as nn
-from transformers.models.opt.configuration_opt import OPTConfig
-from transformers.models.opt.modeling_opt import OPTModel
+from transformers import OPTConfig, OPTModel
 
 from .reward_model import RewardModel
 
@@ -14,6 +13,7 @@ class OPTRM(RewardModel):
     Args:
         pretrained (str): Pretrained model name or path.
         config (OPTConfig): Model config.
+        checkpoint (bool): Enable gradient checkpointing.
         lora_rank (int): Rank of the low-rank approximation.
         lora_train_bias (str): LoRA bias training mode.
     """
@@ -21,6 +21,7 @@ class OPTRM(RewardModel):
     def __init__(self,
                  pretrained: Optional[str] = None,
                  config: Optional[OPTConfig] = None,
+                 checkpoint: bool = False,
                  lora_rank: int = 0,
                  lora_train_bias: str = 'none') -> None:
         if pretrained is not None:
@@ -29,5 +30,8 @@ class OPTRM(RewardModel):
             model = OPTModel(config)
         else:
             model = OPTModel(OPTConfig())
-        value_head = nn.Linear(model.config.hidden_size, 1)
+        if checkpoint:
+            model.gradient_checkpointing_enable()
+
+        value_head = nn.Linear(model.config.word_embed_proj_dim, 1)
         super().__init__(model, value_head, lora_rank, lora_train_bias)
