e4920a58a6cc1bf6d5427de90f5538cf8c491050
yangwjd <yangwjd@chanjet.com>
2023-03-08 00:44:35 +0800

    fix neglect of attention mask

--- applications/ChatGPT/chatgpt/models/base/critic.py
+++ applications/ChatGPT/chatgpt/models/base/critic.py
@@ -41,7 +41,7 @@ class Critic(LoRAModule):
         if action_mask is not None:
             num_actions = action_mask.size(1)
             prompt_mask = attention_mask[:, :-num_actions]
-            values = values[:, :num_actions]
+            values = values[:, :-num_actions]
             value = masked_mean(values, prompt_mask, dim=1)
             return value
         values = values[:, :-1]
