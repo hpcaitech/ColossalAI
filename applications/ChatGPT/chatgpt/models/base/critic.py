c39dca8c844310e1e6ebe61f7f7b1c497a78c845
yangwenjun <yangwenjun@soyoung.com>
2023-03-07 19:44:54 +0800

    fix Critic

--- applications/ChatGPT/chatgpt/models/base/critic.py
+++ applications/ChatGPT/chatgpt/models/base/critic.py
@@ -39,11 +39,10 @@ class Critic(LoRAModule):
         values = self.value_head(last_hidden_states).squeeze(-1)
 
         if action_mask is not None:
-            prompt_mask = torch.ones_like(values)
+            prompt_mask = torch.ones_like(values, dtype=torch.bool)
             num_actions = action_mask.size(1)
-            prompt_mask[:, -(num_actions+1):] = 0
+            prompt_mask[:, -(num_actions+1):] = False
             value = masked_mean(values, prompt_mask, dim=1)
             return value
         values = values[:, :-1]
         value = values.mean(dim=1).squeeze(1)
-        return value
