fix neglect of attention mask

applications/ChatGPT/chatgpt/models/base/critic.py
@@ -39,9 +39,9 @@ class Critic(LoRAModule):
         values = self.value_head(last_hidden_states).squeeze(-1)
 
         if action_mask is not None:
-            prompt_mask = torch.ones_like(values, dtype=torch.bool)
             num_actions = action_mask.size(1)
-            prompt_mask[:, -(num_actions+1):] = False
+            prompt_mask = attention_mask[:, :-(num_actions + 1)]
+            values = values[:, :-(num_actions + 1)]
             value = masked_mean(values, prompt_mask, dim=1)
             return value
         values = values[:, :-1]