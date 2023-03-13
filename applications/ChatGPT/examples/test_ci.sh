42d4bc7127fcafecf86e6a3067fec4cb1b284beb
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2023-03-13 12:17:51 +0800

    polish

--- applications/ChatGPT/examples/test_ci.sh
+++ applications/ChatGPT/examples/test_ci.sh
@@ -51,21 +51,21 @@ torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_zero2 --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'facebook/opt-350m' --model opt --lora_rank 4\
-         --save_path ${BASE}/actor_checkpoint_dummy.pt
+         --save_path ${BASE}/actor_checkpoint_prompts.pt
 python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'facebook/opt-350m' --model opt
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_gemini --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'bigscience/bloom-560m' --model bloom --lora_rank 4\
-         --save_path ${BASE}/actor_checkpoint_dummy.pt
+         --save_path ${BASE}/actor_checkpoint_prompts.pt
 python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'bigscience/bloom-560m' --model bloom
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_gemini --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'gpt2' --model gpt2 --lora_rank 4\
-         --save_path ${BASE}/actor_checkpoint_dummy.pt
+         --save_path ${BASE}/actor_checkpoint_prompts.pt
 python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'gpt2' --model gpt2
 
 rm -rf ${BASE}/actor_checkpoint_prompts.pt
