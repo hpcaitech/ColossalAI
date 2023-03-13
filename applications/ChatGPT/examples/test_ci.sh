3e1fd3a26e0a70da9b17b265aa727d1ed6435a0f
BlueRum <70618399+ht-zhou@users.noreply.github.com>
2023-03-13 11:30:29 +0800

    Update test_ci.sh

--- applications/ChatGPT/examples/test_ci.sh
+++ applications/ChatGPT/examples/test_ci.sh
@@ -18,29 +18,24 @@ pip install -r ${BASE}/requirements.txt
 python ${BASE}/train_dummy.py --strategy naive --num_episodes 1 \
                               --max_timesteps 2 --update_timesteps 2 \
                               --max_epochs 1 --train_batch_size 2 --lora_rank 4
-for strategy in ddp colossalai_gemini colossalai_zero2; do
-    torchrun --standalone --nproc_per_node=2 ${BASE}/train_dummy.py \
-             --strategy ${strategy} --num_episodes 1 --max_timesteps 2 \
-             --update_timesteps 2 --max_epochs 1 --train_batch_size 2 --lora_rank 4
-done
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_dummy.py \
          --strategy colossalai_gemini --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'facebook/opt-350m' --model opt --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'facebook/opt-350m' --model opt
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'facebook/opt-350m' --model opt
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_dummy.py \
          --strategy colossalai_zero2 --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'bigscience/bloom-560m' --model bloom --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'bigscience/bloom-560m' --model bloom
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'bigscience/bloom-560m' --model bloom
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_dummy.py \
          --strategy colossalai_zero2 --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'gpt2' --model gpt2 --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'gpt2' --model gpt2
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_dummy.pt --pretrain 'gpt2' --model gpt2
 
 rm -rf ${BASE}/actor_checkpoint_dummy.pt
 
@@ -48,28 +43,23 @@ rm -rf ${BASE}/actor_checkpoint_dummy.pt
 python ${BASE}/train_prompts.py $PROMPT_PATH --strategy naive --num_episodes 1 \
                                              --max_timesteps 2 --update_timesteps 2 \
                                              --max_epochs 1 --train_batch_size 2 --lora_rank 4
-for strategy in ddp colossalai_gemini colossalai_zero2; do
-    torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
-             --strategy ${strategy} --num_episodes 1 --max_timesteps 2 --update_timesteps 2 \
-             --max_epochs 1 --train_batch_size 2 --lora_rank 4
-done
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_zero2 --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'facebook/opt-350m' --model opt --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'facebook/opt-350m' --model opt
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'facebook/opt-350m' --model opt
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_gemini --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'bigscience/bloom-560m' --model bloom --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'bigscience/bloom-560m' --model bloom
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'bigscience/bloom-560m' --model bloom
 
 torchrun --standalone --nproc_per_node=2 ${BASE}/train_prompts.py $PROMPT_PATH \
          --strategy colossalai_gemini --num_episodes 1 --max_timesteps 2 \
          --update_timesteps 2 --max_epochs 1 --train_batch_size 2\
          --pretrain 'gpt2' --model gpt2 --lora_rank 4
-python inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'gpt2' --model gpt2
+python ${BASE}/inference.py --model_path ${BASE}/actor_checkpoint_prompts.pt --pretrain 'gpt2' --model gpt2
 
 rm -rf ${BASE}/actor_checkpoint_prompts.pt
