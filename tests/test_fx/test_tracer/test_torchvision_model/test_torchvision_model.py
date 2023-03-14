dc8e3734e08d0939ad5357768076d4d5ee617af8
FrankLeeeee <somerlee.9@gmail.com>
2023-03-14 15:07:04 +0800

    polish code

--- tests/test_fx/test_tracer/test_torchvision_model/test_torchvision_model.py
+++ tests/test_fx/test_tracer/test_torchvision_model/test_torchvision_model.py
@@ -8,7 +8,7 @@ def test_torchvision_models():
     torch.backends.cudnn.deterministic = True
     tv_sub_registry = model_zoo.get_sub_registry('torchvision')
 
-    for name, (model_fn, data_gen_fn, model_attribute) in tv_sub_registry.items():
+    for name, (model_fn, data_gen_fn, output_transform_fn, model_attribute) in tv_sub_registry.items():
         data = data_gen_fn()
 
         if model_attribute is not None and model_attribute.has_stochastic_depth_prob:
@@ -21,11 +21,23 @@ def test_torchvision_models():
         model.eval()
         gm.eval()
 
-        with torch.no_grad():
-            fx_out = gm(data)
-            non_fx_out = model(data)
-        assert torch.allclose(
-            fx_out, non_fx_out), f'{model.__class__.__name__} has inconsistent outputs, {fx_out} vs {non_fx_out}'
+        try:
+            with torch.no_grad():
+                fx_out = gm(**data)
+                non_fx_out = model(**data)
+                transformed_out = output_transform_fn(fx_out)
+                transformed_non_fx_out = output_transform_fn(non_fx_out)
+
+            assert len(transformed_out) == len(transformed_non_fx_out)
+
+            for key in transformed_out.keys():
+                fx_val = transformed_out[key]
+                non_fx_val = transformed_non_fx_out[key]
+                assert torch.allclose(
+                    fx_val,
+                    non_fx_val), f'{model.__class__.__name__} has inconsistent outputs, {fx_val} vs {non_fx_val}'
+        except Exception as e:
+            print(name, e)
 
 
 if __name__ == '__main__':
