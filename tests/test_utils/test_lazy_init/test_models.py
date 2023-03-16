8d19462c8895a19f7c659ece099887a259490ca6
ver217 <lhx0217@gmail.com>
2023-03-16 14:53:05 +0800

    [tests] refactor lazy init test models

---
+++ tests/test_utils/test_lazy_init/test_models.py
@@ -0,0 +1,15 @@
+import pytest
+from utils import check_lazy_init
+
+from tests.kit.model_zoo import model_zoo
+
+
+@pytest.mark.parametrize('subset', ['torchvision', 'diffusers'])
+def test_torchvision_models_lazy_init(subset):
+    sub_model_zoo = model_zoo.get_sub_registry(subset)
+    for name, entry in sub_model_zoo.items():
+        check_lazy_init(entry, verbose=True)
+
+
+if __name__ == '__main__':
+    test_torchvision_models_lazy_init('torchvision')
