101fb0cd71b3c1041ccc95b171c2e9c248edc23f
FrankLeeeee <somerlee.9@gmail.com>
2023-03-17 17:17:04 +0800

    [test] fixed torchrec model test

--- tests/test_booster/test_mixed_precision/test_fp16_torch.py
+++ tests/test_booster/test_mixed_precision/test_fp16_torch.py
@@ -7,11 +7,17 @@ from tests.kit.model_zoo import model_zoo
 
 def test_torch_amp():
     for name, (model_fn, data_gen_fn, output_transform_fn, _) in model_zoo.items():
+        # dlrm_interactionarch has not parameters, so skip
+        if name == 'dlrm_interactionarch':
+            continue
+
         model = model_fn().cuda()
         optimizer = Adam(model.parameters(), lr=1e-3)
         criterion = lambda x: x.mean()
         data = data_gen_fn()
-        data = {k: v.cuda() if torch.is_tensor(v) else v for k, v in data.items()}
+        data = {
+            k: v.to('cuda') if torch.is_tensor(v) or 'Tensor' in v.__class__.__name__ else v for k, v in data.items()
+        }
         mixed_precision = FP16TorchMixedPrecision()
         model, optimizer, criterion = mixed_precision.configure(model, optimizer, criterion)
         output = model(**data)
