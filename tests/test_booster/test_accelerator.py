e7f3bed2d36c5406e9a9ab92438be46a5f9258d7
Frank Lee <somerlee.9@gmail.com>
2023-03-21 17:39:30 +0800

    [booster] added the plugin base and torch ddp plugin (#3180)
    * [booster] added the plugin base and torch ddp plugin
    
    * polish code
    
    * polish code
    
    * polish code

--- tests/test_booster/test_accelerator.py
+++ tests/test_booster/test_accelerator.py
@@ -1,13 +1,27 @@
-import pytest
+from functools import partial
+
+import torch.multiprocessing as mp
 import torch.nn as nn
-from torchvision.models import resnet18
 
 from colossalai.booster.accelerator import Accelerator
+from colossalai.testing import parameterize, rerun_if_address_is_in_use
 
 
-@pytest.mark.parametrize('device', ['cpu', 'cuda'])
-def test_accelerator(device):
+@parameterize('device', ['cpu', 'cuda'])
+def run_accelerator(device):
     acceleartor = Accelerator(device)
     model = nn.Linear(8, 8)
     model = acceleartor.configure_model(model)
     assert next(model.parameters()).device.type == device
+    del model, acceleartor
+
+
+def run_dist(rank):
+    run_accelerator()
+
+
+@rerun_if_address_is_in_use()
+def test_accelerator():
+    world_size = 1
+    run_func = partial(run_dist)
+    mp.spawn(run_func, nprocs=world_size)
