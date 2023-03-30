a376086e1a85e02b68e8a92c39ff58b4e2be9965
jiangwen <zxl265370@antgroup.com>
2023-03-30 13:34:56 +0800

    [fix]add dependencies

--- applications/Chat/examples/train_prompts_on_ray_job_submission_script.py
+++ applications/Chat/examples/train_prompts_on_ray_job_submission_script.py
@@ -5,6 +5,10 @@ client.submit_job(
     entrypoint=
     "python examples/train_prompts_on_ray.py --strategy colossalai_zero2 --prompt_csv_url https://huggingface.co/datasets/fka/awesome-chatgpt-prompts/resolve/main/prompts.csv",
     runtime_env={
-        "working_dir": "../",
-        "pip": ["torch==1.13.1", "transformers>=4.20.1", "datasets", "loralib", "colossalai>=0.2.4"]
+        "working_dir":
+            "../",
+        "pip": [
+            "torch==1.13.1", "transformers>=4.20.1", "datasets", "loralib", "colossalai>=0.2.4", "langchain",
+            "tokenizers", "fastapi", "sse_starlette", "wandb", "sentencepiece", "gpustat"
+        ]
     })
