f200cebaed3fc944136823d3f579b48abff1c74a
jiangwen <zxl265370@antgroup.com>
2023-04-03 23:05:52 +0800

    [fix]file path

--- applications/Chat/experimental/ray/ray_job_script.py
+++ applications/Chat/experimental/ray/ray_job_script.py
@@ -7,7 +7,7 @@ def main(api_server_endpoint="http://127.0.0.1:8265"):
     client = JobSubmissionClient(api_server_endpoint)
     client.submit_job(
         entrypoint=
-        "python examples/train_prompts_on_ray.py --strategy colossalai_zero2 --prompt_csv_url https://huggingface.co/datasets/fka/awesome-chatgpt-prompts/resolve/main/prompts.csv",
+        "python experimental/ray/train_prompts_on_ray.py --strategy colossalai_zero2 --prompt_csv_url https://huggingface.co/datasets/fka/awesome-chatgpt-prompts/resolve/main/prompts.csv",
         runtime_env={
             "working_dir":
                 "../../",
