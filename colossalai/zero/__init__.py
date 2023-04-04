b95e7c066f88c3005f9a0b80f6ff8a9a700ccce2
ver217 <lhx0217@gmail.com>
2023-04-04 12:25:15 +0800

    [zero] update import

--- colossalai/zero/__init__.py
+++ colossalai/zero/__init__.py
@@ -1,8 +1,16 @@
-from .gemini import ColoInitContext, GeminiAdamOptimizer, GeminiDDP, ZeroDDP, ZeroOptimizer, post_process_colo_init_ctx
+from .gemini import (
+    ColoInitContext,
+    GeminiAdamOptimizer,
+    GeminiDDP,
+    ZeroDDP,
+    ZeroOptimizer,
+    get_static_torch_model,
+    post_process_colo_init_ctx,
+)
 from .low_level import LowLevelZeroOptimizer
 from .wrapper import zero_model_wrapper, zero_optim_wrapper
 
 __all__ = [
     'ZeroDDP', 'GeminiDDP', 'ZeroOptimizer', 'GeminiAdamOptimizer', 'zero_model_wrapper', 'zero_optim_wrapper',
-    'LowLevelZeroOptimizer', 'ColoInitContext', 'post_process_colo_init_ctx'
+    'LowLevelZeroOptimizer', 'ColoInitContext', 'post_process_colo_init_ctx', 'get_static_torch_model'
 ]
