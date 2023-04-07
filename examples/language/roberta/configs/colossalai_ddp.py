e026c227f81ea558692ea1da75264d365f575170
mandoxzhang <111039218+mandoxzhang@users.noreply.github.com>
2023-04-07 10:34:51 +0800

    [example] update roberta with newer ColossalAI (#3472)
    * update roberta example
    
    * update roberta example

--- examples/language/roberta/configs/colossalai_ddp.py
+++
@@ -1,9 +0,0 @@
-from colossalai.nn.optimizer import FusedAdam
-
-try:
-    from colossalai.zero.shard_utils import TensorShardStrategy
-except ImportError:
-    # colossalai > 0.2.8
-    from colossalai.zero.legacy import TensorShardStrategy
-
-clip_grad_norm = 1.0
