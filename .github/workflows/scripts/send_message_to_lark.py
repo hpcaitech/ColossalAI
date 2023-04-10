bae51620e5a4db06801ec598a3c28641ba93317c
digger-yu <digger@meshbox.io>
2023-04-10 15:56:34 +0800

    Update send_message_to_lark.py
    Optimization Code
    
    1. Remove the parse_args() function and directly access the command line arguments using sys.argv. This will eliminate the need for the argparse module and make the code simpler.
    2. Use the json parameter of the requests.post() function to directly pass the message data as a dictionary instead of creating a separate dictionary and then passing it as a JSON payload.

--- .github/workflows/scripts/send_message_to_lark.py
+++ .github/workflows/scripts/send_message_to_lark.py
@@ -1,20 +1,11 @@
-import argparse
-
+import sys
 import requests
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--message', type=str)
-    parser.add_argument('-u', '--url', type=str)
-    return parser.parse_args()
-
 
 def send_message_to_lark(message, webhook_url):
     data = {"msg_type": "text", "content": {"text": message}}
     requests.post(webhook_url, json=data)
 
-
 if __name__ == '__main__':
-    args = parse_args()
-    send_message_to_lark(args.message, args.url)
+    message = sys.argv[1]
+    webhook_url = sys.argv[2]
+    send_message_to_lark(message, webhook_url)
