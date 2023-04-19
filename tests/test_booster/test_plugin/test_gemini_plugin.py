bc579cad8fa4c2d4278321640ab99ec194a61241
ver217 <lhx0217@gmail.com>
2023-04-19 17:19:28 +0800

    [test] fix booster plugin test oom

--- tests/test_booster/test_plugin/test_gemini_plugin.py
+++ tests/test_booster/test_plugin/test_gemini_plugin.py
@@ -150,7 +150,7 @@ def run_dist(rank, world_size, port, early_stop: bool = True):
 
 @rerun_if_address_is_in_use()
 def test_gemini_plugin(early_stop: bool = True):
-    spawn(run_dist, 2, early_stop=early_stop)
+    spawn(run_dist, 4, early_stop=early_stop)
 
 
 if __name__ == '__main__':
