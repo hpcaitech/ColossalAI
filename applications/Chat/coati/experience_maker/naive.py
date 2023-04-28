3f2b8a069e2d37aacdb296812ea12570023b6482
zhang-yi-chi <673865549@qq.com>
2023-04-28 13:22:47 +0800

    fix gemini strategy bug

--- applications/Chat/coati/experience_maker/naive.py
+++ applications/Chat/coati/experience_maker/naive.py
@@ -1,6 +1,8 @@
 import torch
 from coati.models.utils import compute_reward, normalize
 
+from colossalai.tensor import ColoTensor
+
 from .base import Experience, ExperienceMaker
 
 
@@ -22,9 +24,15 @@ class NaiveExperienceMaker(ExperienceMaker):
         num_actions = action_mask.size(1)
 
         action_log_probs = self.actor(sequences, num_actions, attention_mask)
-        base_action_log_probs = self.initial_model(sequences, num_actions, attention_mask)
         value = self.critic(sequences, action_mask, attention_mask)
-        r = self.reward_model(sequences, attention_mask)
+        if isinstance(sequences, ColoTensor):
+            torch_sequences = sequences.data.to(torch.cuda.current_device())
+            torch_attention_mask = attention_mask.data.to(torch.cuda.current_device())
+            base_action_log_probs = self.initial_model(torch_sequences, num_actions, torch_attention_mask)
+            r = self.reward_model(torch_sequences, torch_attention_mask)
+        else:
+            base_action_log_probs = self.initial_model(sequences, num_actions, attention_mask)
+            r = self.reward_model(sequences, attention_mask)
         reward = compute_reward(r, self.kl_coef, action_log_probs, base_action_log_probs, action_mask=action_mask)
 
         advantage = reward - value
