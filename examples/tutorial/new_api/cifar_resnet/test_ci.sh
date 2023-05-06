23c1c2ed105f289e69eb730c91119af345ebe0c0
ver217 <lhx0217@gmail.com>
2023-05-06 16:07:59 +0800

    [example] add train resnet with booster example

---
+++ examples/tutorial/new_api/cifar_resnet/test_ci.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xe
+
+export DATA=/data/scratch/cifar-10
+
+for plugin in "torch_ddp" "torch_ddp_fp16" "low_level_zero"; do
+    colossalai run --nproc_per_node 4 train.py --interval 0 --target_acc 0.84 --plugin $plugin
+done
