99c542a291c17f531c5c4817156bdbbb2e1c1292
ver217 <lhx0217@gmail.com>
2023-05-18 19:34:32 +0800

    [test] fix low level zero plugin test

--- tests/test_booster/test_plugin/test_low_level_zero_plugin.py
+++ tests/test_booster/test_plugin/test_low_level_zero_plugin.py
@@ -13,7 +13,7 @@ from tests.kit.model_zoo import model_zoo
 # These models are not compatible with AMP
 _AMP_ERR_MODELS = ['timm_convit', 'dlrm', 'deepfm_interactionarch', 'deepfm_simpledeepfmnn']
 # These models have no parameters
-_LOW_LEVEL_ZERO_ERR_MODELS = ['dlrm_interactionarch', 'deepfm_overarch', 'deepfm_sparsearch']
+_LOW_LEVEL_ZERO_ERR_MODELS = ['dlrm_interactionarch', 'deepfm_overarch', 'deepfm_sparsearch', 'dlrm_sparsearch']
 # These models will get stuck
 _STUCK_MODELS = [
     'diffusers_vq_model', 'transformers_albert', 'transformers_albert_for_pretraining', 'transformers_bert',
