08de5dad15ebb145a6528e6870834d8e1aabad61
Mingyan Jiang <1829166702@qq.com>
2023-05-19 18:15:12 +0800

    [api] add docstring to apex amp/ naive amp

--- colossalai/booster/mixed_precision/fp16_apex.py
+++ colossalai/booster/mixed_precision/fp16_apex.py
@@ -9,18 +9,18 @@ class FP16ApexMixedPrecision(MixedPrecision):
 
     Args:
         opt_level(str, optional, default="O1" ): Pure or mixed precision optimization level.
-Accepted values are “O0”, “O1”, “O2”, and “O3”, explained in detail above Apex AMP Documentation.
+            Accepted values are “O0”, “O1”, “O2”, and “O3”, explained in detail above Apex AMP Documentation.
         num_losses(int, optional, default=1): Option to tell AMP in advance how many losses/backward passes you plan to use.
-When used in conjunction with the loss_id argument to `amp.scale_loss`, enables Amp to use a different loss scale per
-loss/backward pass, which can improve stability. If num_losses is left to 1, Amp will still support multiple
-losses/backward passes, but use a single global loss scale for all of them.
+            When used in conjunction with the loss_id argument to `amp.scale_loss`, enables Amp to use a different loss scale per loss/backward pass,
+            which can improve stability. If num_losses is left to 1, Amp will still support multiple losses/backward passes,
+            but use a single global loss scale for all of them.
         verbosity(int, default=1): Set to 0 to suppress Amp-related output.
         min_loss_scale(float, default=None): Sets a floor for the loss scale values that can be chosen by dynamic loss scaling.
-The default value of None means that no floor is imposed. If dynamic loss scaling is not used, min_loss_scale is ignored.
-        max_loss_scale(float, default=2.**24 ): Sets a ceiling for the loss scale values that can be chosen by dynamic loss
-scaling. If dynamic loss scaling is not used, max_loss_scale is ignored.
+            The default value of None means that no floor is imposed. If dynamic loss scaling is not used, min_loss_scale is ignored.
+        max_loss_scale(float, default=2.**24 ): Sets a ceiling for the loss scale values that can be chosen by dynamic loss scaling.
+            If dynamic loss scaling is not used, max_loss_scale is ignored.
         **kwargs:Currently, the under-the-hood properties that govern pure or mixed precision training are the following:
-                cast_model_type, patch_torch_functions, keep_batchnorm_fp32, master_weights, loss_scale.
+            cast_model_type, patch_torch_functions, keep_batchnorm_fp32, master_weights, loss_scale.
             cast_model_type: Casts your model’s parameters and buffers to the desired type.patch_torch_functions: Patch all Torch functions and Tensor methods to perform Tensor Core-friendly ops like GEMMs and convolutions in FP16, and any ops that benefit from FP32 precision in FP32.
             keep_batchnorm_fp32: To enhance precision and enable cudnn batchnorm (which improves performance), it’s often beneficial to keep batchnorm weights in FP32 even if the rest of the model is FP16.
             master_weights: Maintain FP32 master weights to accompany any FP16 model weights. FP32 master weights are stepped by the optimizer to enhance precision and capture small gradients.
