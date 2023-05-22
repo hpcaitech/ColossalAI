febd2b434afd20cfe53570650d1df996ea29a464
Camille Zhong <44392324+Camille7777@users.noreply.github.com>
2023-05-22 17:30:40 +0800

    Update eval.py
    Update eval.py

--- applications/Chat/evaluate/eval.py
+++ applications/Chat/evaluate/eval.py
@@ -6,7 +6,7 @@ from utils import jload
 
 def main(args):
      # load config
-    config=jload(args.config_file)
+    config = jload(args.config_file)
 
     if config["language"] == "cn":
         # get metric settings for all categories
@@ -14,22 +14,30 @@ def main(args):
         for category in config["category"].keys():
             metrics_all = {}
             for metric_type, metrics in config["category"][category].items():
-                metrics_all[metric_type]=metrics
-            metrics_per_category[category]=metrics_all
-
-        targets = jload(args.target_file)
-        answers = jload(args.predict_file)
+                metrics_all[metric_type] = metrics
+            metrics_per_category[category] = metrics_all
             
         # initialize evaluator
         evaluator = Evaluator(metrics_per_category)
-        evaluator.evaluate(answers = answers, targets = targets)
-        evaluator.save(args.save_path)
+        if args.answers2_file:
+            answers1 = jload(args.answers1_file)
+            answers2 = jload(args.answers2_file)
+            evaluator.battle(answers1=answers1, answers2=answers2)
+        else:
+            targets = jload(args.target_file)
+            answers = jload(args.answers1_file)
+            evaluator.evaluate(answers=answers, targets=targets)
+            evaluator.save(args.save_path)
+    else:
+        raise ValueError(f'Unsupported language')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_file', type=str, default=None, help='path to the file of target results')
     parser.add_argument('--target_file', type=str, default=None, help='path to the file of target results')
-    parser.add_argument('--predict_file', type=str, default=None, help='path to the file of model prediction results')
+    parser.add_argument('--answers1_file', type=str, default=None, help='path to the file of one model prediction')
+    parser.add_argument('--answers2_file', type=str, default=None, help='path to the file of the other model prediction')
     parser.add_argument('--save_path', type=str, default="results.csv", help='path to the csv file to save evaluation results')
     args = parser.parse_args()
     main(args)
