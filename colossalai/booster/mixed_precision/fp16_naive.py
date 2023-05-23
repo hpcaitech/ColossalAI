410cf73f282b3f95c4ae7653ac6e332e9f3e8211
Mingyan Jiang <1829166702@qq.com>
2023-05-23 10:24:32 +0800

    [api] add docstring to apex amp/ naive amp

--- colossalai/booster/mixed_precision/fp16_naive.py
+++ colossalai/booster/mixed_precision/fp16_naive.py
@@ -6,13 +6,13 @@ class FP16NaiveMixedPrecision(MixedPrecision):
     Precision for mixed precision training in FP16 using naive AMP.
 
     Args:
-        log_num_zeros_in_grad(bool): return number of zeros in the gradients.
-        initial_scale(int): initial scale of gradient scaler
-        growth_factor(int): the growth rate of loss scale
-        backoff_factor(float): the decrease rate of loss scale
-        hysteresis(int): delay shift in dynamic loss scaling
-        max_scale(int): maximum loss scale allowed
-        verbose(bool): if set to `True`, will print debug info
+    log_num_zeros_in_grad(bool): return number of zeros in the gradients.
+    initial_scale(int): initial scale of gradient scaler.
+    growth_factor(int): the growth rate of loss scale.
+    backoff_factor(float): the decrease rate of loss scale.
+    hysteresis(int): delay shift in dynamic loss scaling.
+    max_scale(int): maximum loss scale allowed.
+    verbose(bool): if set to `True`, will print debug info.
     """
 
     def __init__(self,
