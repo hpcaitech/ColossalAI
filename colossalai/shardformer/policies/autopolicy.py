ffacf0fcbade77300ce52568e20a7720a89946a8
FoolPlayer <45593998+FoolPlayer@users.noreply.github.com>
2023-05-24 10:26:46 +0800

    [shardformer]: Feature/shardformer, add some docstring and readme (#3816)
    * init shardformer code structure
    
    * add implement of sharder (inject and replace)
    
    * add implement of replace layer to colossal layer
    
    * separate different layer policy, add some notion
    
    * implement 1d and 2d slicer, can tell col or row
    
    * fix bug when slicing and inject model
    
    * fix some bug; add inference test example
    
    * add share weight and train example
    
    * add train
    
    * add docstring and readme
    
    * add docstring for other files
    
    * pre-commit

--- colossalai/shardformer/policies/autopolicy.py
+++ colossalai/shardformer/policies/autopolicy.py
@@ -1,39 +1,46 @@
 import torch.nn as nn
 
+
 def build_policies():
-    """
+    r"""
     Build the policies for the model
-    
+
     Return:
         The dict for the policies
     """
     auto_policy_dict = {}
 
     from transformers.models.bert.modeling_bert import BertForMaskedLM
+
     from .bert import BertForMaskedLMPolicy
     auto_policy_dict[BertForMaskedLM] = BertForMaskedLMPolicy
 
     from transformers.models.bert.modeling_bert import BertForSequenceClassification
+
     from .bert import BertForSequenceClassificationPolicy
     auto_policy_dict[BertForSequenceClassification] = BertForSequenceClassificationPolicy
-    
+
     return auto_policy_dict
 
-def get_autopolicy(model:nn.Module):
-    """
+
+def get_autopolicy(model: nn.Module):
+    r"""
     Return the auto policy for the model
 
     Args:
-        model: The model to be used
+        model (:class:`nn.Module`): The model to get the auto policy
 
     Return:
-        The auto policy for the model
+        :class:`Policy`: The auto policy for the model
     """
     auto_policy_dict = build_policies()
     policy = auto_policy_dict.get(model.__class__, None)
-    if policy is None:   
-        raise NotImplementedError(f"Auto policy for {model.__class__.__qualname__} is not implemented\n Supported models are {[i.__qualname__ for i in auto_policy_dict.keys()]}")
+    if policy is None:
+        raise NotImplementedError(
+            f"Auto policy for {model.__class__.__qualname__} is not implemented\n Supported models are {[i.__qualname__ for i in auto_policy_dict.keys()]}"
+        )
     return policy
+
 
 # from transformers.models.bert.modeling_bert import BertForMaskedLM, BertForPreTraining
 # model = BertForPreTraining
