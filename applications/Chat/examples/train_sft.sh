cb518a6bd501d37ec2504d3c1f8e767d39e09fe9
jiangmingyan <1829166702@qq.com>
2023-05-30 16:41:19 +0800

    [doc] update document of zero with chunk, fix zero with chunk doc

--- applications/Chat/examples/train_sft.sh
+++ applications/Chat/examples/train_sft.sh
@@ -1,5 +1,5 @@
-python -m torch.distributed.run --standalone --nproc_per_node=4 train_sft.py \
-    --pretrain "path/to/LLaMa-7B/" \
+torchrun --standalone --nproc_per_node=4 train_sft.py \
+    --pretrain "/path/to/LLaMa-7B/" \
     --model 'llama' \
     --strategy colossalai_zero2 \
     --log_interval 10 \
