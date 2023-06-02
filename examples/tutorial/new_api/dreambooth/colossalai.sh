9b29db5676532db1902b4793d436c71455737cf2
Maruyama_Aya <china6280111@126.com>
2023-06-02 16:50:51 +0800

    Add a new example of Dreambooth training using the booster API

---
+++ examples/tutorial/new_api/dreambooth/colossalai.sh
@@ -0,0 +1,17 @@
+HF_DATASETS_OFFLINE=1
+TRANSFORMERS_OFFLINE=1 
+DIFFUSERS_OFFLINE=1
+
+torchrun --nproc_per_node 4 --master_port=25641 train_dreambooth_colossalai.py \
+  --pretrained_model_name_or_path="Your Pretrained Model Path"  \
+  --instance_data_dir="Your Input Pics Path" \
+  --output_dir="path-to-save-model" \
+  --instance_prompt="your_prompt" \
+  --resolution=512 \
+  --plugin="gemini" \
+  --train_batch_size=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --placement="cuda" \
---
+++ examples/tutorial/new_api/dreambooth/test_ci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xe
+pip install -r requirements.txt
+
+HF_DATASETS_OFFLINE=1
+TRANSFORMERS_OFFLINE=1
+DIFFUSERS_OFFLINE=1
+
+for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero"; do
+  torchrun --nproc_per_node 4 --master_port=25641 train_dreambooth_colossalai.py \
+  --pretrained_model_name_or_path="Your Pretrained Model Path"  \
+  --instance_data_dir="Your Input Pics Path" \
+  --output_dir="path-to-save-model" \
+  --instance_prompt="your prompt" \
+  --resolution=512 \
+  --plugin=$plugin \
+  --train_batch_size=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --placement="cuda"
+done
