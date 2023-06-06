79c9f776a9ea42991df54d11e2c4b3ac4a7eeea9
Maruyama_Aya <china6280111@126.com>
2023-06-06 16:20:45 +0800

    fixed port

--- examples/images/dreambooth/test_ci.sh
+++ examples/images/dreambooth/test_ci.sh
@@ -7,7 +7,7 @@ TRANSFORMERS_OFFLINE=1
 DIFFUSERS_OFFLINE=1
 
 for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero"; do
-  torchrun --nproc_per_node 4 --master_port=25641 train_dreambooth_colossalai.py \
+  torchrun --nproc_per_node 4 --standalone train_dreambooth_colossalai.py \
   --pretrained_model_name_or_path="Your Pretrained Model Path"  \
   --instance_data_dir="Your Input Pics Path" \
   --output_dir="path-to-save-model" \
