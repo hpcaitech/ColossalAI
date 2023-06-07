89e385ed99e0403c4dcb824d5c0304f9e92dbcfd
wukong1992 <wukong1992@users.noreply.github.com>
2023-06-07 15:51:00 +0800

    [booster] update bert example, using booster api (#3885)

--- examples/language/bert/test_ci.sh
+++ examples/language/bert/test_ci.sh
@@ -1,2 +1,8 @@
-set -x
-env GPUNUM=1 bash run_gemini.sh
+#!/bin/bash
+set -xe
+
+pip install -r requirements.txt
+
+for plugin in "torch_ddp" "torch_ddp_fp16" "gemini" "low_level_zero"; do
+   torchrun --standalone --nproc_per_node 4  finetune.py --target_f1 0.86 --plugin $plugin --model_type "bert"
+done
