7c29259c9c3a229c1b844bf00aa113d40f4c9ebf
FoolPlayer <45593998+FoolPlayer@users.noreply.github.com>
2023-06-07 16:09:40 +0800

    [shardformer] add gpt2 policy and modify shard and slicer to support (#3883)
    * add gpt2 policy and modify shard and slicer to support
    
    * remove unused code
    
    * polish code

--- colossalai/shardformer/shard/slicer.py
+++ colossalai/shardformer/shard/slicer.py
@@ -19,6 +19,8 @@ class Slicer():
         weight: torch.Tensor,
         bias: torch.Tensor,
         policy_layer_cls: Layer,
+        n_cast: int = None,
+        reversed: bool = False,
     ):
         r"""
         Slice the weight and bias according to policy layer cls
@@ -33,13 +35,18 @@ class Slicer():
         """
         if policy_layer_cls == Layer:
             return weight, bias
-        elif policy_layer_cls == Col_Layer:
-            weight = self.slice_tensor(weight, 1, False)
+
+        dim = dim_mapping[policy_layer_cls] if not reversed else (1 - dim_mapping[policy_layer_cls])
+        # print(weight.shape, dim)
+        if policy_layer_cls == Col_Layer:
+            weight = self.slice_tensor(weight, dim, False, n_cast)
             bias = self.slice_tensor(bias, 0, True)
         elif policy_layer_cls == Row_Layer:
-            weight = self.slice_tensor(weight, 0, False)
+            weight = self.slice_tensor(weight, dim, False, n_cast)
         else:
             raise NotImplementedError(f"The policy layer class {policy_layer_cls} is not supported")
+        if reversed:
+            weight = weight.transpose(0, 1).contiguous()
         return weight, bias
 
     def slice_tensor(
@@ -47,6 +54,7 @@ class Slicer():
         tensor_in: torch.Tensor,
         dim: int,
         is_bias: bool,
+        n_cast: int = None,
     ) -> torch.Tensor:
         r"""
         Slice tensor according to the config
@@ -59,14 +67,15 @@ class Slicer():
         if tensor_in is None:
             return None
         if not is_bias:
-            return self.slice_2d(tensor_in, dim)
+            return self.slice_2d(tensor_in, dim, n_cast)
         else:
-            return self.slice_1d(tensor_in)
+            return self.slice_1d(tensor_in, n_cast)
 
     def slice_2d(
         self,
         tensor: torch.Tensor,
         dim: int,
+        n_cast: int = None,
     ) -> torch.Tensor:
         r"""
         Slice the 2D tensor
@@ -77,13 +86,14 @@ class Slicer():
         """
         assert dim in [0, 1], f"Only support 2D tensor, but got {dim}D tensor"
         if dim == 0:
-            return self.slice_row(tensor)
+            return self.slice_row(tensor, n_cast)
         elif dim == 1:
-            return self.slice_col(tensor)
+            return self.slice_col(tensor, n_cast)
 
     def slice_1d(
         self,
         tensor: torch.Tensor,
+        n_cast: int = None,
     ) -> torch.Tensor:
         r"""
         Slice the 1D tensor
@@ -94,11 +104,19 @@ class Slicer():
         Returns:
             :class:`torch.Tensor`: The sliced tensor
         """
-        return tensor.chunk(self.shardconfig.world_size, dim=0)[self.shardconfig.rank].contiguous()
+        if n_cast is None:
+            return tensor.chunk(self.shardconfig.world_size, dim=0)[self.shardconfig.rank].contiguous()
+        else:
+            tensor_chunks = tensor.chunk(self.shardconfig.world_size * n_cast, dim=0)
+            chunk_list = [
+                tensor_chunks[i] for i in range(self.shardconfig.rank, len(tensor_chunks), self.shardconfig.world_size)
+            ]
+            return torch.cat(chunk_list, dim=0).contiguous()
 
     def slice_col(
         self,
         tensor: torch.Tensor,
+        n_cast: int = None,
     ) -> torch.Tensor:
         r"""
         Slice the tensor in column
@@ -110,11 +128,19 @@ class Slicer():
             :class:`torch.Tensor`: The sliced tensor
 
         """
-        return tensor.chunk(self.shardconfig.world_size, dim=0)[self.shardconfig.rank].contiguous()
+        if n_cast is None:
+            return tensor.chunk(self.shardconfig.world_size, dim=0)[self.shardconfig.rank].contiguous()
+        else:
+            tensor_chunks = tensor.chunk(self.shardconfig.world_size * n_cast, dim=0)
+            chunk_list = [
+                tensor_chunks[i] for i in range(self.shardconfig.rank, len(tensor_chunks), self.shardconfig.world_size)
+            ]
+            return torch.cat(chunk_list, dim=0).contiguous()
 
     def slice_row(
         self,
         tensor: torch.Tensor,
+        n_cast: int = None,
     ) -> torch.Tensor:
         r"""
         Slice the tensor in column
@@ -125,4 +151,11 @@ class Slicer():
         Returns:
             :class:`torch.Tensor`: The sliced tensor
         """
-        return tensor.chunk(self.shardconfig.world_size, dim=1)[self.shardconfig.rank].contiguous()
+        if n_cast is None:
+            return tensor.chunk(self.shardconfig.world_size, dim=1)[self.shardconfig.rank].contiguous()
+        else:
+            tensor_chunks = tensor.chunk(self.shardconfig.world_size * n_cast, dim=1)
+            chunk_list = [
+                tensor_chunks[i] for i in range(self.shardconfig.rank, len(tensor_chunks), self.shardconfig.world_size)
+            ]
+            return torch.cat(chunk_list, dim=1).contiguous()
