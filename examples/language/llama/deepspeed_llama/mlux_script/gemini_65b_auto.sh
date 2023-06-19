04540580187532a6832aefeb61950331f535ee3e
Maruyama_Aya <china6280111@126.com>
2023-06-19 16:24:32 +0800

    test llama slurm

--- examples/language/llama/deepspeed_llama/mlux_script/gemini_65b_auto.sh
+++ examples/language/llama/deepspeed_llama/mlux_script/gemini_65b_auto.sh
@@ -16,7 +16,20 @@ rank=$SLURM_PROCID
 nprocs_per_node=4
 
 # WORLD_SIZE=$SLURM_NPROCS
-conda init bash
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/users/u100034/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/users/u100034/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/users/u100034/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/users/u100034/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 conda activate llama
 
 
