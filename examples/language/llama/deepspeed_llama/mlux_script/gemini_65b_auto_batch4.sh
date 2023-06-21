c08d0edf0e0c60a4fc32405bc3722e9e83e6eb46
Maruyama_Aya <china6280111@126.com>
2023-06-21 17:50:55 +0800

    test llama slurm

--- examples/language/llama/deepspeed_llama/mlux_script/gemini_65b_auto_batch4.sh
+++ examples/language/llama/deepspeed_llama/mlux_script/gemini_65b_auto_batch4.sh
@@ -40,7 +40,7 @@ DISTRIBUTED_ARGS="--nproc_per_node $nprocs_per_node \
                   --master_port ${MASTER_PORT}"
 
 cd ../..
-torchrun $DISTRIBUTED_ARGS benchmark.py --plugin "gemini" -l 512 -c '65b' -b 4
+torchrun $DISTRIBUTED_ARGS benchmark.py --plugin "gemini" -l 512 -c '65b' -b 4 -g
 
 
 
