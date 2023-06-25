bd14d9271fcac3b9e33c34d38194e4e64f5123a4
cuiqing.li <lixx3527@gmail.com>
2023-06-25 14:22:41 +0800

    adding inference file

--- applications/stable-diffusion/text-to-images/text_to_image_colossalai.py
+++ applications/stable-diffusion/text-to-images/text_to_image_colossalai.py
@@ -1,37 +1,60 @@
 import torch
 from diffusers import DDPMScheduler, StableDiffusionPipeline, UNet2DConditionModel, AutoencoderKL
 from transformers import CLIPTextModel, CLIPTokenizer
+from colossalai.booster import Booster
 
-model_id = "CompVis/stable-diffusion-v1-4"
+def main():
+    parser = argparse.ArgumentParser(description="Simple example of a inference script.")
+    parser.add_argument(
+        "--validation_prompts",
+        type=str,
+        default=None,
+        nargs="+",
+        help=("A set of prompts evaluated every `--validation_epochs` and logged to `--report_to`."),
+    )
+    parser.add_argument("--unet_saved_path", type=str, default=None, help=("path of your trained unet model"))
+    args = parser.parse_args()
 
-noise_scheduler = DDPMScheduler.from_pretrained(model_id, subfolder="scheduler")
-tokenizer = CLIPTokenizer.from_pretrained(
-    model_id, subfolder="tokenizer", revision=None
-)
+    model_id = "CompVis/stable-diffusion-v1-4"
 
-text_encoder = CLIPTextModel.from_pretrained(
-    model_id, subfolder="text_encoder", revision=None
-)
-vae = AutoencoderKL.from_pretrained(
-    model_id, subfolder="vae", revision=None
-)
+    noise_scheduler = DDPMScheduler.from_pretrained(model_id, subfolder="scheduler")
+    tokenizer = CLIPTokenizer.from_pretrained(
+        model_id, subfolder="tokenizer", revision=None
+    )
 
-unet = UNet2DConditionModel.from_pretrained(
-    model_id, subfolder="unet", revision=None
-)
+    text_encoder = CLIPTextModel.from_pretrained(
+        model_id, subfolder="text_encoder", revision=None
+    )
+    vae = AutoencoderKL.from_pretrained(
+        model_id, subfolder="vae", revision=None
+    )
 
-pipeline = StableDiffusionPipeline.from_pretrained(
-    model_id,
-    text_encoder=text_encoder,
-    vae=vae,
-    unet=unet,
-)
-
-pipe = pipeline.to("cuda")
-pipe.enable_attention_slicing()
-
-prompt = "a photo of an astronaut riding a horse on moon"
-image = pipe(prompt).images[0]  
+    unet = UNet2DConditionModel.from_pretrained(
+        model_id, subfolder="unet", revision=None
+    )
     
-image.save("astronaut_rides_horse_colossalai.png")
+    if args.unet_saved_path is not None:
+        unet.load_state_dict(torch.load(args.unet_saved_path))
+
+    pipeline = StableDiffusionPipeline.from_pretrained(
+        model_id,
+        text_encoder=text_encoder,
+        vae=vae,
+        unet=unet,
+    )
+
+    pipe = pipeline.to("cuda")
+    pipe.enable_attention_slicing()
+
+    prompt = "a photo of an astronaut riding a horse on moon"
+    if args.validation_prompts is not None:
+        prompt = args.validation_prompts
+
+    image = pipe(prompt).images[0]  
+        
+    image.save("astronaut_rides_horse_colossalai.png")
+
+
+if __name__ == "__main__":
+    main()
 
