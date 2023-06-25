326b3cde40f7af64c2c728dacf17f52b9e0ae3a7
klhhhhh <1412841649@qq.com>
2023-06-25 16:18:48 +0800

    vit shard add vitattention vitlayer

--- colossalai/shardformer/policies/vit.py
+++ colossalai/shardformer/policies/vit.py
@@ -2,9 +2,9 @@ rom typing import Dict, Union
 
 import torch.nn as nn
 
-from transformers.models.vit.modeling_vit import ViTModel, ViTLayer
+from transformers.models.vit.modeling_vit import ViTModel, ViTLayer, ViTEmbeddings, ViTAttention
 
-from colossalai.shardformer.layer.layers import Linear1D_Col, Linear1D_Row, VocabParallelEmbedding1D
+from colossalai.shardformer.layer.layers import Linear1D_Col, Linear1D_Row, VocabParallelEmbedding1D, LayerNorm1D, Dropout1D
 
 from .basepolicy import ModulePolicyDescription, Policy, SubModuleReplacementDescription
 
@@ -23,35 +23,92 @@ class ViTPolicy(Policy):
     
     def module_policy(self) -> Dict[Union[str, nn.Module], ModulePolicyDescription]:
         return  {
-            ViTLayer:ModulePolicyDescription(
-                
-            ),
-            ViTModel:
+            ViTEmbeddings:
+                ModulePolicyDescription(
+                    attribute_replacement{},
+                    param_replacement=[],
+                    sub_module_replacement=[
+                        SubModuleReplacementDescription(
+                            suffix="dropout",
+                            target_module=Dropout1D,
+                        )
+                    ]
+                ),
+            ViTLayer:
+                ModulePolicyDescription(
+                    attribute_replacement{},
+                    param_replacement=[],
+                    sub_module_replacement=[
+                        SubModuleReplacementDescription(
+                            suffix="intermediate.dense",
+                            target_module=Linear1D_Col,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="output.dense",
+                            target_module=Linear1D_Row,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="output.dropout",
+                            target_module=Dropout1D,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="layernorm_before",
+                            target_module=LayerNorm1D,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="layernorm_after",
+                            target_module=LayerNorm1D,
+                        ),
+                    ]
+                ),
+            ViTAttention:
                 ModulePolicyDescription(
                     attribute_replacement{
-                        
-                    }
+                        "attention.num_attention_heads":
+                            self.config.num_attention_heads//self.shard_config.tensor_parallel_size,
+                            
+                    },
+                    param_replacement=[],
+                    sub_module_replacement=[
+                        SubModuleReplacementDescription(
+                            suffix="attention.query",
+                            target_module=Linear1D_Col,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="attention.key",
+                            target_module=Linear1D_Col,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="attention.value",
+                            target_module=Linear1D_Col,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="attention.dropout",
+                            target_module=Dropout1D,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="output.dense",
+                            target_module=Linear1D_Row,
+                        ),
+                        SubModuleReplacementDescription(
+                            suffix="output.dropout",
+                            target_module=Dropout1D,
+                        ),
+                    ],
                 ),
-            
+            ViTModel:
+                ModulePolicyDescription(
+                    attribute_replacement{},
+                    param_replacement=[],
+                    sub_module_replacement=[
+                        SubModuleReplacementDescription(
+                            suffix="layernorm",
+                            target_module=LayerNorm1D,
+                        )
+                    ]
+                ),
         }
 
-    @staticmethod
-    def embedding() -> List:
-        return[
-            Embedding_Layer(
-                suffix="",
-                weight="weight",
-                replace_layer=col_nn.Embedding1D,
-            )
-        ]
-
-    @staticmethod
-    def dropout():
-        return [Dropout_Layer(
-            suffix="dropout",
-            p="p",
-            replace_layer=col_nn.Dropout1D,
-        )]
 
 
 
