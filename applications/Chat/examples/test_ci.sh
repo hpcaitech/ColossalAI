6b54b042712131a1e8173f5811e03c50c29024da
CWHer <cwher@outlook.com>
2023-06-25 10:28:05 +0800

    test: set CUDA_VISIBLE_DEVICES in ci

--- applications/Chat/examples/test_ci.sh
+++ applications/Chat/examples/test_ci.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+set_n_least_used_CUDA_VISIBLE_DEVICES() {
+    local n=${1:-"9999"}
+    echo "GPU Memory Usage:"
+    local FIRST_N_GPU_IDS=$(nvidia-smi --query-gpu=memory.used --format=csv |
+        tail -n +2 |
+        nl -v 0 |
+        tee /dev/tty |
+        sort -g -k 2 |
+        awk '{print $1}' |
+        head -n $n)
+    export CUDA_VISIBLE_DEVICES=$(echo $FIRST_N_GPU_IDS | sed 's/ /,/g')
+    echo "Now CUDA_VISIBLE_DEVICES is set to:"
+    echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+}
+
+set_n_least_used_CUDA_VISIBLE_DEVICES 4
+
 set -xue
 
 if [ -z "$SFT_DATASET" ]; then
