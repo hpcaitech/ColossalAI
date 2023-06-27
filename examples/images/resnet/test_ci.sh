53c24ac6de34648642164cf6fdfc65cfb7e251b5
CjhHa1 <cjh18671720497@outlook.com>
2023-06-27 16:20:09 +0800

    skip test_ci

--- examples/images/resnet/test_ci.sh
+++ examples/images/resnet/test_ci.sh
@@ -5,6 +5,6 @@ export DATA=/data/scratch/cifar-10
 
 pip install -r requirements.txt
 
-for plugin in "torch_ddp" "torch_ddp_fp16" "low_level_zero"; do
-    #colossalai run --nproc_per_node 4 train.py --interval 0 --target_acc 0.84 --plugin $plugin
-done
+# for plugin in "torch_ddp" "torch_ddp_fp16" "low_level_zero"; do
+#     colossalai run --nproc_per_node 4 train.py --interval 0 --target_acc 0.84 --plugin $plugin
+# done
