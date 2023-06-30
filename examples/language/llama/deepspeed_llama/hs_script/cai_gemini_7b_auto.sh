c7113a62ad43602904859ef6c13214ff69f4e6e6
Maruyama_Aya <china6280111@126.com>
2023-06-30 10:21:21 +0800

    test llama slurm

---
+++ examples/language/llama/deepspeed_llama/hs_script/cai_gemini_7b_auto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module load cuda/11.7.1-gcc-9.4.0
+module load nccl/2.14.3-1-gcc-9.4.0
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/mnt/vepfs/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/mnt/vepfs/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/mnt/vepfs/miniconda3//etc/profile.d/conda.sh"
+    else
+        export PATH="/mnt/vepfs/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate llama
+
+ROOT=$(pwd)
+
+cd ../..
+
+colossalai run --nproc_per_node 8 --hostfile ${ROOT}/cai_host_2.txt --master_addr 192.168.0.189 benchmark.py --plugin "gemini" -l 512 -g
