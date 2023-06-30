57d4fabdf036ca9b6c59f1fe53d41b3d15d70b64
CjhHa1 <cjh18671720497@outlook.com>
2023-06-30 19:19:24 +0800

    add pipeline policy and bert forward to be done

---
+++ colossalai/pipeline/policy/bert.py
@@ -0,0 +1,295 @@
+from functools import partial
+from types import MethodType
+from typing import Dict, List, Optional, Tuple, Union
+
+import torch
+from torch import Tensor
+from torch.nn import CrossEntropyLoss, Module
+from transformers.modeling_outputs import (BaseModelOutputWithPast,
+                                           BaseModelOutputWithPastAndCrossAttentions,
+                                           BaseModelOutputWithPoolingAndCrossAttentions)
+from transformers.models.bert.modeling_bert import BertModel    
+from transformers.utils import logging
+
+from colossalai.pipeline.stage_manager import PipelineStageManager
+
+from .base import Policy
+
+logger = logging.get_logger(__name__)
+
+def bert_model_forward(self:BertModel,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        encoder_hidden_states: Optional[torch.Tensor] = None,
+        encoder_attention_mask: Optional[torch.Tensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+       #labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        stage_manager: Optional[PipelineStageManager] = None,
+        hidden_states: Optional[torch.FloatTensor] = None, #this is from the previous stage
+    ) :
+        #TODO: add explaination of the output here.
+
+        r"""
+        encoder_hidden_states  (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`, *optional*):
+            Sequence of hidden-states at the output of the last layer of the encoder. Used in the cross-attention if
+            the model is configured as a decoder.
+        encoder_attention_mask (`torch.FloatTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Mask to avoid performing attention on the padding token indices of the encoder input. This mask is used in
+            the cross-attention if the model is configured as a decoder. Mask values selected in `[0, 1]`:
+
+            - 1 for tokens that are **not masked**,
+            - 0 for tokens that are **masked**.
+        past_key_values (`tuple(tuple(torch.FloatTensor))` of length `config.n_layers` with each tuple having 4 tensors of shape `(batch_size, num_heads, sequence_length - 1, embed_size_per_head)`):
+            Contains precomputed key and value hidden states of the attention blocks. Can be used to speed up decoding.
+
+            If `past_key_values` are used, the user can optionally input only the last `decoder_input_ids` (those that
+            don't have their past key value states given to this model) of shape `(batch_size, 1)` instead of all
+            `decoder_input_ids` of shape `(batch_size, sequence_length)`.
+        use_cache (`bool`, *optional*):
+            If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding (see
+            `past_key_values`).
+        """
+        # preprocess:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if self.config.is_decoder:
+            use_cache = use_cache if use_cache is not None else self.config.use_cache
+        else:
+            use_cache = False
+
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif inputs_embeds is not None:
+            input_shape = inputs_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+        
+        if output_attentions:
+            logger.warning_once('output_attentions=True is not supported for pipeline models at the moment.')
+            output_attentions = False
+        if output_hidden_states:
+            logger.warning_once('output_hidden_states=True is not supported for pipeline models at the moment.')
+            output_hidden_states = False
+        if use_cache:
+            logger.warning_once('use_cache=True is not supported for pipeline models at the moment.')
+            use_cache = False
+        
+        batch_size, seq_length = input_shape
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+        
+        # past_key_values_length
+        past_key_values_length = past_key_values[0][0].shape[2] if past_key_values is not None else 0
+
+        if attention_mask is None:
+            attention_mask = torch.ones(((batch_size, seq_length + past_key_values_length)), device=device)
+
+        if token_type_ids is None:
+            if hasattr(self.embeddings, "token_type_ids"):
+                buffered_token_type_ids = self.embeddings.token_type_ids[:, :seq_length]
+                buffered_token_type_ids_expanded = buffered_token_type_ids.expand(batch_size, seq_length)
+                token_type_ids = buffered_token_type_ids_expanded
+            else:
+                token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+
+        # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
+        # ourselves in which case we just need to make it broadcastable to all heads.
+        extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
+
+        # If a 2D or 3D attention mask is provided for the cross-attention
+        # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
+        if self.config.is_decoder and encoder_hidden_states is not None:
+            encoder_batch_size, encoder_sequence_length, _ = encoder_hidden_states.size()
+            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
+            if encoder_attention_mask is None:
+                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
+            encoder_extended_attention_mask = self.invert_attention_mask(encoder_attention_mask)
+        else:
+            encoder_extended_attention_mask = None
+
+        # Prepare head mask if needed
+        # 1.0 in head_mask indicate we keep the head
+        # attention_probs has shape bsz x n_heads x N x N
+        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
+        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
+        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+       
+        # assure that the input is embedding_output and is the hidden_states of previous stages.
+
+        hidden_states = input_ids if input_ids is not None else None
+        if stage_manager.is_first_stage():        
+                hidden_states= self.embeddings(
+                input_ids=input_ids,
+                position_ids=position_ids,
+                token_type_ids=token_type_ids,
+                inputs_embeds=inputs_embeds,
+                past_key_values_length=past_key_values_length,
+            )
+       
+
+        encoder_outputs = None
+        #inherit from bert_layer
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attentions = () if output_attentions else None
+        all_cross_attentions = () if output_attentions and self.config.add_cross_attention else None
+
+        if self.encoder.gradient_checkpointing and self.encoder.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+        next_decoder_cache = () if use_cache else None
+        
+        #calculate the num_layers
+        num_layers_per_stage = len(self.encoder.layer) // stage_manager.num_stages
+        start_layer = stage_manager.stage * num_layers_per_stage
+        end_layer = (stage_manager.stage + 1) * num_layers_per_stage
+
+        for idx, encoder_layer in enumerate(self.encoder.layer[start_layer:end_layer], start=start_layer):
+            if stage_manager.is_first_stage() and idx == 0:
+                attention_mask = extended_attention_mask
+
+            if output_hidden_states:
+                all_hidden_states = all_hidden_states + (hidden_states,)
+            
+            layer_head_mask = head_mask[idx] if head_mask is not None else None
+            past_key_value = past_key_values[idx] if past_key_values is not None else None
+            
+            ###
+            print('where is the model now',start_layer,idx,end_layer)
+            print('what stage is now',stage_manager.stage)
+
+            if self.encoder.gradient_checkpointing and self.encoder.training:
+                
+                def create_custom_forward(module):
+                    def custom_forward(*inputs):
+                        return module(*inputs, past_key_value, output_attentions)
+
+                    return custom_forward
+
+                layer_outputs = torch.utils.checkpoint.checkpoint(
+                    create_custom_forward(encoder_layer),
+                    hidden_states,
+                    attention_mask,
+                    layer_head_mask,
+                    encoder_hidden_states,
+                    encoder_attention_mask,
+                )
+            else:
+                if stage_manager.stage == 1:
+                    if hidden_states is not None :
+                        print('shape of hidden_states',hidden_states.shape)
+                    if attention_mask is not None :
+                        print('shape of attention_mask',attention_mask.shape)
+                    ## TODO: check for this layer_head_mask
+                    if layer_head_mask is not None :
+                        print('shape of layer_head_mask',layer_head_mask.shape)
+                    if encoder_hidden_states is not None :
+                        print('shape of encoder_hidden_states',encoder_hidden_states.shape)
+                layer_outputs = encoder_layer(
+                    hidden_states,
+                    attention_mask,
+                    layer_head_mask,
+                    encoder_hidden_states,
+                    encoder_attention_mask,
+                    past_key_value,
+                    output_attentions,
+                )
+            hidden_states = layer_outputs[0]
+            if use_cache:
+                next_decoder_cache += (layer_outputs[-1],)
+            if output_attentions:
+                all_self_attentions = all_self_attentions + (layer_outputs[1],)
+                if self.config.add_cross_attention:
+                    all_cross_attentions = all_cross_attentions + (layer_outputs[2],)
+        
+        if output_hidden_states:
+            all_hidden_states = all_hidden_states + (hidden_states,)   
+
+        #end of a stage loop
+        sequence_output = layer_outputs[0] if layer_outputs is not None else None
+
+        if stage_manager.is_last_stage():    
+            pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
+            if not return_dict:
+                return (sequence_output, pooled_output) + encoder_outputs[1:]
+        
+
+        #output of non-first and non-last stages: 
+        if not return_dict:
+            return tuple(v 
+                    for v in [
+                    hidden_states,
+                    next_decoder_cache,
+                    all_hidden_states,
+                    all_self_attentions,
+                    all_cross_attentions,
+                ] if v is not None)
+        
+        #return dict is not supported at this moment
+        return BaseModelOutputWithPastAndCrossAttentions(
+                last_hidden_state=hidden_states,
+                past_key_values=next_decoder_cache,
+                hidden_states=all_hidden_states,
+                attentions=all_self_attentions,
+                cross_attentions=all_cross_attentions,
+        )
+
+
+# class BertModelPolicy(Policy):
+#     def get_hold_layers(self, module: BertModel) -> List[Module]:
+#         # get pipeline layers for curerent stage
+#         hold_layers = []
+#         if self.stage_manager.is_first_stage():
+#             hold_layers.append(module.embeddings)
+#         #Fix: num_layers_per_stage should be calculated based on the number of layers in the model
+#         num_layers_per_stage = len(module.encoder.layer) // self.stage_manager.num_stages
+        
+#         hold_layers.extend(module.encoder.layer[self.stage_manager.stage*
+#                                                 num_layers_per_stage : (self.stage_manager.stage+1)* num_layers_per_stage])
+#         if self.stage_manager.is_last_stage():
+#             hold_layers.append(module.pooler)
+
+#         return hold_layers
+    
+#     def get_shared_params(self, module: BertModel) -> List[Dict[int, Tensor]]:
+#         if id(module.embeddings.parameters) == id(module.pooler.parameters)
+#             return [dict(module.embeddings.named_parameters())]
+#         return []
+#     def replace_forward(self, module: Module) -> None:
+#         return super().replace_forward(module)
+
+'''
+def bert_pretraining_model_forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        next_sentence_label: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        hidden_states: Optional[torch.LongTensor] = None,
+        stage_manager: Optional[PipelineStageManager] = None,
+
+        ) -> Union[Tuple[torch.Tensor], BertForPreTrainingOutput]:
+    pass
+'''
---
+++ tests/test_pipeline/test_policy/test_bert.py
@@ -0,0 +1,57 @@
+import torch
+import pytest
+import torch.distributed as dist
+from colossalai.cluster import ProcessGroupMesh
+import colossalai
+from colossalai.testing import rerun_if_address_is_in_use, spawn
+
+from colossalai.pipeline.policy.bert import bert_model_forward
+from colossalai.pipeline.stage_manager import PipelineStageManager
+from transformers.models.bert.modeling_bert import BertModel
+
+def check_bert_model_forward():
+    model = BertModel.from_pretrained('bert-base-uncased')
+    DP_DIM, PP_DIM = 0, 1
+    DP_SIZE, PP_SIZE = 2, 2
+    RANK_TO_COORDINATE = {
+        0: (0, 0),
+        1: (0, 1),
+        2: (1, 0),
+        3: (1, 1),
+    }
+    PP_RANKS_IN_GROUP = {
+        0: [0, 1],
+        1: [0, 1],
+        2: [2, 3],
+        3: [2, 3],
+    }   
+    pg_mesh = ProcessGroupMesh(DP_SIZE, PP_SIZE)
+    #print(pg_mesh)
+
+    stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
+    rank = dist.get_rank()
+    #print(rank)
+    
+    x = torch.randint(0, 1000, (2, 3))  
+    attention_mask = torch.ones_like(x)
+    
+    output = bert_model_forward(self=model, input_ids=x, attention_mask=attention_mask,
+                                stage_manager=stage_manager)
+    print(output)
+    assert output[0].shape == (2, 3, 768)
+    # assert output[1].shape == (2, 768)
+
+
+
+def run_dist(rank, world_size, port):
+    colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
+    check_bert_model_forward()
+
+@pytest.mark.dist
+@rerun_if_address_is_in_use()
+def test_bert_model_forward():
+    spawn(run_dist, 4)
+
+
+if __name__ == "__main__":
+    test_bert_model_forward()
