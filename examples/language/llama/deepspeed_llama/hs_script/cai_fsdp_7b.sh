8e27388dd06d987e7697bde38ee017a6791a005e
Maruyama_Aya <china6280111@126.com>
2023-07-02 19:38:49 +0800

    test llama slurm

--- examples/language/llama/deepspeed_llama/hs_script/cai_fsdp_7b.sh
+++ examples/language/llama/deepspeed_llama/hs_script/cai_fsdp_7b.sh
@@ -5,20 +5,20 @@ module load nccl/2.14.3-1-gcc-9.4.0
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/mnt/vepfs/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/mnt/vepfs/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/mnt/vepfs/miniconda3//etc/profile.d/conda.sh"
+    if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/root/miniconda3//etc/profile.d/conda.sh"
     else
-        export PATH="/mnt/vepfs/miniconda3/bin:$PATH"
+        export PATH="/root/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
 
-conda activate llama
+conda activate /mnt/vepfs/conda/envs/llama_2
 
 ROOT=$(pwd)
 
