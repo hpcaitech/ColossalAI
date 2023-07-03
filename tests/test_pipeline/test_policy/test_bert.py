246b6d3b7a3eba548f88eb4be604b85ff516bf60
CjhHa1 <cjh18671720497@outlook.com>
2023-07-03 16:27:18 +0800

    add Bert_Policy and test for policy

--- tests/test_pipeline/test_policy/test_bert.py
+++ tests/test_pipeline/test_policy/test_bert.py
@@ -5,7 +5,7 @@ from colossalai.cluster import ProcessGroupMesh
 import colossalai
 from colossalai.testing import rerun_if_address_is_in_use, spawn
 
-from colossalai.pipeline.policy.bert import bert_model_forward
+from colossalai.pipeline.policy.bert import bert_model_forward,BertModelPolicy
 from colossalai.pipeline.stage_manager import PipelineStageManager
 from transformers.models.bert.modeling_bert import BertModel
 
@@ -52,17 +52,53 @@ def check_bert_model_forward():
     
     # assert output[1].shape == (2, 768)
 
+def check_bert_model_policy():
+    model = BertModel.from_pretrained('bert-base-uncased')
+    DP_DIM, PP_DIM = 0, 1
+    DP_SIZE, PP_SIZE = 2, 2
+    RANK_TO_COORDINATE = {
+        0: (0, 0),
+        1: (0, 1),
+        2: (1, 0),
+        3: (1, 1),
+    }
+    PP_RANKS_IN_GROUP = {
+        0: [0, 1],
+        1: [0, 1],
+        2: [2, 3],
+        3: [2, 3],
+    }   
+    pg_mesh = ProcessGroupMesh(DP_SIZE, PP_SIZE)
+    #print(pg_mesh)
+
+    stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
+    rank = dist.get_rank()
+
+    model_policy = BertModelPolicy(stage_manager,len(model.encoder.layer),2)
+    assert model_policy.layers_per_stage == [6,6]
+    layers=model_policy.get_hold_layers(model)
+    for layer in layers:
+        print(layer)
 
 
-def run_dist(rank, world_size, port):
+def run_dist_model(rank, world_size, port):
     colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
     check_bert_model_forward()
+
+def run_dist_policy(rank, world_size, port):
+    colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
+    check_bert_model_policy()   
 
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
 def test_bert_model_forward():
-    spawn(run_dist, 4)
+    spawn(run_dist_model, 4)
 
+@pytest.mark.dist
+@rerun_if_address_is_in_use()
+def test_bert_model_policy():
+    spawn(run_dist_policy, 4)
 
 if __name__ == "__main__":
     test_bert_model_forward()
+    test_bert_model_policy()
