c6331b7aa7d3c762c00067dcb4dd1ec080779e52
Wenhao Chen <cwher@outlook.com>
2023-07-04 13:49:09 +0800

    [chat] use official transformers and fix some issues (#4117)
    * feat: remove on_learn_epoch fn as not used
    
    * revert: add _on_learn_epoch fn
    
    * feat: remove NaiveStrategy
    
    * test: update train_prompts tests
    
    * fix: remove prepare_llama_tokenizer_and_embedding
    
    * test: add lora arg
    
    * feat: remove roberta support in train_prompts due to runtime errs
    
    * feat: remove deberta & roberta in rm as not used
    
    * test: remove deberta and roberta tests
    
    * feat: remove deberta and roberta models as not used
    
    * fix: remove calls to roberta
    
    * fix: remove prepare_llama_tokenizer_and_embedding
    
    * chore: update transformers version
    
    * docs: update transformers version
    
    * fix: fix actor inference
    
    * fix: fix ci
    
    * feat: change llama pad token to unk
    
    * revert: revert ddp setup_distributed
    
    * fix: change llama pad token to unk
    
    * revert: undo unnecessary changes
    
    * fix: use pip to install transformers

--- applications/Chat/examples/community/peft/train_peft_prompts.py
+++ applications/Chat/examples/community/peft/train_peft_prompts.py
@@ -10,7 +10,6 @@ from coati.models.llama import LlamaActor, LlamaCritic, LlamaRM
 from coati.models.opt import OPTRM, OPTActor, OPTCritic
 from coati.trainer import PPOTrainer
 from coati.trainer.strategies import DDPStrategy, GeminiStrategy, LowLevelZeroStrategy
-from coati.utils import prepare_llama_tokenizer_and_embedding
 from easy_dataset import EasyPromptsDataset, EasySupervisedDataset
 from easy_models import BLOOMActor
 from peft import PeftModel
@@ -112,20 +111,19 @@ def main(args):
     # configure tokenizer
     if args.model == 'gpt2':
         tokenizer = GPT2Tokenizer.from_pretrained(args.rm_pretrain)
+        tokenizer.pad_token = tokenizer.eos_token
     elif args.model == 'bloom':
         tokenizer = BloomTokenizerFast.from_pretrained(args.rm_pretrain)
+        tokenizer.pad_token = tokenizer.eos_token
     elif args.model == 'opt':
         tokenizer = AutoTokenizer.from_pretrained(args.rm_pretrain)
+        tokenizer.pad_token = tokenizer.eos_token
     elif args.model == 'llama':
         tokenizer = LlamaTokenizer.from_pretrained(args.pretrain)
         tokenizer.eos_token = '<\s>'
+        tokenizer.pad_token = tokenizer.unk_token
     else:
         raise ValueError(f'Unsupported model "{args.model}"')
-
-    if args.model == 'llama':
-        tokenizer = prepare_llama_tokenizer_and_embedding(tokenizer, actor)
-    else:
-        tokenizer.pad_token = tokenizer.eos_token
 
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
 
