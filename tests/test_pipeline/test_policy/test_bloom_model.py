90a65ea682789e013c4a8b4682587a1ec5d43d3d
Jianghai <72591262+CjhHa1@users.noreply.github.com>
2023-07-05 10:52:53 +0800

    [pipeline] build bloom model and policy , revise the base class of policy (#4161)
    * add pipeline policy and bert forward to be done
    
    * add bertmodel pipeline forward and make tests
    
    * add Bert_Policy and test for policy
    
    * update formatting
    
    * update formatting
    
    * update the code
    
    * fix bugs
    
    * fix name confilt
    
    * add bloom model and policy ,revise the base class of policy
    
    * revise
    
    * revision
    
    * add bert_for_pretraining

--- tests/test_pipeline/test_policy/test_bloom_model.py
+++ tests/test_pipeline/test_policy/test_bloom_model.py
@@ -1,17 +1,19 @@
 import pytest
 import torch
 import torch.distributed as dist
-from transformers.models.bert.modeling_bert import BertModel
+from transformers.models.bloom import BloomConfig, BloomModel
 
 import colossalai
 from colossalai.cluster import ProcessGroupMesh
-from colossalai.pipeline.policy.bert import BertModelPolicy, bert_model_forward
+from colossalai.pipeline.policy.bloom import BloomModelPolicy, bloom_model_forward
 from colossalai.pipeline.stage_manager import PipelineStageManager
 from colossalai.testing import rerun_if_address_is_in_use, spawn
 
 
-def check_bert_model_forward():
-    model = BertModel.from_pretrained('bert-base-uncased')
+def check_bloom_model_forward():
+    # create a BloomModel
+    configuration = BloomConfig()
+    model = BloomModel(configuration)
     DP_DIM, PP_DIM = 0, 1
     DP_SIZE, PP_SIZE = 2, 2
     RANK_TO_COORDINATE = {
@@ -27,7 +29,6 @@ def check_bert_model_forward():
         3: [2, 3],
     }
     pg_mesh = ProcessGroupMesh(DP_SIZE, PP_SIZE)
-
     # print(pg_mesh)
 
     stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
@@ -35,29 +36,34 @@ def check_bert_model_forward():
     # print(rank)
 
     x = torch.randint(0, 1000, (2, 3))
-    hidden_states = torch.randint(0, 1000, (2, 3, 768)).to(torch.float32)
-    if stage_manager.stage == 0:
+    hidden_states = torch.randint(0, 1000, (2, 3, 64)).to(torch.float32)
+    if stage_manager.is_first_stage():
         attention_mask = torch.ones_like(x)
-        output = bert_model_forward(self=model, input_ids=x, attention_mask=attention_mask, stage_manager=stage_manager)
+        output = bloom_model_forward(self=model,
+                                     input_ids=x,
+                                     attention_mask=attention_mask,
+                                     stage_manager=stage_manager)
         print(output[0].shape)
-        assert output[0].shape == (2, 3, 768)
+        assert output[0].shape == (2, 3, 64)
         print('start the training')
     else:
-        attention_mask = torch.ones((2, 12, 3, 3))
-        output = bert_model_forward(self=model,
-                                    hidden_states=hidden_states,
-                                    attention_mask=attention_mask,
-                                    stage_manager=stage_manager)
+        attention_mask = torch.ones((2, 3))
+        output = bloom_model_forward(self=model,
+                                     hidden_states=hidden_states,
+                                     attention_mask=attention_mask,
+                                     stage_manager=stage_manager)
         print(output[0].shape)
-        assert output[0].shape == (2, 3, 768)
+        assert output[0].shape == (2, 3, 64)
         print('end the training')
         print(output)
 
     # assert output[1].shape == (2, 768)
 
 
-def check_bert_model_policy():
-    model = BertModel.from_pretrained('bert-base-uncased')
+def check_bloom_model_policy():
+    # create a BloomModel
+    configuration = BloomConfig()
+    model = BloomModel(configuration)
     DP_DIM, PP_DIM = 0, 1
     DP_SIZE, PP_SIZE = 2, 2
     RANK_TO_COORDINATE = {
@@ -78,8 +84,8 @@ def check_bert_model_policy():
     stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
     rank = dist.get_rank()
 
-    model_policy = BertModelPolicy(stage_manager, len(model.encoder.layer), 2)
-    assert model_policy.layers_per_stage == [6, 6]
+    model_policy = BloomModelPolicy(stage_manager=stage_manager, num_layers=len(model.h), num_stages=2)
+    assert model_policy.layers_per_stage == [1, 1]
     layers = model_policy.get_hold_layers(model)
     for layer in layers:
         print(layer)
@@ -87,27 +93,27 @@ def check_bert_model_policy():
 
 def run_dist_model(rank, world_size, port):
     colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
-    check_bert_model_forward()
+    check_bloom_model_forward()
 
 
 def run_dist_policy(rank, world_size, port):
     colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
-    check_bert_model_policy()
+    check_bloom_model_policy()
 
 
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
-def test_bert_model_forward():
+def test_bloom_model_forward():
     spawn(run_dist_model, 4)
 
 
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
-def test_bert_model_policy():
+def test_bloom_model_policy():
     spawn(run_dist_policy, 4)
 
 
 if __name__ == "__main__":
-    """test the bert model forward and bert model policy"""
-    test_bert_model_forward()
-    test_bert_model_policy()
+    """test the bloom model forward and bloom model policy"""
+    test_bloom_model_forward()
+    test_bloom_model_policy()
