ed5f235decd918e75a41b72c8236e2d1fdc14f9c
Mingyan Jiang <1829166702@qq.com>
2023-07-07 13:56:25 +0800

    [shardformer] gpt2 support flash attention

--- colossalai/shardformer/modeling/gpt2.py
+++ colossalai/shardformer/modeling/gpt2.py
@@ -62,8 +62,8 @@ def get_gpt2_forward():
             else:
                 batch_size, _, tgt_len, src_len = attention_mask.size()
                 attn_bias = attention_mask.expand(batch_size, self.num_heads, tgt_len, src_len).contiguous()
-
-        attn_output = me_attention(query=query, key=key, value=value, attn_bias=attn_bias, p=self.attn_dropout.p, scale=value.size(-1) ** -0.5)
+        scale = value.size(-1) ** -0.5
+        attn_output = me_attention(query=query, key=key, value=value, attn_bias=attn_bias, p=self.attn_dropout.p, scale=scale)
         attn_output = merge_heads(attn_output, self.num_heads, self.head_dim)
         attn_output = self.c_proj(attn_output)
         attn_output = self.resid_dropout(attn_output)
