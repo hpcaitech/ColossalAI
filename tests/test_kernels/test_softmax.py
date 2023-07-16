978de66d62058f6708b2855383123b29db0351b2
cuiqing.li <lixx3527@gmail.com>
2023-07-16 16:10:12 +0800

    upload tets

--- tests/test_kernels/test_softmax.py
+++ tests/test_kernels/test_softmax.py
@@ -2,10 +2,21 @@ import pytest
 import torch
 from torch import nn
 
+from colossalai.kernel.triton.ops import softmax
+
 def test_softmax_op():
     device = "cuda"
-    data = torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32)
+    data_samples = [torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32),
+                    torch.randn((320, 320, 78), device = "cuda", dtype = torch.float32),
+                    ]
+    for data in data_samples:
+        data = torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32)
+        module = nn.Softmax(dim = -1)
+        data_torch_out = module(data)
 
-    
+        data_triton_out = softmax(data)
+        print(torch.allclose(data_torch_out.cpu(), data_triton_out.cpu(), rtol=1e-4, atol=1e-4))
+
+
 if __name__ == "__main__":
     test_softmax_op()
