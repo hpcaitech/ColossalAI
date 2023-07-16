8390da3c63c0102433f16512492b629c425300de
cuiqing.li <lixx3527@gmail.com>
2023-07-16 16:12:20 +0800

    fix tests

--- tests/test_kernels/test_softmax.py
+++ tests/test_kernels/test_softmax.py
@@ -6,16 +6,17 @@ from colossalai.kernel.triton.ops import softmax
 
 def test_softmax_op():
     device = "cuda"
-    data_samples = [torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32),
-                    torch.randn((320, 320, 78), device = "cuda", dtype = torch.float32),
+    data_samples = [
+                        torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32),
+                        torch.randn((320, 320, 78), device = "cuda", dtype = torch.float32),
+                        torch.randn((2345, 4, 5, 64), device = "cuda", dtype = torch.float16)
                     ]
+
     for data in data_samples:
-        data = torch.randn((3, 4, 5, 32), device = "cuda", dtype = torch.float32)
         module = nn.Softmax(dim = -1)
         data_torch_out = module(data)
-
         data_triton_out = softmax(data)
-        print(torch.allclose(data_torch_out.cpu(), data_triton_out.cpu(), rtol=1e-4, atol=1e-4))
+        print(torch.allclose(data_torch_out.cpu(), data_triton_out.cpu(), rtol=1e-3, atol=1e-3))
 
 
 if __name__ == "__main__":
