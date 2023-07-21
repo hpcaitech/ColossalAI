a57a82412fdf8df21affda6bc317be0115714563
flybird1111 <1829166702@qq.com>
2023-07-21 10:55:45 +0800

    [shardformer] merge feature/shardformer-models branch to feature/flash-attention-shardformer branch. (#4290)
    * Feature/vit support (#4182)
    
    * [shardformer] added tests
    
    * [shardformer] vit test finish and support
    
    * fix attention dropout
    
    * [shardformer] support SAM (#4231)
    
    * 1.support sam 2.add fused qkv for nn.Linear
    
    * update utils support set element in list
    
    * overtwrite SamVisionAttention foward to use DropoutForParallelInput
    
    * remove unused code
    
    * [shardformer] support whisper (#4212)
    
    * support whisper
    
    * fix bug in vocabembedding
    
    * support downstream model of whisper
    
    * update readme
    
    * Feature/chatglm (#4240)
    
    * [shardformer] added tests
    
    * [shardformer] vit test finish and support
    
    * [shardformer] chatglm ready
    
    * import chatglm
    
    * [shardformer] add test kit in model zoo for chatglm
    
    * [sharformer] add first version of policy of chatglm
    
    * [shardformer] polish chatglm code
    
    * [shardformer] polish code
    
    * [shardformer] support chatglm without layernorm
    
    * [shardformer] chatglm shard without mlp sharding
    
    * [shardformer] delete some file
    
    * [shardformer] ChatGLM support layernorm sharding
    
    * [shardformer] register without auto policy
    
    * [shardformer] pre-commit check files
    
    * [shardformer] fix chatglm configuration with pre-commit
    
    ---------
    
    Co-authored-by: Kun Lin <81014421+klhhhhh@users.noreply.github.com>
    Co-authored-by: FoolPlayer <45593998+FoolPlayer@users.noreply.github.com>

--- tests/test_shardformer/test_model/test_shard_sam.py
+++ tests/test_shardformer/test_model/test_shard_sam.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 import torch
 
@@ -21,7 +19,8 @@ def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transfo
     # check forward
     org_output, org_loss, shard_output, shard_loss = run_forward(org_model, sharded_model, data_gen_fn,
                                                                  output_transform_fn, loss_fn)
-    assert_hf_output_close(org_output, shard_output, atol=1e-4, rtol=1e-4)
+    assert_hf_output_close(org_output, shard_output, ignore_keys=['pred_masks'])
+
     # do backward
     org_loss.backward()
     shard_loss.backward()
@@ -29,18 +28,16 @@ def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transfo
     assert torch.allclose(org_loss, shard_loss,
                           atol=1e-5), f"shard model loss is not equal to orgin model loss\n{org_loss}\n{shard_loss}"
 
-    # unwrap model
-    if org_model.__class__.__name__ == 'ViTModel':
-        vit_model = org_model
-        shard_vit_model = sharded_model
-    else:
-        vit_model = org_model.vit
-        shard_vit_model = sharded_model.vit
+    # check grad
 
-    # check attention grad
-    org_grad = vit_model.encoder.layer[0].attention.attention.query.weight.grad
-    shard_grad = shard_vit_model.encoder.layer[0].attention.attention.query.weight.grad
-    shard_weight = shard_vit_model.encoder.layer[0].attention.attention.query.weight
+    sam = org_model
+    sharded_sam = sharded_model
+
+    # compare mask decoder grad
+
+    org_grad = sam.mask_decoder.transformer.layers[0].self_attn.q_proj.weight.grad
+    shard_grad = sharded_sam.mask_decoder.transformer.layers[0].self_attn.q_proj.weight.grad
+    shard_weight = sharded_sam.mask_decoder.transformer.layers[0].self_attn.q_proj.weight
 
     if is_distributed_tensor(shard_weight) or is_customized_distributed_tensor(shard_weight):
         shard_grad_list = [torch.zeros([*shard_grad.shape]).to('cuda') for _ in range(2)]
@@ -49,31 +46,47 @@ def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transfo
     else:
         all_shard_grad = shard_grad
     assert torch.allclose(org_grad, all_shard_grad,
-                          atol=1e-5), f"shard model grad is not equal to orgin model grad\n{org_grad}\n{shard_grad}"
-    
+                          atol=1e-5), f"shard model grad is not equal to orgin model grad\n{org_grad}\n{all_shard_grad}"
+
+    # compare vision_encoder grad
+    org_grad = sam.vision_encoder.layers[0].mlp.lin1.weight.grad
+    shard_grad = sharded_sam.vision_encoder.layers[0].mlp.lin1.weight.grad
+    shard_weight = sharded_sam.vision_encoder.layers[0].mlp.lin1.weight
+
+    if is_distributed_tensor(shard_weight) or is_customized_distributed_tensor(shard_weight):
+        shard_grad_list = [torch.zeros([*shard_grad.shape]).to('cuda') for _ in range(2)]
+        shard_grad = torch.distributed.all_gather(shard_grad_list, shard_grad)
+        all_shard_grad = torch.cat(shard_grad_list, dim=0)
+    else:
+        all_shard_grad = shard_grad
+
+    assert torch.allclose(org_grad, all_shard_grad,
+                          atol=1e-5), f"shard model grad is not equal to orgin model grad\n{org_grad}\n{all_shard_grad}"
+
 
 @parameterize('enable_fused_normalization', [True, False])
 @parameterize('enable_tensor_parallelism', [True, False])
-def run_vit_test(enable_fused_normalization, enable_tensor_parallelism):
-    sub_model_zoo = model_zoo.get_sub_registry('transformers_vit')
+def run_sam_test(enable_fused_normalization, enable_tensor_parallelism):
+    sub_model_zoo = model_zoo.get_sub_registry('transformers_sam')
     for name, (model_fn, data_gen_fn, output_transform_fn, loss_fn, _) in sub_model_zoo.items():
         org_model, sharded_model = build_model(model_fn, enable_fused_normalization, enable_tensor_parallelism)
         check_forward_backward(org_model, sharded_model, data_gen_fn, output_transform_fn, loss_fn)
+
     torch.cuda.empty_cache()
 
 
-def check_vit(rank, world_size, port):
+def check_sam(rank, world_size, port):
     disable_existing_loggers()
     colossalai.launch(config={}, rank=rank, world_size=world_size, host='localhost', port=port, backend='nccl')
-    run_vit_test()
+    run_sam_test()
 
 
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
 @clear_cache_before_run()
-def test_vit():
-    spawn(check_vit, 2)
+def test_sam():
+    spawn(check_sam, 2)
 
 
 if __name__ == "__main__":
-    test_vit()
+    test_sam()
