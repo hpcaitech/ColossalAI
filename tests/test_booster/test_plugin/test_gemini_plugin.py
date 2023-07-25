57d1bdf1d1979c639e5a069ad29cb5d211523712
flybird1111 <1829166702@qq.com>
2023-07-25 16:02:42 +0800

    [shardformer] merge blip2/chatglm  (#4321)
    * Feature/vit support (#4182)
    
    * [shardformer] added tests
    
    * [shardformer] vit test finish and support
    
    * fix attention dropout
    
    * [shardformer] support SAM (#4231)
    
    * 1.support sam 2.add fused qkv for nn.Linear
    
    * update utils support set element in list
    
    * overtwrite SamVisionAttention foward to use DropoutForParallelInput
    
    * remove unused code
    
    * [shardformer] support whisper (#4212)
    
    * support whisper
    
    * fix bug in vocabembedding
    
    * support downstream model of whisper
    
    * update readme
    
    * Feature/chatglm (#4240)
    
    * [shardformer] added tests
    
    * [shardformer] vit test finish and support
    
    * [shardformer] chatglm ready
    
    * import chatglm
    
    * [shardformer] add test kit in model zoo for chatglm
    
    * [sharformer] add first version of policy of chatglm
    
    * [shardformer] polish chatglm code
    
    * [shardformer] polish code
    
    * [shardformer] support chatglm without layernorm
    
    * [shardformer] chatglm shard without mlp sharding
    
    * [shardformer] delete some file
    
    * [shardformer] ChatGLM support layernorm sharding
    
    * [shardformer] register without auto policy
    
    * [shardformer] pre-commit check files
    
    * [shardformer] fix chatglm configuration with pre-commit
    
    * [shardformer] added tests
    
    * [shardformer] vit test finish and support
    
    * import chatglm
    
    * [shardformer] add test kit in model zoo for chatglm
    
    * [sharformer] add first version of policy of chatglm
    
    * [shardformer] polish chatglm code
    
    * [shardformer] polish code
    
    * [shardformer] support chatglm without layernorm
    
    * [shardformer] delete some file
    
    * [shardformer] ChatGLM support layernorm sharding
    
    * [shardformer] register without auto policy
    
    * [shardformer] pre-commit check files
    
    * [shardformer] support ChatGLMForConditionalGeneration & add fusedlayernorm for vit
    
    * [shardformer] support Blip2 (#4243)
    
    * support base blip2
    
    * add support for downstream blip2 model
    
    * update readme
    
    * add forward injection
    
    * skip not compatible models test
    
    * fix test for gemini and low_level_zero_pugin
    
    ---------
    
    Co-authored-by: Kun Lin <81014421+klhhhhh@users.noreply.github.com>
    Co-authored-by: FoolPlayer <45593998+FoolPlayer@users.noreply.github.com>
    Co-authored-by: klhhhhh <1412841649@qq.com>

--- tests/test_booster/test_plugin/test_gemini_plugin.py
+++ tests/test_booster/test_plugin/test_gemini_plugin.py
@@ -88,7 +88,8 @@ def check_gemini_plugin(init_method: str = 'none', early_stop: bool = True):
                 'torchvision_vit_b_16', 'torchvision_convnext_base', 'torchvision_swin_s', 'transformers_albert',
                 'transformers_albert_for_pretraining', 'transformers_bert', 'transformers_bert_for_pretraining',
                 'transformers_gpt_double_heads', 'torchaudio_hubert_base', 'torchaudio_wav2vec2_base',
-                'transformers_t5_for_conditional_generation', 'transformers_t5', 'transformers_t5_encoder_model'
+                'transformers_t5_for_conditional_generation', 'transformers_t5', 'transformers_t5_encoder_model',
+                'transformers_blip2', 'transformers_vit', 'transformers_vit_for_masked_image_modeling'
         ]:
             continue
 
