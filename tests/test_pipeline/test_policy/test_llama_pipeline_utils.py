3873db2db0e9c83186a99fbece4bf77e78431958
CjhHa1 <cjh18671720497@outlook.com>
2023-07-26 10:51:41 +0800

    finish policy tests

--- tests/test_pipeline/test_policy/test_llama_pipeline_utils.py
+++ tests/test_pipeline/test_policy/test_llama_pipeline_utils.py
@@ -12,14 +12,14 @@ from colossalai.testing import rerun_if_address_is_in_use, spawn
 from tests.kit.model_zoo import model_zoo
 
 
-def check_bloom_model_policy():
-    # create a BloomModel
+def check_llama_model_policy():
+    # create a LlamaModel
     PP_DIM = 0
     PP_SIZE = 2
     pg_mesh = ProcessGroupMesh(PP_SIZE)
     # print(pg_mesh)
     stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
-    sub_model_zoo = model_zoo.get_sub_registry('transformers_bloom')
+    sub_model_zoo = model_zoo.get_sub_registry('transformers_llama')
     for name, (model_fn, data_gen_fn, output_transform_fn, loss_fn, _) in sub_model_zoo.items():
         model = model_fn()
         stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
@@ -29,27 +29,25 @@ def check_bloom_model_policy():
         policy.set_shard_config(model_config)
         layers = policy.get_held_layers()
         if stage_manager.is_first_stage():
-            assert len(layers) == 0 + 2
+            assert len(layers) == 2 + 1
         else:
-            if name == 'transformers_bloom':
-                assert len(layers) == 1 + 1
-            elif name == 'transformers_bloom_for_token_classification':
-                assert len(layers) == 1 + 3
+            if name == "transformers_llama":
+                assert len(layers) == 2 + 1
             else:
-                assert len(layers) == 1 + 2
+                assert len(layers) == 2 + 2
 
 
 def run_dist_policy(rank, world_size, port):
     colossalai.launch(config={}, rank=rank, world_size=world_size, port=port, host='localhost')
-    check_bloom_model_policy()
+    check_llama_model_policy()
 
 
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
-def test_bloom_model_policy():
+def test_llama_model_policy():
     spawn(run_dist_policy, 2)
 
 
 if __name__ == "__main__":
-    """test the bloom model policy"""
-    test_bloom_model_policy()
+    """test the llama model policy"""
+    test_llama_model_policy()
