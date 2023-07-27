1cf9e01b050704caaeaf2f371c54dfebd92d0ed2
Hongxin Liu <lhx0217@gmail.com>
2023-07-27 13:11:47 +0800

    [hotfix] fix gemini and zero test (#4333)
    * [hotfix] fix gemini and zero test
    
    * [hotfix] fix lazy init test
    
    * [hotfix] fix lazy init test

--- tests/test_booster/test_plugin/test_gemini_plugin.py
+++ tests/test_booster/test_plugin/test_gemini_plugin.py
@@ -88,7 +88,9 @@ def check_gemini_plugin(init_method: str = 'none', early_stop: bool = True):
                 'torchvision_vit_b_16', 'torchvision_convnext_base', 'torchvision_swin_s', 'transformers_albert',
                 'transformers_albert_for_pretraining', 'transformers_bert', 'transformers_bert_for_pretraining',
                 'transformers_gpt_double_heads', 'torchaudio_hubert_base', 'torchaudio_wav2vec2_base',
-                'transformers_t5_for_conditional_generation', 'transformers_t5', 'transformers_t5_encoder_model'
+                'transformers_t5_for_conditional_generation', 'transformers_t5', 'transformers_t5_encoder_model',
+                'transformers_vit', 'transformers_vit_for_masked_image_modeling',
+                'transformers_vit_for_image_classification'
         ]:
             continue
 
@@ -99,7 +101,6 @@ def check_gemini_plugin(init_method: str = 'none', early_stop: bool = True):
                 'torchvision_shufflenet_v2_x0_5', 'torchvision_efficientnet_v2_s'
         ]:
             continue
-
         err = run_fn(init_method, model_fn, data_gen_fn, output_transform_fn)
         torch.cuda.empty_cache()
 
