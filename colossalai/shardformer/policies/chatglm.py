51b476a5bed8e543dc034e49e4c9245b1c2fc591
CjhHa1 <cjh18671720497@outlook.com>
2023-08-01 11:01:16 +0800

    fix some bugs

--- colossalai/shardformer/policies/chatglm.py
+++ colossalai/shardformer/policies/chatglm.py
@@ -28,12 +28,13 @@ class ChatGLMPolicy(Policy):
 
     def preprocess(self):
         # Resize embedding
-        vocab_size = self.model.config.padded_vocab_size
-        world_size = self.shard_config.tensor_parallel_size
+        if self.shard_config.enable_tensor_parallelism:
+            vocab_size = self.model.config.padded_vocab_size
+            world_size = self.shard_config.tensor_parallel_size
 
-        if vocab_size % world_size != 0:
-            new_vocab_size = vocab_size + world_size - vocab_size % world_size
-            self.model.resize_token_embeddings(new_vocab_size)
+            if vocab_size % world_size != 0:
+                new_vocab_size = vocab_size + world_size - vocab_size % world_size
+                self.model.resize_token_embeddings(new_vocab_size)
 
         return self.model
 
