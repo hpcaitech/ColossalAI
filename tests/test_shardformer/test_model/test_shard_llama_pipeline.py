7d5b144faee36e86f83ba66a9e5754fa57b96a6b
Jianghai <72591262+CjhHa1@users.noreply.github.com>
2023-08-01 10:35:17 +0800

    [pipeline] refactor test pipeline and remove useless utils in pipeline (#4324)
    * refactor tests
    
    * refactor bloom model
    
    * finish policy tests
    
    * refactor tests
    
    * fix test pure pipeline
    
    * remove test pipeline and cutdown launch process
    
    * refactor tests
    
    * refactor bloom model
    
    * finish policy tests
    
    * refactor tests
    
    * fix test pure pipeline
    
    * remove test pipeline and cutdown launch process

--- tests/test_shardformer/test_model/test_shard_llama_pipeline.py
+++ tests/test_shardformer/test_model/test_shard_llama_pipeline.py
@@ -5,7 +5,9 @@ import colossalai
 from colossalai.cluster import ProcessGroupMesh
 from colossalai.logging import disable_existing_loggers
 from colossalai.pipeline.stage_manager import PipelineStageManager
+from colossalai.shardformer.policies.auto_policy import get_autopolicy
 from colossalai.shardformer.policies.base_policy import Policy
+from colossalai.shardformer.shard import ShardConfig
 from colossalai.tensor.d_tensor.api import is_customized_distributed_tensor, is_distributed_tensor
 from colossalai.testing import (
     assert_hf_output_close,
@@ -18,9 +20,35 @@ from tests.kit.model_zoo import model_zoo
 from tests.test_shardformer.test_model._utils import build_model, build_pipeline_model, run_forward
 
 
-def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transform_fn, loss_fn):
-    # check forward
-    pass
+def check_llama_model_policy(name, model: torch.nn.Module, stage_manager: PipelineStageManager):
+    policy = get_autopolicy(model)
+    policy.set_model(model)
+    model_config = ShardConfig(pipeline_stage_manager=stage_manager, enable_tensor_parallelism=False)
+    policy.set_shard_config(model_config)
+    layers = policy.get_held_layers()
+    if stage_manager.is_first_stage():
+        assert len(layers) == 2 + 1
+    else:
+        if name == "transformers_llama":
+            assert len(layers) == 2 + 1
+        else:
+            assert len(layers) == 2 + 2
+
+
+def check_llama_model_pipeline_forward(name, sharded_model, stage_manager: PipelineStageManager):
+    x = torch.randint(0, 1000, (2, 3)).cuda()
+    if stage_manager.stage == 0:
+        attention_mask = torch.ones_like(x).cuda()
+        output = sharded_model(input_ids=x, attention_mask=attention_mask)
+        assert output['hidden_states'].shape == (2, 3, 128)
+    else:
+        hidden_states = torch.randint(0, 1000, (2, 3, 128)).to(torch.float32).cuda()
+        attention_mask = torch.ones((2, 3)).cuda()
+        output = sharded_model(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+        )
+        assert output[0] is not None
 
 
 @parameterize('enable_fused_normalization', [False])
@@ -28,40 +56,18 @@ def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transfo
 @parameterize('use_lazy_init', [False])
 #TODO: merge this into test_shard_llama
 def run_llama_test(enable_fused_normalization, enable_tensor_parallelism, use_lazy_init):
-    DP_DIM, PP_DIM = 0, 1
-    DP_SIZE, PP_SIZE = 2, 2
-    RANK_TO_COORDINATE = {
-        0: (0, 0),
-        1: (0, 1),
-        2: (1, 0),
-        3: (1, 1),
-    }
-    PP_RANKS_IN_GROUP = {
-        0: [0, 1],
-        1: [0, 1],
-        2: [2, 3],
-        3: [2, 3],
-    }
-    pg_mesh = ProcessGroupMesh(DP_SIZE, PP_SIZE)
+    PP_DIM = 0
+    PP_SIZE = 2
+    pg_mesh = ProcessGroupMesh(PP_SIZE)
     stage_manager = PipelineStageManager(pg_mesh, PP_DIM)
 
     sub_model_zoo = model_zoo.get_sub_registry('transformers_llama')
-    x = torch.randint(0, 1000, (2, 3)).cuda()
-    hidden_states = torch.randint(0, 1000, (2, 3, 128)).to(torch.float32).cuda()
+
     for name, (model_fn, data_gen_fn, output_transform_fn, loss_fn, _) in sub_model_zoo.items():
         org_model, sharded_model = build_pipeline_model(model_fn, stage_manager, enable_fused_normalization,
                                                         enable_tensor_parallelism, use_lazy_init)
-        if stage_manager.stage == 0:
-            attention_mask = torch.ones_like(x).cuda()
-            output = sharded_model(input_ids=x, attention_mask=attention_mask)
-            assert output['hidden_states'].shape == (2, 3, 128)
-        else:
-            attention_mask = torch.ones((2, 3)).cuda()
-            output = sharded_model(
-                hidden_states=hidden_states,
-                attention_mask=attention_mask,
-            )
-            assert output[0] is not None
+        check_llama_model_policy(name, org_model, stage_manager)
+        check_llama_model_pipeline_forward(name, sharded_model, stage_manager)
 
     torch.cuda.empty_cache()
 
@@ -76,7 +82,7 @@ def check_llama(rank, world_size, port):
 @rerun_if_address_is_in_use()
 @clear_cache_before_run()
 def test_llama():
-    spawn(check_llama, 4)
+    spawn(check_llama, 2)
 
 
 if __name__ == "__main__":
