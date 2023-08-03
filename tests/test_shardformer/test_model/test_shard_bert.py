5c6f183192a203a19e7d1dadbefe3e814c7f05d1
Bin Jia <45593998+FoolPlayer@users.noreply.github.com>
2023-08-03 14:51:36 +0800

    [test] Hotfix/fix some model test and refactor check util api (#4369)
    * fix llama test
    
    * fix test bug of bert, blip2, bloom, gpt2
    
    * fix llama test
    
    * fix opt test
    
    * fix sam test
    
    * fix sam test
    
    * fix t5 test
    
    * fix vit test
    
    * fix whisper test
    
    * fix whisper test
    
    * polish code
    
    * adjust allclose parameter
    
    * Add mistakenly deleted code
    
    * addjust allclose
    
    * change loss function for some base model

--- tests/test_shardformer/test_model/test_shard_bert.py
+++ tests/test_shardformer/test_model/test_shard_bert.py
@@ -15,10 +15,18 @@ from colossalai.testing import (
     spawn,
 )
 from tests.kit.model_zoo import model_zoo
-from tests.test_shardformer.test_model._utils import build_model, check_state_dict, run_forward
+from tests.test_shardformer.test_model._utils import build_model, check_grad, check_state_dict, run_forward
 
 
 def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transform_fn, loss_fn):
+    # unwarp model
+    if org_model.__class__.__name__ == 'BertModel':
+        bert = org_model
+        sharded_bert = sharded_model
+    else:
+        bert = org_model.bert
+        sharded_bert = sharded_model.bert
+
     # check forward
     org_output, org_loss, shard_output, shard_loss = run_forward(org_model, sharded_model, data_gen_fn,
                                                                  output_transform_fn, loss_fn)
@@ -32,42 +40,10 @@ def check_forward_backward(org_model, sharded_model, data_gen_fn, output_transfo
                           atol=1e-5), f"shard model loss is not equal to orgin model loss\n{org_loss}\n{shard_loss}"
 
     # check grad
-
-    if org_model.__class__.__name__ == 'BertModel':
-        bert = org_model
-        sharded_bert = sharded_model
-    else:
-        bert = org_model.bert
-        sharded_bert = sharded_model.bert
-
-    # compare self attention grad
-    org_grad = bert.encoder.layer[0].attention.self.query.weight.grad
-    shard_grad = sharded_bert.encoder.layer[0].attention.self.query.weight.grad
-    shard_weight = sharded_bert.encoder.layer[0].attention.self.query.weight
-
-    if is_distributed_tensor(shard_weight) or is_customized_distributed_tensor(shard_weight):
-        shard_grad_list = [torch.zeros([*shard_grad.shape]).to('cuda') for _ in range(2)]
-        shard_grad = torch.distributed.all_gather(shard_grad_list, shard_grad)
-        all_shard_grad = torch.cat(shard_grad_list, dim=0)
-    else:
-        all_shard_grad = shard_grad
-    assert torch.allclose(org_grad, all_shard_grad,
-                          atol=1e-5), f"shard model grad is not equal to orgin model grad\n{org_grad}\n{all_shard_grad}"
-
-    # compare embedding grad
-    org_grad = bert.embeddings.word_embeddings.weight.grad
-    shard_grad = sharded_bert.embeddings.word_embeddings.weight.grad
-    shard_weight = sharded_bert.embeddings.word_embeddings.weight
-
-    if is_distributed_tensor(shard_weight) or is_customized_distributed_tensor(shard_weight):
-        shard_grad_list = [torch.zeros([*shard_grad.shape]).to('cuda') for _ in range(2)]
-        shard_grad = torch.distributed.all_gather(shard_grad_list, shard_grad)
-        all_shard_grad = torch.cat(shard_grad_list, dim=0)
-    else:
-        all_shard_grad = shard_grad
-
-    assert torch.allclose(org_grad, all_shard_grad,
-                          atol=1e-5), f"shard model grad is not equal to orgin model grad\n{org_grad}\n{all_shard_grad}"
+    col_layer_for_check = ['encoder.layer[0].attention.self.query', 'embeddings.word_embeddings']
+    row_layer_for_check = ['encoder.layer[0].attention.output.dense']
+    check_grad(bert, sharded_bert, col_layer_for_check, atol=1e-7, rtol=1e-3, dim=0, verbose=False)
+    check_grad(bert, sharded_bert, row_layer_for_check, atol=1e-7, rtol=1e-3, dim=1, verbose=False)
 
 
 @parameterize('enable_fused_normalization', [False, True])
