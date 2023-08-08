d8bdf5a6df40d92ca90316bc5b38c7f9078f17e6
Baizhou Zhang <eddiezhang@pku.edu.cn>
2023-08-08 11:36:23 +0800

    erase printing

--- tests/test_shardformer/test_model/test_shard_t5.py
+++ tests/test_shardformer/test_model/test_shard_t5.py
@@ -22,8 +22,6 @@ def check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, 
     org_model, org_optimizer, sharded_model, sharded_optimizer, criterion, booster = \
         build_model_from_hybrid_plugin(model_fn, loss_fn, test_config)
 
-    print(org_model.__class__.__name__, test_config['tp_size'], test_config['pp_size'])
-
     org_loss, org_output, sharded_loss, sharded_output = \
         run_forward_backward_with_hybrid_plugin(
             org_model,
