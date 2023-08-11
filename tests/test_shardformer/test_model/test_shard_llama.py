2a51428125d37a5807c976fc4319ed5ef2abbea1
Mingyan Jiang <1829166702@qq.com>
2023-08-11 17:43:36 +0800

    [shardformer] update bloom/llama/vit/chatglm tests
    [shardformer] update bloom/llama/vit/chatglm tests
    
    [shardformer] update opt tests
    
    [shardformer] update opt tests
    
    [shardformer] update bloom/llama/vit/chatglm tests
    
    [shardformer] update bloom/llama/vit/chatglm tests
    
    [shardformer] update bloom/llama/vit/chatglm tests

--- tests/test_shardformer/test_model/test_shard_llama.py
+++ tests/test_shardformer/test_model/test_shard_llama.py
@@ -41,11 +41,15 @@ def check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, 
 
     # check last hidden state & loss
     if stage_manager is None or stage_manager.is_last_stage():
+        if test_config['precision'] == 'fp32':
+            atol, rtol = 1e-5, 1e-3
+        else:
+            atol, rtol = 5e-3, 5e-3
 
         if org_model.__class__.__name__ == 'LlamaModel':
-            check_output_hidden_state(org_output, sharded_output, stage_manager, atol=1e-5, rtol=1e-3)
+            check_output_hidden_state(org_output, sharded_output, stage_manager, atol=atol, rtol=rtol)
 
-        check_loss(org_loss, sharded_loss, atol=1e-6, rtol=1e-3)
+        check_loss(org_loss, sharded_loss, atol=atol, rtol=rtol)
 
     # unwrap model
     if org_model.__class__.__name__ == 'LlamaModel':
@@ -59,20 +63,24 @@ def check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, 
     row_layer_for_check = ['layers[0].self_attn.q_proj', 'embed_tokens']
     col_layer_for_check = ['layers[0].self_attn.o_proj']
     if stage_manager is None or stage_manager.is_first_stage():
+        if test_config['precision'] == 'fp32':
+            atol, rtol = 1e-6, 1e-4
+        else:
+            atol, rtol = 5e-3, 5e-3
         check_grad(llama_model,
                    shard_llama_model,
                    row_layer_for_check,
                    tp_group,
-                   atol=1e-6,
-                   rtol=1e-4,
+                   atol=atol,
+                   rtol=rtol,
                    dim=0,
                    verbose=False)
         check_grad(llama_model,
                    shard_llama_model,
                    col_layer_for_check,
                    tp_group,
-                   atol=1e-6,
-                   rtol=1e-4,
+                   atol=atol,
+                   rtol=rtol,
                    dim=1,
                    verbose=False)
 
@@ -80,12 +88,16 @@ def check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, 
     org_optimizer.step()
     sharded_optimizer.step()
     if stage_manager is None or stage_manager.is_first_stage():
+        if test_config['precision'] == 'fp32':
+            atol, rtol = 1e-4, 1e-3
+        else:
+            atol, rtol = 5e-3, 5e-3
         check_weight(llama_model,
                      shard_llama_model,
                      col_layer_for_check,
                      tp_group,
-                     atol=1e-4,
-                     rtol=1e-3,
+                     atol=atol,
+                     rtol=rtol,
                      dim=1,
                      verbose=False)
 
@@ -96,33 +108,34 @@ def check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, 
     'tp_size': 2,
     'pp_size': 2,
     'num_microbatches': 2,
-    'enable_fused_normalization': True,
-    'use_lazy_init': True
+    'enable_all_optimization': True,
+    'use_lazy_init': True,
+    'precision': 'fp16',
+    'initial_scale': 1,
 }, {
     'tp_size': 1,
     'pp_size': 2,
     'num_microbatches': 4,
-    'use_lazy_init': False
+    'use_lazy_init': False,
+    'precision': 'fp32',
 }, {
     'tp_size': 4,
     'pp_size': 1,
-    'enable_fused_normalization': True,
-    'use_lazy_init': False
+    'enable_all_optimization': True,
+    'use_lazy_init': False,
+    'precision': 'fp32',
 }, {
     'tp_size': 1,
     'pp_size': 4,
     'num_microbatches': 4,
-    'use_lazy_init': False
+    'use_lazy_init': False,
+    'precision': 'fp32',
 }])
 def run_llama_test(test_config):
 
     # TODO: add test_config for TP+DP after supporting & debugging it
-    # {'tp_size': 2, 'pp_size': 1, 'enable_fused_normalization': True}
-
-    # TODO: add test_config for flash attention & jit operator after supporting
 
     sub_model_zoo = model_zoo.get_sub_registry('transformers_llama')
-    test_config['precision'] = 'float'    # Do not use fp16/bf16 in testing
 
     for name, (model_fn, data_gen_fn, output_transform_fn, loss_fn, _) in sub_model_zoo.items():
         check_forward_backward(model_fn, data_gen_fn, output_transform_fn, loss_fn, test_config)
