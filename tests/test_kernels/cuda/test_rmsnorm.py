0b0b28cb24108e4ae7b893df952a1a92a3b3abbb
cuiqing.li <lixx3527@gmail.com>
2023-08-16 15:12:52 +0800

    added lisense

--- tests/test_kernels/cuda/test_rmsnorm.py
+++ tests/test_kernels/cuda/test_rmsnorm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 import os
 import pytest
 import numpy as np
--- tests/test_kernels/cuda/test_rotary_embedding.py
+++ tests/test_kernels/cuda/test_rotary_embedding.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 import pytest
 from typing import Tuple
 
