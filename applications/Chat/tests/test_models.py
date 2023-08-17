ab759e14df3b82c870cedf13e241946d287cb852
Ying Liu <yingliu@hpc-ai.tech>
2023-08-17 14:42:29 +0800

    add unit test & update chatglm default config & fix bos index issue

--- applications/Chat/tests/test_models.py
+++ applications/Chat/tests/test_models.py
@@ -9,11 +9,12 @@ from coati.models.bloom import BLOOMRM, BLOOMActor, BLOOMCritic
 from coati.models.generation import generate
 from coati.models.gpt import GPTRM, GPTActor, GPTCritic
 from coati.models.llama import LlamaActor, LlamaCritic, LlamaRM
+from coati.models.chatglm import ChatGLMActor
 from coati.models.lora import LoraLinear, convert_to_lora_module
 from coati.models.loss import GPTLMLoss, LogExpLoss, LogSigLoss, PolicyLoss, ValueLoss
 from coati.models.opt import OPTRM, OPTActor, OPTCritic
 from coati.models.utils import calc_action_log_probs, compute_reward, masked_mean
-
+from coati.models.chatglm.chatglm_tokenizer import ChatGLMTokenizer
 
 @pytest.mark.gpu
 @pytest.mark.parametrize("batch_size", [4])
@@ -23,7 +24,8 @@ from coati.models.utils import calc_action_log_probs, compute_reward, masked_mea
     lambda: GPTActor(),
     # HACK: skip llama due to long execution time
     # lambda: LlamaActor(),
-    lambda: OPTActor()
+    lambda: OPTActor(),
+    lambda: ChatGLMActor(),
 ])
 @pytest.mark.parametrize("generate_kwargs", [{
     "max_length": 64,
@@ -129,15 +131,21 @@ def test_lora(lora_rank: int,
     # HACK: skip llama due to long execution time
     # lambda: (LlamaActor(), LlamaCritic(), LlamaRM()),
     lambda: (OPTActor(), OPTCritic(), OPTRM()),
+    lambda: (ChatGLMActor()),
 ])
 @torch.no_grad()
-def test_models(models_maker: Callable[[], Tuple[Actor, Critic, RewardModel]],
+def test_models(models_maker: Callable[[], Tuple[Actor, Critic, RewardModel, Actor]],
                 batch_size: int,
                 seq_len: int):
-
+    tokenizer = ChatGLMTokenizer.from_pretrained( "THUDM/chatglm-6b", trust_remote_code=True)
+    chatglm_special_token = torch.tensor([tokenizer.gmask_token_id, tokenizer.bos_token_id]).repeat(batch_size, 1)
     actor_input = {
         "input_ids": torch.randint(0, 100, (batch_size, seq_len)),
         "attention_mask": torch.randint(0, 2, (batch_size, seq_len))
+    }
+    chatglm_actor_input ={
+        "input_ids": torch.cat((torch.randint(0, 100, (batch_size, seq_len//2)), chatglm_special_token, torch.randint(0, 100, (batch_size, seq_len//2 - 2))), dim=1),
+        "attention_mask": torch.randint(0, 2, (batch_size, 1, seq_len, seq_len))
     }
     critic_input = {
         "sequences": torch.randint(0, 100, (batch_size, seq_len)),
@@ -149,19 +157,23 @@ def test_models(models_maker: Callable[[], Tuple[Actor, Critic, RewardModel]],
         "attention_mask": torch.randint(0, 2, (batch_size, seq_len))
     }
 
-    actor, critic, rm = models_maker()
-    assert isinstance(actor, Actor)
-    base_actor_model = get_base_model(actor)
+    bloom_actor, critic, rm, chatglm_actor = models_maker()
+    assert isinstance(bloom_actor, Actor)
+    assert isinstance(chatglm_actor, Actor)
+    bloom_base_actor_model = get_base_model(bloom_actor)
+    chatglm_base_actor_model = get_base_model(chatglm_actor)
     assert isinstance(critic, Critic)
     base_critic_model = get_base_model(critic)
     assert isinstance(rm, RewardModel)
     base_rm_model = get_base_model(rm)
-
-    actor_output = actor(**actor_input)
+    chatglm_actor = chatglm_actor.float()
+    bloom_actor_output = bloom_actor(**actor_input)
+    chatglm_actor_output = chatglm_actor(**chatglm_actor_input)
     critic_output = critic(**critic_input)
     rm_output = rm(**rm_input)
 
-    assert actor_output.logits.shape[:2] == (batch_size, seq_len)
+    assert bloom_actor_output.logits.shape[:2] == (batch_size, seq_len)
+    assert chatglm_actor_output.logits.shape[:2] == (batch_size, seq_len)
     assert critic_output.shape == (batch_size, )
     assert rm_output.shape == (batch_size, )
 
@@ -228,7 +240,8 @@ if __name__ == "__main__":
 
     test_models(models_maker=lambda: (BLOOMActor(),
                                       BLOOMCritic(),
-                                      BLOOMRM()),
+                                      BLOOMRM(),
+                                      ChatGLMActor()),
                 batch_size=8,
                 seq_len=128)
 
