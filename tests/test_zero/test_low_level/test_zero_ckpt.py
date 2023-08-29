1b440a86a6c58fb8f85fc6da6fbe7e6af1f3435a
lclgy <gyang_lu@foxmail.com>
2023-08-29 17:41:26 +0800

    fix unit test

--- tests/test_zero/test_low_level/test_zero_ckpt.py
+++ tests/test_zero/test_low_level/test_zero_ckpt.py
@@ -90,7 +90,6 @@ def exam_zero_1_torch_ddp_ckpt():
     # examine the original state dict
     for torch_state, zero_state in zip(torch_state_dict['state'].values(), zero_state_dict['state'].values()):
         for t_v, z_v in zip(torch_state.values(), zero_state.values()):
-            print(t_v, z_v)
             loose_close(t_v, z_v)
 
     # empty the optimzer state
