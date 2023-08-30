b3c43bfe44a47f407c212f91549706118fc7af1b
yuehuayingxueluo <867460659@qq.com>
2023-08-30 18:01:00 +0800

    remove unused coda

--- colossalai/inference/tensor_parallel/__init__.py
+++ colossalai/inference/tensor_parallel/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from .modeling.llama import LlamaInferenceForwards
-from .policies.llama import LlamaModelInferPolicy
-=======
->>>>>>> upstream/feature/colossal-inference
 from .engine import TPInferEngine
 from .kvcache_manager import MemoryManager
 
