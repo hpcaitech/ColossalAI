f511cb1ce4d494a416c542a16efa3ecf68fd745a
yuanheng-zhao <jonathan.zhaoyh@gmail.com>
2023-08-30 14:10:34 +0800

    revise bloom test

--- tests/test_infer/test_bloom_infer.py
+++ tests/test_infer/test_bloom_infer.py
@@ -1,48 +1,51 @@
 import pytest
 import torch
-from transformers import AutoTokenizer, BloomForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer, BloomForCausalLM
 
 import colossalai
-from colossalai.cluster import ProcessGroupMesh
+from colossalai.inference.tensor_parallel import TPInferEngine
 from colossalai.logging import disable_existing_loggers
 from colossalai.shardformer import ShardConfig, ShardFormer
-from colossalai.shardformer.inference import InferenceEngine
 from colossalai.testing import clear_cache_before_run, parameterize, rerun_if_address_is_in_use, spawn
 
 TP_SIZE = 2
+MAX_BATCH_SIZE = 4
+MAX_INPUT_LEN = 16
+MAX_OUTPUT_LEN = 8
 
 
-# @parameterize
 def run():
-    # dummly set the model path, will revise later
-    # bloom model
+
     model_path = "/data3/data/model_eval_for_commerical_use/phoenix-inst-chat-7b"
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     tokenizer.pad_token = tokenizer.eos_token
-    model = BloomForCausalLM.from_pretrained(model_path, pad_token_id=tokenizer.eos_token_id)
 
     text = "Introduce some landmarks in Beijing"
     input_ids = tokenizer.encode(text, return_tensors='pt')
 
-    pg_mesh = ProcessGroupMesh(1, 1, TP_SIZE)
-    shardconfig = ShardConfig(
-        tensor_parallel_process_group=pg_mesh.get_group_along_axis(2),
-        enable_tensor_parallelism=True,
-        inference_only=True,
-    )
-    shardformer = ShardFormer(shard_config=shardconfig)
+    # model_config = BloomConfig()
+    # model = BloomForCausalLM(model_config)
+    # model = AutoModelForCausalLM.from_pretrained(model_path, pad_token_id=tokenizer.eos_token_id)
+    model = BloomForCausalLM.from_pretrained(model_path, pad_token_id=tokenizer.eos_token_id)
+    model = model.half()
+    model.to(torch.cuda.current_device())
 
-    infer_engine = InferenceEngine(model.half(), 4, 12, 8, tp_size=TP_SIZE)
+    shard_config = ShardConfig(enable_tensor_parallelism=True, inference_only=True)
+    shardformer = ShardFormer(shard_config=shard_config)
+
+    infer_engine = TPInferEngine(model, MAX_BATCH_SIZE, MAX_INPUT_LEN, MAX_OUTPUT_LEN)
+    infer_engine.prepare_with_shard_config(shard_config=shard_config)
     infer_engine.shard_model_by(shardformer)
 
     generate_kwargs = dict(do_sample=False)
-    outputs = infer_engine.generate_by_set_infer_state(input_ids, generate_kwargs)
+    outputs = infer_engine.generate(input_ids, generate_kwargs)
 
-    output_text = tokenizer.decode(outputs)
+    print(outputs)
+    output_text = tokenizer.decode(outputs[0])
     print(output_text)
 
 
-def check_bloom(rank, world_size, port):
+def check_engine(rank, world_size, port):
     disable_existing_loggers()
     colossalai.launch(config={}, rank=rank, world_size=world_size, host='localhost', port=port, backend='nccl')
     run()
@@ -51,9 +54,9 @@ def check_bloom(rank, world_size, port):
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
 @clear_cache_before_run()
-def test_bloom_infer():
-    spawn(check_bloom, TP_SIZE)
+def test_engine_infer():
+    spawn(check_engine, TP_SIZE)
 
 
 if __name__ == '__main__':
-    test_bloom_infer()
+    test_engine_infer()
