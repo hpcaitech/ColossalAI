aca731a280389a6955553e4de0e32612a17a2388
Yuanheng Zhao <54058983+yuanheng-zhao@users.noreply.github.com>
2023-08-30 18:09:37 +0800

    [infer] Add Bloom inference policy and replaced methods (#4553)
    * add bloom inference methods and policy
    
    * enable pass BatchInferState from model forward
    
    * revise bloom infer layers/policies
    
    * add engine for inference (draft)
    
    * add test for bloom infer
    
    * fix bloom infer policy and flow
    
    * revise bloom test
    
    * fix bloom file path
    
    * remove unused codes
    
    * fix bloom modeling
    
    * fix dir typo
    
    * fix trivial
    
    * fix policy
    
    * clean pr
    
    * trivial fix
    
    * trivial

--- colossalai/shardformer/policies/auto_policy.py
+++ colossalai/shardformer/policies/auto_policy.py
@@ -137,6 +137,11 @@ _INFER_POLICY_LIST = {
         PolicyLocation(file_name="llama", class_name="LlamaModelInferPolicy"),
     "transformers.models.llama.modeling_llama.LlamaForCausalLM":
         PolicyLocation(file_name="llama", class_name="LlamaModelInferPolicy"),
+    # Bloom
+    "transformers.models.bloom.modeling_bloom.BloomModel":
+        PolicyLocation(file_name="bloom", class_name="BloomModelInferPolicy"),
+    "transformers.models.bloom.modeling_bloom.BloomForCausalLM":
+        PolicyLocation(file_name="bloom", class_name="BloomModelInferPolicy"),
 }
 
 
@@ -144,9 +149,8 @@ def import_policy(policy_location: PolicyLocation, inference_only: Optional[bool
     """
     Dynamically import a Policy class based on the policy location.
     """
-    
     if inference_only:
-        module_name = f"colossalai.inference.tensor_parallel.pollcies.{policy_location.file_name}"
+        module_name = f"colossalai.inference.tensor_parallel.policies.{policy_location.file_name}"
     else:
         module_name = f"colossalai.shardformer.policies.{policy_location.file_name}"
     module = importlib.import_module(module_name)
