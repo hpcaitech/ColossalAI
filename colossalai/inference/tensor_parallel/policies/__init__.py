3c42dc296e1532c01ac72f207aa38084eddf8f0f
yuehuayingxueluo <867460659@qq.com>
2023-08-30 18:12:47 +0800

    fix conflict

--- colossalai/inference/tensor_parallel/policies/__init__.py
+++ colossalai/inference/tensor_parallel/policies/__init__.py
@@ -1,8 +1,4 @@
 from .bloom import BloomModelInferPolicy
 from .llama import LlamaModelInferPolicy
 
-<<<<<<< HEAD
 __all__ = ['BloomModelInferPolicy', 'LlamaModelInferPolicy']
-=======
-__all__ = ['BloomModelInferPolicy', 'LlamaModelInferPolicy']
->>>>>>> upstream/feature/colossal-inference
