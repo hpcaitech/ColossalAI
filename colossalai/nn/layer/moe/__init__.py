a0c89ea6df13460b34c7e77ffe11078db81f99cf
Xuanlei Zhao <43881818+oahzxl@users.noreply.github.com>
2023-09-04 00:18:02 +0800

    [moe] support local moe and fix bugs (#4574)
    * add local moe
    
    * update moe layer

--- colossalai/nn/layer/moe/__init__.py
+++ colossalai/nn/layer/moe/__init__.py
@@ -5,6 +5,6 @@ from .routers import MoeRouter, Top1Router, Top2Router
 from .utils import NormalNoiseGenerator, UniformNoiseGenerator, build_ffn_experts
 
 __all__ = [
-    'EPMLPExperts', 'TPMLPExperts', 'Top1Router', 'Top2Router', 'MoeLayer', 'MoeModule', 'NormalNoiseGenerator',
+    'EPMLPExperts', 'TPMLPExperts', 'Top1Router', 'Top2Router', 'MoeModule', 'MoeLayer', 'NormalNoiseGenerator',
     'UniformNoiseGenerator', 'build_ffn_experts', 'SparseMLP', 'MoeRouter', 'MoeCheckpintIO'
 ]
