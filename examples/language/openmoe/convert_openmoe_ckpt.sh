bbae495ef37f7d7d5aa128c34fedff2d9ee504cd
oahzxl <xuanlei.zhao@gmail.com>
2023-09-04 13:25:04 +0800

    init

---
+++ examples/language/openmoe/convert_openmoe_ckpt.sh
@@ -0,0 +1,1 @@
+python convert_openmoe_ckpt.py --t5x_checkpoint_path /data3/users/lczxl/OpenMoE/openmoe_base --config_file ./openmoe_config.json --pytorch_dump_path /data3/users/lczxl/OpenMoE/openmoe_base_pytorch
