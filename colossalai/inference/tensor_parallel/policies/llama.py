b227943d77de11a3e262a97240f08ff8851c65ae
Cuiqing Li <lixx3527@gmail.com>
2023-09-04 10:56:19 +0800

    [BUG FIX]Fix test engine in CI and non-vllm kernels llama forward  (#4592)
    * fix tests
    
    * clean
    
    * clean
    
    * fix bugs
    
    * add
    
    * fix llama non-vllm kernels bug
    
    * modify
    
    * clean codes

--- colossalai/inference/tensor_parallel/policies/llama.py
+++ colossalai/inference/tensor_parallel/policies/llama.py
@@ -16,12 +16,6 @@ class LlamaModelInferPolicy(LlamaForCausalLMPolicy):
         policy = super().module_policy()
         self.shard_config._infer()
 
-        # example for replace layer or decoder
-        # if self.shard_config.enable_flash_attention:
-        #     policy[LlamaAttention] = ModulePolicyDescription(method_replacement={
-        #         'forward': get_llama_flash_attention_forward(),
-        #     })
-
         infer_forward = LlamaInferenceForwards.llama_model_forward
         method_replacement = {'forward': partial(infer_forward)}
         self.append_or_create_method_replacement(description=method_replacement, policy=policy, target_key=LlamaModel)
