b34e44eea3ecdf1374d3666ddb19c933f18c99aa
Yuanheng Zhao <54058983+yuanheng-zhao@users.noreply.github.com>
2023-09-05 11:38:50 +0800

    [kernel] Add triton layer norm & replace norm for bloom (#4609)
    * add layernorm for inference
    
    * add test for layernorm kernel
    
    * add bloom layernorm replacement policy
    
    * trivial: path

--- colossalai/kernel/triton/__init__.py
+++ colossalai/kernel/triton/__init__.py
@@ -1,4 +1,5 @@
-from .context_attention import llama_context_attn_fwd, bloom_context_attn_fwd
-from .softmax import softmax
+from .context_attention import bloom_context_attn_fwd, llama_context_attn_fwd
 from .copy_kv_cache_dest import copy_kv_cache_to_dest
+from .fused_layernorm import layer_norm
 from .rms_norm import rmsnorm_forward
+from .softmax import softmax
