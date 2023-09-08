d1c5f584bef324a7dc3a362e46b54ac44a7e267a
Mingyan Jiang <1829166702@qq.com>
2023-09-08 20:03:24 +0800

    [example] llama2 add finetune example

--- examples/language/llama2/scripts/finetune_7B/finetune.sh
+++ examples/language/llama2/scripts/finetune_7B/finetune.sh
@@ -8,12 +8,10 @@ HOSTFILE=$(realpath hosts.txt)
 
 cd ../..
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-export TORCH_CUDA_ALLOC_HOST_RESERVED=0
 torchrun --standalone --nproc_per_node 4 train.py \
     --mode "finetune" \
     --plugin "hybrid_parallel" \
     --config "7b" \
     --dataset "yizhongw/self_instruct" \
-    --model_path "/home/lcjmy/data3/llama" \
+    --model_path "/path/llama" \
     --task_name "super_natural_instructions"
--- examples/language/llama2/scripts/pretrain_7B/pretrain.sh
+++ examples/language/llama2/scripts/pretrain_7B/pretrain.sh
@@ -8,8 +8,6 @@ HOSTFILE=$(realpath hosts.txt)
 
 cd ../..
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-export TORCH_CUDA_ALLOC_HOST_RESERVED=0
 torchrun --standalone --nproc_per_node 4 train.py \
     --mode "pretrain" \
     --plugin "hybrid_parallel" \
