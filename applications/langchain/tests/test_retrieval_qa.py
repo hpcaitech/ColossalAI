e700b85a31a81361488e4eb43e167e1853653d97
YeAnbang <yab0576@163.com>
2023-09-13 18:35:52 +0800

    fix build_on_pr.yml

--- applications/langchain/tests/test_retrieval_qa.py
+++ applications/langchain/tests/test_retrieval_qa.py
@@ -2,12 +2,6 @@ from colossalqa.retrieval_conversation_universal import UniversalRetrievalConver
 import os
 
 def test_en_retrievalQA():
-    os.environ["ZH_MODEL_PATH"] = "/data3/data/model_eval_for_non_commercial_use/chatglm2-6b"
-    os.environ["ZH_MODEL_NAME"] = "chatglm"
-    os.environ["EN_MODEL_PATH"] = "/data3/data/model_eval_for_commerical_use/Llama-2-7b-hf"
-    os.environ["EN_MODEL_NAME"] = "llama"
-    os.environ["TEST_DATA_PATH_EN"] = "/home/lcyab/data3/ColossalAI/applications/langchain/data/test_data/companies.txt"
-    os.environ["TEST_DATA_PATH_ZH"] = "/home/lcyab/data3/ColossalAI/applications/langchain/data/test_data/companies_zh.txt"
     data_path_en = os.environ.get('TEST_DATA_PATH_EN')
     data_path_zh = os.environ.get('TEST_DATA_PATH_ZH')
     qa_session = UniversalRetrievalConversation(files_en=[[data_path_en, 'company information']], files_zh=[[data_path_zh, 'company information']])
@@ -16,12 +10,6 @@ def test_en_retrievalQA():
     # assert 'Marriott' in ans
 
 def test_zh_retrievalQA():
-    os.environ["ZH_MODEL_PATH"] = "/data3/data/model_eval_for_non_commercial_use/chatglm2-6b"
-    os.environ["ZH_MODEL_NAME"] = "chatglm"
-    os.environ["EN_MODEL_PATH"] = "/data3/data/model_eval_for_commerical_use/Llama-2-7b-hf"
-    os.environ["EN_MODEL_NAME"] = "llama"
-    os.environ["TEST_DATA_PATH_EN"] = "/home/lcyab/data3/ColossalAI/applications/langchain/data/test_data/companies.txt"
-    os.environ["TEST_DATA_PATH_ZH"] = "/home/lcyab/data3/ColossalAI/applications/langchain/data/test_data/companies_zh.txt"
     data_path_en = os.environ.get('TEST_DATA_PATH_EN')
     data_path_zh = os.environ.get('TEST_DATA_PATH_ZH')
     qa_session = UniversalRetrievalConversation(files_en=[[data_path_en, 'company information']], files_zh=[[data_path_zh, 'company information']])
