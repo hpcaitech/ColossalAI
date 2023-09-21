3a608b21f75c7f51a28378f3ea180a6522745489
oahzxl <xuanlei.zhao@gmail.com>
2023-09-21 15:49:34 +0800

    update benchmark

--- examples/language/openmoe/benchmark/benchmark_cai.py
+++ examples/language/openmoe/benchmark/benchmark_cai.py
@@ -1,22 +1,28 @@
 import datasets
 import torch
+import torch.distributed as dist
 import transformers
 from model.modeling_openmoe import OpenMoeForCausalLM
+from model.openmoe_policy import OpenMoeForCausalLMPolicy
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from transformers import Adafactor
 from transformers.models.llama import LlamaConfig
-from utils import SimpleTimer, print_model_numel
+from utils import PerformanceEvaluator, get_model_numel
 
 import colossalai
 from colossalai import get_default_parser
 from colossalai.booster import Booster
 from colossalai.booster.plugin import LowLevelZeroPlugin
+from colossalai.booster.plugin.moe_hybrid_parallel_plugin import MoeHybridParallelPlugin
 from colossalai.cluster import DistCoordinator
 from colossalai.logging import disable_existing_loggers, get_dist_logger
 from colossalai.moe.manager import MOE_MANAGER
-from colossalai.moe.utils import skip_init
 from colossalai.utils import get_current_device
+
+
+def move_to_cuda(batch, device):
+    return {k: v.to(device) for k, v in batch.items()}
 
 
 class RandomDataset(Dataset):
@@ -25,32 +31,64 @@ class RandomDataset(Dataset):
         self.num_samples = num_samples
         self.max_length = max_length
         self.input_ids = torch.randint(0, vocab_size, (num_samples, max_length), device=get_current_device())
-        self.attention_mask = torch.ones_like(self.input_ids, device=get_current_device())
+        self.attention_mask = torch.ones_like(self.input_ids)
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
         return {
-            'input_ids': self.input_ids[idx],
-            'attention_mask': self.attention_mask[idx],
-            'labels': self.input_ids[idx]
+            "input_ids": self.input_ids[idx],
+            "attention_mask": self.attention_mask[idx],
+            "labels": self.input_ids[idx],
         }
 
 
 def parse_args():
+    # basic settings
     parser = get_default_parser()
-    # TODO: add model_name
-    # parser.add_argument("--model_name", type=str, default="base", choices=["base", "8b"],
-    #                     help="Path to pretrained model or model identifier from huggingface.co/models.")
-    parser.add_argument("--num_epoch", type=int, default=1, help="Number of epochs.")
-    parser.add_argument("--batch_size",
-                        type=int,
-                        default=4,
-                        help="Batch size (per dp group) for the training dataloader.")
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="base",
+        choices=["base", "8b"],
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=4,
+        help="Batch size (per dp group) for the training dataloader.",
+    )
+    parser.add_argument(
+        "--seq_length",
+        type=int,
+        default=2048,
+        help="sequence length for the training dataloader.",
+    )
     parser.add_argument("--seed", type=int, default=42, help="A seed for reproducible training.")
-    parser.add_argument("--num_samples", type=int, default=1000, help="Number of samples in the dataset.")
-
+    parser.add_argument(
+        "--plugin",
+        type=str,
+        default="hybrid",
+        help="parallel plugin",
+        choices=["zero1", "zero2", "hybrid"],
+    )
+    # hybrid plugin
+    parser.add_argument("--pp_size", type=int, default=2, help="pp size")
+    parser.add_argument("--dp_size", type=int, default=1, help="dp size")
+    parser.add_argument("--ep_size", type=int, default=2, help="ep size")
+    parser.add_argument("--zero_stage", type=int, default=1, help="zero stage in hybrid plugin")
+    parser.add_argument("--microbatch_size", type=int, default=1, help="microbatch size")
+    # kernel
+    parser.add_argument(
+        "--use_kernel",
+        action="store_true",
+        help="Use kernel optim. Need to install flash attention, apex, triton to enable all kernel optimizations.",
+    )
+    # bench
+    parser.add_argument("--warmup", type=int, default=20)
+    parser.add_argument("--active", type=int, default=20)
     args = parser.parse_args()
     return args
 
@@ -58,52 +96,9 @@ def parse_args():
 def main():
     args = parse_args()
 
-    MDOEL_CONFIG = {
-        "architectures": ["OpenMoeForCausalLM"],
-        "capacity_factor_eval": 2.0,
-        "capacity_factor_train": 1.25,
-        "drop_tks": True,
-        "dropout_rate": 0.0,
-        "expert_parallel": None,
-        "gated": True,
-        "head_dim": 64,
-        "hidden_act": "swiglu",
-        "hidden_size": 768,
-        "intermediate_size": 2048,
-        "label_smoothing": 0.0,
-        "layer_norm_epsilon": 1e-06,
-        "min_capacity": 4,
-        "moe_layer_interval": 4,
-        "noisy_policy": None,
-        "num_attention_heads": 12,
-        "num_experts": 16,
-        "num_hidden_layers": 12,
-        "num_key_value_heads": 12,
-        "pretraining_tp": 1,
-        "rope_scaling": None,
-        "router_aux_loss_factor": 0.01,
-        "router_z_loss_factor": 0.0001,
-        "topk": 2,
-        "torch_dtype": "float32",
-        "vocab_size": 256384,
-        "z_loss_factor": 0.0001
-    }
-    OPTIM_CONFIG = {
-        "decay_rate": -0.8,
-        "weight_decay": 0.01,
-    }
-
-    # update config from args
-    for k in MDOEL_CONFIG:
-        if hasattr(args, k):
-            MDOEL_CONFIG[k] = getattr(args, k)
-
     # Launch ColossalAI
     colossalai.launch_from_torch(config={}, seed=args.seed)
     coordinator = DistCoordinator()
-
-    # Set up moe
-    MOE_MANAGER.setup(seed=42, parallel="EP")
 
     # Manage loggers
     disable_existing_loggers()
@@ -115,70 +110,122 @@ def main():
         datasets.utils.logging.set_verbosity_error()
         transformers.utils.logging.set_verbosity_error()
 
+    # Set plugin
+    booster_kwargs = {}
+    if args.plugin == "zero1":
+        dp_size = dist.get_world_size()
+        plugin = LowLevelZeroPlugin(initial_scale=2**5, stage=1)
+        MOE_MANAGER.setup(
+            seed=42,
+            parallel="EP",
+            use_kernel_optim=args.use_kernel,
+        )
+    elif args.plugin == "zero2":
+        dp_size = dist.get_world_size()
+        plugin = LowLevelZeroPlugin(initial_scale=2**5, stage=2)
+        MOE_MANAGER.setup(
+            seed=42,
+            parallel="EP",
+            use_kernel_optim=args.use_kernel,
+        )
+    elif args.plugin == "hybrid":
+        dp_size = dist.get_world_size() // args.pp_size
+        plugin = MoeHybridParallelPlugin(
+            tp_size=1,
+            pp_size=args.pp_size,
+            zero_stage=args.zero_stage,
+            microbatch_size=args.microbatch_size,
+            custom_policy=OpenMoeForCausalLMPolicy(),
+            enable_fused_normalization=args.use_kernel,
+            enable_jit_fused=args.use_kernel,
+        )
+        MOE_MANAGER.setup(
+            seed=42,
+            parallel="EP",
+            mode="fixed",
+            fixed_dp_size=args.dp_size,
+            fixed_ep_size=args.ep_size,
+            fixed_pp_size=args.pp_size,
+            use_kernel_optim=args.use_kernel,
+        )
+    else:
+        raise ValueError(f"Invalid plugin {args.plugin}")
+    logger.info(f"Set plugin as {plugin}", ranks=[0])
+
     # Build OpenMoe model
-    config = LlamaConfig()
-    for k, v in MDOEL_CONFIG.items():
-        setattr(config, k, v)
-
-    with skip_init():
-        model = OpenMoeForCausalLM(config)
-
+    repo_name = "hpcaitech/openmoe-" + args.model_name
+    config = LlamaConfig.from_pretrained(repo_name)
+    setattr(config, "router_aux_loss_factor", 0.1)
+    setattr(config, "router_z_loss_factor", 0.1)
+    setattr(config, "label_smoothing", 0.1)
+    setattr(config, "z_loss_factor", 0.1)
+    model = OpenMoeForCausalLM(config)
     logger.info(f"Finish init model with config:\n{config}", ranks=[0])
-    model_param = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    logger.info(f"Model param count: {model_param/1e6:.2f}M", ranks=[0])
 
     # Enable gradient checkpointing
     model.gradient_checkpointing_enable()
 
-    # Set plugin
-    plugin = LowLevelZeroPlugin(initial_scale=2**5, stage=2)
-    logger.info(f"Set plugin as {plugin}", ranks=[0])
-
     # Prepare tokenizer and dataloader
-    dataset = RandomDataset(num_samples=args.num_samples)
-    dataloader = plugin.prepare_dataloader(dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+    dataset = RandomDataset(
+        num_samples=args.batch_size * (args.warmup + args.active + 1) * dp_size,
+        max_length=args.seq_length,
+    )
+    dataloader = plugin.prepare_dataloader(dataset, batch_size=args.batch_size)
 
     # Set optimizer
-    optimizer = Adafactor(model.parameters(),
-                          decay_rate=OPTIM_CONFIG["decay_rate"],
-                          weight_decay=OPTIM_CONFIG["weight_decay"])
+    optimizer = Adafactor(model.parameters(), weight_decay=0.01)
+
+    model_numel = get_model_numel(model)
+    performance_evaluator = PerformanceEvaluator(
+        model_numel,
+        enable_grad_checkpoint=True,
+        ignore_steps=args.warmup,
+        dp_world_size=dp_size,
+    )
 
     # Set booster
-    booster = Booster(plugin=plugin)
+    booster = Booster(plugin=plugin, **booster_kwargs)
     model, optimizer, _, dataloader, _ = booster.boost(model=model, optimizer=optimizer, dataloader=dataloader)
+    use_pipeline = (isinstance(booster.plugin, MoeHybridParallelPlugin) and booster.plugin.pp_size > 1)
+    is_pp_last_stage = use_pipeline and booster.plugin.stage_manager.is_last_stage()
+    logger.info(f"Finish init booster", ranks=[0])
 
-    # Start benchmark
+    # Start finetuning
+    logger.info(f"Start finetuning", ranks=[0])
     model.train()
-    logger.info(f"Start benchmark", ranks=[0])
+    train_dataloader_iter = iter(dataloader)
+    total_len = len(train_dataloader_iter) - 1
+    exmaple_data = next(train_dataloader_iter)
+    with tqdm(range(total_len), disable=not coordinator.is_master()) as pbar:
+        for step in pbar:
+            performance_evaluator.on_step_start(step)
+            if use_pipeline:
+                # Forward pass
+                outputs = booster.execute_pipeline(
+                    train_dataloader_iter,
+                    model,
+                    lambda x, y: x.loss,
+                    optimizer,
+                    return_loss=True,
+                    return_outputs=True,
+                )
+                # Backward and optimize
+                if is_pp_last_stage:
+                    loss = outputs["loss"]
+                    pbar.set_postfix({"loss": loss.item()})
+            else:
+                # Forward pass
+                data = move_to_cuda(data, torch.cuda.current_device())
+                outputs = model(**data)
+                loss = outputs["loss"]
+                # Backward
+                booster.backward(loss, optimizer)
+                pbar.set_postfix({"loss": loss.item()})
 
-    timer = SimpleTimer()
-    for epoch in range(args.num_epoch):
-        for batch in tqdm(dataloader, desc=f'Epoch [{epoch + 1}]', disable=not coordinator.is_master()):
-            timer.start("train_step")
-
-            # Forward
-            timer.start("forward")
-            outputs = model(use_cache=False, chunk_head=True, **batch)
-            loss = outputs['loss']
-            torch.cuda.synchronize()
-            timer.stop("forward")
-
-            # Backward
-            timer.start("backward")
-            booster.backward(loss, optimizer)
-            torch.cuda.synchronize()
-            timer.stop("backward")
-
-            # Optimizer step
-            timer.start("optimizer_step")
             optimizer.step()
             optimizer.zero_grad()
-            torch.cuda.synchronize()
-            timer.stop("optimizer_step")
-
-            timer.stop("train_step")
-
-    logger.info(f"Benchmark result:\n{repr(timer)}", ranks=[0])
+            performance_evaluator.on_step_end(exmaple_data["input_ids"])
+    performance_evaluator.on_fit_end()
 
 
 if __name__ == "__main__":
--- examples/language/openmoe/benchmark/utils.py
+++ examples/language/openmoe/benchmark/utils.py
@@ -1,61 +1,126 @@
-import dataclasses
-import time
-from typing import Dict
+from time import time
+from typing import Optional
 
+import torch
 import torch.distributed as dist
 import torch.nn as nn
+from torch import Tensor
+
 from colossalai.logging import DistributedLogger
 
 
-def print_model_numel(logger: DistributedLogger,
-                      model: nn.Module) -> None:
+def print_model_numel(logger: DistributedLogger, model: nn.Module) -> None:
     B = 1024**3
     M = 1024**2
     K = 1024
     outputs = "Model param count: "
     model_param = sum(p.numel() for p in model.parameters() if p.requires_grad)
     if model_param >= B:
-        outputs += f'{model_param / B:.2f} B\n'
+        outputs += f"{model_param / B:.2f} B\n"
     elif model_param >= M:
-        outputs += f'{model_param / M:.2f} M\n'
+        outputs += f"{model_param / M:.2f} M\n"
     elif model_param >= K:
-        outputs += f'{model_param / K:.2f} K\n'
+        outputs += f"{model_param / K:.2f} K\n"
     else:
-        outputs += f'{model_param}\n'
+        outputs += f"{model_param}\n"
     logger.info(outputs, ranks=[0])
 
 
-@dataclasses.dataclass
-class TimingItem():
-    last_time: float = 0.0
-    total_time: float = 0.0
-    count: float = 0
-
-    def __str__(self) -> str:
-        return f"average time: {self.total_time/self.count * 1000:.2f} ms"
+def get_model_numel(model: nn.Module) -> None:
+    model_param = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return model_param
 
 
-class SimpleTimer():
-    def __init__(self, warmup: int = 20) -> None:
-        self.timing_items: Dict[str, TimingItem] = {}
-        self.warmup = warmup
+def divide(x: float, y: float) -> float:
+    if y == 0:
+        return float("inf")
+    elif y == float("inf"):
+        return float("nan")
+    return x / y
 
-    def start(self, name: str):
-        if name not in self.timing_items:
-            self.timing_items[name] = TimingItem()
-        self.timing_items[name].last_time = time.time()
 
-    def stop(self, name: str):
-        assert name in self.timing_items
-        timing_item = self.timing_items[name]
-        timing_item.total_time += time.time() - timing_item.last_time
-        timing_item.count += 1
-        if timing_item.count > self.warmup:
-            timing_item.count = 0
-            timing_item.total_time = 0.0
+@torch.no_grad()
+def all_reduce_mean(x: float, world_size: int) -> float:
+    if world_size == 1:
+        return x
+    tensor = torch.tensor([x], device=torch.cuda.current_device())
+    dist.all_reduce(tensor)
+    tensor = tensor / world_size
+    return tensor.item()
 
-    def __repr__(self) -> str:
-        result = "[Timer]:\n"
-        for name, timing_item in self.timing_items.items():
-            result += f"    {name}: {timing_item}\n"
-        return result
+
+class Timer:
+
+    def __init__(self) -> None:
+        self.start_time: Optional[float] = None
+        self.duration: float = 0.0
+
+    def start(self) -> None:
+        self.start_time = time()
+
+    def end(self) -> None:
+        assert self.start_time is not None
+        self.duration += time() - self.start_time
+        self.start_time = None
+
+    def reset(self) -> None:
+        self.duration = 0.0
+
+
+class PerformanceEvaluator:
+    """
+        Callback for valuate the performance of the model.
+    Args:
+        actor_num_params: The number of parameters of the actor model.
+        critic_num_params: The number of parameters of the critic model.
+        initial_model_num_params: The number of parameters of the initial model.
+        reward_model_num_params: The number of parameters of the reward model.
+        enable_grad_checkpoint: Whether to enable gradient checkpointing.
+        ignore_episodes: The number of episodes to ignore when calculating the performance.
+    """
+
+    def __init__(
+        self,
+        model_numel: int,
+        enable_grad_checkpoint: bool = False,
+        ignore_steps: int = 0,
+        dp_world_size: Optional[int] = None,
+    ) -> None:
+        self.model_numel = model_numel
+        self.enable_grad_checkpoint = enable_grad_checkpoint
+        self.ignore_steps = ignore_steps
+        self.dp_world_size = dp_world_size
+        self.world_size = dist.get_world_size()
+        self.disable: bool = False
+        self.timer = Timer()
+        self.num_samples: int = 0
+        self.flop: int = 0
+
+    def on_step_start(self, step: int) -> None:
+        self.disable = self.ignore_steps > 0 and step < self.ignore_steps
+        if self.disable:
+            return
+        torch.cuda.synchronize()
+        self.timer.start()
+
+    def on_step_end(self, input_ids: Tensor, **kwargs) -> None:
+        if self.disable:
+            return
+        torch.cuda.synchronize()
+        self.timer.end()
+
+        batch_size, seq_len = input_ids.shape
+
+        self.num_samples += batch_size
+        self.flop += (batch_size * seq_len * self.model_numel * 2 * (3 + int(self.enable_grad_checkpoint)))
+
+    def on_fit_end(self) -> None:
+        avg_duration = all_reduce_mean(self.timer.duration, self.world_size)
+        avg_throughput = self.num_samples * self.dp_world_size / (avg_duration + 1e-12)
+        mp_world_size = self.world_size // self.dp_world_size
+        avg_tflops_per_gpu = self.flop / 1e12 / (avg_duration + 1e-12) / mp_world_size
+        if dist.get_rank() == 0:
+            print(
+                f"num_samples: {self.num_samples}, dp_world_size: {self.dp_world_size}, flop: {self.flop}, avg_duration: {avg_duration}, "
+                f"avg_throughput: {avg_throughput}")
+            print(f"Throughput: {avg_throughput:.2f} samples/sec, TFLOPS per GPU: {avg_tflops_per_gpu:.2f}")
