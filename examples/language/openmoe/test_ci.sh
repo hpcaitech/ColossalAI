552133145534a58ea136bd98bcef2563ede6b0ab
oahzxl <xuanlei.zhao@gmail.com>
2023-09-21 17:26:50 +0800

    update ci

--- examples/language/openmoe/test_ci.sh
+++ examples/language/openmoe/test_ci.sh
@@ -4,7 +4,8 @@ pip install -r requirements.txt
 python infer.py --model "test"
 
 # train
-torchrun --standalone --nproc_per_node 2 train.py \
+torchrun --standalone --nproc_per_node 4 train.py \
     --num_epoch 1 \
     --model_name "test" \
-    --plugin zero2
+    --plugin zero2 \
+    --batch_size 1
