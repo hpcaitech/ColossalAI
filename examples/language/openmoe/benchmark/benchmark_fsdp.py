1dd685a6be85f998e6fd17b9d144ce505136cc50
oahzxl <xuanlei.zhao@gmail.com>
2023-09-21 14:44:26 +0800

    fsdp benchmark

--- examples/language/openmoe/benchmark/benchmark_fsdp.py
+++ examples/language/openmoe/benchmark/benchmark_fsdp.py
@@ -4,23 +4,18 @@ import os
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
-import torch.optim as optim
+import tqdm
 from model.modeling_openmoe import LlamaConfig, OpenMoeForCausalLM
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp.fully_sharded_data_parallel import MixedPrecision
 from torch.utils.data import Dataset
 from torch.utils.data.distributed import DistributedSampler
+from transformers import Adafactor
 from transformers.models.llama import LlamaConfig
 
 from colossalai.moe.manager import MOE_MANAGER
 
-
-def setup(rank, world_size):
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "14523"
-
-    # initialize the process group
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+from .utils import PerformanceEvaluator, get_model_numel
 
 
 class RandomDataset(Dataset):
@@ -42,11 +37,51 @@ class RandomDataset(Dataset):
         }
 
 
-def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=None):
+def fsdp_main(rank, world_size, args):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "14523"
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+    MOE_MANAGER.setup(seed=42, parallel=None, use_kernel_optim=False)
+
+    dataset = RandomDataset(max_length=args.seq_length)
+    sampler = DistributedSampler(dataset, rank=rank, num_replicas=world_size, shuffle=False)
+    train_kwargs = {"batch_size": args.batch_size, "sampler": sampler}
+    train_loader = torch.utils.data.DataLoader(dataset, **train_kwargs)
+    torch.cuda.set_device(rank)
+
+    config = LlamaConfig.from_pretrained("hpcaitech/openmoe-%s" % args.model_name)
+    setattr(config, "router_aux_loss_factor", 0.1)
+    setattr(config, "router_z_loss_factor", 0.1)
+    setattr(config, "label_smoothing", 0.1)
+    setattr(config, "z_loss_factor", 0.1)
+    model = OpenMoeForCausalLM(config).to(rank)
+    # 使用FSDP将model warp起来
+    model = FSDP(
+        model,
+        mixed_precision=MixedPrecision(
+            param_dtype=torch.float16,
+            reduce_dtype=torch.float16,
+            buffer_dtype=torch.float16,
+        ),
+    )
+    optimizer = Adafactor(model.parameters())
     model.train()
 
-    for idx, data in enumerate(train_loader):
+    model_numel = get_model_numel(model)
+    performance_evaluator = PerformanceEvaluator(
+        model_numel,
+        enable_grad_checkpoint=True,
+        ignore_steps=args.warm_up,
+        dp_world_size=dist.get_world_size(),
+    )
 
+    for step, data in tqdm.tqdm(enumerate(train_loader), total=args.warm_up + args.active):
+        if step == args.warm_up + args.active:
+            break
+
+        performance_evaluator.on_step_start(step)
         input_ids, attention_mask, labels = (
             data["input_ids"].cuda(),
             data["attention_mask"].cuda(),
@@ -63,103 +98,29 @@ def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler
         loss = output["loss"]
         loss.backward()
         optimizer.step()
+        performance_evaluator.on_step_end(input_ids)
 
-
-def fsdp_main(rank, world_size, args):
-    # 每个进程都要setup一下
-    setup(rank, world_size)
-    MOE_MANAGER.setup(seed=42, parallel=None, use_kernel_optim=False)
-
-    dataset1 = RandomDataset()
-    sampler1 = DistributedSampler(dataset1, rank=rank, num_replicas=world_size, shuffle=True)
-
-    train_kwargs = {"batch_size": args.batch_size, "sampler": sampler1}
-    cuda_kwargs = {"num_workers": 2, "shuffle": False}
-    train_kwargs.update(cuda_kwargs)
-
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    torch.cuda.set_device(rank)
-
-    config = LlamaConfig.from_pretrained("hpcaitech/openmoe-base")
-    setattr(config, "router_aux_loss_factor", 0.1)
-    setattr(config, "router_z_loss_factor", 0.1)
-    setattr(config, "label_smoothing", 0.1)
-    setattr(config, "z_loss_factor", 0.1)
-    model = OpenMoeForCausalLM(config).to(rank)
-    # 使用FSDP将model warp起来
-    model = FSDP(
-        model,
-        mixed_precision=MixedPrecision(
-            param_dtype=torch.float16,
-            reduce_dtype=torch.float16,
-            buffer_dtype=torch.float16,
-        ),
-    )
-
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    for epoch in range(1, args.epochs + 1):
-        train(
-            args,
-            model,
-            rank,
-            world_size,
-            train_loader,
-            optimizer,
-            epoch,
-            sampler=sampler1,
-        )
+    performance_evaluator.on_fit_end()
+    if dist.get_rank() == 0:
+        print(f"Max CUDA memory usage: {torch.cuda.max_memory_allocated()/1024**2:.2f} MB")
 
 
 if __name__ == "__main__":
-    # Training settings
-    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--batch-size",
-        type=int,
-        default=1,
-        metavar="N",
-        help="input batch size for training (default: 64)",
+        "--model_name",
+        type=str,
+        default="base",
+        choices=["base", "8b"],
+        help="base or 8b",
     )
-    parser.add_argument(
-        "--test-batch-size",
-        type=int,
-        default=1000,
-        metavar="N",
-        help="input batch size for testing (default: 1000)",
-    )
-    parser.add_argument(
-        "--epochs",
-        type=int,
-        default=10,
-        metavar="N",
-        help="number of epochs to train (default: 14)",
-    )
-    parser.add_argument(
-        "--lr",
-        type=float,
-        default=1.0,
-        metavar="LR",
-        help="learning rate (default: 1.0)",
-    )
-    parser.add_argument(
-        "--gamma",
-        type=float,
-        default=0.7,
-        metavar="M",
-        help="Learning rate step gamma (default: 0.7)",
-    )
-    parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
-    parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
-    parser.add_argument(
-        "--save-model",
-        action="store_true",
-        default=False,
-        help="For Saving the current Model",
-    )
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--seq_length", type=int, default=2048)
+    parser.add_argument("--warm_up", type=int, default=20)
+    parser.add_argument("--active", type=int, default=20)
     args = parser.parse_args()
 
-    torch.manual_seed(args.seed)
+    torch.manual_seed(42)
 
     WORLD_SIZE = torch.cuda.device_count()
     mp.spawn(fsdp_main, args=(WORLD_SIZE, args), nprocs=WORLD_SIZE, join=True)
