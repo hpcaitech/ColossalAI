23d775c18dceb46769af93bda021afccbd6e47d3
ver217 <lhx0217@gmail.com>
2023-10-18 15:36:00 +0800

    [test] add custom models in model zoo

--- tests/kit/model_zoo/__init__.py
+++ tests/kit/model_zoo/__init__.py
@@ -1,4 +1,4 @@
-from . import diffusers, timm, torchaudio, torchrec, torchvision, transformers
+from . import custom, diffusers, timm, torchaudio, torchrec, torchvision, transformers
 from .registry import model_zoo
 
 __all__ = ["model_zoo"]
---
+++ tests/kit/model_zoo/custom/__init__.py
@@ -0,0 +1,4 @@
+from .hanging_param_model import *
+from .nested_model import *
+from .repeated_computed_layers import *
+from .simple_net import *
