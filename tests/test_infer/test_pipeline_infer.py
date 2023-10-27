c28ee57496533a15627383514a22c599212f85e8
FoolPlayer <498107402@qq.com>
2023-10-27 11:41:14 +0800

    skip infer test

--- tests/test_infer/test_pipeline_infer.py
+++ tests/test_infer/test_pipeline_infer.py
@@ -52,6 +52,7 @@ def check_pipeline_inference(rank, world_size, port):
     run_pipeline_inference_test()
 
 
+@pytest.mark.skipif(not CUDA_SUPPORT, reason="kv-cache manager engine requires cuda version to be higher than 11.5")
 @pytest.mark.dist
 @rerun_if_address_is_in_use()
 @clear_cache_before_run()
