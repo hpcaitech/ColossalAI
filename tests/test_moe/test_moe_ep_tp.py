317dd58c42a6ada9a54a8d35da294ea0edf8d7c7
Wenhao Chen <cwher@outlook.com>
2023-11-01 14:00:49 +0800

    fix: use shard_data in ep & tp model

--- tests/test_moe/test_moe_ep_tp.py
+++ tests/test_moe/test_moe_ep_tp.py
@@ -45,6 +45,49 @@ def sync_tp_from_local(tp_model: SparseMLP, local_model: SparseMLP, assert_grad_
             tp_param.data.copy_(local_param[tuple(tp_slice)].data)
 
 
+def sync_tp_from_ep(tp_model: SparseMLP, ep_model: SparseMLP, assert_grad_flag: bool = False) -> None:
+    """Sync the parameters of tp model from ep model
+
+    Args:
+        tp_model (MoeModule)
+        ep_model (MoeModule)
+    """
+    for (tp_name, tp_param), (ep_name, ep_param) in \
+            zip(tp_model.named_parameters(), ep_model.named_parameters()):
+        assert tp_name == ep_name
+        if not is_moe_tensor(tp_param):
+            if assert_grad_flag:
+                assert torch.allclose(tp_param, ep_param)
+                assert torch.allclose(tp_param.grad, ep_param.grad)
+            else:
+                tp_param.data.copy_(ep_param.data)
+            continue
+
+        # gather param from ep model
+        param_list = [torch.zeros_like(ep_param) for _ in range(get_ep_size(ep_param))]
+        dist.all_gather(param_list, ep_param, group=get_ep_group(ep_param))
+        all_param = torch.cat(param_list, dim=0)
+        if assert_grad_flag:
+            grad_list = [torch.zeros_like(ep_param) for _ in range(get_ep_size(ep_param))]
+            dist.all_gather(grad_list, ep_param.grad, group=get_ep_group(ep_param))
+            all_grad = torch.cat(grad_list, dim=0)
+
+        # get tp param
+        tp_dim = [i for i, (d1, d2) in enumerate(zip(tp_param.shape[1:], all_param.shape[1:])) if d1 != d2][0] + 1
+        tp_rank = get_ep_rank(tp_param)
+        tp_slice = [slice(None)] * tp_dim + [
+            slice(tp_param.shape[tp_dim] * tp_rank, tp_param.shape[tp_dim] * (tp_rank + 1))
+        ]
+        new_tp_param = all_param[tuple(tp_slice)]
+        if assert_grad_flag:
+            new_grad = all_grad[tuple(tp_slice)]
+        if assert_grad_flag:
+            assert torch.allclose(tp_param, new_tp_param)
+            assert torch.allclose(tp_param.grad, new_grad)
+        else:
+            tp_param.data.copy_(new_tp_param.data)
+
+
 def sync_local_from_ep(local_model: SparseMLP, ep_model: SparseMLP, assert_grad_flag: bool = False) -> None:
     """Sync the parameters of tp model from ep model
 
@@ -102,31 +145,34 @@ def run_test(rank: int, world_size: int, port: int, num_experts: int, batch_size
     dist_dict = MOE_MANAGER.parallel_info_dict
     assert_equal_in_group(ep_model.experts.wi.data, dist_dict[world_size].dp_group)
     assert_equal_in_group(ep_model.experts.wo.data, dist_dict[world_size].dp_group)
-    grad_handler = MoeGradientHandler(ep_model)
+    ep_grad_handler = MoeGradientHandler(ep_model)
     # sync local param
     sync_local_from_ep(local_model, ep_model)
     # sync tp param
-    sync_tp_from_local(tp_model, local_model)
+    sync_tp_from_ep(tp_model, ep_model)
+    tp_grad_handler = MoeGradientHandler(tp_model)
 
     rank = dist.get_rank()
     torch.cuda.manual_seed(seed)
     input_data = torch.randn(batch_size, dim, device=get_current_device())
     micro_batch_size = batch_size // world_size
     index = rank * micro_batch_size
+    # NOTE: ep & tp takes in sharded data for each process
     shard_data = input_data.detach()[index:index + micro_batch_size]
 
     out_local = local_model(input_data)
     MOE_MANAGER.reset_loss()
-    out_tp = tp_model(input_data)
+    out_tp = tp_model(shard_data)
     MOE_MANAGER.reset_loss()
     out_ep = ep_model(shard_data)
     MOE_MANAGER.reset_loss()
 
-    assert torch.allclose(out_tp, out_local, atol=1e-6), \
-        f"Rank {rank} failed, max diff: {torch.max(torch.abs(out_tp - out_local))}"
+    assert torch.allclose(out_tp, out_ep, atol=1e-6), \
+        f"Rank {rank} failed, max diff: {torch.max(torch.abs(out_tp - out_ep))}"
     try:
-        assert torch.allclose(out_ep, out_local[index:index + micro_batch_size], atol=1e-6), \
-            f"Rank {rank} failed, max diff: {torch.max(torch.abs(out_ep - out_local[index:index + micro_batch_size]))}"
+        out_local_slice = out_local[index:index + micro_batch_size]
+        assert torch.allclose(out_ep, out_local_slice, atol=1e-6), \
+            f"Rank {rank} failed, max diff: {torch.max(torch.abs(out_ep - out_local_slice))}"
     except AssertionError as e:
         """
         e.g., in local model, tokens = 4, capacity = 2, experts = 2, topk = 1
@@ -138,23 +184,24 @@ def run_test(rank: int, world_size: int, port: int, num_experts: int, batch_size
             The same thing happens on router 1. And finally some tokens are dropped due to the sharded nature.
         """
         warnings.warn(
-            "EP may result in different behavior from local model. "
+            "EP & TP may result in different behavior from local model. "
             "Please check the comments for details."
         )
 
     out_local.mean().backward()
     out_tp.mean().backward()
+    tp_grad_handler.handle_gradient()
     out_ep.mean().backward()
-    grad_handler.handle_gradient()
+    ep_grad_handler.handle_gradient()
 
     assert_equal_in_group(ep_model.experts.wi.grad, dist_dict[world_size].dp_group)
     assert_equal_in_group(ep_model.experts.wo.grad, dist_dict[world_size].dp_group)
-    sync_tp_from_local(tp_model, local_model, assert_grad_flag=True)
+    sync_tp_from_ep(tp_model, ep_model, assert_grad_flag=True)
     try:
         sync_local_from_ep(local_model, ep_model, assert_grad_flag=True)
     except AssertionError as e:
         warnings.warn(
-            "EP may result in different behavior from local model. "
+            "EP & TP may result in different behavior from local model. "
             "Please check the comments for details."
         )
 
@@ -162,7 +209,7 @@ def run_test(rank: int, world_size: int, port: int, num_experts: int, batch_size
 @pytest.mark.dist
 @pytest.mark.parametrize("num_experts", [4, 64])
 @pytest.mark.parametrize("batch_size", [16])
-@pytest.mark.parametrize("dim", [256])
+@pytest.mark.parametrize("dim", [64])
 @pytest.mark.parametrize("seed", [42, 127])
 @rerun_if_address_is_in_use()
 def test_moe_ep_tp(num_experts: int, batch_size: int, dim: int, seed: int):
