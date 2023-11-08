777e7c01205b561877ce8f2e5b973d3cd69e6c62
FoolPlayer <498107402@qq.com>
2023-11-08 11:58:25 +0800

    merge chatglm with pp and tp

--- colossalai/inference/hybridengine/polices/__init__.py
+++ colossalai/inference/hybridengine/polices/__init__.py
@@ -1,4 +1,5 @@
 from .bloom import BloomModelInferPolicy
+from .chatglm import ChatGLM2InferPolicy
 from .llama import LlamaModelInferPolicy
 
-__all__ = ["LlamaModelInferPolicy", "BloomModelInferPolicy"]
+__all__ = ["LlamaModelInferPolicy", "BloomModelInferPolicy", "ChatGLM2InferPolicy"]
