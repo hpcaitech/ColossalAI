7cd753c9b6fcd79882e1551d4739a521401916d8
ver217 <lhx0217@gmail.com>
2023-11-15 18:33:03 +0800

    [npu] refactor device utils

--- colossalai/utils/device.py
+++ colossalai/utils/device.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from typing import Optional
+from typing import Any, Dict, List, Optional, Tuple
 
 import torch
 import torch.distributed as dist
@@ -44,84 +44,164 @@ def get_current_device() -> torch.device:
         return torch.device("cpu")
 
 
-def synchronize():
-    """Similar to cuda.synchronize().
-    Waits for all kernels in all streams on a CUDA device to complete.
-    """
+def _dispatch_device_func(fn_name: str, *args, **kwargs):
     if torch.cuda.is_available():
-        torch.cuda.synchronize()
+        return getattr(torch.cuda, fn_name)(*args, **kwargs)
     elif IS_NPU_AVAILABLE:
-        torch.npu.synchronize()
-
-
-def device_count() -> int:
-    if torch.cuda.is_available():
-        return torch.cuda.device_count()
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.device_count()
+        return getattr(torch.npu, fn_name)(*args, **kwargs)
     else:
         raise RuntimeError("No device available")
 
 
-def empty_cache():
-    """Similar to cuda.empty_cache()
-    Releases all unoccupied cached memory currently held by the caching allocator.
-    """
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-    elif IS_NPU_AVAILABLE:
-        torch.npu.empty_cache()
+# device semantics
+
+
+def can_device_access_peer(device, peer_device) -> bool:
+    return _dispatch_device_func("can_device_access_peer", device, peer_device)
+
+
+def current_device() -> int:
+    return _dispatch_device_func("current_device")
+
+
+def current_stream(device=None):
+    return _dispatch_device_func("current_stream", device)
+
+
+def default_stream(device=None):
+    return _dispatch_device_func("default_stream", device)
+
+
+def device_count() -> int:
+    return _dispatch_device_func("device_count")
+
+
+def get_device_capability(device=None) -> Tuple[int, int]:
+    return _dispatch_device_func("get_device_capability", device)
+
+
+def get_device_name(device=None) -> str:
+    return _dispatch_device_func("get_device_name", device)
+
+
+def get_device_properties(device):
+    return _dispatch_device_func("get_device_properties", device)
 
 
 def set_device(index: Optional[int] = None) -> None:
     if index is None:
         index = dist.get_rank() % device_count()
-    if torch.cuda.is_available():
-        torch.cuda.set_device(index)
-    elif IS_NPU_AVAILABLE:
-        torch.npu.set_device(index)
-
-
-def Stream(device=None, priority=0, **kwargs):
-    if torch.cuda.is_available():
-        return torch.cuda.Stream(device, priority, **kwargs)
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.Stream(device, priority, **kwargs)
-    else:
-        raise RuntimeError("No device available")
-
-
-def stream(stream_):
-    if torch.cuda.is_available():
-        return torch.cuda.stream(stream_)
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.stream(stream_)
-    else:
-        raise RuntimeError("No device available")
+    _dispatch_device_func("set_device", index)
 
 
 def set_stream(stream_):
-    if torch.cuda.is_available():
-        return torch.cuda.set_stream(stream_)
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.set_stream(stream_)
-    else:
-        raise RuntimeError("No device available")
+    return _dispatch_device_func("set_stream", stream_)
 
 
-def current_stream(device=None):
-    if torch.cuda.is_available():
-        return torch.cuda.current_stream(device)
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.current_stream(device)
-    else:
-        raise RuntimeError("No device available")
+def stream(stream_):
+    return _dispatch_device_func("stream", stream_)
 
 
-def default_stream(device=None):
-    if torch.cuda.is_available():
-        return torch.cuda.default_stream(device)
-    elif IS_NPU_AVAILABLE:
-        return torch.npu.default_stream(device)
-    else:
-        raise RuntimeError("No device available")
+def synchronize():
+    return _dispatch_device_func("synchronize")
+
+
+def utilization(device=None) -> int:
+    return _dispatch_device_func("utilization", device)
+
+
+# random number generator
+
+
+def get_rng_state(device="cuda") -> torch.Tensor:
+    return _dispatch_device_func("get_rng_state", device)
+
+
+def get_rng_state_all() -> List[torch.Tensor]:
+    return _dispatch_device_func("get_rng_state_all")
+
+
+def set_rng_state(new_state: torch.ByteTensor, device="cuda") -> None:
+    return _dispatch_device_func("set_rng_state", new_state, device)
+
+
+def set_rng_state_all(new_states: List[torch.ByteTensor]) -> None:
+    return _dispatch_device_func("set_rng_state_all", new_states)
+
+
+def manual_seed(seed: int) -> None:
+    return _dispatch_device_func("manual_seed", seed)
+
+
+def manual_seed_all(seed: int) -> None:
+    return _dispatch_device_func("manual_seed_all", seed)
+
+
+def seed() -> None:
+    return _dispatch_device_func("seed")
+
+
+def seed_all() -> None:
+    return _dispatch_device_func("seed_all")
+
+
+def initial_seed() -> int:
+    return _dispatch_device_func("initial_seed")
+
+
+# streams and events
+
+
+def Stream(device=None, priority=0, **kwargs):
+    return _dispatch_device_func("Stream", device, priority, **kwargs)
+
+
+def Event(enable_timing: bool = False, blocking: bool = False, interprocess: bool = False):
+    return _dispatch_device_func("Event", enable_timing, blocking, interprocess)
+
+
+# memory management
+
+
+def empty_cache() -> None:
+    return _dispatch_device_func("empty_cache")
+
+
+def memory_stats(device=None) -> Dict[str, Any]:
+    return _dispatch_device_func("memory_stats", device)
+
+
+def memory_summary(device=None, abbreviated=False) -> str:
+    return _dispatch_device_func("memory_summary", device, abbreviated)
+
+
+def memory_snapshot():
+    return _dispatch_device_func("memory_snapshot")
+
+
+def memory_allocated(device=None) -> int:
+    return _dispatch_device_func("memory_allocated", device)
+
+
+def max_memory_allocated(device=None) -> int:
+    return _dispatch_device_func("max_memory_allocated", device)
+
+
+def reset_max_memory_allocated(device=None) -> None:
+    return _dispatch_device_func("reset_max_memory_allocated", device)
+
+
+def memory_reserved(device=None) -> int:
+    return _dispatch_device_func("memory_reserved", device)
+
+
+def max_memory_reserved(device=None) -> int:
+    return _dispatch_device_func("max_memory_reserved", device)
+
+
+def set_per_process_memory_fraction(fraction: float, device=None) -> None:
+    return _dispatch_device_func("set_per_process_memory_fraction", fraction, device)
+
+
+def reset_peak_memory_stats(device=None) -> None:
+    return _dispatch_device_func("reset_peak_memory_stats", device)
