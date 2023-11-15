ebcc52a06654ac289711c5812ed42dc41b70b35d
ver217 <lhx0217@gmail.com>
2023-11-15 11:04:45 +0800

    [hotfix] fix import

--- colossalai/utils/device.py
+++ colossalai/utils/device.py
@@ -8,7 +8,8 @@ import torch.distributed as dist
 
 IS_NPU_AVAILABLE: bool = False
 try:
-    # noqa
+    import torch_npu  # noqa
+
     IS_NPU_AVAILABLE = torch.npu.is_available()
 except ImportError:
     pass
