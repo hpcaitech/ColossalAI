c6f4a7d4c6323e7554a78ec106c564b0459a225b
Xu Kai <xukai16@foxmail.com>
2023-11-20 18:42:36 +0800

    update example

--- examples/inference/run_llama_inference.py
+++ examples/inference/run_llama_inference.py
@@ -1,5 +1,4 @@
 import argparse
-import time
 
 import torch
 import torch.distributed as dist
@@ -11,7 +10,9 @@ from colossalai.testing import spawn
 
 
 def run_inference(args):
-    llama_model_path = args.path
+    llama_model_path = args.model_path
+    llama_tokenize_path = args.tokenizer_path
+
     max_input_len = args.max_input_len
     max_output_len = args.max_output_len
     max_batch_size = args.batch_size
@@ -20,10 +21,11 @@ def run_inference(args):
     pp_size = args.pp_size
     rank = dist.get_rank()
 
+    tokenizer = LlamaTokenizer.from_pretrained(llama_tokenize_path, padding_side="left")
+    tokenizer.pad_token_id = tokenizer.unk_token_id
+
     if args.quant is None:
-        tokenizer = LlamaTokenizer.from_pretrained(llama_model_path)
-        tokenizer.pad_token_id = tokenizer.unk_token_id
-        model = LlamaForCausalLM.from_pretrained(llama_model_path, pad_token_id=tokenizer.eos_token_id)
+        model = LlamaForCausalLM.from_pretrained(llama_model_path, pad_token_id=tokenizer.unk_token_id)
         model = model.half()
     elif args.quant == "gptq":
         from auto_gptq import AutoGPTQForCausalLM
@@ -41,8 +43,10 @@ def run_inference(args):
         tp_size=tp_size,
         pp_size=pp_size,
         model=model,
+        max_input_len=max_input_len,
         max_output_len=max_output_len,
         micro_batch_size=micro_batch_size,
+        quant=args.quant,
     )
 
     input_tokens = {
@@ -50,26 +54,9 @@ def run_inference(args):
         "attention_mask": torch.ones((max_batch_size, max_input_len), device="cuda"),
     }
 
-    iters = 10
-    warmup = 3
-    times = []
-
-    for i in range(iters):
-        torch.cuda.synchronize()
-        start = time.time()
-        outputs = engine.generate(input_tokens)
-        torch.cuda.synchronize()
-        end = time.time()
-        if rank == 0:
-            out_len = len(outputs[0])
-            print("generation time {} s".format(str(end - start)))
-            print(out_len)
-            times.append((end - start) / out_len)
+    outputs = engine.generate(input_tokens)
     if rank == 0:
-        times = times[warmup:]
-        latency = sum(times) / len(times)
-        print("total process latency is : " + str(latency) + " s")
-        print("total throughput is : " + str(1 / latency * max_batch_size))
+        print(tokenizer.batch_decode(outputs))
 
 
 def run_tp_pipeline_inference(rank, world_size, port, args):
@@ -79,7 +66,9 @@ def run_tp_pipeline_inference(rank, world_size, port, args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", type=str, help="Model path", required=True)
+    parser.add_argument("-p", "--model_path", type=str, help="Model path", required=True)
+    parser.add_argument("--tokenizer_path", type=str, help="Tokenizer path", required=True)
+
     parser.add_argument(
         "-q",
         "--quant",
@@ -89,12 +78,12 @@ if __name__ == "__main__":
         help="quantization type: 'gptq' or 'smoothquant'",
     )
     parser.add_argument("--smoothquant_base_name", type=str, default=None, help="soothquant base name")
-    parser.add_argument("-tp", "--tp_size", type=int, default=1, help="Tensor parallel size")
-    parser.add_argument("-pp", "--pp_size", type=int, default=1, help="Tensor parallel size")
-    parser.add_argument("-b", "--batch_size", type=int, default=64, help="Maximum batch size")
-    parser.add_argument("--max_input_len", type=int, default=512, help="Maximum input length")
-    parser.add_argument("--max_output_len", type=int, default=256, help="Maximum output length")
-    parser.add_argument("--micro_batch_size", type=int, default=2, help="Micro batch size")
+    parser.add_argument("-tp", "--tp_size", type=int, default=2, help="Tensor parallel size")
+    parser.add_argument("-pp", "--pp_size", type=int, default=2, help="Pipeline parallel size")
+    parser.add_argument("-b", "--batch_size", type=int, default=4, help="Maximum batch size")
+    parser.add_argument("--max_input_len", type=int, default=32, help="Maximum input length")
+    parser.add_argument("--max_output_len", type=int, default=16, help="Maximum output length")
+    parser.add_argument("--micro_batch_size", type=int, default=1, help="Micro batch size")
 
     args = parser.parse_args()
     spawn(run_tp_pipeline_inference, nprocs=args.tp_size * args.pp_size, args=args)
