cc6f6891d6ca6041faf6625ee99eaec1b5b9d944
flybird11111 <1829166702@qq.com>
2023-12-06 12:40:15 +0800

    Update colo_parameter.py

--- colossalai/tensor/colo_parameter.py
+++ colossalai/tensor/colo_parameter.py
@@ -8,7 +8,7 @@ from colossalai.tensor.param_op_hook import ColoParamOpHookManager
 from .colo_tensor import _convert_output
 
 WHITE_LIST_FUNCS = {torch.Tensor.__getitem__}
-IGNORED_FUNCS = {torch.Tensor.is_floating_point, torch.nn.functional.embedding, torch.nn.functional.layer_norm, torch.addmm, torch.nn.functional.linear}
+IGNORED_FUNCS = {torch.Tensor.is_floating_point}
 
 
 def is_no_hook_op(func) -> bool:
