ab0f22662c809364c24f86e9b4448508954eaf78
flybird11111 <1829166702@qq.com>
2023-12-07 18:26:39 +0800

    llama support dist-cross
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- tests/test_shardformer/test_layer/test_dist_crossentropy.py
+++ tests/test_shardformer/test_layer/test_dist_crossentropy.py
@@ -17,21 +17,30 @@ def check_dist_crossentropy(rank, world_size, port, ignore_index):
     colossalai.launch(config=CONFIG, rank=rank, world_size=world_size, port=port, host="localhost", backend="nccl")
 
     # prepare data
-    pred = torch.randn(2, 4, 8, requires_grad=True)
-    labels = torch.randint(8, (2, 4))
+    pred = torch.randn(2, 4, 8, requires_grad=True).cuda()
+    labels = torch.randint(8, (2, 4)).cuda()
     # set some label to -100 to test the ignore index
     labels[0, -1] = ignore_index
 
     org_pred = pred.view(-1, 8)
     org_labels = labels.view(-1)
     org_loss = F.cross_entropy(org_pred, org_labels)
+    pred.retain_grad()
+    org_loss.backward()
 
-    dist_pred = pred.chunk(world_size, -1)[rank]
-    dist_loss = cross_entropy_1d(dist_pred.to("cuda"), labels.to("cuda"), ignore_index=ignore_index)
+    dist_pred = pred.clone().chunk(world_size, -1)[rank].detach()
+    dist_pred.requires_grad = True
+    dist_loss = cross_entropy_1d(dist_pred, labels, ignore_index=ignore_index)
+    dist_pred.retain_grad()
+    dist_loss.backward()
 
     assert torch.allclose(
         org_loss, dist_loss, atol=1e-5
     ), f"dist cross entropy loss is not equal to orgin loss\n{org_loss}\n{dist_loss}"
+
+
+    target_grad = torch.chunk(pred.grad, world_size, dim=-1)[rank]
+    assert torch.allclose(target_grad, dist_pred.grad), f"dist grad is not equal to orgin grad\n{target_grad}\n{dist_pred.grad}"
 
 
 @pytest.mark.dist
