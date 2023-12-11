3de2e622995321b042d4a8cffcd61686cda4a58e
Yuanheng Zhao <54058983+yuanheng-zhao@users.noreply.github.com>
2023-12-11 10:56:18 +0800

    [Inference] Add CacheBlock and KV-Cache Manager (#5156)
    * [Inference] Add KVCache Manager
    
    * function refactored
    
    * add test for KVCache Manager
    
    * add attr beam width
    
    * Revise alloc func in CacheManager
    
    * Fix docs and pytests
    
    * add tp slicing for head number
    
    * optimize shapes of tensors used as physical cache
    
    * Apply using InferenceConfig on KVCacheManager
    
    * rm duplicate config file
    
    * Optimize cache allocation: use contiguous cache
    
    * Fix config in pytest (and config)

--- colossalai/inference/core/config.py
+++ colossalai/inference/core/config.py
@@ -1,8 +1,9 @@
-from typing import Optional, Union
 from dataclasses import dataclass
+from typing import Optional, Union
 
 import torch
 import torch.nn as nn
+
 
 @dataclass
 class InferenceConfig:
@@ -24,8 +25,10 @@ class InferenceConfig:
         max_seq_len: Maximum length of input sentence.
         quant_mode: Quantization mode.
         revision: The specific version(a branch, name, a commit id, or a tag name) of model to use.
+        beam_width: The maximum beam width used to initialize KV Cache.
+            During generation, the beam width provided as sampling parameter should be less than or equivalent to this value.
     """
-    
+
     model: Union[str, nn.Module]
     tokenizer: str = None
     tokenizer_mode: str = "auto"
@@ -34,21 +37,18 @@ class InferenceConfig:
     max_output_len: int = 256
     max_input_len: int = 256
     block_size: int = 16
-    gpu_utilization_rate: float = 0.7
     dtype: Union[str, torch.dtype] = torch.float32
     tp_size: int = 1
     pp_size: int = 1
     max_seq_len: Optional[int] = None
     quant_mode: Optional[str] = None
     revision: Optional[str] = None
+    # TODO: beam search is not support for now
+    beam_width: int = 1
 
     def __post_init__(self):
         self._verify_args()
 
     def _verify_args(self):
-        if self.gpu_utilization_rate > 1.0:
-            raise ValueError(
-                f"GPU utilization should be less than 1.0, but is set to {self.gpu_memory_utilization}."
-            )
         if self.tokenizer_mode not in ["auto", "slow"]:
             raise ValueError("Tokenizer mode must be " "either 'auto' or 'slow'," f"but got {self.tokenizer_mode}")
