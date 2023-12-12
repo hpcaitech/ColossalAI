d88e2d93899f3545d754bb4fc456fa7637f10e84
YeAnbang <anbangy2@outlook.com>
2023-12-12 12:24:16 +0800

    use local model/tokenizer for ci tests

--- applications/ColossalChat/tests/test_data_preparation.sh
+++ applications/ColossalChat/tests/test_data_preparation.sh
@@ -83,13 +83,13 @@ get_data_input_dirs() {
 get_tokenizer_dirs() {
     local model=$1
     if [[ $model == "gpt2" ]]; then
-        echo "gpt2"
+        echo "/data/scratch/models/gpt2"
     elif [[ $model == "bloom" ]]; then
-        echo "bigscience/bloom-560m"
+        echo "/data/scratch/models/bloom-560m"
     elif [[ $model == "opt" ]]; then
-        echo "facebook/opt-350m"
+        echo "/data/scratch/models/opt-350m"
     elif [[ $model == "llama" ]]; then
-        echo "hf-internal-testing/llama-tokenizer"
+        echo "/data/scratch/models/llama-tokenizer"
     else
         echo "Unknown model $model"
         exit 1
