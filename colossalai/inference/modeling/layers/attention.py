aaa34ce07ed9d25004318ed72d874db8c36eb219
CjhHa1 <cjh18671720497@outlook.com>
2024-01-04 16:26:10 +0800

    fix decoding

--- colossalai/inference/modeling/layers/attention.py
+++ colossalai/inference/modeling/layers/attention.py
@@ -59,10 +59,8 @@ def convert_kvcache(source, cache, lengths, block_tables):
     for i in range(bsz):
         _cache = torch.cat(
             (
-                cache[block_tables[i][: needed_blocks[i] - 1]].reshape(-1, num_heads, head_size),
-                cache[block_tables[i][needed_blocks[i] - 1], :, :, : num_remaing_tokens[i]].reshape(
-                    -1, num_heads, head_size
-                ),
+                cache[block_tables[i][: needed_blocks[i] - 1]].permute((3, 0, 1, 2)).reshape(-1, num_heads, head_size),
+                cache[block_tables[i][needed_blocks[i] - 1], :, :, : num_remaing_tokens[i]].permute(2, 1, 0),
             ),
             dim=0,
         )
@@ -124,8 +122,8 @@ class PagedAttention(nn.Module):
         assert q.shape[-1] == k.shape[-1] == v.shape[-1]
         assert q.shape[0] == k.shape[0] == v.shape[0]
         assert context_lengths.shape[0] == block_tables.shape[0]
-        (bsz, max_seq_len, num_heads, head_size)
-        input_shape = (bsz, max_seq_len)
+        shape = (bsz, max_seq_len, num_heads, head_size)
+        input_shape = shape[:2]
         query = self.pad_and_reshape(q, context_lengths, max_seq_len, num_heads, head_size).transpose(1, 2)
         key = self.pad_and_reshape(k, context_lengths, max_seq_len, num_heads, head_size).transpose(1, 2)
         value = self.pad_and_reshape(v, context_lengths, max_seq_len, num_heads, head_size).transpose(1, 2)
@@ -137,7 +135,10 @@ class PagedAttention(nn.Module):
         position_ids = position_ids.unsqueeze(0)
 
         cos, sin = self.rotary_emb(value, max_seq_len)
-        query, value = apply_rotary_pos_emb(query, key, cos, sin, position_ids)
+        query, key = apply_rotary_pos_emb(query, key, cos, sin, position_ids)
+
+        copy_to_cache(key.transpose(1, 2), k_cache, lengths=context_lengths, block_tables=block_tables)
+        copy_to_cache(value.transpose(1, 2), v_cache, lengths=context_lengths, block_tables=block_tables)
 
         attn_weights = torch.matmul(query, key.transpose(2, 3)) / math.sqrt(head_size)
 
@@ -181,8 +182,11 @@ class PagedAttention(nn.Module):
         position_ids = position_ids.unsqueeze(0)
         cos, sin = self.rotary_emb(v, seq_len)
         query, key = apply_rotary_pos_emb(q, k, cos, sin, position_ids)
-        attn_weights = torch.matmul(query, key.transpose(2, 3)) / math.sqrt(head_size)
 
+        copy_to_cache(key.transpose(1, 2), k_cache, lengths=context_lengths, block_tables=block_tables)
+        copy_to_cache(v.transpose(1, 2), v_cache, lengths=context_lengths, block_tables=block_tables)
+
+        attn_weights = torch.matmul(query, key.transpose(2, 3)) / math.sqrt(head_size)
         attn_mask = AttentionMaskConverter._make_causal_mask(input_shape, q.dtype, q.device, past_key_values_length=0)
         self.generate_padding_mask(context_lengths, seq_len)
 
@@ -192,11 +196,13 @@ class PagedAttention(nn.Module):
             attn_weights += attn_mask
 
         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
+
         # attn_weights = nn.functional.dropout(attn_weights,p=self.attention_dropout,training=False) maybe useless
         attn_output = torch.matmul(attn_weights, v)
 
         if attn_output.size() != (bsz, num_heads, seq_len, head_size):
             raise ValueError(f"Got wrong attn_output, should be in shape {(bsz,num_heads,seq_len,head_size)}.")
+
         attn_output = attn_output.transpose(1, 2).contiguous().reshape(bsz, seq_len, -1)
 
         return attn_output
@@ -214,9 +220,9 @@ class PagedAttention(nn.Module):
         bsz, _, num_heads, head_size = q.shape
         block_size = k_cache.shape[-1]
         seq_len = max(lengths)
+
         assert q.shape[0] == k.shape[0] == v.shape[0] == block_tables.shape[0]
         max_seq_len = block_tables.shape[-1] * block_size
-        (bsz, max_seq_len, num_heads, head_size)
         attn_mask = AttentionMaskConverter._make_causal_mask(
             q.shape[:2], q.dtype, q.device, past_key_values_length=seq_len - 1
         )
@@ -227,6 +233,10 @@ class PagedAttention(nn.Module):
         position_ids = position_ids.unsqueeze(1)
 
         query, key = apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=2)
+
+        copy_to_cache(key, k_cache, lengths=lengths, block_tables=block_tables, type="decoding")
+        copy_to_cache(v, v_cache, lengths=lengths, block_tables=block_tables, type="decoding")
+
         key = convert_kvcache(key, k_cache, lengths, block_tables)  # bsz, seqlen,
         value = convert_kvcache(v, v_cache, lengths, block_tables)
 
@@ -247,7 +257,7 @@ class PagedAttention(nn.Module):
 
         if attn_output.size() != (bsz, num_heads, 1, head_size):
             raise ValueError(f"Got wrong attn_output, should be in shape {(bsz,num_heads,1,head_size)}.")
-        attn_output = attn_output.transpose(1, 2).contiguous().reshape(bsz, max_seq_len, -1)
+        attn_output = attn_output.transpose(1, 2).contiguous().reshape(bsz, 1, -1)
 
         return attn_output
 
--- tests/test_infer/test_models/test_attention.py
+++ tests/test_infer/test_models/test_attention.py
@@ -1,5 +1,9 @@
 import pytest
 import torch
+from transformers.cache_utils import DynamicCache
+from transformers.modeling_attn_mask_utils import AttentionMaskConverter
+from transformers.models.llama.configuration_llama import LlamaConfig
+from transformers.models.llama.modeling_llama import LlamaAttention
 
 import colossalai
 from colossalai.inference.modeling.layers.attention import PagedAttention, convert_kvcache, copy_to_cache
@@ -33,6 +37,9 @@ def test_convert_kvcache():
 
 
 def test_context_attention():
+    """
+    test config: head_num = 4, head_size = 4
+    """
     attn = PagedAttention(4, 4)
     q = k = v = torch.randn(8, 4, 4)
     k_cache = torch.empty(8, 4, 4, 8)
@@ -48,24 +55,66 @@ def test_context_attention():
     pad_q = pad_k = pad_v = q.unsqueeze(0)
     attn.pad_context_forward(pad_q, pad_k, pad_v, k_cache, v_cache, context_lengths, block_tables)
 
+    config = LlamaConfig(num_attention_heads=4, num_key_value_heads=None, hidden_size=16)
+    transformer_attn = LlamaAttention(config)
+    transformer_attn.training = False
+
+    # test accuracy with LlamaAttention
+    hidden_states = torch.randn(1, 8, 16)
+    proj_q = transformer_attn.q_proj(hidden_states).view(1, 8, 4, 4)
+    proj_k = transformer_attn.k_proj(hidden_states).view(1, 8, 4, 4)
+    proj_v = transformer_attn.v_proj(hidden_states).view(1, 8, 4, 4)
+    pad_attn_output = attn.pad_context_forward(proj_q, proj_k, proj_v, k_cache, v_cache, context_lengths, block_tables)
+    pad_attn_output = transformer_attn.o_proj(pad_attn_output)
+
+    attn_mask = AttentionMaskConverter._make_causal_mask(
+        hidden_states.shape[:2], q.dtype, q.device, past_key_values_length=0
+    )
+    attn_output, _, _ = transformer_attn.forward(hidden_states, attention_mask=attn_mask)
+    assert torch.allclose(pad_attn_output, attn_output, atol=1e-3, rtol=1e-2)
+
 
 def test_decoding_attention():
+    # test the pipeline of decoding attention
     attn = PagedAttention(4, 4)
     q = k = v = torch.randn(2, 1, 4, 4)
     k_cache = torch.empty(8, 4, 4, 8)
     v_cache = torch.empty(8, 4, 4, 8)
     past_kv = torch.randn(2, 8, 4, 4)
     context_lenghths = torch.tensor([8, 8])
+    lengths = context_lenghths + 1
     block_tables = torch.tensor([[0, 1], [2, 3]])
     copy_to_cache(past_kv, k_cache, lengths=context_lenghths, block_tables=block_tables)
     copy_to_cache(past_kv, v_cache, lengths=context_lenghths, block_tables=block_tables)
-    attn.pad_decoding_forward(q, k, v, k_cache, v_cache, lengths=context_lenghths + 1, block_tables=block_tables)
+    attn.pad_decoding_forward(q, k, v, k_cache, v_cache, lengths=lengths, block_tables=block_tables)
+    # test decoding accuracy, past_kv is reused
+    config = LlamaConfig(num_attention_heads=4, num_key_value_heads=None, hidden_size=16)
+    transformer_attn = LlamaAttention(config)
+    transformer_attn.layer_idx = 0
+    transformer_attn.training = False
+    hidden_states = torch.randn(2, 1, 16)
+    proj_q = transformer_attn.q_proj(hidden_states).view(2, 1, 4, 4)
+    proj_k = transformer_attn.k_proj(hidden_states).view(2, 1, 4, 4)
+    proj_v = transformer_attn.v_proj(hidden_states).view(2, 1, 4, 4)
+
+    llama_past_kv = DynamicCache()
+    llama_past_kv.update(key_states=past_kv.transpose(1, 2), value_states=past_kv.transpose(1, 2), layer_idx=0)
+
+    # past_key_value shape in Llama: bsz, num_heads, seq_len, head_dim
+    pad_attn_output = attn.pad_decoding_forward(proj_q, proj_k, proj_v, k_cache, v_cache, lengths, block_tables)
+    attn_mask = AttentionMaskConverter._make_causal_mask(proj_q.shape[:2], q.dtype, q.device, past_key_values_length=8)
+    pad_attn_output = transformer_attn.o_proj(pad_attn_output)
+    position_ids = context_lenghths.unsqueeze(1)
+    attn_output, _, _ = transformer_attn.forward(
+        hidden_states, past_key_value=llama_past_kv, position_ids=position_ids, attention_mask=attn_mask
+    )
+    assert torch.allclose(pad_attn_output, attn_output, atol=1e-3, rtol=1e-2)
 
 
 def check_attention_layer():
-    test_copy_to_cache()
-    test_convert_kvcache()
-    test_context_attention()
+    # test_copy_to_cache()
+    # test_convert_kvcache()
+    # test_context_attention()
     test_decoding_attention()
 
 
