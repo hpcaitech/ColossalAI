83312ddd91a2b9af4132c9707c87545b7c124fc0
yuehuayingxueluo <867460659@qq.com>
2024-01-29 17:21:13 +0800

    add nopadding_llama.py

--- colossalai/inference/modeling/models/padding_llama.py
+++ colossalai/inference/modeling/models/padding_llama.py
@@ -102,10 +102,7 @@ def llama_model_forward(
 
     hidden_states = self.embed_tokens(input_ids)
 
-    # When testing, the performance of get_xine_cache is lower than that of get_cos_sin.
     cos_sin = get_xine_cache(sequence_lengths, self._cos_cached, self._sin_cached, batch.is_prompts)
-
-    # cos_sin = get_cos_sin(sequence_lengths, self._cos_cached, self._sin_cached, batch.is_prompts, batch.dtype)
 
     if batch.is_prompts:
         output_tensor = torch.zeros(
@@ -328,26 +325,3 @@ def unpading_input(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, attention_
     k = index_first_axis(k.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim), indices)
     v = index_first_axis(v.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim), indices)
     return (q, k, v, indices)
-
-
-@torch.no_grad()
-def get_cos_sin(lengths, cos_cache, sin_cache, is_prompts, dtype):
-    """
-    Get cos and sin for the cache, and return nopad format.
-    Args:
-        lengths: shape(num_seqs,), stores lenghth of each sequence.
-        cos_cache: shape(max_rotary_position(e.g.2048), head_dim), cos cache constrcuted in model.
-        sin_cache: shape(max_rotary_position(e.g.2048), head_dim), sin cache constrcuted in model.
-        is_prompts: bool, mark if in prefill mode.
-        dtype: The data type of this inference process.
-    """
-
-    if is_prompts:
-        index_arrays = [torch.arange(length) for length in lengths]
-    else:
-        index_arrays = [(length - 1).view(-1) for length in lengths]
-    indices = torch.cat(index_arrays, dim=-1)
-    cos_output = cos_cache[indices].to(dtype=dtype)
-    sin_output = sin_cache[indices].to(dtype=dtype)
-
-    return (cos_output, sin_output)
