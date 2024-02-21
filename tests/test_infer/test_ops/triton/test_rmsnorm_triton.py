ada455f04af7f85207c03f8646924073efc91aab
yuehuayingxueluo <867460659@qq.com>
2024-02-21 11:54:53 +0800

    fix ci bugs

--- tests/test_infer/test_ops/triton/test_rmsnorm_triton.py
+++ tests/test_infer/test_ops/triton/test_rmsnorm_triton.py
@@ -29,14 +29,27 @@ def test_layer_norm(M, N):
     x_shape = (M, N)
     w_shape = (x_shape[-1],)
     weight = torch.ones(w_shape, dtype=dtype, device="cuda")
+    residual = torch.rand(x_shape, dtype=dtype, device="cuda")
+    residual_copy = residual.clone()
     rms_norm = LlamaRMSNorm(hidden_size=N, eps=eps).cuda()
     x = -2.3 + 0.5 * torch.randn(x_shape, dtype=dtype, device="cuda")
+    x_copy = x.clone()
 
     y_triton, _ = rms_layernorm(x, weight, eps=eps)
     y_llama = rms_norm.forward(x).to(dtype)
 
     assert y_triton.shape == y_llama.shape
     assert torch.allclose(y_triton, y_llama, atol=1e-5, rtol=1e-3)
+
+    y_triton, residual = rms_layernorm(x, weight, eps=eps, residual=residual)
+
+    x = x_copy + residual_copy
+
+    y_llama = rms_norm.forward(x).to(dtype)
+
+    assert y_triton.shape == y_llama.shape
+    assert torch.allclose(y_triton, y_llama, atol=1e-5, rtol=1e-3)
+    assert torch.allclose(x, residual, atol=1e-5, rtol=1e-3)
 
 
 # Triton benchmark plot attributions
@@ -45,8 +58,8 @@ configs = [
         x_names=["SEQUENCE_TOTAL"],
         x_vals=[i for i in range(128, 1025, 128)],
         line_arg="provider",
-        line_vals=["torch_rms_layernorm", "triton_rms_layernorm"],
-        line_names=["torch_rms_layernorm", "triton_rms_layernorm"],
+        line_vals=["torch_rms_layernorm", "triton_rms_layernorm", "triton_rms_layernorm_with_residual"],
+        line_names=["torch_rms_layernorm", "triton_rms_layernorm", "triton_rms_layernorm_with_residual"],
         styles=[("red", "-"), ("blue", "-")],
         ylabel="ms",
         plot_name=f"RMSNorm benchmarking results",
@@ -68,6 +81,7 @@ def benchmark_rms_layernorm(
     eps = 1e-5
     x_shape = (SEQUENCE_TOTAL, HIDDEN_SIZE)
     w_shape = (x_shape[-1],)
+    residual = torch.rand(x_shape, dtype=dtype, device="cuda")
     weight = torch.ones(w_shape, dtype=dtype, device="cuda")
     torch_norm = LlamaRMSNorm(hidden_size=HIDDEN_SIZE).to(dtype=dtype, device="cuda")
     x = -2.3 + 0.5 * torch.randn(x_shape, dtype=dtype, device="cuda")
@@ -75,6 +89,8 @@ def benchmark_rms_layernorm(
         fn = lambda: torch_norm(x)
     elif provider == "triton_rms_layernorm":
         fn = lambda: rms_layernorm(x, weight, eps=eps)
+    elif provider == "triton_rms_layernorm_with_residual":
+        fn = lambda: rms_layernorm(x, weight, eps=eps, residual=residual)
     else:
         raise ValueError("Undefined provider.")
 
