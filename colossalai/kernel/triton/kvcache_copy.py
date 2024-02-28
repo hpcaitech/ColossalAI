d63c469f45bc20115aaf5ba01e62dc67ab47953f
Yuanheng Zhao <54058983+yuanheng-zhao@users.noreply.github.com>
2024-02-28 13:47:00 +0800

    [Infer] Revise and Adapt Triton Kernels for Spec-Dec (#5401)
    * [Infer/Fix] Fix Dependency in test - RMSNorm kernel (#5399)
    
    fix dependency in pytest
    
    * resolve conflicts for revising flash-attn
    
    * adapt kv cache copy kernel for spec-dec
    
    * fix seqlen-n kvcache copy kernel/tests
    
    * test kvcache copy - use torch.equal
    
    * add assertions
    
    * (trivial) comment out

--- colossalai/kernel/triton/kvcache_copy.py
+++ colossalai/kernel/triton/kvcache_copy.py
@@ -5,6 +5,50 @@ import triton.language as tl
 
 # Triton 2.1.0
 @triton.jit
+def _copy_to_kcache_seqlen_n_kernel(
+    KV,  # K or V
+    KVCache,  # KCache or VCache
+    BLOCK_TABLES,
+    context_lengths,
+    stride_kt,
+    stride_kh,
+    stride_kd,
+    stride_cacheb,
+    stride_cacheh,
+    stride_cachebs,
+    stride_cached,
+    stride_bts,
+    stride_btb,
+    block_size,
+    n,
+    HEAD_DIM: tl.constexpr,
+):
+    cur_token_idx = tl.program_id(0)
+    cur_seq_idx = cur_token_idx // n
+    cur_token_shift = cur_token_idx - (n * (cur_seq_idx + 1))
+    # cur_token_shift = cur_token_idx - n * cur_seq_idx
+    cur_kv_head_idx = tl.program_id(1)
+
+    past_kv_seq_len = tl.load(context_lengths + cur_seq_idx) + cur_token_shift
+    last_bt_block_idx = past_kv_seq_len // block_size
+    block_table_ptr = BLOCK_TABLES + cur_seq_idx * stride_bts
+    block_id = tl.load(block_table_ptr + last_bt_block_idx * stride_btb)
+    offset_last_block = past_kv_seq_len % block_size
+    offsets_dmodel = tl.arange(0, HEAD_DIM)
+    offsets_kv = cur_token_idx * stride_kt + cur_kv_head_idx * stride_kh + offsets_dmodel * stride_kd
+    kv = tl.load(KV + offsets_kv)
+    offsets_kvcache = (
+        block_id * stride_cacheb
+        + cur_kv_head_idx * stride_cacheh
+        + offset_last_block * stride_cachebs
+        + offsets_dmodel * stride_cached
+    )
+    tl.store(KVCache + offsets_kvcache, kv)
+    return
+
+
+# Triton 2.1.0
+@triton.jit
 def _copy_to_kvcache_seqlen1_kernel(
     K,  # K
     V,  # V
@@ -40,10 +84,11 @@ def _copy_to_kvcache_seqlen1_kernel(
     block_id = tl.load(block_table_ptr + last_bt_block_idx * stride_btb)
     offsets_in_last_block = past_kv_seq_len % block_size
     offsets_dmodel = tl.arange(0, HEAD_DIM)
-    offsets_kv = cur_seq_idx * stride_kt + cur_kv_head_idx * stride_kh + offsets_dmodel * stride_kd
+    offsets_k = cur_seq_idx * stride_kt + cur_kv_head_idx * stride_kh + offsets_dmodel * stride_kd
+    offsets_v = cur_seq_idx * stride_vt + cur_kv_head_idx * stride_vh + offsets_dmodel * stride_vd
 
-    k = tl.load(K + offsets_kv)
-    v = tl.load(V + offsets_kv)
+    k = tl.load(K + offsets_k)
+    v = tl.load(V + offsets_v)
 
     offsets_kcache = (
         block_id * stride_cachekb
@@ -61,6 +106,64 @@ def _copy_to_kvcache_seqlen1_kernel(
     tl.store(KCache + offsets_kcache, k)
     tl.store(VCache + offsets_vcache, v)
     return
+
+
+def copy_k_to_blocked_cache(
+    k: torch.Tensor, k_cache: torch.Tensor, kv_lengths: torch.Tensor, block_tables: torch.Tensor, n: int = 1
+):
+    """
+    Copy keys or values to the blocked key/value cache during decoding stage.
+
+    Args:
+        k (torch.Tensor): [bsz, 1, num_kv_heads, head_dim]/[bsz, num_kv_heads, head_dim] - Keys or values during decoding with seq len 1.
+            [bsz * n, num_kv_heads, head_dim] - Keys or values with seq len n
+        k_cache (torch.Tensor): [num_blocks, num_kv_heads, block_size, head_dim] - Blocked key or value cache.
+        kv_lengths (torch.Tensor): [bsz] - Past key/value sequence lengths plus current sequence length for each sequence.
+        block_tables (torch.Tensor): [bsz, max_blocks_per_sequence] - Block tables for each sequence.
+        n (int): Number of tokens to copy for each sequence. Default to 1.
+    """
+    assert k.size(-1) == k_cache.size(-1), "Incompatible head dim"
+    assert k.dtype == k_cache.dtype, "Expected consistent dtype for tensor and cache."
+
+    k = k.reshape(-1, k.size(-2), k.size(-1)) if k.dim() == 4 else k
+    assert k.dim() == 3, f"Invalid k dim {k.dim()}"
+    bsz, num_kv_heads, head_dim = k.shape
+    # NOTE when n > 1, the shape of k is [bsz * n, num_kv_heads, head_dim]
+    if n > 1:
+        assert bsz % n == 0, "Each sequence should have the same number of tokens to be copied"
+        bsz = bsz // n
+
+    assert kv_lengths.shape[0] == block_tables.shape[0] == bsz, (
+        f"Got incompatible batch size (number of seqs):\n"
+        f"  Past kv sequence lengths bsz {kv_lengths.shape[0]}; "
+        f" block tables bsz {block_tables.shape[0]}, input k batch size {bsz}"
+    )
+
+    # Modify if the shape of kv cahce is changed.
+    block_size = k_cache.size(-2)
+
+    num_warps = 8 if head_dim > 128 else 4
+
+    grid = (bsz * n, num_kv_heads)
+    _copy_to_kcache_seqlen_n_kernel[grid](
+        k,
+        k_cache,
+        block_tables,
+        kv_lengths,
+        k.stride(0),
+        k.stride(1),
+        k.stride(2),
+        k_cache.stride(0),
+        k_cache.stride(1),
+        k_cache.stride(2),
+        k_cache.stride(3),
+        block_tables.stride(0),
+        block_tables.stride(1),
+        block_size,
+        n=n,
+        HEAD_DIM=head_dim,
+        num_warps=num_warps,
+    )
 
 
 def copy_kv_to_blocked_cache(
--- tests/test_infer/test_ops/triton/test_decoding_attn.py
+++ tests/test_infer/test_ops/triton/test_decoding_attn.py
@@ -21,7 +21,6 @@ except ImportError:
 
 TRITON_CUDA_SUPPORT = version.parse(torch.version.cuda) > version.parse("11.4")
 
-Q_LEN = 1
 HEAD_DIM = 128
 
 
@@ -64,6 +63,7 @@ def prepare_data(
 @pytest.mark.parametrize("num_attn_heads", [16])
 @pytest.mark.parametrize("kv_group_num", [1, 2, 16])
 @pytest.mark.parametrize("same_context_len", [True, False])
+@pytest.mark.parametrize("q_len", [1, 5])
 def test_flash_decoding(
     bsz: int,
     block_size: int,
@@ -71,6 +71,7 @@ def test_flash_decoding(
     num_attn_heads: int,
     kv_group_num: int,
     same_context_len: bool,
+    q_len: int,
 ):
     torch.manual_seed(123)
     torch.cuda.empty_cache()
@@ -82,47 +83,57 @@ def test_flash_decoding(
     max_seq_len = block_size * max_num_blocks_per_seq
     dtype = torch.float16
     device = get_current_device()
-
-    q, k_unpad, v_unpad, kv_seq_lengths = prepare_data(
-        bsz, num_attn_heads, num_kv_heads, HEAD_DIM, same_context_len, Q_LEN, max_seq_len, dtype, device
+    q, k_unpad, v_unpad, kv_lengths = prepare_data(
+        bsz, num_attn_heads, num_kv_heads, HEAD_DIM, same_context_len, q_len, max_seq_len, dtype, device
     )
+    # The maximum sequence length in the batch (if context lengths randomly generated)
+    max_kv_len_in_b = kv_lengths.max().item()
+
+    k_torch = convert_kv_unpad_to_padded(k_unpad, kv_lengths, bsz, max_kv_len_in_b)
+    v_torch = convert_kv_unpad_to_padded(v_unpad, kv_lengths, bsz, max_kv_len_in_b)
+    torch_padding_mask = prepare_padding_mask(kv_lengths, bsz, q_len, max_kv_len_in_b, q.device)
+    out_torch = torch_attn_ref(
+        q, k_torch, v_torch, torch_padding_mask, bsz, q_len, max_kv_len_in_b, num_attn_heads, num_kv_heads, HEAD_DIM
+    )
+
     k_cache, v_cache, block_tables = generate_caches_and_block_tables_v2(
-        k_unpad, v_unpad, kv_seq_lengths, bsz, max_num_blocks_per_seq, block_size, dtype, device
+        k_unpad, v_unpad, kv_lengths, bsz, max_num_blocks_per_seq, block_size, dtype, device
     )
     block_tables = block_tables.to(device=device)
-    # The maximum sequence length in the batch (if context lengths randomly generated)
-    max_seq_len_in_b = kv_seq_lengths.max().item()
     # The maximum block length splitted on kv should be the kv cache block size
-    kv_max_split_num = (max_seq_len_in_b + block_size - 1) // block_size
-    output = torch.empty((bsz, num_attn_heads, HEAD_DIM), dtype=q.dtype, device=q.device)
+    kv_max_split_num = (max_kv_len_in_b + block_size - 1) // block_size
+    output = torch.empty((bsz * q_len, num_attn_heads, HEAD_DIM), dtype=q.dtype, device=q.device)
     mid_output = torch.empty(
-        size=(bsz, num_attn_heads, kv_max_split_num, HEAD_DIM), dtype=torch.float32, device=q.device
+        size=(bsz * q_len, num_attn_heads, kv_max_split_num, HEAD_DIM), dtype=torch.float32, device=q.device
     )
-    mid_output_lse = torch.empty(size=(bsz, num_attn_heads, kv_max_split_num), dtype=torch.float32, device=q.device)
+    mid_output_lse = torch.empty(
+        size=(bsz * q_len, num_attn_heads, kv_max_split_num), dtype=torch.float32, device=q.device
+    )
     sm_scale = 1.0 / (HEAD_DIM**0.5)
+    # Here we use different methods to hide the q_len dimension,
+    # refer to attention forward function in modeling.
+    if q_len > 1:
+        q = q.transpose(1, 2).contiguous()  # [bsz, q_len, num_heads, head_dim]
+        q = q.view(-1, q.size(-2), q.size(-1))  # [bsz * q_len, num_heads, head_dim]
+    else:
+        q = q.squeeze(2)
+    assert q.shape == (bsz * q_len, num_attn_heads, HEAD_DIM)
+
     out_triton = flash_decoding_attention(
-        # Here we use q.squeeze(2) because we hide the q_len dimension (which is equivalent to 1),
-        # refer to attention forward in modeling.
-        q.squeeze(2),
+        q,
         k_cache,
         v_cache,
-        kv_seq_lengths,
+        kv_lengths,
         block_tables,
         block_size,
-        max_seq_len_in_b,
+        max_kv_len_in_b,
         output,
         mid_output,
         mid_output_lse,
         sm_scale=sm_scale,
         kv_group_num=kv_group_num,
-    )  # [bsz, 1, num_heads, head_dim]
-
-    k_torch = convert_kv_unpad_to_padded(k_unpad, kv_seq_lengths, bsz, max_seq_len_in_b)
-    v_torch = convert_kv_unpad_to_padded(v_unpad, kv_seq_lengths, bsz, max_seq_len_in_b)
-    torch_padding_mask = prepare_padding_mask(kv_seq_lengths, bsz, max_seq_len_in_b, q.device)
-    out_torch = torch_attn_ref(
-        q, k_torch, v_torch, torch_padding_mask, bsz, 1, max_seq_len_in_b, num_attn_heads, num_kv_heads, HEAD_DIM
-    )
+        q_len=q_len,
+    )  # [bsz * q_len, num_heads, head_dim]
 
     assert out_torch.shape == out_triton.shape
     assert torch.allclose(out_torch, out_triton, atol=1e-3, rtol=1e-4)
