5e1c93d732a3ee77657fe68c031dacdeae4d7807
digger yu <digger-yu@outlook.com>
2024-03-05 21:52:30 +0800

    [hotfix] fix typo change MoECheckpintIO to MoECheckpointIO (#5335)
    Co-authored-by: binmakeswell <binmakeswell@gmail.com>

--- colossalai/booster/plugin/hybrid_parallel_plugin.py
+++ colossalai/booster/plugin/hybrid_parallel_plugin.py
@@ -1067,7 +1067,7 @@ class HybridParallelPlugin(PipelinePluginBase):
         self.max_norm = max_norm
 
     def __del__(self):
-        """Destroy the prcess groups in ProcessGroupMesh"""
+        """Destroy the process groups in ProcessGroupMesh"""
         self.pg_mesh.destroy_mesh_process_groups()
 
     @property
