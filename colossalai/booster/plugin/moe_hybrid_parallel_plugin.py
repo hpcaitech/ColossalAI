f2e8b9ef9ff3032513732a699d766bcde1a3506e
Hongxin Liu <lhx0217@gmail.com>
2024-03-13 15:24:13 +0800

    [devops] fix compatibility (#5444)
    * [devops] fix compatibility
    
    * [hotfix] update compatibility test on pr
    
    * [devops] fix compatibility
    
    * [devops] record duration during comp test
    
    * [test] decrease test duration
    
    * fix falcon

--- colossalai/booster/plugin/moe_hybrid_parallel_plugin.py
+++ colossalai/booster/plugin/moe_hybrid_parallel_plugin.py
@@ -182,7 +182,7 @@ class MoeHybridParallelPlugin(HybridParallelPlugin):
         overlap_communication: bool = True,
         use_ep_inside: bool = True,
         custom_policy: Policy = None,
-        checkpoint_io: Optional[MoECheckpintIO] = None,
+        checkpoint_io: Optional[MoECheckpointIO] = None,
     ) -> None:
         assert (
             dist.get_world_size() % (tp_size * pp_size) == 0
@@ -340,7 +340,6 @@ class MoeHybridParallelPlugin(HybridParallelPlugin):
             num_workers=num_workers,
             **_kwargs,
         )
-
 
     def get_checkpoint_io(self) -> MoECheckpointIO:
         if self.checkpoint_io is None:
