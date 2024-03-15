ba9546943e05d7d11df0b2c1c9517ea075f61c70
ver217 <lhx0217@gmail.com>
2024-03-15 14:16:24 +0800

    [misc] refactor folder structure

--- extensions/flash_attention/__init__.py
+++ extensions/flash_attention/__init__.py
@@ -3,18 +3,12 @@ from .flash_attention_npu import FlashAttentionNpuExtension
 from .flash_attention_sdpa_cuda import FlashAttentionSdpaCudaExtension
 
 try:
+    # TODO: remove this after updating openmoe example
     import flash_attention  # noqa
 
     HAS_FLASH_ATTN = True
 except:
     HAS_FLASH_ATTN = False
-
-try:
-    import xformers  # noqa
-
-    HAS_MEM_EFF_ATTN = True
-except:
-    HAS_MEM_EFF_ATTN = False
 
 
 __all__ = ["FlashAttentionDaoCudaExtension", "FlashAttentionSdpaCudaExtension", "FlashAttentionNpuExtension"]
