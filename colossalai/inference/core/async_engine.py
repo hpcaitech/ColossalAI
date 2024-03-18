3455f3d2cd6e3ef300eeb12cfcc762d35e1c552f
CjhHa1 <cjh18671720497@outlook.com>
2024-03-18 16:11:44 +0800

    revision

--- colossalai/inference/core/async_engine.py
+++ colossalai/inference/core/async_engine.py
@@ -1,13 +1,13 @@
 import asyncio
+import logging
 from functools import partial
-from logging import Logger
-from typing import AsyncIterator, Dict, Iterable, List, Optional, Set, Tuple, Type
+from typing import AsyncIterator, Dict, Iterable, List, Optional, Tuple, Type
 
 from colossalai.inference.core.engine import InferenceEngine
 
-
-class AsyncEngineDeadError(RuntimeError):
-    pass
+# CLI logger
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("colossalai-inference")
 
 
 def _raise_exception_on_finish(task: asyncio.Task, request_tracker: "RequestTracker") -> None:
@@ -18,14 +18,14 @@ def _raise_exception_on_finish(task: asyncio.Task, request_tracker: "RequestTrac
         except asyncio.CancelledError:
             return
         except Exception as exc:
-            raise AsyncEngineDeadError(msg + " See stack trace above for the actual cause.") from exc
-        raise AsyncEngineDeadError(msg)
+            raise RuntimeError(msg + " See stack trace above for the actual cause.") from exc
+        raise RuntimeError(msg)
     except Exception as exc:
         request_tracker.propagate_exception(exc)
         raise exc
 
 
-class AsyncStream:
+class RequstStream:
     """A stream of Output for a request that can be
     iterated over asynchronously."""
 
@@ -48,13 +48,15 @@ class AsyncStream:
         return self._future.done()
 
 
-class RequestTracker:
-    """Synchronous abstraction for tracking requests."""
+class Tracer:
+    """
+    Recording new requests and finished requests.
+    """
 
     def __init__(self) -> None:
-        self._request_streams: Dict[int, AsyncStream] = {}
+        self._request_streams: Dict[int, RequstStream] = {}
         self._finished_requests: asyncio.Queue[int] = asyncio.Queue()
-        self._new_requests: asyncio.Queue[Tuple[AsyncStream, dict]] = asyncio.Queue()
+        self._new_requests: asyncio.Queue[Tuple[RequstStream, dict]] = asyncio.Queue()
         self.new_requests_event = None
 
     def __contains__(self, item):
@@ -82,7 +84,7 @@ class RequestTracker:
             raise RuntimeError(f"The request_id {request_id} is not found in our stream, please check")
         self.abort_request(request_id)
 
-    def add_request(self, request_id: int, **engine_add_request_kwargs) -> AsyncStream:
+    def add_request(self, request_id: int, **engine_add_request_kwargs) -> RequstStream:
         """
         Add a request to be sent to the engine on the next background
         loop iteration.
@@ -90,7 +92,7 @@ class RequestTracker:
         if request_id in self._request_streams:
             raise KeyError(f"Request {request_id} already exists.")
 
-        stream = AsyncStream(request_id)
+        stream = RequstStream(request_id)
         self._new_requests.put_nowait((stream, {"request_id": request_id, **engine_add_request_kwargs}))
 
         self.new_requests_event.set()
@@ -100,7 +102,7 @@ class RequestTracker:
     def abort_request(self, request_id: int, *, verbose: bool = False) -> None:
         """Abort a request during next background loop iteration."""
         if verbose:
-            Logger.info(f"Aborted request {request_id}.")
+            logger.info(f"Aborted request {request_id}.")
 
         self._finished_requests.put_nowait(request_id)
 
@@ -124,30 +126,6 @@ class RequestTracker:
         self.new_requests_event.clear()
 
         return new_requests
-
-    def get_new_and_finished_requests(self) -> Tuple[List[Dict], Set[int]]:
-        """Get the new requests and finished requests to be
-        sent to the engine."""
-        new_requests: List[Dict] = []
-        finished_requests: Set[int] = set()
-
-        while not self._finished_requests.empty():
-            request_id = self._finished_requests.get_nowait()
-            finished_requests.add(request_id)
-            self._request_streams.pop(request_id, None)
-
-        while not self._new_requests.empty():
-            stream, new_request = self._new_requests.get_nowait()
-            if stream.request_id in finished_requests:
-                # The request has already been aborted.
-                stream.finish()
-                continue
-            self._request_streams[stream.request_id] = stream
-            new_requests.append(new_request)
-
-        self.new_requests_event.clear()
-
-        return new_requests, finished_requests
 
     async def wait_for_new_requests(self):
         await self.new_requests_event.wait()
@@ -209,7 +187,7 @@ class AsyncInferenceEngine:
         # reference to the unshielded loop
         self._background_loop_unshielded = None
         self.start_engine_loop = start_engine_loop
-        self._request_tracker = RequestTracker()
+        self._request_tracer = Tracer()
 
     @property
     def background_loop_status(self):
@@ -219,11 +197,11 @@ class AsyncInferenceEngine:
         if self.background_loop_status:
             raise RuntimeError("Existing loop is running")
 
-        self._request_tracker.init_event()
+        self._request_tracer.init_event()
 
         self._background_loop_unshielded = asyncio.get_event_loop().create_task(self.run_engine_loop())
         self._background_loop_unshielded.add_done_callback(
-            partial(_raise_exception_on_finish, request_tracker=self._request_tracker)
+            partial(_raise_exception_on_finish, request_tracker=self._request_tracer)
         )
         self.background_loop = asyncio.shield(self._background_loop_unshielded)
 
@@ -236,13 +214,13 @@ class AsyncInferenceEngine:
 
         Returns True if there are in-progress requests.
         """
-        new_requests = self._request_tracker.get_new_requests()
+        new_requests = self._request_tracer.get_new_requests()
         for new_request in new_requests:
             self.engine.add_single_request(**new_request)
         newly_finished_seqs, has_running_requests = await self.engine.async_step()
 
         for seq in newly_finished_seqs:
-            self._request_tracker.process_finished_request(seq)
+            self._request_tracer.process_finished_request(seq)
 
         return has_running_requests
 
@@ -258,13 +236,13 @@ class AsyncInferenceEngine:
         return self._abort(request_id)
 
     def _abort(self, request_id: int):
-        self._request_tracker.abort_request(request_id)
+        self._request_tracer.abort_request(request_id)
 
     async def run_engine_loop(self):
         processing_requests = False
         while True:
             if not processing_requests:
-                await self._request_tracker.wait_for_new_requests()
+                await self._request_tracer.wait_for_new_requests()
             processing_requests = await self.step()
             await asyncio.sleep(0)
 
@@ -273,7 +251,7 @@ class AsyncInferenceEngine:
         request_id: int,
         prompt: Optional[str],
         prompt_token_ids: Optional[List[int]] = None,
-    ) -> AsyncStream:
+    ) -> RequstStream:
         """
         Add a request to the background tracker(waitting queue), start the background loop if needed.
         """
@@ -282,7 +260,7 @@ class AsyncInferenceEngine:
                 self.start_background_loop()
             else:
                 raise RuntimeError("Background loop is not running.")
-        stream = self._request_tracker.add_request(
+        stream = self._request_tracer.add_request(
             request_id,
             prompt=prompt,
             prompt_token_ids=prompt_token_ids,
