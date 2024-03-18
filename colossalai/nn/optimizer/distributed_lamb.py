1c6383fb4e214a699962e8a092ee1295ef7e6ffb
Edenzzzz <wenxuan.tan@wisc.edu>
2024-03-18 00:43:26 -0500

    init: add dist lamb; add debiasing for lamb

--- colossalai/nn/optimizer/distributed_lamb.py
+++ colossalai/nn/optimizer/distributed_lamb.py
@@ -1,15 +1,16 @@
-"""
-Adapted from the pytorch-lamb library at https://github.com/cybertronai/pytorch-lamb
-"""
+# Disclaimer: Modified from https://github.com/NUS-HPC-AI-Lab/pytorch-lamb/blob/master/optim/lamb.py
 
 import torch
 from torch.optim import Optimizer
+from .lamb import Lamb
 
 
-class Lamb(Optimizer):
-    r"""Implements Lamb algorithm.
-    It has been proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
+__all__ = ["LAMB"]
 
+
+class DistributedLamb(Lamb):
+    r"""Implements the Lamb algorithm, with extra support for ZeRO 2 and Tensor Parallel.
+    Proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
@@ -17,16 +18,15 @@ class Lamb(Optimizer):
         betas (Tuple[float, float], optional): coefficients used for computing
             running averages of gradient and its square (default: (0.9, 0.999))
         eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-6)
+            numerical stability (default: 1e-8)
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
         adam (bool, optional): always use trust ratio = 1, which turns this into
             Adam. Useful for comparison purposes.
-
-    .. _Large Batch Optimization for Deep Learning\: Training BERT in 76 minutes:
+    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
         https://arxiv.org/abs/1904.00962
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6, weight_decay=0, adam=False, bias_correction=False):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6, weight_decay=0, bias_correction=True):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
@@ -35,13 +35,12 @@ class Lamb(Optimizer):
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
-        self.adam = adam
-        super(Lamb, self).__init__(params, defaults)
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, bias_correction=bias_correction)
+        super().__init__(params, defaults)
 
+    @torch.no_grad()
     def step(self, closure=None):
         """Performs a single optimization step.
-
         Arguments:
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
@@ -50,13 +49,17 @@ class Lamb(Optimizer):
         if closure is not None:
             loss = closure()
 
+        torch.nn.utils.clip_grad_norm_(
+            parameters=[p for group in self.param_groups for p in group["params"]], max_norm=1.0, norm_type=2
+        )
+
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None:
                     continue
                 grad = p.grad.data
                 if grad.is_sparse:
-                    raise RuntimeError("Lamb does not support sparse gradients, consider SparseAdam instead.")
+                    raise RuntimeError("Lamb does not support sparse gradients, consider SparseAdam instad.")
 
                 state = self.state[p]
 
@@ -64,9 +67,9 @@ class Lamb(Optimizer):
                 if len(state) == 0:
                     state["step"] = 0
                     # Exponential moving average of gradient values
-                    state["exp_avg"] = torch.zeros_like(p)
+                    state["exp_avg"] = torch.zeros_like(p.data)
                     # Exponential moving average of squared gradient values
-                    state["exp_avg_sq"] = torch.zeros_like(p)
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
 
                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
                 beta1, beta2 = group["betas"]
@@ -79,34 +82,26 @@ class Lamb(Optimizer):
                 # v_t
                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
-                # NOTE: Paper v3 does not use debiasing.
+
+                scaled_lr = group["lr"]
                 if group["bias_correction"]:
                     bias_correction1 = 1 - beta1 ** state["step"]
                     bias_correction2 = 1 - beta2 ** state["step"]
                     # Apply debiasing to lr to avoid broadcast
-                    group["lr"] *= (bias_correction2 ** 0.5) / bias_correction1
+                    scaled_lr *= (bias_correction2 ** 0.5) / bias_correction1
                     # exp_avg.div_(bias_correction1)
                     # exp_avg_sq.div_(bias_correction2)
                     
-                step_size = group["lr"]
-
-                weight_norm = p.data.pow(2).sum().sqrt()
-
-                adam_step = exp_avg / exp_avg_sq.sqrt().add(group["eps"])
+                update = exp_avg / exp_avg_sq.sqrt().add(group["eps"])
                 if group["weight_decay"] != 0:
-                    adam_step.add_(p.data, alpha=group["weight_decay"])
+                    update.add_(p.data, alpha=group["weight_decay"])
+                    w_norm = torch.norm(p)
+                    g_norm = torch.norm(update)
+                    # TODO: Call all-reduce here
+                    
+                    trust_ratio = torch.where(w_norm > 0 and g_norm > 0, w_norm / g_norm, torch.ones_like(w_norm))
+                    scaled_lr *= trust_ratio.item()
 
-                adam_norm = adam_step.pow(2).sum().sqrt()
-                if weight_norm == 0 or adam_norm == 0:
-                    trust_ratio = 1
-                else:
-                    trust_ratio = weight_norm / adam_norm
-                state["weight_norm"] = weight_norm
-                state["adam_norm"] = adam_norm
-                state["trust_ratio"] = trust_ratio
-                if self.adam:
-                    trust_ratio = 1
-
-                p.data.add_(adam_step, alpha=-step_size * trust_ratio)
+                p.data.add_(update, alpha=-scaled_lr)
 
         return loss
