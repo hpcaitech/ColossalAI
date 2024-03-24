f45e6a240d16f8245e7a70e4f451a9ecd51df92f
Yuanheng <jonathan.zhaoyh@gmail.com>
2024-03-24 17:24:16 +0800

    remove unused arg in scripts

--- examples/language/grok-1/run_inference_fast.sh
+++ examples/language/grok-1/run_inference_fast.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 PRETRAINED=${1:-"hpcaitech/grok-1"}
-TOKENIZER=${2:-"tokenizer.model"}
 
 torchrun --standalone --nproc_per_node 8 inference_tp.py --pretrained "$PRETRAINED" \
-    --tokenizer "$TOKENIZER" \
     --max_new_tokens 100 \
     --text "The company's annual conference, featuring keynote speakers and exclusive product launches, will be held at the Los Angeles Convention Center from October 20th to October 23rd, 2021. Extract the date mentioned in the above sentence." \
             "将以下句子翻译成英语。 我喜欢看电影和读书。" \
