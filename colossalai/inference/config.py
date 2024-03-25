4ef59b894ebaa840aaa5558ae44b15badf59c4c4
Runyu Lu <runyulu@umich.edu>
2024-03-25 15:21:42 +0800

    [fix] docstring align

--- colossalai/inference/config.py
+++ colossalai/inference/config.py
@@ -44,8 +44,8 @@ class InputMetaData:
     use_cuda_graph (bool, optional): Indicates whether to use the CUDA graph. Defaults to False.
     kv_seq_len (int, optional): Key-value sequence length. Defaults to 512.
     head_dim (int, optional): Head dimension. Defaults to 32.
-    high_precision(bool, optional): Whether to use float32 for underlying calculations of float16 data to achieve higher precision, defaults to False.
-    dtype (torch.dtype, optional): the computation type of tensor
+    high_precision(bool, optional): Whether to use float32 for underlying calculations of float16 data to achieve higher precision, Defaults to False.
+    dtype (torch.dtype, optional): the computation type of tensor, Defaults to torch.float32.
     """
 
     block_tables: torch.Tensor = None
