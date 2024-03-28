36c4bb2893e73022b1060bd6ad5c0685869e5465
Yuanheng Zhao <54058983+yuanheng-zhao@users.noreply.github.com>
2024-03-28 16:30:04 +0800

    [Fix] Grok-1 use tokenizer from the same pretrained path (#5532)
    * [fix] use tokenizer from the same pretrained path
    
    * trust remote code

--- examples/language/grok-1/inference.py
+++ examples/language/grok-1/inference.py
@@ -1,7 +1,7 @@
 import time
 
 import torch
-from transformers import AutoModelForCausalLM, LlamaTokenizerFast
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from utils import get_defualt_parser, inference, print_output
 
 if __name__ == "__main__":
@@ -9,6 +9,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     start = time.time()
     torch.set_default_dtype(torch.bfloat16)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained, trust_remote_code=True)
+
     model = AutoModelForCausalLM.from_pretrained(
         args.pretrained,
         trust_remote_code=True,
@@ -17,10 +20,6 @@ if __name__ == "__main__":
     )
     model.eval()
     init_time = time.time() - start
-
-    # A transformers-compatible version of the grok-1 tokenizer by Xenova
-    # https://huggingface.co/Xenova/grok-1-tokenizer
-    tokenizer = LlamaTokenizerFast.from_pretrained("Xenova/grok-1-tokenizer")
 
     for text in args.text:
         output = inference(
