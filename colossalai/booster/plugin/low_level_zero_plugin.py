d2d465f1fb59dc815a9c52ebd1cb0064a0c22f7a
linsj20 <linsj20@mails.tsinghua.edu.cn>
2024-04-15 18:05:32 +0800

    migrate qutization folder to colossalai/

--- colossalai/booster/plugin/low_level_zero_plugin.py
+++ colossalai/booster/plugin/low_level_zero_plugin.py
@@ -16,7 +16,7 @@ from torch.utils._pytree import tree_map
 from torch.utils.data import DataLoader
 
 from colossalai.accelerator import get_accelerator
-from colossalai.booster.quantization import BnbQuantizationConfig, quantize_model
+from colossalai.quantization import BnbQuantizationConfig, quantize_model
 from colossalai.checkpoint_io import CheckpointIndexFile, CheckpointIO
 from colossalai.checkpoint_io.utils import (
     get_optimizer_base_filenames,
