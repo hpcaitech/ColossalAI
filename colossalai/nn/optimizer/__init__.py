e873bc591dc468d8505271b166247de9cd47beed
chongqichuizi875 <107315010+chongqichuizi875@users.noreply.github.com>
2024-04-15 21:32:09 +0800

    [optim] add distributed came (#5526)
    * test CAME under LowLevelZeroOptimizer wrapper
    
    * test CAME TP row and col pass
    
    * test CAME zero pass
    
    * came zero add master and worker param id convert
    
    * came zero test pass
    
    * came zero test pass
    
    * test distributed came passed
    
    * reform code, Modify some expressions and add comments
    
    * minor fix of test came
    
    * minor fix of dist_came and test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor fix of dist_came and test
    
    * rebase dist-optim
    
    * rebase dist-optim
    
    * fix remaining comments
    
    * add test dist came using booster api
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- colossalai/nn/optimizer/__init__.py
+++ colossalai/nn/optimizer/__init__.py
@@ -1,4 +1,6 @@
+from .came import CAME
 from .cpu_adam import CPUAdam
+from .distributed_came import DistributedCAME
 from .distributed_lamb import DistributedLamb
 from .fused_adam import FusedAdam
 from .fused_lamb import FusedLAMB
@@ -7,4 +9,15 @@ from .hybrid_adam import HybridAdam
 from .lamb import Lamb
 from .lars import Lars
 
-__all__ = ["FusedLAMB", "FusedAdam", "FusedSGD", "Lamb", "Lars", "CPUAdam", "HybridAdam", "DistributedLamb"]
+__all__ = [
+    "FusedLAMB",
+    "FusedAdam",
+    "FusedSGD",
+    "Lamb",
+    "Lars",
+    "CPUAdam",
+    "HybridAdam",
+    "DistributedLamb",
+    "CAME",
+    "DistributedCAME",
+]
