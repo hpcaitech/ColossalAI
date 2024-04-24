8ec4117f5b4bb0e60d5a6108bc171736f36fcd2d
Season <caizheng@pjlab.org.cn>
2024-04-24 14:42:52 +0800

    refactor singleton implementation

--- colossalai/context/singleton_meta.py
+++ colossalai/context/singleton_meta.py
@@ -1,24 +1,8 @@
 import threading
 
-"""
-Synchronization decorator
-"""
-
-
-def synchronized(lock):
-    def wrap(func):
-        def synchronized_func(*args, **kwargs):
-            with lock:
-                return func(*args, **kwargs)
-
-        return synchronized_func
-
-    return wrap
-
-
 class SingletonMeta(type):
     """
-    Thread-safe Singleton Meta, and we use double-checked locking to ensure no degradation on performance
+    Thread-safe Singleton Meta with double-checked locking.
     Reference: https://en.wikipedia.org/wiki/Double-checked_locking
     """
 
@@ -26,16 +10,17 @@ class SingletonMeta(type):
     _lock = threading.Lock()
 
     def __call__(cls, *args, **kwargs):
+        # First check (without locking) for performance reasons
         if cls not in cls._instances:
-            instance = cls._locked_call(*args, **kwargs)
-            cls._instances[cls] = instance
+            # Acquire a lock before proceeding to the second check
+            with cls._lock:
+                # Second check with lock held to ensure thread safety
+                if cls not in cls._instances:
+                    instance = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
         else:
             assert (
                 len(args) == 0 and len(kwargs) == 0
-            ), f"{cls.__name__} is a singleton class and a instance has been created."
-        return cls._instances[cls]
+            ), f"{cls.__name__} is a singleton class and an instance has been created."
 
-    @synchronized(_lock)
-    def _locked_call(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
