9f8ecfd31087126faa25f7cf449447160884de91
flybird11111 <1829166702@qq.com>
2024-04-25 02:51:10 +0000

    fix opt

--- colossalai/shardformer/modeling/opt.py
+++ colossalai/shardformer/modeling/opt.py
@@ -59,20 +59,6 @@ class OPTPipelineForwards:
     """
 
     @staticmethod
-    def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
-        """
-        Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
-        """
-        bsz, src_len = mask.size()
-        tgt_len = tgt_len if tgt_len is not None else src_len
-
-        expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
-
-        inverted_mask = 1.0 - expanded_mask
-
-        return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
-
-    @staticmethod
     def opt_model_forward(
         self: OPTModel,
         input_ids: torch.LongTensor = None,
