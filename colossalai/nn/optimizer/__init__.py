341057dc81f6b8660b36f8bf09fb72bb78e25361
Edenzzzz <wtan45@wisc.edu>
2024-05-23 06:02:46 +0000

    logger

--- colossalai/nn/optimizer/__init__.py
+++ colossalai/nn/optimizer/__init__.py
@@ -1,5 +1,6 @@
-import torch.distributed as dist
 from galore_torch import GaLoreAdafactor, GaLoreAdamW
+
+from colossalai.logging import get_dist_logger
 
 from .came import CAME
 from .cpu_adam import CPUAdam
@@ -42,12 +43,12 @@ optim2DistOptim = {
     CAME: DistributedCAME,
     Adafactor: DistributedAdaFactor,
 }
+_logger = get_dist_logger()
 
 
 def cast_to_distributed(optim):
     if optim.__class__ in optim2DistOptim:
-        if dist.is_initialized() and dist.get_rank() == 0:
-            print(f"Converting optimizer {optim.__class__.__name__} to its distributed version.")
+        _logger.info(f"Converting optimizer {optim.__class__.__name__} to its distributed version.", ranks=[0])
 
         if isinstance(optim, GaLoreAdamW8bit):
             return optim2DistOptim[GaLoreAdamW8bit](optim.param_groups, args=optim.args)
