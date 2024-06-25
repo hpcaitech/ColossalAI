a6b602e3933d8a53609dc2df1ddb8979051b1224
hxwang <wang1570@e.ntu.edu.sg>
2024-06-25 03:23:54 +0000

    [test] enable flash attention for benchmark by default

--- examples/language/llama/benchmark.py
+++ examples/language/llama/benchmark.py
@@ -229,8 +229,13 @@ def main():
         init_kwargs["empty_init"] = False
 
     with init_ctx:
-        model = AutoModelForCausalLM.from_config(config, trust_remote_code=True, **init_kwargs)
-
+        model = AutoModelForCausalLM.from_config(
+            config,
+            trust_remote_code=True,
+            **init_kwargs,
+            attn_implementation="flash_attention_2",
+            torch_dtype=torch.float16,
+        )
     if args.grad_checkpoint:
         model.gradient_checkpointing_enable()
         if config.model_type == "chatglm":
