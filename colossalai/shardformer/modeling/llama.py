dc5f907abf7cad540c1b5af68c758930062c297f
wangbluo <2538539015@qq.com>
2024-06-26 08:05:49 +0000

    fix llama

--- colossalai/shardformer/modeling/llama.py
+++ colossalai/shardformer/modeling/llama.py
@@ -803,8 +803,6 @@ def get_lm_forward_with_dist_cross_entropy(shard_config: ShardConfig):
                 shift_logits,
                 shift_labels,
                 process_group=shard_config.tensor_parallel_process_group,
-                vocab_size=self.lm_head.out_features,
-                dtype=self.model.dtype,
             )
 
         if not return_dict:
