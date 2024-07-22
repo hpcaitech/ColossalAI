e31d2ebcf7fd427154f8fcb7e3d1437d0b65721f
hxwang <wang1570@e.ntu.edu.sg>
2024-07-22 05:36:20 +0000

    [test] fix test: test_zero1_2

--- tests/test_shardformer/test_model/test_shard_mixtral.py
+++ tests/test_shardformer/test_model/test_shard_mixtral.py
@@ -180,9 +180,9 @@ def run_dist(rank, world_size, port):
 @pytest.mark.dist
 @pytest.mark.parametrize("world_size", [4])
 @rerun_if_address_is_in_use()
-def test_mistral(world_size):
+def test_mixtral(world_size):
     spawn(run_dist, world_size)
 
 
 if __name__ == "__main__":
-    test_mistral(world_size=8)
+    test_mixtral(world_size=4)
