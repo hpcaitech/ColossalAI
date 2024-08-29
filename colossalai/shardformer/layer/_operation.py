fc798f443397e315b0bd75bf7352452266015f68
Edenzzzz <wtan45@wisc.edu>
2024-08-29 16:48:46 +0800

    fix typos

--- colossalai/shardformer/layer/_operation.py
+++ colossalai/shardformer/layer/_operation.py
@@ -1103,10 +1103,13 @@ def gather_sp_output(hidden_states, shard_config, sp_dim=1):
     """
     sp_group = shard_config.sequence_parallel_process_group
     sp_mode = shard_config.sequence_parallelism_mode
+    fp8_comm = shard_config.fp8_communication
     if dist.get_world_size(sp_group) == 1:
         return hidden_states
 
     # Rescale grad (HybridParallelPlugin applies ZeRO grad averaging on the DP * SP group)
     scale = None if is_share_sp_tp(sp_mode) else dist.get_world_size(sp_group)
-    hidden_states = gather_forward_split_backward(hidden_states, sp_dim, sp_group, grad_scale=scale)
+    hidden_states = gather_forward_split_backward(
+        hidden_states, sp_dim, sp_group, grad_scale=scale, fp8_communication=fp8_comm
+    )
     return hidden_states
