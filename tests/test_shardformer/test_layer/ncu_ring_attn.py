04e1c1e7c4d2f7a1a4eed77691e694a103346fde
Edenzzzz <wtan45@wisc.edu>
2024-09-02 09:31:50 +0000

    remove

--- tests/test_shardformer/test_layer/ncu_ring_attn.py
+++
@@ -1,6 +0,0 @@
-import torch
-from flash_attn import flash_attn_qkvpacked_func
-
-bs, seq_len, nheads, d = 4, 4096, 32, 128
-qkv = torch.randn(bs, seq_len, 3, nheads, d, device="cuda:0", dtype=torch.bfloat16)
-out, lse, _ = flash_attn_qkvpacked_func(qkv, causal=True, return_attn_probs=True)
