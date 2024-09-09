a5ec3d4285195109f6b03c4266e11ba261d06ef7
duanjunwen <935724073@qq.com>
2024-09-09 06:38:31 +0000

    [fix] fix mem; use a new model shape; only assert mem less and equal than theo;

--- tests/test_pipeline/test_schedule/test_zerobubble_pp.py
+++ tests/test_pipeline/test_schedule/test_zerobubble_pp.py
@@ -558,7 +558,7 @@ def run_fwd_bwd_vschedule_with_optim(test_config):
     batch_size = test_config["batch_size"]
     num_layers = 8
     assert num_layers % num_model_chunk == 0, f"Model with {num_layers} layer can not dist on {num_model_chunk} chunk"
-    in_dim = out_dim = 4096
+    in_dim = out_dim = 8192
     before_init_memory = torch.cuda.memory_allocated() / 1024**3
     print(f"Before init Model: {before_init_memory :.3f} GB on device {stage_manager.get_rank()};")
     model = MlpModel(in_dim=in_dim, out_dim=out_dim, num_layers=num_layers).to(rank)
@@ -617,15 +617,15 @@ def run_fwd_bwd_vschedule_with_optim(test_config):
     if rank != 0:
         # w.grad hid_dim * hid_dim * 4(fp32) * 2 (2 layer in each stage) / 1024**3
         # output hid_dim * hid_dim * 4(fp32) / 1024**3
-        print(f"rank {rank}: {(after_pp_step_memory - after_init_memory)} == {(in_dim * in_dim * 4 * 3 / 1024**3)}")
-        assert (after_pp_step_memory - after_init_memory) == (in_dim * in_dim * 4 * 3 / 1024**3)
+        print(f"rank {rank}: {(after_pp_step_memory - after_init_memory)} <= {(in_dim * in_dim * 4 * 3 / 1024**3)}")
+        assert (after_pp_step_memory - after_init_memory) <= (in_dim * in_dim * 4 * 3 / 1024**3)
         # pass
     else:
         # rank0 will also hold output;
         print(
-            f"rank {rank}: {(after_pp_step_memory - after_init_memory)} == {(in_dim * in_dim * 4 * 3 / 1024**3 + batch_size * in_dim * in_dim * 4 / 1024**3)}"
+            f"rank {rank}: {round((after_pp_step_memory - after_init_memory), 5)} <= {round((in_dim * in_dim * 4 * 3 / 1024**3 + batch_size * in_dim * in_dim * 4 / 1024**3), 5)}"
         )
-        assert round((after_pp_step_memory - after_init_memory), 5) == round(
+        assert round((after_pp_step_memory - after_init_memory), 5) <= round(
             (in_dim * in_dim * 4 * 3 / 1024**3 + batch_size * in_dim * in_dim * 4 / 1024**3), 5
         )
         # pass
