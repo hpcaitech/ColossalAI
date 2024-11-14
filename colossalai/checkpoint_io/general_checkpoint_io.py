014081b3270ca4585129defea4cac653dd2774fe
Hongxin Liu <lhx0217@gmail.com>
2024-11-14 11:38:10 +0800

    [checkpointio] support async model save (#6131)
    * [checkpointio] support async model save
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- colossalai/checkpoint_io/general_checkpoint_io.py
+++ colossalai/checkpoint_io/general_checkpoint_io.py
@@ -61,7 +61,6 @@ class GeneralCheckpointIO(CheckpointIO):
                 self.pinned_state_dicts[id(model)] = create_pinned_state_dict(state_dict)
             self.async_writers.append(writer)
             move_and_save(writer, state_dict, self.pinned_state_dicts[id(model)])
-
         else:
             # save the checkpoint
             save_state_dict(state_dict, checkpoint, use_safetensors)
