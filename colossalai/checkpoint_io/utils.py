f127766a119346fc1b871968aad036c3dd1fa366
flybird11111 <1829166702@qq.com>
2024-12-10 15:13:20 +0800

    Update utils.py

--- colossalai/checkpoint_io/utils.py
+++ colossalai/checkpoint_io/utils.py
@@ -19,7 +19,7 @@ from colossalai.tensor.d_tensor import (
     to_global,
     to_global_for_customized_distributed_tensor,
 )
-from colossalai.utils.safetensors import _flatten_optim_state_dict, move_and_save, save
+from colossalai.utils.safetensors import _flatten_optim_state_dict, move_and_save
 
 SAFE_WEIGHTS_NAME = "model.safetensors"
 WEIGHTS_NAME = "pytorch_model.bin"
@@ -289,6 +289,7 @@ def async_save_state_dict_shards(
     Returns:
         int: the total size of shards
     """
+    from colossalai.utils.safetensors import save
 
     total_size = 0
     shard_filenames = []
