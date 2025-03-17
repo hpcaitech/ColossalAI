dddd062cfb903856885fbb046422e950c94f64b3
duanjunwen <935724073@qq.com>
2025-03-17 16:32:57 +0800

    [fix] rm comments

--- applications/ColossalChat/coati/distributed/utils.py
+++ applications/ColossalChat/coati/distributed/utils.py
@@ -68,21 +68,6 @@ def log_probs_from_logits(logits: torch.Tensor, labels: torch.Tensor) -> torch.T
     return per_label_logps.squeeze(-1)
 
 
-# def calc_action_log_probs(logits: torch.Tensor, sequences: torch.LongTensor, num_actions: int) -> torch.Tensor:
-#     """Calculate action log probs.
-
-#     Args:
-#         output (torch.Tensor): Output tensor of Actor.forward.logits.
-#         sequences (torch.LongTensor): Input sequences.
-#         num_actions (int): Number of actions.
-
-#     Returns:
-#         torch.Tensor: Action log probs.
-#     """
-#     log_probs = log_probs_from_logits(logits[:, :-1, :], sequences[:, 1:])
-#     return log_probs[:, -num_actions:]
-
-
 def calc_action_log_probs(
     logits: torch.Tensor,
     sequences: torch.LongTensor,
