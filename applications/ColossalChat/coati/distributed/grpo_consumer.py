c7c73df60ab61775c25c8a360ceee73c51520a34
YeAnbang <anbangy2@outlook.com>
2025-05-17 21:12:58 +0800

    fix logging rollouts

--- applications/ColossalChat/coati/distributed/grpo_consumer.py
+++ applications/ColossalChat/coati/distributed/grpo_consumer.py
@@ -120,12 +120,16 @@ class GRPOConsumer(BaseConsumer):
                     "either max_tokens (vllm) or max_new_tokens (transformers) must be set in generate_config."
                 )
         # Initialize verifiable reward.
-        response_format_tags = {
-            "think_start": {"text": "<think>", "num_occur": 1},
-            "think_end": {"text": "</think>", "num_occur": 1},
-            "answer_start": {"text": "<answer>", "num_occur": 1},
-            "answer_end": {"text": "</answer>", "num_occur": 1},
-        }
+        response_format_tags = (
+            {
+                "think_start": {"text": "<think>", "num_occur": 1},
+                "think_end": {"text": "</think>", "num_occur": 1},
+                "answer_start": {"text": "<answer>", "num_occur": 1},
+                "answer_end": {"text": "</answer>", "num_occur": 1},
+            }
+            if grpo_config.get("reward_fn_type") == "think_answer_tags"
+            else None
+        )
         reward_model_kwargs = {
             k: v
             for k, v in grpo_config.items()
