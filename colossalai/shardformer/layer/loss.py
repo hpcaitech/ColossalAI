bf5b1537002d896b1244fd2fce0077bb8e2fa4a3
duanjunwen <935724073@qq.com>
2025-05-23 11:38:21 +0800

    [feat] support ColossalaiRL on Ascend

--- colossalai/shardformer/layer/loss.py
+++ colossalai/shardformer/layer/loss.py
@@ -190,7 +190,8 @@ class DistLogProb(Function):
         # mask
         mask = (target < down_threshold) | (target >= up_threshold)
         masked_target = target.clone() - down_threshold
-        masked_target[mask] = 0
+        # masked_target[mask] = 0
+        masked_target *= ~mask
         masked_target_1d = masked_target.view(-1).contiguous()
         handle.wait()
  
