90c3b12474f135dad6b7f2d6c3a5af6eb34d2954
Tong Li <tong.li35271158@gmail.com>
2025-06-27 13:14:35 +0800

    update assert

--- applications/ColossalChat/rl_example.py
+++ applications/ColossalChat/rl_example.py
@@ -319,6 +319,7 @@ if __name__ == "__main__":
                 else None
             ),
         }
+        assert args.n_behind == 0, "DAPO does not support N behined."
     else:
         raise ValueError(f"Unsupported algorithm: {args.algo}")
 
